1feeea10aca9a33d7303ce7107c686e6bf037bfb
chihuahua <chizeng@google.com>
2017-07-26 15:01:33 -0700

    .

--- tensorboard/plugins/pr_curve/pr_curve_plugin_test.py
+++ tensorboard/plugins/pr_curve/pr_curve_plugin_test.py
@@ -82,13 +82,18 @@ class PrCurveTest(tf.test.TestCase):
     correct_shape = [6, 1, 10]
     self.assertListEqual(correct_shape, list(tensor_nd_array.shape))
 
-    # The counts within the various bins must be correct.
     np.testing.assert_allclose(([
+      # True positives.
       [[2.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0]],
+      # False positives.
       [[2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]],
+      # True negatives.
       [[0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0]],
+      # False negatives.
       [[0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]],
+      # Precision.
       [[0.5, 0.5, 2/3, 2/3, 0.5, 0.5, 1.0, 1.0, 1.0, 1.0]],
+      # Recall.
       [[1.0, 1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 0.5, 0.0, 0.0]]
     ], tensor_nd_array.tolist())
 
