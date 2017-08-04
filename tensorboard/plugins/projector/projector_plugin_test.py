8ea90a4755fd3483f6528b41717f784219351b73
chihuahua <chizeng@google.com>
2017-08-04 16:17:21 -0700

    .

--- tensorboard/plugins/projector/projector_plugin_test.py
+++ tensorboard/plugins/projector/projector_plugin_test.py
@@ -38,37 +38,6 @@ from tensorboard.plugins.projector import projector_config_pb2
 from tensorboard.plugins.projector import projector_plugin
 
 
-class FakeThread(object):
-  """Fakes threading behavior so that threads can be effectively tested."""
-
-  def __init__(self, target):
-    """A 'thread' that lets a test run its logic at any time.
-  
-    Specifically, a test makes the logic run by calling actually_run.
-
-    Args:
-      target: The function to call.
-    """
-    self._started = False
-    self._target = target
-
-  def start(self):  # pylint: disable=invalid-name
-    """Starts the thread. Part of the API for python threads."""
-    self._started = True
-
-  def actually_run(self):
-    """Actually runs the target.
-
-    Called within tests to simulate the logic of the thread being executed.
-
-    Raises:
-      RuntimeError: If this method is called before the thread is started.
-    """
-    if not self._started:
-      raise RuntimeError('Attempted to run a FakeThread before it started')
-    self._target()
-
-
 class ProjectorAppTest(tf.test.TestCase):
 
   def __init__(self, *args, **kwargs):
@@ -79,9 +48,6 @@ class ProjectorAppTest(tf.test.TestCase):
 
   def setUp(self):
     self.log_dir = self.get_temp_dir()
-    
-  def tearDown(self):
-    tf.test.mock.patch.stopall()
 
   def testRunsWithValidCheckpoint(self):
     self._GenerateProjectorTestData()
@@ -227,20 +193,22 @@ class ProjectorAppTest(tf.test.TestCase):
     self._GenerateProjectorTestData()
     self._SetupWSGIApp()
 
-    # The is_active method makes use of a separate thread, so we mock threading
-    # behavior to make this test deterministic.
-    tf.test.mock.patch('threading.Thread', FakeThread).start()
+    patcher = tf.test.mock.patch('threading.Thread.start', autospec=True)
+    mock = patcher.start()
+    self.addCleanup(patcher.stop)
 
+    mock.assert_not_called()
     # The projector plugin has not yet determined whether it is active, but it
     # should now start a thread to determine that.
     self.assertFalse(self.plugin.is_active())
+    mock.assert_called_once_with(self.plugin._thread_for_determining_is_active)
 
     # The logic has not finished running yet, so the plugin should still not
     # have deemed itself to be active.
     self.assertFalse(self.plugin.is_active())
+    mock.assert_called_once_with(self.plugin._thread_for_determining_is_active)
 
-    # We simulate the logic of the thread executing.
-    self.plugin._thread_for_determining_is_active.actually_run()
+    self.plugin._thread_for_determining_is_active.run()
 
     # The plugin later finds that embedding data is available.
     self.assertTrue(self.plugin.is_active())
@@ -250,14 +218,16 @@ class ProjectorAppTest(tf.test.TestCase):
 
     # The is_active method makes use of a separate thread, so we mock threading
     # behavior to make this test deterministic.
-    tf.test.mock.patch('threading.Thread', FakeThread).start()
+    patcher = tf.test.mock.patch('threading.Thread.start', autospec=True)
+    mock = patcher.start()
+    self.addCleanup(patcher.stop)
 
     # The projector plugin has not yet determined whether it is active, but it
     # should now start a thread to determine that.
     self.assertFalse(self.plugin.is_active())
+    mock.assert_called_once_with(self.plugin._thread_for_determining_is_active)
 
-    # We simulate the logic of the thread executing.
-    self.plugin._thread_for_determining_is_active.actually_run()
+    self.plugin._thread_for_determining_is_active.run()
 
     # The plugin later finds that embedding data is not available.
     self.assertFalse(self.plugin.is_active())
