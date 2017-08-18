2e8c26663c272a1ff4379b99af6f9a2bcbf5cb23
chihuahua <chizeng@google.com>
2017-08-17 21:13:46 -0700

    Test pr curves plugin

--- tensorboard/plugins/pr_curve/pr_curves_plugin_test.py
+++ tensorboard/plugins/pr_curve/pr_curves_plugin_test.py
@@ -26,6 +26,7 @@ import os.path
 
 from six import StringIO
 from six.moves import xrange  # pylint: disable=redefined-builtin
+import numpy as np
 import tensorflow as tf
 
 from tensorboard.backend.event_processing import plugin_event_accumulator as event_accumulator  # pylint: disable=line-too-long
@@ -38,8 +39,8 @@ from tensorboard.plugins.pr_curve import pr_curves_plugin
 class PrCurvesPluginTest(tf.test.TestCase):
 
   def setUp(self, *args, **kwargs):
-    super(PrCurveTest, self).setUp(*args, **kwargs)
-    logdir = self.get_temp_dir()
+    super(PrCurvesPluginTest, self).setUp(*args, **kwargs)
+    logdir = os.path.join(self.get_temp_dir(), 'logdir')
     tf.reset_default_graph()
 
     # Generate data.
@@ -50,12 +51,35 @@ class PrCurvesPluginTest(tf.test.TestCase):
         verbose=False)
 
     # Create a multiplexer for reading the data we just wrote.
-    self.multiplexer = event_multiplexer.EventMultiplexer()
-    self.multiplexer.AddRunsFromDirectory(logdir)
-    self.multiplexer.Reload()
+    multiplexer = event_multiplexer.EventMultiplexer()
+    multiplexer.AddRunsFromDirectory(logdir)
+    multiplexer.Reload()
 
     context = base_plugin.TBContext(logdir=logdir, multiplexer=multiplexer)
     self.plugin = pr_curves_plugin.PrCurvesPlugin(context)
+
+  def validatePrCurveEntry(
+      self,
+      expected_step,
+      expected_precision,
+      expected_recall,
+      pr_curve_entry):
+    """Checks that the values stored within a tensor are correct.
+
+    Args:
+      expected_step: The expected step.
+      expected_precision: A list of float precision values.
+      expected_recall: A list of float recall values.
+      pr_curve_entry: The PR curve entry to evaluate.
+    """
+    self.assertEqual(expected_step, pr_curve_entry['step'])
+    # We use an absolute error instead of a relative one because the expected
+    # values are small. The default relative error (trol) of 1e-7 yields many
+    # undesired test failures.
+    np.testing.assert_allclose(
+        expected_precision, pr_curve_entry['precision'], rtol=0, atol=1e-7)
+    np.testing.assert_allclose(
+        expected_recall, pr_curve_entry['recall'], rtol=0, atol=1e-7)
 
   def testRoutesProvided(self):
     """Tests that the plugin offers the correct routes."""
@@ -66,4 +90,92 @@ class PrCurvesPluginTest(tf.test.TestCase):
 
   def testTagsProvided(self):
     """Tests that tags are provided."""
-    self.assertDictEqual({}, self.plugin.tags_impl())
+    tags_response = self.plugin.tags_impl()
+
+    # Assert that the runs are right.
+    self.assertItemsEqual(
+        ['colors', 'mask_every_other_prediction'], list(tags_response.keys()))
+    
+    # Assert that runs are mapped to correct tags.
+    self.assertItemsEqual(
+        ['red/pr_curves', 'green/pr_curves', 'blue/pr_curves'],
+        tags_response['colors'])
+    self.assertItemsEqual(
+        ['red/pr_curves', 'green/pr_curves', 'blue/pr_curves'],
+        tags_response['mask_every_other_prediction'])
+
+  def testAvailableSteps(self):
+    """Tests that runs are mapped to correct available steps."""
+    self.assertDictEqual({
+        'colors': [0, 1, 2],
+        'mask_every_other_prediction': [0, 1, 2],
+    }, self.plugin.available_steps_impl())
+
+  def testPrCurves(self):
+    """Tests that responses for PR curves for run-tag combos are correct."""
+    pr_curves_response = self.plugin.pr_curves_impl(
+        ['colors', 'mask_every_other_prediction'], 'blue/pr_curves')
+    
+    # Assert that the runs are correct.
+    self.assertItemsEqual(
+        ['colors', 'mask_every_other_prediction'],
+        list(pr_curves_response.keys()))
+
+    # Assert that PR curve data is correct for the colors run.
+    entries = pr_curves_response['colors']
+    self.assertEqual(3, len(entries))
+    self.validatePrCurveEntry(
+        expected_step=0,
+        expected_precision=[0.3333333, 0.3853211, 0.5421686, 0.75, 1.0],
+        expected_recall=[1.0, 0.8400000, 0.3000000, 0.0400000, 0.0],
+        pr_curve_entry=entries[0])
+    self.validatePrCurveEntry(
+        expected_step=1,
+        expected_precision=[0.3333333, 0.3855421, 0.5357142, 0.4000000, 1.0],
+        expected_recall=[1.0, 0.8533333, 0.3000000, 0.0266666, 0.0],
+        pr_curve_entry=entries[1])
+    self.validatePrCurveEntry(
+        expected_step=2,
+        expected_precision=[0.3333333, 0.3934426, 0.5064935, 0.6666666, 1.0],
+        expected_recall=[1.0, 0.8000000, 0.2599999, 0.0266666, 0.0],
+        pr_curve_entry=entries[2])
+
+    # Assert that PR curve data is correct for the mask_every_other_prediction
+    # run.
+    entries = pr_curves_response['mask_every_other_prediction']
+    self.assertEqual(3, len(entries))
+    self.validatePrCurveEntry(
+        expected_step=0,
+        expected_precision=[0.3333333, 0.3786982, 0.5384616, 1.0, 1.0],
+        expected_recall=[1.0, 0.8533334, 0.28, 0.0666667, 0.0],
+        pr_curve_entry=entries[0])
+    self.validatePrCurveEntry(
+        expected_step=1,
+        expected_precision=[0.3333333, 0.3850932, 0.5, 0.25, 1.0],
+        expected_recall=[1.0, 0.8266667, 0.28, 0.0133333, 0.0],
+        pr_curve_entry=entries[1])
+    self.validatePrCurveEntry(
+        expected_step=2,
+        expected_precision=[0.3333333, 0.3986928, 0.4444444, 0.6666667, 1.0],
+        expected_recall=[1.0, 0.8133333, 0.2133333, 0.0266666, 0.0],
+        pr_curve_entry=entries[2])
+
+  def testPluginIsNotActive(self):
+    """Tests that the plugin is inactive when no relevant data exists."""
+    empty_logdir = os.path.join(self.get_temp_dir(), 'empty_logdir')
+    multiplexer = event_multiplexer.EventMultiplexer()
+    multiplexer.AddRunsFromDirectory(empty_logdir)
+    multiplexer.Reload()
+    context = base_plugin.TBContext(
+        logdir=empty_logdir, multiplexer=multiplexer)
+    plugin = pr_curves_plugin.PrCurvesPlugin(context)
+    self.assertFalse(plugin.is_active())
+
+  def testPluginIsActive(self):
+    """Tests that the plugin is active when relevant data exists."""
+    # The set up for this test generates relevant data.
+    self.assertTrue(self.plugin.is_active())
+
+
+if __name__ == '__main__':
+  tf.test.main()
