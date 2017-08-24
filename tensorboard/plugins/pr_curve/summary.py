455a95a5965e45fa3d01ed42a5c1dac98e80cdfd
Chi Zeng <zeng.chi@gmail.com>
2017-08-24 16:52:06 -0700

    Fix lint issues related to the pr_curves plugin (#389)
    Fixed some lint issues related to indentation, trailing whitespaces, and un-used imports.

--- tensorboard/plugins/pr_curve/summary.py
+++ tensorboard/plugins/pr_curve/summary.py
@@ -27,8 +27,8 @@ import tensorflow as tf
 from google.protobuf import json_format
 from tensorboard.plugins.pr_curve import plugin_data_pb2
 
-# A tiny value. Used to prevent division by 0 as well as to make precision 1
-# when the threshold is 0.
+# A tiny value. Used to prevent division by 0 as well as to make
+# precision 1 when the threshold is 0.
 _TINY_EPISILON = 1e-7
 
 def op(
@@ -58,7 +58,7 @@ def op(
     predictions: A float32 `Tensor` whose values are in the range `[0, 1]`.
         Dimensions must match those of `labels`.
     num_thresholds: Number of thresholds, evenly distributed in `[0, 1]`, to
-        compute PR metrics for. Should be `>= 2`. This value should be a 
+        compute PR metrics for. Should be `>= 2`. This value should be a
         constant integer value, not a Tensor that stores an integer.
     weights: Optional float32 `Tensor`. Individual counts are multiplied by this
         value. This tensor must be either the same shape as or broadcastable to
@@ -129,7 +129,7 @@ def op(
     # Compute the bucket indices for each prediction value.
     bucket_indices = tf.cast(
         tf.floor(predictions * (num_thresholds - 1)), tf.int32)
-    
+
     # Bucket predictions.
     tp_buckets = tf.reduce_sum(
         tf.one_hot(bucket_indices, depth=num_thresholds) * true_labels,
@@ -138,9 +138,6 @@ def op(
         tf.one_hot(bucket_indices, depth=num_thresholds) * false_labels,
         axis=0)
 
-    thresholds = tf.cast(
-        tf.linspace(0.0, 1.0, num_thresholds), dtype=dtype)
-    
     # Set up the cumulative sums to compute the actual metrics.
     tp = tf.cumsum(tp_buckets, reverse=True, name='tp')
     fp = tf.cumsum(fp_buckets, reverse=True, name='fp')
