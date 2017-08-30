e1be87be04cbd5717c45f7108d8b2a72a6f41d7b
Justine Tunney <jart@google.com>
2017-08-30 13:51:57 -0700

    Fix import

--- tensorboard/main.py
+++ tensorboard/main.py
@@ -21,7 +21,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import logging as base_logging
 import os
 import socket
 import sys
