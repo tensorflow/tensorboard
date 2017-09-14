2d8f7ad56a491097aca712bd3a7bbfaac3d02dd4
chihuahua <chizeng@google.com>
2017-09-14 11:41:15 -0700

    s/command/data

--- tensorboard/plugins/text/summary.py
+++ tensorboard/plugins/text/summary.py
@@ -92,7 +92,7 @@ def pb(name, data, display_name=None, description=None):
   Returns:
     A `tf.Summary` protobuf object.
   """
-  if isinstance(data, six.binary_type) or isinstance(command, six.string_types):
+  if isinstance(data, six.binary_type) or isinstance(data, six.string_types):
     # Check if data is of type bytes or string. For python3, binary_type checks
     # for bytes instead of string, so we also check for six.string_types.
     if isinstance(data, six.text_type):
