edc45b67a52293b1778fdebb9a1d9134337bc397
Chi Zeng <chizeng@google.com>
2017-09-16 11:18:50 -0700

    Prefix strings with b to make them bytes

--- tensorboard/plugins/text/summary_test.py
+++ tensorboard/plugins/text/summary_test.py
@@ -103,19 +103,19 @@ class SummaryTest(tf.test.TestCase):
     pb = self.compute_and_check_summary_pb('mi', 'A name I call myself.')
     value = tf.make_ndarray(pb.value[0].tensor).item()
     self.assertIsInstance(value, six.binary_type)
-    self.assertEqual('A name I call myself.', value)
+    self.assertEqual(b'A name I call myself.', value)
 
   def test_np_array_string_value(self):
     pb = self.compute_and_check_summary_pb('fa', 'A long, long way to run.')
     value = tf.make_ndarray(pb.value[0].tensor).item()
     self.assertIsInstance(value, six.binary_type)
-    self.assertEqual('A long, long way to run.', value)
+    self.assertEqual(b'A long, long way to run.', value)
 
   def test_np_array_unicode_value(self):
     pb = self.compute_and_check_summary_pb('fa', u'A long, long way to run.')
     value = tf.make_ndarray(pb.value[0].tensor).item()
     self.assertIsInstance(value, six.binary_type)
-    self.assertEqual('A long, long way to run.', value)
+    self.assertEqual(b'A long, long way to run.', value)
 
   def test_non_string_value_in_op(self):
     with six.assertRaisesRegex(
@@ -137,7 +137,7 @@ class SummaryTest(tf.test.TestCase):
         'ti', u'A drink with jam and bread.')
     value = tf.make_ndarray(pb.value[0].tensor).item()
     self.assertIsInstance(value, six.binary_type)
-    self.assertEqual('A drink with jam and bread.', value)
+    self.assertEqual(b'A drink with jam and bread.', value)
 
 
 if __name__ == '__main__':
