2d09db1fe8000b42093ce40110fe6f962c5a8256
Justine Tunney <jart@google.com>
2018-01-16 20:17:09 -0800

    Fix minor build breakage
    This snuck in because Travis doesn't seem to be launching our test jobs at all
    today. See also: 50097ae30b5ca15a740c4902cd1ba8d1ca896ea1

--- tensorboard/plugins/graph/tf_graph_common/op.ts
+++ tensorboard/plugins/graph/tf_graph_common/op.ts
@@ -329,11 +329,8 @@ module tf.graph.op {
   /**
    * Returns true if the node's inferred device is not the TPU.
    * Note that this is only a best-effort check.
-   *
-   * @param opDevice String device object
-   * @returns {boolean}
    */
-  export function isNotTpuOp(string: opDevice): boolean {
+  export function isNotTpuOp(opDevice: string): boolean {
     if (opDevice.toLowerCase().search('cpu:') != -1) {
       return true;
     }
@@ -346,9 +343,6 @@ module tf.graph.op {
   /**
    * Returns true if OpNode graph object represents a
    * Tensorflow operation that is valid for the TPU.
-   *
-   * @param opNode OpNode graph object
-   * @returns {boolean}
    */
   export function opValid(opNode: OpNode): boolean {
     // If assigned a device that is not TPU-related assume op is valid.
