0e71b09a9c514d4c4f7fa2566ee253a9c0290e98
Aidan Gomez <hello@aidangomez.ca>
2018-02-15 14:52:18 -0500

    Update summary.py

--- tensorboard/plugins/histogram/summary.py
+++ tensorboard/plugins/histogram/summary.py
@@ -37,7 +37,6 @@ import numpy as np
 
 from tensorboard.plugins.histogram import metadata
 
-
 DEFAULT_BUCKET_COUNT = 30
 
 
@@ -73,17 +72,16 @@ def _buckets(data, bucket_count=None):
       def when_nonsingular():
         bucket_width = range_ / tf.cast(bucket_count, tf.float64)
         offsets = data - min_
-        bucket_indices = tf.cast(tf.floor(offsets / bucket_width),
-                                 dtype=tf.int32)
+        bucket_indices = tf.cast(
+            tf.floor(offsets / bucket_width), dtype=tf.int32)
         clamped_indices = tf.minimum(bucket_indices, bucket_count - 1)
         one_hots = tf.one_hot(clamped_indices, depth=bucket_count)
-        bucket_counts = tf.cast(tf.reduce_sum(one_hots, axis=0),
-                                dtype=tf.float64)
+        bucket_counts = tf.cast(
+            tf.reduce_sum(one_hots, axis=0), dtype=tf.float64)
         edges = tf.lin_space(min_, max_, bucket_count + 1)
         left_edges = edges[:-1]
         right_edges = edges[1:]
-        return tf.transpose(tf.stack(
-            [left_edges, right_edges, bucket_counts]))
+        return tf.transpose(tf.stack([left_edges, right_edges, bucket_counts]))
 
       def when_singular():
         center = min_
@@ -100,6 +98,8 @@ def _buckets(data, bucket_count=None):
 
 def op(name,
        data,
+       edges=None,
+       bucket_heights=None,
        bucket_count=None,
        display_name=None,
        description=None,
@@ -109,6 +109,10 @@ def op(name,
   Arguments:
     name: A unique name for the generated summary node.
     data: A `Tensor` of any shape. Must be castable to `float64`.
+    edges: Optional `Tensor` of shape `[bucket_count + 1]` specifying the
+      boundaries of each bucket. If specified, `data` must be a `Tensor` of
+      shape `[bucket_count]` containing the heights of each bucket. Note:
+      specification of bucket_count is not necessary.
     bucket_count: Optional positive `int`. The output will have this
       many buckets, except in two edge cases. If there is no data, then
       there are no buckets. If there is data but all points have the
@@ -124,20 +128,43 @@ def op(name,
 
   Returns:
     A TensorFlow summary op.
+
+  Raises:
+    ValueError: If data is not rank 1 when `edges` is specified.
+    ValueError: If the shape of data is not [bucket_count] or edges is not
+      [bucket_count+1].
   """
   if display_name is None:
     display_name = name
   summary_metadata = metadata.create_summary_metadata(
       display_name=display_name, description=description)
   with tf.name_scope(name):
-    tensor = _buckets(data, bucket_count=bucket_count)
-    return tf.summary.tensor_summary(name='histogram_summary',
-                                     tensor=tensor,
-                                     collections=collections,
-                                     summary_metadata=summary_metadata)
+    if edges is None:
+      tensor = _buckets(data, bucket_count=bucket_count)
+    else:
+      if len(data.get_shape()) != 1 or len(edges.get_shape()) != 1:
+        raise ValueError('`data` and `edges` must be rank 1.')
+      if data.get_shape()[0] != edges.get_shape()[0] - 1:
+        raise ValueError(
+            'Shapes of `data` and `edges` must be [bucket_count] and '
+            '[bucket_count + 1], but are %s and %s.' % (data.get_shape(),
+                                                        edges.get_shape()))
+      edges = tf.cast(edges, tf.float64)
+      data = tf.cast(data, tf.float64)
+      tensor = tf.transpose(tf.stack([edges[:-1], edges[1:], data]))
+    return tf.summary.tensor_summary(
+        name='histogram_summary',
+        tensor=tensor,
+        collections=collections,
+        summary_metadata=summary_metadata)
 
 
-def pb(name, data, bucket_count=None, display_name=None, description=None):
+def pb(name,
+       data,
+       edges=None,
+       bucket_count=None,
+       display_name=None,
+       description=None):
   """Create a histogram summary protobuf.
 
   Arguments:
@@ -145,6 +172,10 @@ def pb(name, data, bucket_count=None, display_name=None, description=None):
       name scopes.
     data: A `np.array` or array-like form of any shape. Must have type
       castable to `float`.
+    edges: Optional `np.array` of shape `[bucket_count + 1]` specifying the
+      boundaries of each bucket. If specified, `data` must be a `np.array` of
+      shape `[bucket_count]` containing the heights of each bucket. Note:
+      specification of bucket_count is not necessary.
     bucket_count: Optional positive `int`. The output will have this
       many buckets, except in two edge cases. If there is no data, then
       there are no buckets. If there is data but all points have the
@@ -157,42 +188,60 @@ def pb(name, data, bucket_count=None, display_name=None, description=None):
 
   Returns:
     A `tf.Summary` protobuf object.
+
+  Raises:
+    ValueError: If data is not rank 1 when `edges` is specified.
+    ValueError: If the shape of data is not [bucket_count] or edges is not
+      [bucket_count+1].
   """
   if bucket_count is None:
     bucket_count = DEFAULT_BUCKET_COUNT
-  data = np.array(data).flatten().astype(float)
-  if data.size == 0:
-    buckets = np.array([]).reshape((0, 3))
-  else:
-    min_ = np.min(data)
-    max_ = np.max(data)
-    range_ = max_ - min_
-    if range_ == 0:
-      center = min_
-      buckets = np.array([[center - 0.5, center + 0.5, float(data.size)]])
+  if edges is None:
+    data = np.array(data).flatten().astype(float)
+    if data.size == 0:
+      buckets = np.array([]).reshape((0, 3))
     else:
-      bucket_width = range_ / bucket_count
-      offsets = data - min_
-      bucket_indices = np.floor(offsets / bucket_width).astype(int)
-      clamped_indices = np.minimum(bucket_indices, bucket_count - 1)
-      one_hots = (np.array([clamped_indices]).transpose()
-                  == np.arange(0, bucket_count))  # broadcast
-      assert one_hots.shape == (data.size, bucket_count), (
-          one_hots.shape, (data.size, bucket_count))
-      bucket_counts = np.sum(one_hots, axis=0)
-      edges = np.linspace(min_, max_, bucket_count + 1)
-      left_edges = edges[:-1]
-      right_edges = edges[1:]
-      buckets = np.array([left_edges, right_edges, bucket_counts]).transpose()
-  tensor = tf.make_tensor_proto(buckets, dtype=tf.float64)
+      min_ = np.min(data)
+      max_ = np.max(data)
+      range_ = max_ - min_
+      if range_ == 0:
+        center = min_
+        buckets = np.array([[center - 0.5, center + 0.5, float(data.size)]])
+      else:
+        bucket_width = range_ / bucket_count
+        offsets = data - min_
+        bucket_indices = np.floor(offsets / bucket_width).astype(int)
+        clamped_indices = np.minimum(bucket_indices, bucket_count - 1)
+        one_hots = (np.array([clamped_indices]).transpose() == np.arange(
+            0, bucket_count))  # broadcast
+        assert one_hots.shape == (data.size, bucket_count), (one_hots.shape,
+                                                             (data.size,
+                                                              bucket_count))
+        bucket_counts = np.sum(one_hots, axis=0)
+        edges = np.linspace(min_, max_, bucket_count + 1)
+        left_edges = edges[:-1]
+        right_edges = edges[1:]
+        buckets = np.array([left_edges, right_edges, bucket_counts]).transpose()
+  else:
+    if len(data.shape) != 1 or len(edges.shape) != 1:
+      raise ValueError('`data` and `edges` must be rank 1.')
+    if data.shape[0] != edges.shape[0] - 1:
+      raise ValueError(
+          'Shapes of `data` and `edges` must be [bucket_count] and '
+          '[bucket_count + 1], but are %s and %s.' % (data.shape, edges.shape))
+    data = np.array(data).astype(float)
+    edges = np.array(edges).astype(float)
+    buckets = np.array([edges[:-1], edges[1:], data]).transpose()
 
+  tensor = tf.make_tensor_proto(buckets, dtype=tf.float64)
   if display_name is None:
     display_name = name
   summary_metadata = metadata.create_summary_metadata(
       display_name=display_name, description=description)
 
   summary = tf.Summary()
-  summary.value.add(tag='%s/histogram_summary' % name,
-                    metadata=summary_metadata,
-                    tensor=tensor)
+  summary.value.add(
+      tag='%s/histogram_summary' % name,
+      metadata=summary_metadata,
+      tensor=tensor)
   return summary
