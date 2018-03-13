7a6ac2c1881444c963d5923b8401cb7723492d6d
Nick Felt <nickfelt@google.com>
2018-03-12 18:49:56 -0700

    Add VideoWriter class with PNGVideoOutput

--- tensorboard/plugins/beholder/beholder.py
+++ tensorboard/plugins/beholder/beholder.py
@@ -34,9 +34,13 @@ from tensorboard.plugins.beholder.visualizer import Visualizer
 class Beholder(object):
 
   def __init__(self, logdir):
-    self.video_writer = None
-
     self.PLUGIN_LOGDIR = logdir + '/plugins/' + PLUGIN_NAME
+
+    self.video_writer = video_writing.VideoWriter(
+        self.PLUGIN_LOGDIR,
+        outputs=[
+            video_writing.FFmpegVideoOutput,
+            video_writing.PNGVideoOutput])
 
     self.frame_placeholder = tf.placeholder(tf.uint8, [None, None, None])
     self.summary_op = tf.summary.tensor_summary(TAG_NAME,
@@ -124,29 +128,14 @@ class Beholder(object):
 
 
   def _update_recording(self, frame, config):
-    '''Adds a frame to the video using ffmpeg if possible. If not, writes
-    individual frames as png files in a directory.
-    '''
+    '''Adds a frame to the current video output.'''
     # pylint: disable=redefined-variable-type
     is_recording = config['is_recording']
-    filename = self.PLUGIN_LOGDIR + '/video-{}.mp4'.format(time.time())
 
     if is_recording:
-      if self.video_writer is None or frame.shape != self.video_writer.size:
-        try:
-          self.video_writer = video_writing.FFMPEG_VideoWriter(filename,
-                                                               frame.shape,
-                                                               15)
-        except OSError:
-          message = ('Either ffmpeg is not installed, or something else went '
-                     'wrong. Saving individual frames to disk instead.')
-          print(message)
-          self.video_writer = video_writing.PNGWriter(self.PLUGIN_LOGDIR,
-                                                      frame.shape)
       self.video_writer.write_frame(frame)
-    elif not is_recording and self.video_writer is not None:
-      self.video_writer.close()
-      self.video_writer = None
+    elif self.video_writer is not None:
+      self.video_writer.finish()
 
 
   # TODO: blanket try and except for production? I don't someone's script to die
--- tensorboard/plugins/beholder/video_writing.py
+++ tensorboard/plugins/beholder/video_writing.py
@@ -12,4 +12,119 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO(@jart): Rewrite me.
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+import sys
+import time
+
+import numpy as np
+import tensorflow as tf
+
+from tensorboard.plugins.beholder import im_util
+
+
+class VideoWriter(object):
+  """Video file writer that can use different output types.
+
+  Each VideoWriter instance writes video files to a specified directory, using
+  the first available VideoOutput from the provided list.
+  """
+
+  def __init__(self, directory, outputs):
+    self.directory = directory
+    # Filter to the available outputs
+    self.outputs = [out for out in outputs if out.available()]
+    if not self.outputs:
+      raise Exception('No available video outputs')
+    self.output = None
+    self.frame_shape = None
+
+  def default_output(self):
+    return self.outputs[0]
+
+  def _select_output(self):
+    for i, output in enumerate(self.outputs):
+      output_type = output.__name__
+      if i > 0:
+        sys.stderr.write('Falling back to video output %s\n' % output_type)
+      try:
+        return output(self.directory, self.frame_shape)
+      except Exception:
+        sys.stderr.write('Video output type %s not available\n' % output_type)
+        if i == len(self.outputs) - 1:
+          raise
+
+  def write_frame(self, np_array):
+    if self.frame_shape != np_array.shape:
+      if self.output:
+        self.output.close()
+      self.frame_shape = np_array.shape
+      self.output = self._select_output()
+    self.output.emit_frame(np_array)
+
+  def finish(self):
+    if self.output:
+      self.output.close()
+    self.output = None
+    self.frame_shape = None
+
+
+class VideoOutput(object):
+  """Base class for video outputs supported by VideoWriter."""
+
+  __metaclass__ = abc.ABCMeta
+
+  # Would add @abc.abstractmethod in python 3.3+
+  @classmethod
+  def available(cls):
+    raise NotImplementedError()
+
+  @abc.abstractmethod
+  def emit_frame(self, np_array):
+    raise NotImplementedError()
+
+  @abc.abstractmethod
+  def close(self):
+    raise NotImplementedError()
+
+
+class PNGVideoOutput(VideoOutput):
+  """Video output implemented by writing individual PNGs to disk."""
+
+  @classmethod
+  def available(cls):
+    return True
+
+  def __init__(self, directory, frame_shape):
+    del frame_shape  # unused
+    self.directory = directory + '/video-frames-{}'.format(time.time())
+    self.frame_num = 0
+    tf.gfile.MakeDirs(self.directory)
+
+  def emit_frame(self, np_array):
+    filename = self.directory + '/{:05}.png'.format(self.frame_num)
+    im_util.write_image(np_array.astype(np.uint8), filename)
+    self.frame_num += 1
+
+  def close(self):
+    pass
+
+
+class FFmpegVideoOutput(VideoOutput):
+  """Video output implemented by streaming to FFmpeg with .mp4 output."""
+
+  @classmethod
+  def available(cls):
+    return False
+
+  def __init__(self, directory, frame_shape):
+    self.filename = directory + '/video-{}.mp4'.format(time.time())
+
+  def emit_frame(self, np_array):
+    pass
+
+  def close(self):
+    pass
