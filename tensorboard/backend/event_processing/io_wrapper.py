f9e44b9735eb0eb3268a0758ae24247482bffa3f
Chi Zeng <4221553+chihuahua@users.noreply.github.com>
2018-03-31 12:02:38 -0700

    Glob to find runs for GCS and CNS paths
    Previously, TensorBoard found runs within the log directory by
    performing a `tf.gfile.Walk` across directories. That turned out to be
    very slow for users with expansive log directories (with many
    subdirectories and files) because `tf.gfile.Walk` makes many calls at
    the python level to C++ logic for reading data from disk (as opposed to
    performing much of that logic directly at the C++ level). Disk reads can
    be very costly for certain (such as remote and distributed) file systems.
    
    For those file systems, we find runs (subdirectories) much more quickly
    by iteratively glob-ing deeper and deeper directory levels. For a
    certain internal user, this reduced the time it took to find all runs
    from 20 minutes to 20 seconds.
    
    For other file systems, we preserve the previous behavior of performing
    a recursive walk aross directories. For certain file systems, using the
    glob-based method to find runs might actually be slower because those
    file systems do not implement analogs of TensorFlow's method for
    matching files on disk via a pattern, resulting in costly disk
    accesses.
    
    This change also re-organizes code to make that logic possible. We move
    code away from the 2 multiplexers into the io_wrapper helper module. I
    am generally not a huge of fan of large helper modules, but in this
    case, it seems desired because we have duplicate entire multiplexers and
    event accumulators. We also generalize the logic for listing files via
    blobbing into a `GlobAndListFiles` method.

--- tensorboard/backend/event_processing/io_wrapper.py
+++ tensorboard/backend/event_processing/io_wrapper.py
@@ -22,8 +22,30 @@ import os
 import tensorflow as tf
 
 
+# TODO(chihuahua): Rename this method to use camel-case for GCS (Gcs).
 def IsGCSPath(path):
   return path.startswith("gs://")
+
+
+def IsCnsPath(path):
+  return path.startswith("/cns/")
+
+
+def IsTensorFlowEventsFile(path):
+  """Check the path name to see if it is probably a TF Events file.
+
+  Args:
+    path: A file path to check if it is an event file.
+
+  Raises:
+    ValueError: If the path is an empty string.
+
+  Returns:
+    If path is formatted like a TensorFlowEventsFile.
+  """
+  if not path:
+    raise ValueError('Path must be a nonempty string')
+  return 'tfevents' in tf.compat.as_str_any(os.path.basename(path))
 
 
 def ListDirectoryAbsolute(directory):
@@ -32,21 +54,108 @@ def ListDirectoryAbsolute(directory):
           for path in tf.gfile.ListDirectory(directory))
 
 
-def ListRecursively(top):
+def GlobAndListFiles(top):
+  """Recursively lists all files within the directory.
+
+  This method does so by glob-ing deeper and deeper directories, ie
+  /foo/*, foo/*/*, foo/*/*/* and so on until all files are listed. All file
+  paths are absolute, and this method lists subdirectories too.
+
+  For certain file systems, Globbing via this method may prove
+  significantly faster than recursively walking a directory.
+  Specifically, file systems that implement analogs to TensorFlow's
+  FileSystem.GetMatchingPaths method could save costly disk reads by using
+  this method. However, for other file systems, this method might prove slower
+  because the file system performs a walk per call to glob (in which case it
+  might as well just perform 1 walk).
+
+  Args:
+    top: A path to a directory.
+
+  Yields:
+    The absolute path of each file.
+  """
+  current_glob_string = top
+  level = 0
+  while True:
+    tf.logging.info('GlobAndListFiles: Starting to glob level %d', level)
+    glob = tf.gfile.Glob(current_glob_string)
+    tf.logging.info(
+        'GlobAndListFiles: %d files glob-ed at level %d', len(glob), level)
+
+    if not glob:
+      # This subdirectory level lacks files. Terminate.
+      return
+
+    for absolute_file_path in glob:
+      yield absolute_file_path
+
+    # Iterate to the next level of subdirectories.
+    current_glob_string = os.path.join(current_glob_string, '*')
+    level += 1
+
+
+def WalkAndListFilesPerDirectory(top):
   """Walks a directory tree, yielding (dir_path, file_paths) tuples.
 
   For each of `top` and its subdirectories, yields a tuple containing the path
   to the directory and the path to each of the contained files.  Note that
   unlike os.Walk()/tf.gfile.Walk(), this does not list subdirectories and the
-  file paths are all absolute.
+  file paths are all absolute. If the directory does not exist, this yields
+  nothing.
 
-  If the directory does not exist, this yields nothing.
+  Walking may be incredibly slow on certain file systems.
 
   Args:
-    top: A path to a directory..
+    top: A path to a directory.
+
   Yields:
-    A list of (dir_path, file_paths) tuples.
+    A (dir_path, file_paths) tuple for each directory/subdirectory.
   """
   for dir_path, _, filenames in tf.gfile.Walk(top):
     yield (dir_path, (os.path.join(dir_path, filename)
                       for filename in filenames))
+
+
+def GetLogdirSubdirectories(path):
+  """Obtains all subdirectories with events files.
+
+  Args:
+    path: The path to a directory under which to find subdirectories.
+
+  Returns:
+    A tuple of absolute paths of all subdirectories each with at least 1 events
+    file directly within the subdirectory.
+
+  Raises:
+    ValueError: If the path passed to the method exists and is not a directory.
+  """
+  if not tf.gfile.Exists(path):
+    # No directory to traverse.
+    return ()
+
+  if not tf.gfile.IsDirectory(path):
+    raise ValueError('GetLogdirSubdirectories: path exists and is not a '
+                     'directory, %s' % path)
+
+  if IsGCSPath(path) or IsCnsPath(path):
+    # Glob-ing for files can be significantly faster than recursively
+    # walking through directories for some file systems.
+    # Use a dict comprehension to unique-ify subdirectories.
+    tf.logging.info(
+        'GetLogdirSubdirectories: Starting to list directories via glob-ing.')
+    return tuple({
+        os.path.dirname(absolute_file_path): True
+        for absolute_file_path in GlobAndListFiles(path)
+        if IsTensorFlowEventsFile(absolute_file_path)
+    })
+
+  # For other file systems, the glob-ing based method might be slower because
+  # each call to glob could involve performing a recursive walk.
+  tf.logging.info(
+      'GetLogdirSubdirectories: Starting to list directories via walking.')
+  return (
+      subdir
+      for (subdir, files) in WalkAndListFilesPerDirectory(path)
+      if any(IsTensorFlowEventsFile(f) for f in files)
+  )
--- tensorboard/backend/event_processing/io_wrapper_test.py
+++ tensorboard/backend/event_processing/io_wrapper_test.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 import os
 import tempfile
 
+import six
 import tensorflow as tf
 
 from tensorboard.backend.event_processing import io_wrapper
@@ -31,6 +32,27 @@ class IoWrapperTest(tf.test.TestCase):
 
   def testIsGcsPathIsFalse(self):
     self.assertFalse(io_wrapper.IsGCSPath('/tmp/foo'))
+
+  def testIsCnsPathTrue(self):
+    self.assertTrue(io_wrapper.IsCnsPath('/cns/foo/bar'))
+
+  def testIsCnsPathFalse(self):
+    self.assertFalse(io_wrapper.IsCnsPath('/tmp/foo'))
+
+  def testIsIsTensorFlowEventsFileTrue(self):
+    self.assertTrue(
+        io_wrapper.IsTensorFlowEventsFile(
+            '/logdir/events.out.tfevents.1473720042.com'))
+
+  def testIsIsTensorFlowEventsFileFalse(self):
+    self.assertFalse(
+        io_wrapper.IsTensorFlowEventsFile('/logdir/model.ckpt'))
+
+  def testIsIsTensorFlowEventsFileWithEmptyInput(self):
+    with six.assertRaisesRegex(self,
+                               ValueError,
+                               r'Path must be a nonempty string'):
+      io_wrapper.IsTensorFlowEventsFile('')
 
   def testListDirectoryAbsolute(self):
     temp_dir = tempfile.mkdtemp(prefix=self.get_temp_dir())
@@ -64,32 +86,30 @@ class IoWrapperTest(tf.test.TestCase):
         (os.path.join(temp_dir, f) for f in expected_files),
         io_wrapper.ListDirectoryAbsolute(temp_dir))
 
-  def testListRecursivelyForNestedFiles(self):
+  def testGlobAndListFiles(self):
     temp_dir = tempfile.mkdtemp(prefix=self.get_temp_dir())
-
-    # Add a few subdirectories.
-    directory_names = (
+    self._CreateDeepDirectoryStructure(temp_dir)
+    expected = (
         'foo',
         'bar',
-        'bar/baz',
-    )
-    for directory_name in directory_names:
-      os.makedirs(os.path.join(temp_dir, directory_name))
-
-    # Add a few files to the directory.
-    file_names = (
-        'events.out.tfevents.1473720381.meep.com',
-        'model.ckpt',
         'bar/events.out.tfevents.1473720382.bar.com',
         'bar/red_herring.txt',
+        'bar/baz',
         'bar/baz/events.out.tfevents.1473720383.baz.com',
         'bar/baz/events.out.tfevents.1473720384.baz.com',
+        'events.out.tfevents.1473720381.meep.com',
+        'model.ckpt',
     )
-    for file_name in file_names:
-      open(os.path.join(temp_dir, file_name), 'w').close()
+    self.assertItemsEqual(
+        [os.path.join(temp_dir, f) for f in expected] + [temp_dir],
+        list(io_wrapper.GlobAndListFiles(temp_dir)))
+
+  def testWalkAndListFilesPerDirectoryForNestedFiles(self):
+    temp_dir = tempfile.mkdtemp(prefix=self.get_temp_dir())
+    self._CreateDeepDirectoryStructure(temp_dir)
 
     # There were 4 subdirectories in total.
-    listing = io_wrapper.ListRecursively(temp_dir)
+    listing = io_wrapper.WalkAndListFilesPerDirectory(temp_dir)
     directory_to_listing = {
         dir: list(generator) for (dir, generator) in listing}
     expected = (
@@ -131,14 +151,43 @@ class IoWrapperTest(tf.test.TestCase):
         (os.path.join(temp_dir, 'bar/baz', f) for f in expected),
         directory_to_listing[os.path.join(temp_dir, 'bar/baz')])
 
-  def testListRecursivelyForEmptyDirectory(self):
+  def testWalkAndListFilesPerDirectoryForEmptyDirectory(self):
     empty_dir = tempfile.mkdtemp(prefix=self.get_temp_dir())
-    subdirectory_entries = list(io_wrapper.ListRecursively(empty_dir))
+    subdirectory_entries = list(
+        io_wrapper.WalkAndListFilesPerDirectory(empty_dir))
     self.assertEqual(1, len(subdirectory_entries))
 
     entry = subdirectory_entries[0]
     self.assertEqual(empty_dir, entry[0])
     self.assertItemsEqual((), entry[1])
+
+  def _CreateDeepDirectoryStructure(self, top_directory):
+    """Creates a reasonable deep structure of subdirectories with files.
+
+    Args:
+      top_directory: The absolute path of the top level directory in
+        which to create the directory structure.
+    """
+    # Add a few subdirectories.
+    directory_names = (
+        'foo',
+        'bar',
+        'bar/baz',
+    )
+    for directory_name in directory_names:
+      os.makedirs(os.path.join(top_directory, directory_name))
+
+    # Add a few files to the directory.
+    file_names = (
+        'events.out.tfevents.1473720381.meep.com',
+        'model.ckpt',
+        'bar/events.out.tfevents.1473720382.bar.com',
+        'bar/red_herring.txt',
+        'bar/baz/events.out.tfevents.1473720383.baz.com',
+        'bar/baz/events.out.tfevents.1473720384.baz.com',
+    )
+    for file_name in file_names:
+      open(os.path.join(top_directory, file_name), 'w').close()
 
 
 if __name__ == '__main__':
