72ce62f5001b15e7a0e05628d438a83b164313b0
Don Viszneki <dviszneki@influential.co>
2018-10-30 14:36:02 -0700

    hash bytes not strs in python 3

--- tensorboard/plugins/hparams/hparams_demo.py
+++ tensorboard/plugins/hparams/hparams_demo.py
@@ -46,6 +46,9 @@ HEAT_COEFFICIENT_LIST = [0.001, 0.005]
 
 def fingerprint(string):
   m = hashlib.md5()
+  # in python3 we must update our hash from a bytes, not a str
+  try: string = string.encode('utf-8')
+  except: pass
   m.update(string)
   return m.hexdigest()
 
