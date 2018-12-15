5a74dcc345822abbf4f79d5b9b13f79858254507
Nick Felt <nickfelt@google.com>
2018-12-14 19:25:28 -0800

    make tensorboard.compat provide tf and tf_v2 but via optional deps

--- tensorboard/compat/__init__.py
+++ tensorboard/compat/__init__.py
@@ -12,26 +12,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Compatibility interfaces for TensorBoard."""
+"""Compatibility interfaces for TensorBoard.
+
+This module provides aliases for importing variations on the TensorFlow APIs.
+
+`from tensorboard.compat import tf`: this provides the TF API. By default
+this will try to `import tensorflow` and if it fails, will be undefined. This
+is useful primarily when also depending on //tensorboard/compat:tensorflow.
+
+`from tensorboard.compat import tf_v2`: this provides the TF 2.0 API. By
+default this tries to import the TF 2.0 API from a few places where it may
+be accessible, and if it fails, will be undefined.
+"""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+# First, check if using TF is explicitly disabled by request.
 USING_TF = True
-
-# Don't attempt to use TF at all if this import exists due to build rules.
 try:
   from tensorboard.compat import notf
   USING_TF = False
 except ImportError:
   pass
 
+# If TF is not disabled, check if it's available.
 if USING_TF:
   try:
     import tensorflow as tf
   except ImportError:
     USING_TF = False
 
-if not USING_TF:
-  from tensorboard.compat import tensorflow_stub as tf
+if USING_TF:
+  # If we can use TF, try to provide `tf_v2` as well.
+  # Check if this is TF 2.0 by looking for a known 2.0-only tf.summary symbol.
+  # TODO(nickfelt): determine a cleaner way to do this.
+  if hasattr(tf.summary, 'write'):
+    tf_v2 = tf
+  else:
+    # As a fallback, try `tensorflow.compat.v2` if it's defined.
+    try:
+      from tensorflow.compat import v2 as tf_v2
+    except ImportError:
+      pass
+else:
+  # If we can't use TF, try to provide the stub instead.
+  # This will only work if the tensorflow_stub dep is included
+  # in the build, via the `tensorboard/compat:tensorflow` target.
+  try:
+    from tensorboard.compat import tensorflow_stub as tf
+  except ImportError:
+    pass
