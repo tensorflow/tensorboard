47787752c2c7bbdd2ea6788207817d534da8b585
Nick Felt <nickfelt@google.com>
2018-12-17 06:43:10 -0800

    de-flake summary_tests by reading last event file in case of multiple (spanning a second boundary)

--- tensorboard/plugins/image/summary_test.py
+++ tensorboard/plugins/image/summary_test.py
@@ -192,9 +192,8 @@ class SummaryV2OpTest(SummaryBaseTest, tf.test.TestCase):
     return self.read_single_event_from_eventfile().summary
 
   def read_single_event_from_eventfile(self):
-    event_files = glob.glob(os.path.join(self.get_temp_dir(), '*'))
-    self.assertEqual(len(event_files), 1)
-    events = list(tf.compat.v1.train.summary_iterator(event_files[0]))
+    event_files = sorted(glob.glob(os.path.join(self.get_temp_dir(), '*')))
+    events = list(tf.compat.v1.train.summary_iterator(event_files[-1]))
     # Expect a boilerplate event for the file_version, then the summary one.
     self.assertEqual(len(events), 2)
     return events[1]
