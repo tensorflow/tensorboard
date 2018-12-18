797f7ade87798d34f13784efc37b4195acf83ac2
Justine Tunney <jart@google.com>
2018-12-18 09:41:07 -0800

    Integrate 485deb240440c3fbb5d5369980f2a91ce871f88e

---
+++ tensorboard/util/platform_util_test.py
@@ -0,0 +1,31 @@
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from tensorboard.util import platform_util
+
+
+class PlatformUtilTest(tf.test.TestCase):
+
+  def test_readahead_file_path(self):
+    self.assertEqual('foo/bar', platform_util.readahead_file_path('foo/bar'))
+
+
+if __name__ == '__main__':
+  tf.test.main()
