8f0df537c17b4a42700621f9ba0565bb733304fa
Xuguang Wang <xuguang@google.com>
2018-12-28 12:55:13 +0800

    polish code of run_toco_impl.py
    remove unused file: freeze_graph.py suggestion.py and tflite_convert.py
    
    Change-Id: I321a92e53523c4b7707826d70ce277be0b5d36f8

--- tensorboard/plugins/lite/run_toco_impl.py
+++ tensorboard/plugins/lite/run_toco_impl.py
@@ -2,105 +2,97 @@ import os
 import tensorflow as tf
 
 from google.protobuf import text_format
-from tensorflow.core.framework import graph_pb2
-from tensorflow.python.platform import gfile
-from tensorboard.plugins.lite.tflite_convert import run_toco_with_suggestion
-from tensorboard.plugins.lite.freeze_graph import freeze_with_suggestion
 
-from tensorboard.plugins.lite.suggestion import Suggestion
-
-class ArgsSuggestion(Suggestion):
-  def __init__(self, from_exception, stack_trace=None):
-    Suggestion.__init__(self, from_exception, stack_trace)
-
-  @classmethod
-  def _get_error_map(cls):
-    return {"a Tensor which does not exist": "please check your input_array arguments"}
+try:
+  from tensorflow.python.framework.errors import Suggestion
+  from tensorflow.lite.python.tflite_convert import run_tflite_convert
+  from tensorflow.python.tools.freeze_graph import run_freeze_graph
+  tflite_support = True
+except ImportError as e:
+  tflite_support = False
 
 
-def parse_input_graph_proto(input_graph):
+if tflite_support:
+
+  def list_supported_ops():
+    return tf.lite.TFLiteConverter.list_supported_ops()
+
+  class ArgsSuggestion(Suggestion):
+    def __init__(self, from_exception, stack_trace=None):
+      Suggestion.__init__(self, from_exception, stack_trace)
+
+    @classmethod
+    def _get_error_map(cls):
+      return {"a Tensor which does not exist": "please check your input_nodes and output_nodes argument"}
+
+
+  def parse_input_graph_proto(input_graph):
 
     # https://www.tensorflow.org/extend/tool_developers/
-    if not tf.gfile.Exists(input_graph):
-        print("Input graph file '" + input_graph + "' does not exist!")
-        return -1
-
-    if input_graph.split('.')[-1] == 'pb':
-        input_binary = True
-    else:
-        input_binary = False
+    input_binary = (input_graph.split('.')[-1] == 'pb')
+    mode = "rb" if input_binary else "r"
 
     input_graph_def = tf.GraphDef()
 
-    mode = "rb" if input_binary else "r"
     with tf.gfile.FastGFile(input_graph, mode) as f:
-        if input_binary:
-            input_graph_def.ParseFromString(f.read())
-        else:
-            text_format.Merge(f.read(), input_graph_def)
+      if input_binary:
+        input_graph_def.ParseFromString(f.read())
+      else:
+        text_format.Merge(f.read(), input_graph_def)
 
     return input_graph_def
 
 
-def write_summary_of_graph(pb_file, log_dir):
-    with tf.Session() as sess:
-        graph = parse_input_graph_proto(pb_file)
-        tf.import_graph_def(graph, name='')
-        train_writer = tf.summary.FileWriter(log_dir)
-        train_writer.add_graph(sess.graph)
-
-
-def is_frozen_graph(sess):
+  def is_frozen_graph(sess):
     for op in sess.graph.get_operations():
-        if op.type.startswith("Variable") or op.type.endswith("VariableOp"):
-            return False
+      if op.type.startswith("Variable") or op.type.endswith("VariableOp"):
+        return False
     return True
 
 
-def make_args_from_session(sess, graph_def_file, tflite_file, options):
+  def make_args_from_session(sess, graph_def_file, tflite_file, options):
     input_nodes = []
     input_shapes = []
     output_nodes = []
     output_dir = os.path.dirname(tflite_file)
 
     for node in options['input_nodes']:
-        tensor = sess.graph.get_tensor_by_name(node + ":0")
-        input_nodes.append(node)
-        shape = tensor.shape.as_list()
-        print("xxxxxxxxxxxxxxxxx")
-        print(shape)
-        if len(shape) > 0 and (shape[0] == -1 or shape[0] is None):
-            shape[0] = options['batch_size']
-        input_shapes.append(','.join(map(str, shape)))
+      tensor = sess.graph.get_tensor_by_name(node + ":0")
+      input_nodes.append(node)
+      shape = tensor.shape.as_list()
+      if len(shape) > 0 and (shape[0] == -1 or shape[0] is None):
+        shape[0] = options['batch_size']
+      input_shapes.append(','.join(map(str, shape)))
 
     for node in options['output_nodes']:
-        tensor = sess.graph.get_tensor_by_name(node + ":0")
-        output_nodes.append(node)
+      # check whether output node is valid.
+      _ = sess.graph.get_tensor_by_name(node + ":0")
+      output_nodes.append(node)
 
     if is_frozen_graph(sess):
-        frozen_graph_file = graph_def_file
-        freeze_args = None
+      frozen_graph_file = graph_def_file
+      freeze_args = None
     else:
-        frozen_graph_file = os.path.join(output_dir, 'frozen.pb')
-        checkpoint = options['checkpoint']
-        freeze_args = [
-            '--input_checkpoint={}'.format(checkpoint),
-            '--output_graph={}'.format(frozen_graph_file),
-            '--output_node_names={}'.format(','.join(output_nodes))]
+      frozen_graph_file = os.path.join(output_dir, 'frozen.pb')
+      checkpoint = options['checkpoint']
+      freeze_args = [
+        '--input_checkpoint={}'.format(checkpoint),
+        '--output_graph={}'.format(frozen_graph_file),
+        '--output_node_names={}'.format(','.join(output_nodes))]
 
-        if tf.gfile.Exists(checkpoint + '.meta'):
-            freeze_args.append('--input_meta_graph={}'.format(checkpoint + '.meta'))
-            freeze_args.append('--input_binary=true')
-        else:
-            freeze_args.append('--input_graph={}'.format(graph_def_file))
-            freeze_args.append('--input_binary=false')
+      if tf.gfile.Exists(checkpoint + '.meta'):
+        freeze_args.append('--input_meta_graph={}'.format(checkpoint + '.meta'))
+        freeze_args.append('--input_binary=true')
+      else:
+        freeze_args.append('--input_graph={}'.format(graph_def_file))
+        freeze_args.append('--input_binary=false')
 
     convert_args = ["--graph_def_file={}".format(frozen_graph_file),
                     "--input_format=TENSORFLOW_GRAPHDEF",
                     "--output_format=TFLITE",
                     "--output_file={}".format(tflite_file),
-                    "--inference_type=FLOAT", #TODO how to set it
-                    "--input_type=FLOAT", #TODO how to set it
+                    "--inference_type=FLOAT",  # TODO how to set it
+                    "--input_type=FLOAT",  # TODO how to set it
                     "--input_arrays={}".format(",".join(input_nodes)),
                     "--output_arrays={}".format(",".join(output_nodes)),
                     "--input_shapes={}".format(":".join(input_shapes))]
@@ -109,55 +101,72 @@ def make_args_from_session(sess, graph_def_file, tflite_file, options):
     return freeze_args, convert_args
 
 
-def make_args(graph_def_file, tflite_file, options):
+  def make_args(graph_def_file, tflite_file, options):
     if tf.gfile.Exists(graph_def_file):
-        graph_def = parse_input_graph_proto(graph_def_file)
-        with tf.Graph().as_default():
-            with tf.Session() as sess:
-                tf.import_graph_def(graph_def, name='')
-                return make_args_from_session(sess, graph_def_file, tflite_file, options)
+      graph_def = parse_input_graph_proto(graph_def_file)
+      with tf.Graph().as_default():
+        with tf.Session() as sess:
+          tf.import_graph_def(graph_def, name='')
+          return make_args_from_session(sess, graph_def_file, tflite_file, options)
     else:
-        meta_file = options['checkpoint'] + '.meta'
-        if tf.gfile.Exists(meta_file):
-            with tf.Session() as sess:
-                saver = tf.train.import_meta_graph(meta_file, clear_devices=True)
-                saver.restore(sess, options['checkpoint'])
-                return make_args_from_session(sess, graph_def_file, tflite_file, options)
-        else:
-            raise ValueError('no model file or checkpoint meta file')
+      meta_file = options['checkpoint'] + '.meta'
+      if tf.gfile.Exists(meta_file):
+        with tf.Session() as sess:
+          saver = tf.train.import_meta_graph(meta_file, clear_devices=True)
+          saver.restore(sess, options['checkpoint'])
+          return make_args_from_session(sess, graph_def_file, tflite_file, options)
+      else:
+        raise ValueError('no model file or checkpoint meta file')
 
 
-def freeze_and_convert(graph_def_file, tflite_file, options):
+  def freeze_and_convert(graph_def_file, tflite_file, options):
     try:
       freeze_args, convert_args = make_args(graph_def_file, tflite_file, options)
     except Exception as e:
-        raise ArgsSuggestion(e)
+      raise ArgsSuggestion(e)
 
     if freeze_args is not None:
-        freeze_with_suggestion(freeze_args)
+      run_freeze_graph(freeze_args)
 
-    run_toco_with_suggestion(convert_args)
+    run_tflite_convert(convert_args)
+
+
+  def get_freeze_and_convert_script(graph_def_file, tflite_file, options):
+    try:
+      freeze_args, convert_args = make_args(graph_def_file, tflite_file, options)
+    except Exception as e:
+      raise ArgsSuggestion(e)
 
     script = ''
     line_continuation = ' \\\n    '
-    if freeze_args is not None:
-        script += 'freeze_graph ' + line_continuation.join(freeze_args)
 
-    script += '\n\n'
+    if freeze_args is not None:
+      script += '# freeze graph\n'
+      script += 'freeze_graph ' + line_continuation.join(freeze_args)
+      script += '\n\n'
+
+    script += '# convert to tflite file\n'
     script += 'tflite_convert ' + line_continuation.join(convert_args)
     script += '\n'
 
     return script
 
-
-if __name__ == '__main__':
-
+  if __name__ == '__main__':
     graph_def_file = "model_dir/graph.pbtxt"
     options = {
-        "input_nodes": ["dnn/input_from_feature_columns/input_layer/concat"],
-        "output_nodes": ["dnn/head/predictions/probabilities"],
-        "batch_size": 1,
-        'checkpoint': 'model_dir/model.ckpt-400'
+      "input_nodes": ["dnn/input_from_feature_columns/input_layer/concat"],
+      "output_nodes": ["dnn/head/predictions/probabilities", "dnn/head/predictions/str_classes"],
+      "batch_size": 1,
+      'checkpoint': 'model_dir/model.ckpt-400'
     }
-    script = freeze_and_convert(graph_def_file, "model_dir/test.tflite", options)
-    print(script)
+
+    try:
+      freeze_and_convert(graph_def_file, "model_dir/test.tflite", options)
+    except Exception as e:
+      print e
+
+    try:
+      script = get_freeze_and_convert_script(graph_def_file, "model_dir/test.tflite", options)
+      print(script)
+    except Exception as e:
+      print e
