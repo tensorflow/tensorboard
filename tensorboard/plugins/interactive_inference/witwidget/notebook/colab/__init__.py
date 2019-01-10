882bef863faa25f0ebe7ea9af528b5a47268773d
Justine Tunney <jart@google.com>
2019-01-10 08:41:15 -0800

    Integrate 280c5cc4ec878429c53817c64aec5d74257b5813

--- tensorboard/plugins/interactive_inference/witwidget/notebook/colab/__init__.py
+++ tensorboard/plugins/interactive_inference/witwidget/notebook/colab/__init__.py
@@ -11,7 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Contains the version string."""
-
-VERSION = '0.1.6a1'
