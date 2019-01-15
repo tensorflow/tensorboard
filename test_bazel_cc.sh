cae1cc75b53ac326169593532c8fc9b195a317f3
William Chargin <wchargin@gmail.com>
2019-01-15 14:31:36 -0800

    DO NOT SUBMIT: squashed previous commits
    Summary:
    Travis is not running my jobs for some reason. Maybe something in the
    Git history scared it off?
    
    wchargin-branch: travis-debug-2

---
+++ test_bazel_cc.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+printf -- 'Testing Bazel CC behavior: %s\n' "$*"
+
+set -eux
+cd "$(mktemp -d)"
+
+describe_status() {
+    last_status=0
+    "$@" || last_status=$?
+    printf >&3 'exited with: %s\n' "${last_status}"
+}
+
+>empty.cc
+describe_status gcc -fcolor-diagnostics -o /dev/null ./empty.cc 2>&1 3>&1 >/dev/null
+rm -f ./empty.cc
+
+>WORKSPACE
+
+cat >hello.cc <<EOF
+#include <iostream>
+int main(int argc, char **argv) {
+    std::cout << "Hello World!\n";
+    return 0;
+}
+EOF
+
+cat >BUILD <<EOF
+cc_binary(
+    name = "hello",
+    srcs = ["hello.cc"],
+)
+EOF
+
+describe_status "${2-bazel}" run :hello 3>&1
