7441f07df1a33e2b97ebe97fba92567fce2e26b9
Johannes Meyer <meyerjo@informatik.uni-freiburg.de>
2019-01-25 17:34:53 +0100

    * removed TODO and return valid http response in case of an error

--- tensorboard/plugins/scalar/scalars_plugin.py
+++ tensorboard/plugins/scalar/scalars_plugin.py
@@ -192,10 +192,14 @@ class ScalarsPlugin(base_plugin.TBPlugin):
   @wrappers.Request.application
   def scalars_route(self, request):
     """Given a tag and single run, return array of ScalarEvents."""
-    # TODO: return HTTP status code for malformed requests
     tag = request.args.get('tag')
     run = request.args.get('run')
     experiment = request.args.get('experiment')
     output_format = request.args.get('format')
-    (body, mime_type) = self.scalars_impl(tag, run, experiment, output_format)
-    return http_util.Respond(request, body, mime_type)
+    try:
+      (body, mime_type) = self.scalars_impl(tag, run, experiment, output_format)
+      code = 200
+    except ValueError as e:
+      (body, mime_type) = (str(e), 'text/plain')
+      code = 400
+    return http_util.Respond(request, body, mime_type, code=code)
