1b532291e8f15c0f4235a9a8e5c6fce7e583a7d6
Nick Felt <nfelt@users.noreply.github.com>
2019-01-31 12:33:03 -0800

    Update protobuf dep to >=3.6.0 which is what is actually required (#1792)

--- tensorboard/pip_package/setup.py
+++ tensorboard/pip_package/setup.py
@@ -29,7 +29,7 @@ REQUIRED_PACKAGES = [
     'grpcio >= 1.6.3',
     'markdown >= 2.6.8',
     'numpy >= 1.12.0',
-    'protobuf >= 3.4.0',
+    'protobuf >= 3.6.0',
     'six >= 1.10.0',
     'werkzeug >= 0.11.15',
     # python3 specifically requires wheel 0.26
