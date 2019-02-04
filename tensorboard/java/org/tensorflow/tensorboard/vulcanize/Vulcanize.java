6e36461c5c037fbd33e505a573f83bd5954316ff
Stephan Lee <stephanwlee@gmail.com>
2019-02-04 10:50:05 -0800

    Bump up JSCompiler langout to ES2015
    JSCompiler turns Object.create into ES5 compatible way which causes
    HTMLElement to throw. More specifically, Polymer uses Object.create to
    create a subclass of HTMLElement but JSC turns it into
    HTMLElement.call(this) which causes browser runtime to throw.
    
    Object.create was introduced to main stream as part of ES2015.

--- tensorboard/java/org/tensorflow/tensorboard/vulcanize/Vulcanize.java
+++ tensorboard/java/org/tensorflow/tensorboard/vulcanize/Vulcanize.java
@@ -421,8 +421,8 @@ public final class Vulcanize {
     // Nice options.
     options.setColorizeErrorOutput(true);
     options.setContinueAfterErrors(true);
-    options.setLanguageIn(CompilerOptions.LanguageMode.ECMASCRIPT_2017);
-    options.setLanguageOut(CompilerOptions.LanguageMode.ECMASCRIPT5);
+    options.setLanguageIn(CompilerOptions.LanguageMode.ECMASCRIPT_2018);
+    options.setLanguageOut(CompilerOptions.LanguageMode.ECMASCRIPT_2015);
     options.setGenerateExports(true);
     options.setStrictModeInput(false);
     options.setExtraAnnotationNames(EXTRA_JSDOC_TAGS);
