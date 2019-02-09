a957fdf24bfdd23abdb81dcc87c8c9355f149f9c
William Chargin <wchargin@gmail.com>
2019-02-08 16:40:45 -0800

    review: add docstring with general notes
    wchargin-branch: notebook-colab-integration

--- tensorboard/notebook.py
+++ tensorboard/notebook.py
@@ -278,6 +278,26 @@ def _display(port=None, height=None, print_message=False, display_handle=None):
 
 
 def _display_colab(port, height, display_handle):
+  """Display a TensorBoard instance in a Colab output frame.
+
+  The Colab VM is not directly exposed to the network, so the Colab
+  runtime provides a service worker tunnel to proxy requests from the
+  end user's browser through to servers running on the Colab VM: the
+  output frame may issue requests to https://localhost:<port> (HTTPS
+  only), which will be forwarded to the specified port on the VM.
+
+  It does not suffice to create an `iframe` and let the service worker
+  redirect its traffic (`<iframe src="https://localhost:6006">`),
+  because for security reasons service workers cannot intercept iframe
+  traffic. Instead, we manually fetch the TensorBoard index page with an
+  XHR in the output frame, and inject the raw HTML into `document.body`.
+
+  By default, the TensorBoard web app requests resources against
+  relative paths, like `./data/logdir`. Within the output frame, these
+  requests must instead hit `https://localhost:<port>/data/logdir`. To
+  redirect them, we change the document base URI, which transparently
+  affects all requests (XHRs and resources alike).
+  """
   import IPython.display
   shell = """
     <script>
