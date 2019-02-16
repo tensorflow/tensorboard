1dea1078142b11b10442135f4b7ef173f6d5a4bf
Stephan Lee <stephanwlee@gmail.com>
2019-02-15 23:11:41 -0800

    Utility method for combining protos naively
    Graph plugin soon will support FunctionGraphs in the RunMetadata.
    Because it is a tree of GraphDefs, for our frontend, we need to be
    able to compose one GraphDef.

---
+++ tensorboard/plugins/graph/graph_util.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Utilities for graph plugin."""
+from tensorboard.compat.proto.graph_pb2 import GraphDef
+from tensorboard.compat.tensorflow_stub import dtypes
+
+
+def combine_graph_defs(to_proto, from_proto):
+  """Combines two GraphDefs by adding nodes from from_proto into to_proto.
+
+  Args:
+    to_proto: a destination GraphDef.
+    from_proto: a GraphDef to copy contents from.
+
+  Returns:
+    to_proto
+  """
+  if from_proto.version != to_proto.version:
+    raise ValueError('Cannot combine GraphDefs of different versions.')
+
+  for from_node in from_proto.node:
+    to_node = to_proto.node.add()
+    to_node.CopyFrom(from_node)
+
+  if from_proto.library:
+    if not to_proto.library:
+      to_proto.library = function_pb2.FunctionDefLibrary()
+
+    for from_function in from_proto.library.function:
+      to_function = to_proto.library.function.add()
+      to_function.CopyFrom(from_function)
+    for from_gradient in from_proto.library.gradient:
+      to_gradient = to_proto.library.gradient.add()
+      to_gradient.CopyFrom(from_gradient)
+
+  return to_proto
---
+++ tensorboard/plugins/graph/graph_util_test.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from tensorboard.compat.proto.graph_pb2 import GraphDef
+from tensorboard.plugins.graph import graph_util
+from tensorflow.python.platform import test
+
+class GraphUtilTest(tf.test.TestCase):
+  def test_combine_graph_defs(self):
+      expected_proto = '''
+        node {
+          name: "X"
+          op: "Input"
+        }
+        node {
+          name: "W"
+          op: "Input"
+        }
+        node {
+          name: "Y"
+          op: "MatMul"
+          input: "X"
+          input: "W"
+        }
+        node {
+          name: "X"
+          op: "Input"
+        }
+        node {
+          name: "Z"
+          op: "Input"
+        }
+        node {
+          name: "Q"
+          op: "MatMul"
+          input: "X"
+          input: "Z"
+        }
+        versions {
+          producer: 21
+        }
+      '''
+
+      graph_def_a = GraphDef()
+      text_format.Merge('''
+        node {
+          name: "X"
+          op: "Input"
+        }
+        node {
+          name: "W"
+          op: "Input"
+        }
+        node {
+          name: "Y"
+          op: "MatMul"
+          input: "X"
+          input: "W"
+        }
+        versions {
+          producer: 21
+        }
+      ''', graph_def_a)
+
+      graph_def_b = GraphDef()
+      text_format.Merge('''
+        node {
+          name: "X"
+          op: "Input"
+        }
+        node {
+          name: "Z"
+          op: "Input"
+        }
+        node {
+          name: "Q"
+          op: "MatMul"
+          input: "X"
+          input: "Z"
+        }
+        versions {
+          producer: 21
+        }
+      ''', graph_def_b)
+
+      self.assertProtoEquals(
+          expected_proto,
+          graph_util.combine_graph_defs(graph_def_a, graph_def_b))
+
+
+if __name__ == '__main__':
+  tf.test.main()
