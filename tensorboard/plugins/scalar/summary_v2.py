345d41f4522c0c2cbd92fc7ebc06c5feaae51fe8
Justine Tunney <jart@google.com>
2019-02-27 12:41:21 -0800

    Integrate f572c22fb860899bf2d4384e42cbf40f1aa25544

--- tensorboard/plugins/scalar/summary_v2.py
+++ tensorboard/plugins/scalar/summary_v2.py
@@ -29,20 +29,26 @@ from tensorboard.plugins.scalar import metadata
 from tensorboard.util import tensor_util
 
 
-def scalar(name, data, step, description=None):
+def scalar(name, data, step=None, description=None):
   """Write a scalar summary.
 
   Arguments:
     name: A name for this summary. The summary tag used for TensorBoard will
       be this name prefixed by any active name scopes.
     data: A real numeric scalar value, convertible to a `float32` Tensor.
-    step: Required `int64`-castable monotonic step value.
+    step: Explicit `int64`-castable monotonic step value for this summary. If
+      omitted, this defaults to `tf.summary.experimental.get_step()`, which must
+      not be None.
     description: Optional long-form description for this summary, as a
       constant `str`. Markdown is supported. Defaults to empty.
 
   Returns:
     True on success, or false if no summary was written because no default
     summary writer was available.
+
+  Raises:
+    ValueError: if a default writer exists, but no step was provided and
+      `tf.summary.experimental.get_step()` is None.
   """
   summary_metadata = metadata.create_summary_metadata(
       display_name=None, description=description)
