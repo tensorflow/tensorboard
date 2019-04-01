7bf21f599c93931de581f6035fcf1b4f53efbead
Tzu-Wei Huang <huang.dexter@gmail.com>
2019-04-02 01:11:08 +0800

    rewrite record_writer

--- tensorboard/writer/record_writer.py
+++ tensorboard/writer/record_writer.py
@@ -13,48 +13,34 @@
 # limitations under the License.
 # ==============================================================================
 
-"""
-To write tf_record into file. Here we use it for tensorboard's event writting.
-The code was borrowed from https://github.com/TeamHG-Memex/tensorboard_logger
-"""
-
-import copy
-import io
-import os.path
-import re
 import struct
-
 from .crc32c import crc32c
-
-
-_VALID_OP_NAME_START = re.compile('^[A-Za-z0-9.]')
-_VALID_OP_NAME_PART = re.compile('[A-Za-z0-9_.\\-/]+')
-
-
 class RecordWriter(object):
-    """write encoded protobuf along with its checksum.
+    def __init__(self, logfile):
+        self._writer = open(logfile, 'wb')
 
-    """
-    def __init__(self, path):
-        self._name_to_tf_name = {}
-        self._tf_names = set()
-        self._path = path
-        self._writer = None
-        self._writer = open(path, 'wb')
-
-    def write(self, event_str):
-        w = self._writer.write
-        header = struct.pack('Q', len(event_str))
-        w(header)
-        w(struct.pack('I', masked_crc32c(header)))
-        w(event_str)
-        w(struct.pack('I', masked_crc32c(event_str)))
+    # Format of a single record:
+    # uint64    length
+    # uint32    masked crc of length
+    # byte      data[length]
+    # uint32    masked crc of data
+    def write(self, data):
+        header_len = struct.pack('Q', len(data))
+        header_len_crc = struct.pack('I', masked_crc32c(header_len))
+        footer_crc = struct.pack('I', masked_crc32c(data))
+        self._writer.write(header_len + header_len_crc + data + footer_crc)
 
     def flush(self):
-        self._writer.flush()
+        if self._writer is not None:
+            self._writer.flush()
+        else:
+            raise OSError('file writer is missing')
 
     def close(self):
-        self._writer.close()
+        if self._writer is not None:
+            self._writer.close()
+        else:
+            raise OSError('file writer is missing')
 
 
 def masked_crc32c(data):
@@ -64,10 +50,3 @@ def masked_crc32c(data):
 
 def u32(x):
     return x & 0xffffffff
-
-
-def make_valid_tf_name(name):
-    if not _VALID_OP_NAME_START.match(name):
-        # Must make it valid somehow, but don't want to remove stuff
-        name = '.' + name
-    return '_'.join(_VALID_OP_NAME_PART.findall(name))
