1c7d2ca15d6c485d5a657595d6e9e9859fcc4937
Stephan Lee <stephanwlee@gmail.com>
2019-04-04 10:53:46 -0700

    graph: fixed broken context menu
    Problem: used global query selector to find ".context-menu" in the
    entire document.
    
    Solution: graph scene now provides an API for accessing the DOM useful
    for rendering the context-menu.

--- tensorboard/plugins/graph/tf_graph_common/tf-graph-scene.ts
+++ tensorboard/plugins/graph/tf_graph_common/tf-graph-scene.ts
@@ -34,5 +34,6 @@ module tf.graph.scene {
     abstract isNodeSelected(nodeName: string): boolean;
     abstract getAnnotationGroupsIndex(name: string): Selection;
     abstract getGraphSvgRoot(): SVGElement;
+    abstract getContextMenu(): HTMLElement;
   }
 }
