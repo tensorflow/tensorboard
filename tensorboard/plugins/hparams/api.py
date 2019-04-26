c65e35273c33306f878f46d6b6f9b4a048371e0a
William Chargin <wchargin@gmail.com>
2019-04-26 14:59:00 -0700

    hparams: extract `api` wrapper over `summary_v2`
    Summary:
    We’ll shortly extract the Keras callback out of `summary_v2` such that
    `summary_v2` does not need to depend on TensorFlow. It will continue to
    be exposed from `api`.
    
    Part of a reorganization to match the structure described here:
    <https://github.com/tensorflow/tensorboard/pull/2139#discussion_r279106791>
    
    wchargin-branch: hparams-api-refactor

---
+++ tensorboard/plugins/hparams/api.py
@@ -0,0 +1,37 @@
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Experimental public APIs for the HParams plugin."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensorboard.plugins.hparams import summary_v2
+
+
+Discrete = summary_v2.Discrete
+Domain = summary_v2.Domain
+HParam = summary_v2.HParam
+IntInterval = summary_v2.IntInterval
+KerasCallback = summary_v2.KerasCallback
+Metric = summary_v2.Metric
+RealInterval = summary_v2.RealInterval
+hparams_config = summary_v2.hparams_config
+
+
+del absolute_import
+del division
+del print_function
+del summary_v2
