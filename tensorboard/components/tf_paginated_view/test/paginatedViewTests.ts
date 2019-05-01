97eb73909a556afffa8d1c40627867152267ab46
Stephan Lee <stephanwlee@gmail.com>
2019-05-01 10:02:12 -0700

    Fix test for tf-category-paginated-view

--- tensorboard/components/tf_paginated_view/test/paginatedViewTests.ts
+++ tensorboard/components/tf_paginated_view/test/paginatedViewTests.ts
@@ -21,10 +21,19 @@ type Item = {
   id: String,
   content: String,
 };
+type ItemCategory = tf_categorization_utils.Category<Item>;
 
-function createItems(num: Number): Item[] {
-  return Array.from(Array(num))
-      .map((_, i) => ({id: `id${i}`, content: `content_${i}`}));
+function createCategory(numItems: Number): ItemCategory {
+  const items = Array.from(Array(numItems))
+    .map((_, i) => ({id: `id${i}`, content: `content_${i}`}));
+
+  return {
+    name: 'Category1',
+    metadata: {
+      type: tf_categorization_utils.CategoryType.PREFIX_GROUP,
+    },
+    items,
+  };
 }
 function flushP(): Promise<void> {
   return new Promise(resolve => window.flush(resolve));
@@ -35,20 +44,21 @@ function flushAnimationFrameP(): Promise<void> {
 function flushAllP(): Promise<[void, void]> {
   return Promise.all([flushP(), flushAnimationFrameP()]);
 }
-
 describe('tf-paginated-view tests', () => {
   let view: any;
 
+  function querySelector(cssSelector: string): HTMLElement {
+    return view.$.view.root.querySelector(cssSelector);
+  }
   async function goNext() {
-    view.querySelector('.big-page-buttons paper-button:last-of-type').click();
+    querySelector('.big-page-buttons paper-button:last-of-type').click();
     await flushAllP();
   }
 
   beforeEach(async () => {
     view = fixture('paginatedViewFixture');
     view._limit = 2;
-    view.getItemKey = ({id}) => id;
-    view.items = createItems(5);
+    view.category = createCategory(5);
     view.randomNumber = 42;
 
     // allow dom-if to be flushed.
@@ -56,79 +66,90 @@ describe('tf-paginated-view tests', () => {
   });
 
   it('renders a page', () => {
-    expect(view.querySelector('#id0')).to.be.not.null;
-    expect(view.querySelector('#id1')).to.be.not.null;
+    expect(querySelector('#id0')).to.be.not.null;
+    expect(querySelector('#id1')).to.be.not.null;
 
     // 2-4 should be in another page.
-    expect(view.querySelector('#id2')).to.be.null;
+    expect(querySelector('#id2')).to.be.null;
   });
 
   it('responds to ancestor prop change that is bound on template', () => {
-    expect(view.querySelector('#id0').getAttribute('number')).to.equal('42');
+    expect(querySelector('#id0').getAttribute('number')).to.equal('42');
 
     view.randomNumber = 7;
-    expect(view.querySelector('#id0').getAttribute('number')).to.equal('7');
+    expect(querySelector('#id0').getAttribute('number')).to.equal('7');
   });
 
   it('navigates to next page when clicked on a button', async () => {
     // Sanity check
-    expect(view.querySelector('#id2')).to.be.null;
-    expect(view.querySelector('#id4')).to.be.null;
-    expect(view.querySelector('paper-input')).to.have.property('value', '1');
+    expect(querySelector('#id2')).to.be.null;
+    expect(querySelector('#id4')).to.be.null;
+    expect(querySelector('paper-input')).to.have.property('value', '1');
 
     await goNext();
 
-    expect(view.querySelector('#id1')).to.be.null;
-    expect(view.querySelector('#id2')).to.be.not.null;
-    expect(view.querySelector('#id3')).to.be.not.null;
-    expect(view.querySelector('#id4')).to.be.null;
-    expect(view.querySelector('paper-input')).to.have.property('value', '2');
+    expect(querySelector('#id1')).to.be.null;
+    expect(querySelector('#id2')).to.be.not.null;
+    expect(querySelector('#id3')).to.be.not.null;
+    expect(querySelector('#id4')).to.be.null;
+    expect(querySelector('paper-input')).to.have.property('value', '2');
 
     await goNext();
 
-    expect(view.querySelector('#id3')).to.be.null;
-    expect(view.querySelector('#id4')).to.be.not.null;
-    expect(view.querySelector('paper-input')).to.have.property('value', '3');
+    expect(querySelector('#id3')).to.be.null;
+    expect(querySelector('#id4')).to.be.not.null;
+    expect(querySelector('paper-input')).to.have.property('value', '3');
   });
 
   it('reacts to limit change', () => {
     // 2-4 should be in another page, initially.
-    expect(view.querySelector('#id2')).to.be.null;
+    expect(querySelector('#id2')).to.be.null;
 
     view._limit = 4;
-    expect(view.querySelector('#id2')).to.be.not.null;
-    expect(view.querySelector('#id3')).to.be.not.null;
-    expect(view.querySelector('#id4')).to.be.null;
+    expect(querySelector('#id2')).to.be.not.null;
+    expect(querySelector('#id3')).to.be.not.null;
+    expect(querySelector('#id4')).to.be.null;
   });
 
-  it('reacts to items update', () => {
-    view.items = view.items.slice().reverse();
-    expect(view.querySelector('#id4')).to.be.not.null;
-    expect(view.querySelector('#id3')).to.be.not.null;
+  it('reacts to category update', async () => {
+    view.category = Object.assign(
+      {},
+      view.category,
+      {items: view.category.items.slice().reverse()},
+    );
+    await flushAllP();
+    expect(querySelector('#id4')).to.be.not.null;
+    expect(querySelector('#id3')).to.be.not.null;
+  });
+
+  it('reacts to items update', async () => {
+    // Mutate the property of category in Polymeric way.
+    view.set('category.items', view.category.items.slice().reverse());
+    await flushAllP();
+    expect(querySelector('#id4')).to.be.not.null;
+    expect(querySelector('#id3')).to.be.not.null;
   });
 
   it('handles shrinking the number of pages', async () => {
-    view.items = createItems(1);
+    view.category = createCategory(1);
     await flushAllP();
 
-    expect(view.querySelector('#id0')).to.be.not.null;
-    expect(view.querySelector('#id1')).to.be.null;
+    expect(querySelector('#id0')).to.be.not.null;
+    expect(querySelector('#id1')).to.be.null;
     expect(_getPageCount()).to.equal(1);
   });
 
   it('handles growing the number of pages', async () => {
     expect(_getPageCount()).to.equal(3);
 
-    view.items = createItems(10);
+    view.category = createCategory(10);
 
     await flushAllP();
     expect(_getPageCount()).to.equal(5);
   });
 
   function _getPageCount(): number {
-    // str = `Page <some gibbersh from paper-input> of [pageCount]`
-    const str = view.querySelector('#controls-container').innerText.trim();
-    return parseInt(str.match(/\d+$/)[0], 10);
+    return view.$.view._pageCount;
   }
 
 });
