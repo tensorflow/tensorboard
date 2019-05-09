ba607bcff4e5e1936c35204b29013167fd90b030
Stephan Lee <stephanwlee@gmail.com>
2019-05-09 16:34:37 -0700

    Zoom with mouse wheel only when alt key is pressed
    Default mouse wheel to zoom is rather obnoxious since large area of the
    TensorBoard is zoomable area.
    
    Fixes #2214.

--- tensorboard/components/vz_line_chart2/panZoomDragLayer.ts
+++ tensorboard/components/vz_line_chart2/panZoomDragLayer.ts
@@ -34,7 +34,8 @@ export class PanZoomDragLayer extends Plottable.Components.Group {
    * When not pressing alt-key, it behaves like DragZoomLayer -- dragging a
    * region zooms the area under the gray box and double clicking resets the
    * zoom. When pressing alt-key, it lets user pan around while having mousedown
-   * on the chart and let user zoom-in/out of cursor when scroll.
+   * on the chart and let user zoom-in/out of cursor when scroll with alt key
+   * pressed.
    */
   constructor(
       xScale: Plottable.QuantitativeScale<number|{valueOf(): number}>,
@@ -46,6 +47,7 @@ export class PanZoomDragLayer extends Plottable.Components.Group {
     this.panZoom.dragInteraction().mouseFilter((event: MouseEvent) => {
       return PanZoomDragLayer.isPanKey(event) && event.button === 0;
     });
+    this.panZoom.wheelFilter((event) => event.altKey);
     this.panZoom.attachTo(this);
 
     this.dragZoomLayer = new vz_line_chart.DragZoomLayer(
