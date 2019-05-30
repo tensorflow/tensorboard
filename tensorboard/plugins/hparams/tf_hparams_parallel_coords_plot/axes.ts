f3177072bca99867ba4f128d825484883b57efbe
Erez Louidor <erez@google.com>
2019-05-30 13:08:01 -0700

    Addressed reviewer's comments.

--- tensorboard/plugins/hparams/tf_hparams_parallel_coords_plot/axes.ts
+++ tensorboard/plugins/hparams/tf_hparams_parallel_coords_plot/axes.ts
@@ -1,19 +1,17 @@
-/*
-@license
-Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
+Licensed under the Apache License, Version 2.0 (the 'License');
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
+distributed under the License is distributed on an 'AS IS' BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+==============================================================================*/
 
 /* Defines classes that manage the axes in the parallel coordinates plot. */
 
@@ -507,16 +505,11 @@ export class AxesCollection {
    * the order of axes indexed by visibleColIndices that are already visible.
    */
   private _updateStationaryAxesPositions(visibleColIndices: Set<number>) {
-    // We're going to modify visibleColIndices so make a copy first, since
-    // the caller may count on it being unmodified.
-    visibleColIndices = new Set<number>(visibleColIndices);    
-    let newDomain: number[] = this._stationaryAxesPositions.domain().filter(
+    const visibleDomain = this._stationaryAxesPositions.domain().filter(
       colIndex => visibleColIndices.has(colIndex));
-    newDomain.forEach(colIndex => visibleColIndices.delete(colIndex));
-    this._stationaryAxesPositions.domain(
-      /* TypeScript doesn't allow spreading a Set, so we convert to an 
-         Array first. */
-      newDomain.concat(Array.from(visibleColIndices)));
+    const newDomain = Array.from(new Set([
+      ...visibleDomain, ...Array.from(visibleColIndices)]));
+    this._stationaryAxesPositions.domain(newDomain);
   }
   
   private _updateAxesPositionsInDOM(selectionOrTransition) {
--- tensorboard/plugins/hparams/tf_hparams_parallel_coords_plot/lines.ts
+++ tensorboard/plugins/hparams/tf_hparams_parallel_coords_plot/lines.ts
@@ -1,19 +1,17 @@
-/*
-@license
-Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
+Licensed under the Apache License, Version 2.0 (the 'License');
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
+distributed under the License is distributed on an 'AS IS' BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+==============================================================================*/
 
 /* Defines classes for managing the collection of lines in the parallel
    coordinates plot.
@@ -202,15 +200,14 @@ export class LinesCollection {
         () => {
           const _this = this;
           this._fgPathsSel.each(
-            function(sessionGroup) {
-              // Here 'this' is the <path> element, and '_this' is the
-              // 'LinesCollection' instance.
+            function(this: SVGPathElement, sessionGroup) {
+              // '_this' refers to the 'LinesCollection' instance.
               _this._setControlPointsProperty(this, sessionGroup);
             });
         });
     }
   }
-
+erez
   /**
    * Rerenders the foreground lines so that their visibility matches the 
    * current brush filters.
