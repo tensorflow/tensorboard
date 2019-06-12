48665fe421d726a33744c12562fbd30519fac33b
William Chargin <wchargin@gmail.com>
2019-06-11 17:21:37 -0700

    frontend: reinstate registration for conditional plugins (#2340)
    Summary:
    Plugins whose backends are provided via “loaders” rather than the
    plugins themselves may fail to load. We still want to allow those
    plugins to display a message in the frontend indicating what must be
    done to enable them (e.g., update TensorFlow, start a `tfdbg` debugger),
    which requires registering their frontends unconditionally.
    
    This is a short-term hack to retain functionality prior to #2304 (with
    the minor change that these plugins will now always appear at the end of
    the list). We’ll want to replace this with a better mechanism when time
    permits.
    
    Fixes #2338.
    
    Test Plan:
    Launch TensorBoard without TensorFlow installed and with no flags other
    than `--logdir`. Note that the beholder, debugger, hparams, what-if
    tool, and profile dashboards all have valid entries in the “inactive”
    dropdown, displaying appropriate help messages.
    
    Verify that of the plugins listed in `default.py`, all those provided
    via loaders now have `registerDashboard` statements.
    
    wchargin-branch: register-conditional-plugins

--- tensorboard/plugins/hparams/hparams_plugin.py
+++ tensorboard/plugins/hparams/hparams_plugin.py
@@ -84,6 +84,8 @@ class HParamsPlugin(base_plugin.TBPlugin):
         metadata.PLUGIN_NAME))
 
   def frontend_metadata(self):
+    # TODO(#2338): Keep this in sync with the `registerDashboard` call
+    # on the frontend until that call is removed.
     return super(HParamsPlugin, self).frontend_metadata()._replace(
         element_name='tf-hparams-dashboard',
     )
