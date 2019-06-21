0a2ac6f14a78c1d9fc4b6e2b51d359c32ec2bc75
William Chargin <wchargin@gmail.com>
2019-06-20 18:17:04 -0700

    [update patch]
    wchargin-source: 30e936b83964ab4b78b9c9755fc62849b182c3d2
    wchargin-branch: deterministic-tgz

--- tensorboard/pip_package/deterministic_tar_gz.py
+++ tensorboard/pip_package/deterministic_tar_gz.py
@@ -22,6 +22,13 @@
 #
 """Generate `.tar.gz` archives deterministically.
 
+Some differences from `tar czf ARCHIVE [FILES]...`:
+
+  - Timestamps (modification time, access time) are omitted.
+  - Owners and groups are omitted.
+  - Archive entries are not prefixed with the directory name or path.
+  - Dirnames are stripped.
+
 See <https://reproducible-builds.org/docs/archives/>.
 """
 
@@ -30,15 +37,9 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
-import contextlib
-import functools
 import gzip
-import itertools
-import locale
 import os
-import shutil
 import tarfile
-import tempfile
 
 
 def main():
@@ -46,104 +47,51 @@ def main():
   parser.add_argument(
       "archive",
       metavar="ARCHIVE",
-      help="name for the output file; should probably end in '.tar.gz'",
+      help="name for the output `.tar.gz` archive",
   )
   parser.add_argument(
-      "directory",
-      metavar="DIRECTORY",
-      help="path to directory to be archived",
+      "files",
+      metavar="files",
+      nargs="*",
+      help="files to include in the archive; basenames must be distinct",
   )
   args = parser.parse_args()
-  archive_deterministically(archive=args.archive, directory=args.directory)
+  archive = args.archive
+  files = args.files
+  del args
+
+  if len(frozenset(os.path.basename(f) for f in files)) != len(files):
+    sys.stderr.write("Input basenames must be distinct; got: %r" % files)
+  files.sort(key=os.path.basename)
+
+  # (`fd` will be closed by `fdopen` context manager below)
+  fd = os.open(archive, os.O_WRONLY | os.O_CREAT, 0o644)
+  with \
+      os.fdopen(fd, "wb") as out_file, \
+      gzip.GzipFile("wb", fileobj=out_file, mtime=0) as gzip_file, \
+      tarfile.open(fileobj=gzip_file, mode="w:") as tar_file:
+    for f in files:
+      arcname = os.path.basename(f)
+      tar_file.add(f, filter=cleanse, recursive=False, arcname=arcname)
 
 
-def archive_deterministically(archive, directory):
-  """Create a `.tar.gz` archive in a deterministic, reproducible manner.
+def cleanse(tarinfo):
+  """Cleanse sources of nondeterminism from tar entries.
 
-  Some differences from `tar czf ARCHIVE DIRECTORY`:
-
-    - Timestamps (modification time, access time) are omitted.
-    - Owners and groups are omitted.
-    - Archive entries are not prefixed with the directory name or path.
+  To be passed as the `filter` kwarg to `tarfile.TarFile.add`.
 
   Args:
-    archive: Path to output file to be created.
-    directory: Path to directory to be recursively archived.
+    tarinfo: A `tarfile.TarInfo` object to be mutated.
+
+  Returns:
+    The same `tarinfo` object, but mutated.
   """
-  def reset(tarinfo):
-    """Cleanse sources of nondeterminism from tar entries.
-
-    To be passed as the `filter` kwarg to `tarfile.TarFile.add`.
-
-    Args:
-      tarinfo: A `tarfile.TarInfo` object to be mutated.
-
-    Returns:
-      The same `tarinfo` object, but mutated.
-    """
-    tarinfo.uid = 0
-    tarinfo.gid = 0
-    tarinfo.uname = "root"
-    tarinfo.gname = "root"
-    tarinfo.mtime = 0
-    return tarinfo
-
-  archive = os.path.abspath(archive)
-  with cd(directory):  # otherwise we need to strip path prefixes
-    file_list = []
-    for (root, dirs, files) in os.walk("."):
-      for name in itertools.chain(dirs, files):
-        file_list.append(os.path.join(root, name))
-
-    # Sort file entries with a fixed locale.
-    with setlocale("C"):
-      file_list.sort(key=functools.cmp_to_key(locale.strcoll))
-
-    # Use a temporary file and atomic rename to avoid partially-formed
-    # packaging (in case of exceptional situations like running out of
-    # disk space).
-    with mkdtemp() as temp_directory:
-      temp_archive = os.path.join(temp_directory, ".result.tar.gz")
-      # (`fd` will be closed by `fdopen` context manager below)
-      fd = os.open(temp_archive, os.O_WRONLY | os.O_CREAT, 0o644)
-      with \
-          os.fdopen(fd, "wb") as out_file, \
-          gzip.GzipFile("wb", fileobj=out_file, mtime=0) as gzip_file, \
-          tarfile.open(fileobj=gzip_file, mode="w:") as tar_file:
-        for entry in file_list:
-          arcname = os.path.normpath(entry)
-          tar_file.add(entry, filter=reset, recursive=False, arcname=arcname)
-      shutil.move(temp_archive, archive)
-
-
-@contextlib.contextmanager
-def cd(new_path):
-  """Context manager for changing the current working directory."""
-  previous_path = os.getcwd()
-  try:
-    os.chdir(new_path)
-    yield
-  finally:
-    os.chdir(previous_path)
-
-
-@contextlib.contextmanager
-def setlocale(name):
-  """Context manager for changing the current locale."""
-  saved_locale = locale.setlocale(locale.LC_ALL)
-  try:
-    yield locale.setlocale(locale.LC_ALL, name)
-  finally:
-    locale.setlocale(locale.LC_ALL, saved_locale)
-
-
-@contextlib.contextmanager
-def mkdtemp():
-  tmpdir = tempfile.mkdtemp()
-  try:
-    yield tmpdir
-  finally:
-    shutil.rmtree(tmpdir, ignore_errors=True)
+  tarinfo.uid = 0
+  tarinfo.gid = 0
+  tarinfo.uname = "root"
+  tarinfo.gname = "root"
+  tarinfo.mtime = 0
+  return tarinfo
 
 
 if __name__ == "__main__":
