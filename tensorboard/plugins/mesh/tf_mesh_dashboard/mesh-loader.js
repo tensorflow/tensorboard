58275add6e04df99806f2f3b5e0960e5a5a3b2ce
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-07-15 10:52:20 -0700

    Integrate 5e5badc666480d98d389f6ca46e0b4febc253df8

--- tensorboard/plugins/mesh/tf_mesh_dashboard/mesh-loader.js
+++ tensorboard/plugins/mesh/tf_mesh_dashboard/mesh-loader.js
@@ -109,7 +109,8 @@ Polymer({
 
   observers: [
     'reload(run, tag, active, _dataProvider)',
-    '_updateScene(_currentStep)',
+    '_updateScene(_currentStep.*)',
+    '_debouncedFetchMesh(_currentStep)',
     '_updateView(selectedView)'
   ],
 
@@ -147,13 +148,8 @@ Polymer({
       if (!steps) return;  // Happens when request was cancelled at some point.
       this.set('_steps', steps);
       this.set('_stepIndex', steps.length - 1);
-      this.set('_isMeshLoading', false);
-      if (!this._meshViewerAttached) {
-        // Mesh viewer should be added to the dom once.
-        this.appendChild(this._meshViewer.getRenderer().domElement);
-        this._meshViewerAttached = true;
-      }
-    }).catch((error) => {
+    })
+    .catch((error) => {
       if (!error || !error.code || error.code != vz_mesh.ErrorCodes.CANCELLED) {
         error = error || 'Response processing failed.';
         throw new Error(error);
@@ -163,14 +159,53 @@ Polymer({
 
   /**
    * Updates the scene.
-   * @param {!Object} currentStep Step datum.
    * @private
    */
-  _updateScene: function(currentStep) {
+  _updateScene: function() {
+    const currentStep = this._currentStep;
+    // Mesh data is not fetched yet. Please see `_maybeFetchMesh`.
+    if (!currentStep.mesh) return;
+
     this._meshViewer.updateScene(currentStep, this);
     if (!this._cameraPositionInitialized) {
       this._meshViewer.resetView();
       this._cameraPositionInitialized = true;
+    }
+    if (!this._meshViewerAttached) {
+      // Mesh viewer should be added to the dom once.
+      this.appendChild(this._meshViewer.getRenderer().domElement);
+      this._meshViewerAttached = true;
+    }
+  },
+
+  _debouncedFetchMesh() {
+    this.debounce('fetchMesh', () => this._maybeFetchMesh(), 100);
+  },
+
+  async _maybeFetchMesh() {
+    const currentStep = this._currentStep;
+    if (currentStep.mesh || currentStep.meshFetching) return;
+    currentStep.meshFetching = true;
+    this._isMeshLoading = true;
+
+    try {
+      const meshData = await this._dataProvider.fetchData(
+        currentStep,
+        this.run,
+        this.tag,
+        this.sample,
+        this._stepIndex,
+      );
+      currentStep.mesh = meshData[0];
+      this.notifyPath('_currentStep.mesh');
+    } catch (error) {
+      if (!error || !error.code || error.code != vz_mesh.ErrorCodes.CANCELLED) {
+        error = error || 'Response processing failed.';
+        throw new Error(error);
+      }
+    } finally {
+      this._isMeshLoading = false;
+      currentStep.meshFetching = false;
     }
   },
 
