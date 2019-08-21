40a028fff54e668ea4155d35a8bc6ac7e95922d8
Stephan Lee <stephanwlee@gmail.com>
2019-08-21 16:49:30 -0700

    licensing is important

--- tensorboard/components/tf_ng_tensorboard/testing/rxjs_shims.js
+++ tensorboard/components/tf_ng_tensorboard/testing/rxjs_shims.js
@@ -1,3 +1,22 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+/**
+ * This is a fork from https://github.com/angular/angular-bazel-example/blob/master/src/rxjs_shims.js that
+ * is originally licensed as below.
+ * This fork reformat the code using prettier.
+ */
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
