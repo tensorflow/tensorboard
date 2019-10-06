b0fc7ba7508570a5aee52a94f90b18897f04592e
William Chargin <wchargin@gmail.com>
2019-10-05 21:24:15 -0700

    [update patch]
    wchargin-branch: eid-middleware
    wchargin-source: e956be9e14a712e424594550d58ef337710a0a2b

--- tensorboard/backend/experiment_id_test.py
+++ tensorboard/backend/experiment_id_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for `tensorboard.experiment_id`."""
+"""Tests for `tensorboard.backend.experiment_id`."""
 
 from __future__ import absolute_import
 from __future__ import division
