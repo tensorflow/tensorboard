548e512eb2fe274c1b28a8ccc62f4d8f301033db
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-10-17 11:06:44 -0700

    Integrate 7e1876ffbbd6289e98052b68054c923903a88cbf

--- tensorboard/plugins/interactive_inference/witwidget/notebook/base.py
+++ tensorboard/plugins/interactive_inference/witwidget/notebook/base.py
@@ -360,7 +360,8 @@ class WitWidgetBase(object):
       examples, self.config.get('inference_address'),
       self.config.get('model_name'), self.config.get('model_signature'),
       self.config.get('force_json_input'), self.adjust_example_fn,
-      self.adjust_prediction_fn, self.adjust_attribution_fn)
+      self.adjust_prediction_fn, self.adjust_attribution_fn,
+      self.config.get('aip_service_name'), self.config.get('aip_service_version'))
 
   def _predict_aip_compare_model(self, examples):
     return self._predict_aip_impl(
@@ -369,16 +370,20 @@ class WitWidgetBase(object):
       self.config.get('compare_force_json_input'),
       self.compare_adjust_example_fn,
       self.compare_adjust_prediction_fn,
-      self.compare_adjust_attribution_fn)
+      self.compare_adjust_attribution_fn,
+      self.config.get('compare_aip_service_name'),
+      self.config.get('compare_aip_service_version'))
 
   def _predict_aip_impl(self, examples, project, model, version, force_json,
-                        adjust_example, adjust_prediction, adjust_attribution):
+                        adjust_example, adjust_prediction, adjust_attribution,
+                        service_name, service_version):
     """Custom prediction function for running inference through AI Platform."""
 
     # Set up environment for GCP call for specified project.
     os.environ['GOOGLE_CLOUD_PROJECT'] = project
 
-    service = googleapiclient.discovery.build('ml', 'v1', cache_discovery=False)
+    service = googleapiclient.discovery.build(
+      service_name, service_version, cache_discovery=False)
     name = 'projects/{}/models/{}'.format(project, model)
     if version is not None:
       name += '/versions/{}'.format(version)
