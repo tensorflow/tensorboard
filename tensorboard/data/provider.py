6a632c7c0543eda4a1dc97dbbca7716ec962ea5f
David Soergel <soergel@google.com>
2019-10-18 16:37:54 -0400

    Add docstring text re. url and blob key security

--- tensorboard/data/provider.py
+++ tensorboard/data/provider.py
@@ -474,14 +474,22 @@ class BlobReference(object):
 
   Attributes:
     url: A string containing a URL from which the blob data may be fetched
-      directly, bypassing the data provider (optional).
+      directly, bypassing the data provider (optional).  URLs may be a vector
+      for data leaks (e.g. via browser history, web proxies, etc.), so these
+      URLs should not expose secret information.
     blob_key: A string containing an key uniquely identifying a blob, which
-      may be dereferenced via `provider.read_blob(blob_key)`.  These keys must
-      be constructed such that they can be included directly in a URL, with no
-      further encoding.  Concretely, this means that they consist exclusively
-      of "unreserved characters" per RFC 3986, namely [a-zA-Z0-9._~-].
-      These keys are case-sensitive; it may be wise for implementations to
-      normalize case to reduce confusion.  The empty string is not a valid key. 
+      may be dereferenced via `provider.read_blob(blob_key)`.
+      
+      These keys must be constructed such that they can be included directly in
+      a URL, with no further encoding.  Concretely, this means that they consist
+      exclusively of "unreserved characters" per RFC 3986, namely 
+      [a-zA-Z0-9._~-].  These keys are case-sensitive; it may be wise for
+      implementations to normalize case to reduce confusion.  The empty string
+      is not a valid key.
+
+      As with URLs above, blob keys must not contain information that should be
+      kept secret.  Privacy-sensitive applications should use random keys
+      (e.g. UUIDs), or encrypt keys containing secret fields. 
   """
   __slots__ = ("_url", "_blob_key")
 
@@ -502,6 +510,13 @@ class BlobReference(object):
 
   @property
   def blob_key(self):
+    """Provide a key uniquely identifying a blob.
+
+    Callers should consider these keys to be opaque-- i.e., to have no intrinsic
+    meaning.  Some data providers may use random IDs; but others may encode
+    information into the key, in which case callers must make no attempt to
+    decode it.
+    """
     return self._blob_key
 
   def __eq__(self, other):
