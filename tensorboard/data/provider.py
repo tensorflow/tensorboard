70a430782d8138bac83df8c41362df73b3154e2c
David Soergel <soergel@google.com>
2019-10-21 16:40:16 -0400

    One space after a period, for consistency, even though I hate it ;)

--- tensorboard/data/provider.py
+++ tensorboard/data/provider.py
@@ -138,7 +138,7 @@ class DataProvider(object):
       plugin_name: String name of the TensorBoard plugin that created the data
         to be queried. Required.
       run_tag_filter: Optional `RunTagFilter` value. If omitted, all runs and
-        tags will be included.  The result will only contain keys for run-tag
+        tags will be included. The result will only contain keys for run-tag
         combinations that actually exist, which may not include all entries in
         the `run_tag_filter`.
 
@@ -164,7 +164,7 @@ class DataProvider(object):
         (e.g., `1000`). Required.
       run_tag_filter: Optional `RunTagFilter` value. If provided, a time series
         will only be included in the result if its run and tag both pass this
-        filter. If `None`, all time series will be included.  The result will
+        filter. If `None`, all time series will be included. The result will
         only contain keys for run-tag combinations that actually exist, which
         may not include all entries in the `run_tag_filter`.
 
@@ -470,25 +470,25 @@ class BlobSequenceTimeSeries(_TimeSeries):
 
 
 class BlobReference(object):
-  """A reference to a blob.  
+  """A reference to a blob.
 
   Attributes:
     url: A string containing a URL from which the blob data may be fetched
-      directly, bypassing the data provider (optional).  URLs may be a vector
+      directly, bypassing the data provider (optional). URLs may be a vector
       for data leaks (e.g. via browser history, web proxies, etc.), so these
       URLs should not expose secret information.
     blob_key: A string containing an key uniquely identifying a blob, which
       may be dereferenced via `provider.read_blob(blob_key)`.
       
       These keys must be constructed such that they can be included directly in
-      a URL, with no further encoding.  Concretely, this means that they consist
+      a URL, with no further encoding. Concretely, this means that they consist
       exclusively of "unreserved characters" per RFC 3986, namely 
-      [a-zA-Z0-9._~-].  These keys are case-sensitive; it may be wise for
-      implementations to normalize case to reduce confusion.  The empty string
+      [a-zA-Z0-9._~-]. These keys are case-sensitive; it may be wise for
+      implementations to normalize case to reduce confusion. The empty string
       is not a valid key.
 
       As with URLs above, blob keys must not contain information that should be
-      kept secret.  Privacy-sensitive applications should use random keys
+      kept secret. Privacy-sensitive applications should use random keys
       (e.g. UUIDs), or encrypt keys containing secret fields. 
   """
 
@@ -503,7 +503,7 @@ class BlobReference(object):
     """Provide the direct-access URL for this blob, if available.
     
     Note that this method is *not* expected to construct a URL to the
-    data-loading endpoint provided by TensorBoard.  If this method returns
+    data-loading endpoint provided by TensorBoard. If this method returns
     None, then the caller should proceed to use `blob_key()` to build the URL,
     as needed.
     """
@@ -514,7 +514,7 @@ class BlobReference(object):
     """Provide a key uniquely identifying a blob.
 
     Callers should consider these keys to be opaque-- i.e., to have no intrinsic
-    meaning.  Some data providers may use random IDs; but others may encode
+    meaning. Some data providers may use random IDs; but others may encode
     information into the key, in which case callers must make no attempt to
     decode it.
     """
