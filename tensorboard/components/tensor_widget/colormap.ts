99e315f75df71adf299f66adabc61645e671a9d0
Shanqing Cai <cais@google.com>
2019-10-22 09:05:35 -0400

    [tensor-widget] Add colormap selection and Ctrl/Alt/Shift+wheel zooming (#2764)
    * Motivation for features / changes
      * Continue developing TensorWidget
    
    * Technical description of changes
      * Support Ctrl/Alt/Shift + mouse wheel zooming
      * Add an optional field to TensorWidgetOptions: wheelZoomKey to allow specifying whether alt, ctrl or shift key activates wheel zooming.
      * Add jet colormap
      * Add colormap selection menu
      * Added a color bar in the value tooltip that indicates where the currently pointed-to value is within the color scale.
    
    * Screenshots of UI changes
      * ![image](https://user-images.githubusercontent.com/16824702/66793119-dbc0a100-eeb0-11e9-950d-a72c2599062c.png)
      * ![image](https://user-images.githubusercontent.com/16824702/66793166-04e13180-eeb1-11e9-83d3-123802de1247.png)
      * ![image](https://user-images.githubusercontent.com/16824702/66793021-5b01a500-eeb0-11e9-9abd-4c90401106b5.png)
      * ![image](https://user-images.githubusercontent.com/16824702/66793103-b338a700-eeb0-11e9-86a6-f8d351d88acf.png)

--- tensorboard/components/tensor_widget/colormap.ts
+++ tensorboard/components/tensor_widget/colormap.ts
@@ -17,6 +17,24 @@ limitations under the License.
 
 const MAX_RGB = 255;
 
+/** Configuration for a colormap. */
+export interface ColorMapConfig {
+  /**
+   * Minimum value that the color map can map to without clipping.
+   * Must be a finite value.
+   */
+  min: number;
+
+  /**
+   * Minimum value that the color map can map to without clipping.
+   * Must be a finite value and be `>= min`.
+   *
+   * In the case of `max === min`, all finite values mapped to the
+   * midpoint of the color scale.
+   */
+  max: number;
+}
+
 /**
  * Abstract base class for colormap.
  *
@@ -28,15 +46,15 @@ export abstract class ColorMap {
    * @param min Minimum. Must be a finite value.
    * @param max Maximum. Must be finite and >= `min`.
    */
-  constructor(protected readonly min: number, protected readonly max: number) {
-    if (!isFinite(min)) {
-      throw new Error(`min value (${min}) is not finite`);
+  constructor(protected config: ColorMapConfig) {
+    if (!isFinite(config.min)) {
+      throw new Error(`min value (${config.min}) is not finite`);
     }
-    if (!isFinite(max)) {
-      throw new Error(`max value (${max}) is not finite`);
+    if (!isFinite(config.max)) {
+      throw new Error(`max value (${config.max}) is not finite`);
     }
-    if (max < min) {
-      throw new Error(`max (${max}) is < min (${min})`);
+    if (config.max < config.min) {
+      throw new Error(`max (${config.max}) is < min (${config.min})`);
     }
   }
 
@@ -47,6 +65,67 @@ export abstract class ColorMap {
    *   The range of RGB values is [0, 255].
    */
   abstract getRGB(value: number): [number, number, number];
+
+  /**
+   * Render the colormap as a horizontal scale, while highlighting a specifc
+   * value.
+   *
+   * The value is highlighted if and only if it is `>= this.config.min` and
+   * `<= this.config.max`.
+   *
+   * @param canvas The canvas on which
+   * @param value The number to highlight (optional).
+   */
+  render(canvas: HTMLCanvasElement, value?: number) {
+    if (this.config.min === this.config.max) {
+      return;
+    }
+    const context = canvas.getContext('2d');
+    if (context == null) {
+      return;
+    }
+    const steps = 100;
+    const cellWidth = canvas.width / steps;
+    const height = canvas.height;
+    const verticalMargin = 0.2;
+    const barHeight = height * (1 - 2 * verticalMargin);
+    for (let i = 0; i < steps; ++i) {
+      const value =
+        (this.config.max - this.config.min) * (i / steps) + this.config.min;
+      const x = cellWidth * i;
+      const y = height * verticalMargin;
+      const [r, g, b] = this.getRGB(value);
+      context.beginPath();
+      context.fillStyle = `rgba(${r}, ${g}, ${b}, 1)`;
+      context.fillRect(x, y, cellWidth, barHeight);
+      context.stroke();
+    }
+
+    if (value != null) {
+      const tickWidth = 8;
+      if (value >= this.config.min && value <= this.config.max) {
+        // Highlight the relative position of `value` along the color scale.
+        const tickX =
+          ((value - this.config.min) / (this.config.max - this.config.min)) *
+          canvas.width;
+
+        // Draw the triangle on the top.
+        context.beginPath();
+        context.fillStyle = 'rgba(0, 0, 0, 1)';
+        context.moveTo(tickX, verticalMargin * height);
+        context.lineTo(tickX - tickWidth / 2, 0);
+        context.lineTo(tickX + tickWidth / 2, 0);
+        context.fill();
+
+        // Draw the triangle on the bottom.
+        context.beginPath();
+        context.moveTo(tickX, (1 - verticalMargin) * height);
+        context.lineTo(tickX - tickWidth / 2, height);
+        context.lineTo(tickX + tickWidth / 2, height);
+        context.fill();
+      }
+    }
+  }
 }
 
 /**
@@ -68,13 +147,52 @@ export class GrayscaleColorMap extends ColorMap {
         return [MAX_RGB, MAX_RGB / 2, 0];
       }
     }
-    let relativeValue =
-      this.min === this.max ? 0.5 : (value - this.min) / (this.max - this.min);
-    relativeValue = Math.max(Math.min(relativeValue, 1), 0);
-    return [
-      MAX_RGB * relativeValue,
-      MAX_RGB * relativeValue,
-      MAX_RGB * relativeValue,
-    ];
+    let relValue =
+      this.config.min === this.config.max
+        ? 0.5
+        : (value - this.config.min) / (this.config.max - this.config.min);
+    relValue = Math.max(Math.min(relValue, 1), 0);
+    return [MAX_RGB * relValue, MAX_RGB * relValue, MAX_RGB * relValue];
+  }
+}
+
+export class JetColorMap extends ColorMap {
+  getRGB(value: number): [number, number, number] {
+    if (isNaN(value)) {
+      // NaN.
+      return [MAX_RGB * 0.25, MAX_RGB * 0.25, MAX_RGB * 0.25];
+    } else if (!isFinite(value)) {
+      if (value < 0) {
+        // -Infinity.
+        return [MAX_RGB * 0.5, MAX_RGB * 0.5, MAX_RGB * 0.5];
+      } else {
+        // +Infinity.
+        return [MAX_RGB * 0.75, MAX_RGB * 0.75, MAX_RGB * 0.75];
+      }
+    }
+
+    let relR = 0;
+    let relG = 0;
+    let relB = 0;
+    const lim0 = 0.35;
+    const lim1 = 0.65;
+
+    let relValue =
+      this.config.min === this.config.max
+        ? 0.5
+        : (value - this.config.min) / (this.config.max - this.config.min);
+    relValue = Math.max(Math.min(relValue, 1), 0);
+    if (relValue <= lim0) {
+      relG = relValue / lim0;
+      relB = 1;
+    } else if (relValue > lim0 && relValue <= lim1) {
+      relR = (relValue - lim0) / (lim1 - lim0);
+      relG = 1;
+      relB = (lim1 - relValue) / (lim1 - lim0);
+    } else if (relValue > lim1) {
+      relR = 1;
+      relG = (1 - relValue) / (1 - lim1);
+    }
+    return [relR * MAX_RGB, relG * MAX_RGB, relB * MAX_RGB];
   }
 }
