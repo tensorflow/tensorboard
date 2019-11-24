958bfb940af8f8c94b4f11800c5956ebe4be381f
Tzu-Wei Huang <huang.dexter@gmail.com>
2019-11-24 19:44:01 +0800

    implements gcs for gfile module

--- tensorboard/compat/tensorflow_stub/io/gfile.py
+++ tensorboard/compat/tensorflow_stub/io/gfile.py
@@ -43,6 +43,16 @@ try:
 except ImportError:
     S3_ENABLED = False
 
+try:
+    from google.cloud import storage
+    from google.cloud import exceptions as gc_exceptions
+    from six.moves import http_client
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "cred.json"
+
+    GCS_ENABLED = True
+except ImportError:
+    GCS_ENABLED = False
+
 if sys.version_info < (3, 0):
     # In Python 2 FileExistsError is not defined and the
     # error manifests it as OSError.
@@ -419,6 +429,154 @@ class S3FileSystem(object):
 register_filesystem("", LocalFileSystem())
 if S3_ENABLED:
     register_filesystem("s3", S3FileSystem())
+
+
+class GCSFileSystem(object):
+    """Writes tensorboard protocol buffer files to Google Cloud Storage."""
+
+    def __init__(self):
+        if not GCS_ENABLED:
+            raise ImportError("`google-cloud-storage` must be installed in order to use "
+                              "the 'gs://' protocol")
+
+
+        self.client = storage.Client()
+        def get_blob(filename):
+            bucket_name, filepath = self.bucket_and_path(filename)
+            bucket = storage.Bucket(self.client, bucket_name)
+            return storage.Blob(filepath, bucket, chunk_size=_DEFAULT_BLOCK_SIZE)
+        self.blob = get_blob
+
+    def bucket_and_path(self, url):
+        url = compat.as_str_any(url)
+        if url.startswith("gs://"):
+            url = url[len("gs://"):]
+        bp = url.split("/")
+        bucket = bp[0]
+        path = url[1 + len(bucket):]
+        return bucket, path
+
+    def exists(self, filename):
+        """Determines whether a path exists or not."""
+        bucket, path = self.bucket_and_path(filename)
+        r = self.client.list_blobs(bucket_or_name=bucket, prefix=path)
+        if len(list(r)) != 0:
+            return True
+        return False
+
+    def join(self, path, *paths):
+        """Join paths with a slash."""
+        return "/".join((path,) + paths)
+
+    def read(self, filename, binary_mode=False, size=None, continue_from=None):
+
+        if continue_from is None:
+            continue_from = 0
+
+        if size is not None:
+            end = continue_from + size
+        else:
+            end = None
+
+        try:
+            stream = self.blob(filename).download_as_string(start=continue_from, end=end)
+        except Exception as e:
+            if e.code == http_client.REQUESTED_RANGE_NOT_SATISFIABLE:
+                return "", continue_from
+
+            else:
+                raise            
+
+        continue_from += len(stream)
+        if binary_mode:
+            return (bytes(stream), continue_from)
+        else:
+            return (stream.decode('utf-8'), continue_from)
+
+
+    def write(self, filename, file_content, binary_mode=False):
+        file_content = compat.as_bytes(file_content)
+        self.blob(filename).upload_from_string(file_content)  # this will overwrite!
+
+    def glob(self, filename):
+        """Returns a list of files that match the given pattern(s)."""
+        # Only support prefix with * at the end and no ? in the string
+        star_i = filename.find('*')
+        quest_i = filename.find('?')
+        if quest_i >= 0:
+            raise NotImplementedError(
+                "{} not supported by compat glob".format(filename))
+        if star_i != len(filename) - 1:
+            # Just return empty so we can use glob from directory watcher
+            #
+            # TODO: Remove and instead handle in GetLogdirSubdirectories.
+            # However, we would need to handle it for all non-local registered
+            # filesystems in some way.
+            return []
+        filename = filename[:-1]
+        bucket, path = self.bucket_and_path(filename)
+        result = list(self.client.list_blobs(bucket_or_name=bucket, prefix=path))
+
+        keys = []
+        for r in result:
+            # glob.glob('./*') returns folder as well. 
+            if r.name[-1] != '/':  # in order to pass the unit test
+                keys.append(filename + r.name[len(path):])
+
+        return keys
+
+    def isdir(self, dirname):
+        """Returns whether the path is a directory or not."""
+        bucket, path = self.bucket_and_path(dirname)
+        if path[-1] != '/':
+            path += '/'
+        result = list(self.client.list_blobs(bucket_or_name=bucket, prefix=path, delimiter='/'))
+        return len(result) > 0 
+
+    def listdir(self, dirname):
+        """Returns a list of entries contained within a directory."""
+        bucket, path = self.bucket_and_path(dirname)
+
+        if path[-1] != '/':
+            path += '/'
+        path_depth = len(path.split('/'))-1
+        result = list(self.client.list_blobs(bucket_or_name=bucket, prefix=path))
+        keys = set()
+
+        for r in result:
+            dirs = r.name.split('/')
+            if len(dirs)>path_depth:
+                if dirs[path_depth] != '':
+                    keys.add(dirs[path_depth])
+        return keys
+
+
+
+    def makedirs(self, dirname):
+        """Creates a directory and all parent/intermediate directories."""
+        if self.exists(dirname):
+            raise errors.AlreadyExistsError(None, None, "Directory already exists")
+        if not dirname.endswith("/"):
+            dirname += "/"  # This will make sure we don't override a file
+        self.blob(dirname).upload_from_string('') 
+
+
+    def stat(self, filename):
+        """Returns file statistics for a given path."""
+        # NOTE: Size of the file is given by ContentLength from S3,
+        # but we convert to .length
+        bucket_name, path = self.bucket_and_path(filename)
+        bucket = storage.Bucket(self.client, bucket_name)
+        blob = bucket.get_blob(path)
+        if blob == None:
+            raise errors.NotFoundError(None, None, "Could not find file")
+        
+        # use get_blob to get metadata
+        return StatData(bucket.get_blob(path).size)
+
+
+if GCS_ENABLED:
+    register_filesystem("gs", GCSFileSystem())
 
 
 class GFile(object):
---
+++ tensorboard/summary/writer/event_file_writer_gcs_test.py
@@ -0,0 +1,94 @@
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# """Tests for EventFileWriter and _AsyncWriter"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import unittest
+from tensorboard.summary.writer.event_file_writer import EventFileWriter
+from tensorboard.summary.writer.event_file_writer import _AsyncWriter
+from tensorboard.compat import tf
+from tensorboard.compat.proto import event_pb2
+from tensorboard.compat.proto.summary_pb2 import Summary
+from tensorboard.compat.tensorflow_stub.io import gfile
+from tensorboard.compat.tensorflow_stub.pywrap_tensorflow import PyRecordReader_New
+from tensorboard import test as tb_test
+from google.cloud import storage
+
+# Placeholder values to make sure any local keys are overridden
+# and moto mock is being called
+
+USING_REAL_TF = tf.__version__ != 'stub'
+
+
+def gcs_temp_dir(top_directory='event-test', bucket_name='lanpa-tbx'):
+  """Creates a test GCS bucket and returns directory location.
+  The files in `top_directory` will be cleared after this call.
+  Args:
+    top_directory: The path of the top level GCS directory in which
+      to create the directory structure. Defaults to 'top_dir'.
+    bucket_name: The GCS bucket name.
+
+  Returns GCS URL of the top directory in the form 'gs://bucket/path'
+  """
+  gcs_url = 'gs://{}/{}'.format(bucket_name, top_directory)
+  client = storage.Client()
+  bucket = storage.Bucket(client, bucket_name)
+  bloblist = bucket.list_blobs(prefix=top_directory)
+  for f in bloblist:
+    f.delete()
+  return gcs_url
+
+
+def GCS_join(*args):
+    """Joins an GCS directory path as a replacement for os.path.join."""
+    return '/'.join(args)
+
+
+class EventFileWriterTest(tb_test.TestCase):
+
+  @unittest.skipIf(USING_REAL_TF, 'Test only passes when using stub TF')
+  def test_event_file_writer_roundtrip(self):
+    _TAGNAME = 'dummy'
+    _DUMMY_VALUE = 42
+    logdir = gcs_temp_dir()
+    w = EventFileWriter(logdir)
+    summary = Summary(value=[Summary.Value(tag=_TAGNAME, simple_value=_DUMMY_VALUE)])
+    fakeevent = event_pb2.Event(summary=summary)
+    w.add_event(fakeevent)
+    w.close()
+    event_files = sorted(gfile.glob(GCS_join(logdir, '*')))
+    self.assertEqual(len(event_files), 1)
+    r = PyRecordReader_New(event_files[0])
+    r.GetNext()  # meta data, so skip
+    r.GetNext()
+    self.assertEqual(fakeevent.SerializeToString(), r.record())
+
+  @unittest.skipIf(USING_REAL_TF, 'Test only passes when using stub TF')
+  def test_setting_filename_suffix_works(self):
+    logdir = gcs_temp_dir()
+
+    w = EventFileWriter(logdir, filename_suffix='.event_horizon')
+    w.close()
+    event_files = sorted(gfile.glob(GCS_join(logdir, '*')))
+    self.assertEqual(event_files[0].split('.')[-1], 'event_horizon')
+
+
+if __name__ == '__main__':
+  tb_test.main()
