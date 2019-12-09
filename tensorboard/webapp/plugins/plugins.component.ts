83c32c538af054586f2186f250f815e40e0814dc
Stephan Lee <stephanwlee@gmail.com>
2019-12-09 14:08:27 -0800

    ng-tensorboard: Fix iframe based plugin rendering
    Instead of JavaScript inside an iframe document, for security, we now
    serve the document from the server.
    
    Note that it was non-trivial to render the iframe in the Angular
    template since it does not have virtual DOM and any prop changes would
    trigger iframe's `src` to "change" which caused a flicker on the iframe.

---
+++ tensorboard/webapp/plugins/plugins.component.ts
@@ -0,0 +1,117 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+
+import {UiPluginMetadata} from './plugins.container';
+import {
+  LoadingMechanismType,
+  CustomElementLoadingMechanism,
+} from '../types/api';
+
+@Component({
+  selector: 'plugins-component',
+  templateUrl: './plugins.component.html',
+  styles: [
+    '.plugins { height: 100%; }',
+    'iframe { border: 0; height: 100%; width: 100%; }',
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PluginsComponent implements OnChanges {
+  @ViewChild('pluginContainer', {static: true, read: ElementRef})
+  private readonly pluginsContainer!: ElementRef<HTMLDivElement>;
+
+  @Input()
+  activePlugin?: UiPluginMetadata;
+
+  @Input()
+  lastUpdated?: number;
+
+  readonly LoadingMechanismType = LoadingMechanismType;
+
+  private readonly pluginInstances = new Map<string, HTMLElement>();
+
+  ngOnChanges(change: SimpleChanges): void {
+    if (change.activePlugin && this.activePlugin) {
+      this.renderPlugin(this.activePlugin!);
+    }
+    if (change.lastUpdated) {
+      this.reload();
+    }
+  }
+
+  private renderPlugin(plugin: UiPluginMetadata) {
+    for (const element of this.pluginInstances.values()) {
+      element.style.display = 'none';
+    }
+
+    if (this.pluginInstances.has(plugin.id)) {
+      const instance = this.pluginInstances.get(plugin.id) as HTMLElement;
+      instance.style.display = null;
+      return;
+    }
+
+    this.appendPlugin(plugin);
+  }
+
+  private appendPlugin(plugin: UiPluginMetadata) {
+    const pluginId = plugin.id;
+
+    let pluginElement = null;
+    switch (plugin.loading_mechanism.type) {
+      case LoadingMechanismType.CUSTOM_ELEMENT: {
+        const customElementPlugin = plugin.loading_mechanism as CustomElementLoadingMechanism;
+        pluginElement = document.createElement(
+          customElementPlugin.element_name
+        );
+        this.pluginsContainer.nativeElement.appendChild(pluginElement);
+        break;
+      }
+      case LoadingMechanismType.IFRAME: {
+        pluginElement = document.createElement('iframe');
+        pluginElement.id = plugin.id;
+        // Ideally should use the DOMSanitizer but it is not usable in TypeScript.
+        pluginElement.src = `data/plugin_entry.html?name=${plugin.id}`;
+        this.pluginsContainer.nativeElement.appendChild(pluginElement);
+        break;
+      }
+      case LoadingMechanismType.NONE:
+        return;
+      default:
+        console.error('Unexpected plugin');
+    }
+    if (pluginElement) {
+      pluginElement.id = pluginId;
+      this.pluginInstances.set(pluginId, pluginElement);
+    }
+  }
+
+  private reload() {
+    for (const instance of this.pluginInstances.values()) {
+      const maybePolymerDashboard = instance as any;
+      if (maybePolymerDashboard.reload) {
+        maybePolymerDashboard.reload();
+      }
+    }
+  }
+}
