937604d82b549387000c9dd949795b1f1b084169
William Chargin <wchargin@gmail.com>
2019-12-11 16:39:35 -0800

    pylint: remove `g-import-not-at-top` suppressions (#3022)
    Summary:
    This is a Google-specific lint that we kind of just cargo cult around
    and don’t actually lint against in CI, which makes it less than useful.
    
    Generated with:
    
    ```
    git ls-files -z '*.py' |
        xargs -0 sed -i \
            -e 's/g-import-not-at-top,//' \
            -e 's/,g-import-not-at-top//' \
            -e 's/ *# pylint:.*=g-import-not-at-top$//' \
            ;
    ```
    
    Note: This cherry-pick only includes files under tensorboard/uploader
    28 files from the original commit were omitted.
    
    Test Plan:
    Our standard lint still passes:
    
    ```
    $ flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
    0
    ```
    
    wchargin-branch: unsuppress-nontop-imports

--- tensorboard/uploader/util_test.py
+++ tensorboard/uploader/util_test.py
@@ -26,9 +26,9 @@ import mock
 
 try:
   # python version >= 3.3
-  from unittest import mock  # pylint: disable=g-import-not-at-top
+  from unittest import mock
 except ImportError:
-  import mock  # pylint: disable=g-import-not-at-top,unused-import
+  import mock  # pylint: disable=unused-import
 
 
 from google.protobuf import timestamp_pb2
