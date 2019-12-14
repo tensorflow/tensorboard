15b33e450a93536bf42a330db471f74f0fb7ce40
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-12-14 13:07:39 -0800

    Integrate ba4e12754d4be543d5b6aa16568081a7155f1e5f

--- tensorboard/backend/empty_path_redirect.py
+++ tensorboard/backend/empty_path_redirect.py
@@ -31,20 +31,20 @@ from __future__ import print_function
 
 
 class EmptyPathRedirectMiddleware(object):
-  """WSGI middleware to redirect from "" to "/"."""
+    """WSGI middleware to redirect from "" to "/"."""
 
-  def __init__(self, application):
-    """Initializes this middleware.
+    def __init__(self, application):
+        """Initializes this middleware.
 
-    Args:
-      application: The WSGI application to wrap (see PEP 3333).
-    """
-    self._application = application
+        Args:
+          application: The WSGI application to wrap (see PEP 3333).
+        """
+        self._application = application
 
-  def __call__(self, environ, start_response):
-    path = environ.get("PATH_INFO", "")
-    if path:
-      return self._application(environ, start_response)
-    location = environ.get("SCRIPT_NAME", "") + "/"
-    start_response("301 Moved Permanently", [("Location", location)])
-    return []
+    def __call__(self, environ, start_response):
+        path = environ.get("PATH_INFO", "")
+        if path:
+            return self._application(environ, start_response)
+        location = environ.get("SCRIPT_NAME", "") + "/"
+        start_response("301 Moved Permanently", [("Location", location)])
+        return []
--- tensorboard/backend/path_prefix_test.py
+++ tensorboard/backend/path_prefix_test.py
@@ -29,85 +29,85 @@ from tensorboard.backend import path_prefix
 
 
 class PathPrefixMiddlewareTest(tb_test.TestCase):
-  """Tests for `PathPrefixMiddleware`."""
+    """Tests for `PathPrefixMiddleware`."""
 
-  def _echo_app(self, environ, start_response):
-    # https://www.python.org/dev/peps/pep-0333/#environ-variables
-    data = {
-        "path": environ.get("PATH_INFO", ""),
-        "script": environ.get("SCRIPT_NAME", ""),
-    }
-    body = json.dumps(data, sort_keys=True)
-    start_response("200 OK", [("Content-Type", "application/json")])
-    return [body]
+    def _echo_app(self, environ, start_response):
+        # https://www.python.org/dev/peps/pep-0333/#environ-variables
+        data = {
+            "path": environ.get("PATH_INFO", ""),
+            "script": environ.get("SCRIPT_NAME", ""),
+        }
+        body = json.dumps(data, sort_keys=True)
+        start_response("200 OK", [("Content-Type", "application/json")])
+        return [body]
 
-  def _assert_ok(self, response, path, script):
-    self.assertEqual(response.status_code, 200)
-    actual = json.loads(response.get_data())
-    expected = dict(path=path, script=script)
-    self.assertEqual(actual, expected)
+    def _assert_ok(self, response, path, script):
+        self.assertEqual(response.status_code, 200)
+        actual = json.loads(response.get_data())
+        expected = dict(path=path, script=script)
+        self.assertEqual(actual, expected)
 
-  def test_bad_path_prefix_without_leading_slash(self):
-    with self.assertRaises(ValueError) as cm:
-      path_prefix.PathPrefixMiddleware(self._echo_app, "hmm")
-    msg = str(cm.exception)
-    self.assertIn("must start with slash", msg)
-    self.assertIn(repr("hmm"), msg)
+    def test_bad_path_prefix_without_leading_slash(self):
+        with self.assertRaises(ValueError) as cm:
+            path_prefix.PathPrefixMiddleware(self._echo_app, "hmm")
+        msg = str(cm.exception)
+        self.assertIn("must start with slash", msg)
+        self.assertIn(repr("hmm"), msg)
 
-  def test_bad_path_prefix_with_trailing_slash(self):
-    with self.assertRaises(ValueError) as cm:
-      path_prefix.PathPrefixMiddleware(self._echo_app, "/hmm/")
-    msg = str(cm.exception)
-    self.assertIn("must not end with slash", msg)
-    self.assertIn(repr("/hmm/"), msg)
+    def test_bad_path_prefix_with_trailing_slash(self):
+        with self.assertRaises(ValueError) as cm:
+            path_prefix.PathPrefixMiddleware(self._echo_app, "/hmm/")
+        msg = str(cm.exception)
+        self.assertIn("must not end with slash", msg)
+        self.assertIn(repr("/hmm/"), msg)
 
-  def test_empty_path_prefix(self):
-    app = path_prefix.PathPrefixMiddleware(self._echo_app, "")
-    server = werkzeug_test.Client(app, werkzeug.BaseResponse)
+    def test_empty_path_prefix(self):
+        app = path_prefix.PathPrefixMiddleware(self._echo_app, "")
+        server = werkzeug_test.Client(app, werkzeug.BaseResponse)
 
-    with self.subTest("at empty"):
-      self._assert_ok(server.get(""), path="", script="")
+        with self.subTest("at empty"):
+            self._assert_ok(server.get(""), path="", script="")
 
-    with self.subTest("at root"):
-      self._assert_ok(server.get("/"), path="/", script="")
+        with self.subTest("at root"):
+            self._assert_ok(server.get("/"), path="/", script="")
 
-    with self.subTest("at subpath"):
-      response = server.get("/foo/bar")
-      self._assert_ok(server.get("/foo/bar"), path="/foo/bar", script="")
+        with self.subTest("at subpath"):
+            response = server.get("/foo/bar")
+            self._assert_ok(server.get("/foo/bar"), path="/foo/bar", script="")
 
-  def test_nonempty_path_prefix(self):
-    app = path_prefix.PathPrefixMiddleware(self._echo_app, "/pfx")
-    server = werkzeug_test.Client(app, werkzeug.BaseResponse)
+    def test_nonempty_path_prefix(self):
+        app = path_prefix.PathPrefixMiddleware(self._echo_app, "/pfx")
+        server = werkzeug_test.Client(app, werkzeug.BaseResponse)
 
-    with self.subTest("at root"):
-      response = server.get("/pfx")
-      self._assert_ok(response, path="", script="/pfx")
+        with self.subTest("at root"):
+            response = server.get("/pfx")
+            self._assert_ok(response, path="", script="/pfx")
 
-    with self.subTest("at root with slash"):
-      response = server.get("/pfx/")
-      self._assert_ok(response, path="/", script="/pfx")
+        with self.subTest("at root with slash"):
+            response = server.get("/pfx/")
+            self._assert_ok(response, path="/", script="/pfx")
 
-    with self.subTest("at subpath"):
-      response = server.get("/pfx/foo/bar")
-      self._assert_ok(response, path="/foo/bar", script="/pfx")
+        with self.subTest("at subpath"):
+            response = server.get("/pfx/foo/bar")
+            self._assert_ok(response, path="/foo/bar", script="/pfx")
 
-    with self.subTest("at non-path-component extension"):
-      with self.assertRaises(errors.NotFoundError):
-        server.get("/pfxz")
+        with self.subTest("at non-path-component extension"):
+            with self.assertRaises(errors.NotFoundError):
+                server.get("/pfxz")
 
-    with self.subTest("above path prefix"):
-      with self.assertRaises(errors.NotFoundError):
-        server.get("/hmm")
+        with self.subTest("above path prefix"):
+            with self.assertRaises(errors.NotFoundError):
+                server.get("/hmm")
 
-  def test_composition(self):
-    app = self._echo_app
-    app = path_prefix.PathPrefixMiddleware(app, "/bar")
-    app = path_prefix.PathPrefixMiddleware(app, "/foo")
-    server = werkzeug_test.Client(app, werkzeug.BaseResponse)
+    def test_composition(self):
+        app = self._echo_app
+        app = path_prefix.PathPrefixMiddleware(app, "/bar")
+        app = path_prefix.PathPrefixMiddleware(app, "/foo")
+        server = werkzeug_test.Client(app, werkzeug.BaseResponse)
 
-    response = server.get("/foo/bar/baz/quux")
-    self._assert_ok(response, path="/baz/quux", script="/foo/bar")
+        response = server.get("/foo/bar/baz/quux")
+        self._assert_ok(response, path="/baz/quux", script="/foo/bar")
 
 
 if __name__ == "__main__":
-  tb_test.main()
+    tb_test.main()
