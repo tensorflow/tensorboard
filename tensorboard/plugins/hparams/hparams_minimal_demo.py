7cbd738186757aff4986c49b378f3e1ea301b8f4
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-12-14 11:45:22 -0800

    black: reformat directory tensorboard/plugins/hparams

--- tensorboard/plugins/hparams/hparams_minimal_demo.py
+++ tensorboard/plugins/hparams/hparams_minimal_demo.py
@@ -55,23 +55,27 @@ from tensorboard.plugins.hparams import summary
 FLAGS = flags.FLAGS
 
 
-flags.DEFINE_integer('num_session_groups', 50,
-                     'The approximate number of session groups to create.')
-flags.DEFINE_string('logdir', '/tmp/hparams_minimal_demo',
-                    'The directory to write the summary information to.')
-flags.DEFINE_integer('summary_freq', 1,
-                     'Summaries will be every n steps, '
-                     'where n is the value of this flag.')
-flags.DEFINE_integer('num_steps', 100,
-                     'Number of steps per trial.')
+flags.DEFINE_integer(
+    "num_session_groups",
+    50,
+    "The approximate number of session groups to create.",
+)
+flags.DEFINE_string(
+    "logdir",
+    "/tmp/hparams_minimal_demo",
+    "The directory to write the summary information to.",
+)
+flags.DEFINE_integer(
+    "summary_freq",
+    1,
+    "Summaries will be every n steps, " "where n is the value of this flag.",
+)
+flags.DEFINE_integer("num_steps", 100, "Number of steps per trial.")
 
 
 # Total number of sessions is given by:
 # len(TEMPERATURE_LIST)^2 * len(HEAT_COEFFICIENTS) * 2
-HEAT_COEFFICIENTS = {
-    'water': 0.001,
-    'air': 0.003
-}
+HEAT_COEFFICIENTS = {"water": 0.001, "air": 0.003}
 TEMPERATURE_LIST = []
 
 
@@ -79,191 +83,223 @@ TEMPERATURE_LIST = []
 # depends on a flag and flag parsing hasn't happened yet. Instead, we use
 # a function that we call in main() below.
 def init_temperature_list():
-  global TEMPERATURE_LIST
-  TEMPERATURE_LIST = [
-      270+i*50.0
-      for i in xrange(
-          0, int(math.sqrt(FLAGS.num_session_groups/len(HEAT_COEFFICIENTS))))]
+    global TEMPERATURE_LIST
+    TEMPERATURE_LIST = [
+        270 + i * 50.0
+        for i in xrange(
+            0, int(math.sqrt(FLAGS.num_session_groups / len(HEAT_COEFFICIENTS)))
+        )
+    ]
 
 
 def fingerprint(string):
-  m = hashlib.md5()
-  m.update(string.encode('utf-8'))
-  return m.hexdigest()
+    m = hashlib.md5()
+    m.update(string.encode("utf-8"))
+    return m.hexdigest()
 
 
 def create_experiment_summary():
-  """Returns a summary proto buffer holding this experiment."""
+    """Returns a summary proto buffer holding this experiment."""
 
-  # Convert TEMPERATURE_LIST to google.protobuf.ListValue
-  temperature_list = struct_pb2.ListValue()
-  temperature_list.extend(TEMPERATURE_LIST)
-  materials = struct_pb2.ListValue()
-  materials.extend(HEAT_COEFFICIENTS.keys())
-  return summary.experiment_pb(
-      hparam_infos=[
-          api_pb2.HParamInfo(name='initial_temperature',
-                             display_name='Initial temperature',
-                             type=api_pb2.DATA_TYPE_FLOAT64,
-                             domain_discrete=temperature_list),
-          api_pb2.HParamInfo(name='ambient_temperature',
-                             display_name='Ambient temperature',
-                             type=api_pb2.DATA_TYPE_FLOAT64,
-                             domain_discrete=temperature_list),
-          api_pb2.HParamInfo(name='material',
-                             display_name='Material',
-                             type=api_pb2.DATA_TYPE_STRING,
-                             domain_discrete=materials)
-      ],
-      metric_infos=[
-          api_pb2.MetricInfo(
-              name=api_pb2.MetricName(
-                  tag='temperature/current/scalar_summary'),
-              display_name='Current Temp.'),
-          api_pb2.MetricInfo(
-              name=api_pb2.MetricName(
-                  tag='temperature/difference_to_ambient/scalar_summary'),
-              display_name='Difference To Ambient Temp.'),
-          api_pb2.MetricInfo(
-              name=api_pb2.MetricName(
-                  tag='delta/scalar_summary'),
-              display_name='Delta T')
-      ]
-  )
+    # Convert TEMPERATURE_LIST to google.protobuf.ListValue
+    temperature_list = struct_pb2.ListValue()
+    temperature_list.extend(TEMPERATURE_LIST)
+    materials = struct_pb2.ListValue()
+    materials.extend(HEAT_COEFFICIENTS.keys())
+    return summary.experiment_pb(
+        hparam_infos=[
+            api_pb2.HParamInfo(
+                name="initial_temperature",
+                display_name="Initial temperature",
+                type=api_pb2.DATA_TYPE_FLOAT64,
+                domain_discrete=temperature_list,
+            ),
+            api_pb2.HParamInfo(
+                name="ambient_temperature",
+                display_name="Ambient temperature",
+                type=api_pb2.DATA_TYPE_FLOAT64,
+                domain_discrete=temperature_list,
+            ),
+            api_pb2.HParamInfo(
+                name="material",
+                display_name="Material",
+                type=api_pb2.DATA_TYPE_STRING,
+                domain_discrete=materials,
+            ),
+        ],
+        metric_infos=[
+            api_pb2.MetricInfo(
+                name=api_pb2.MetricName(
+                    tag="temperature/current/scalar_summary"
+                ),
+                display_name="Current Temp.",
+            ),
+            api_pb2.MetricInfo(
+                name=api_pb2.MetricName(
+                    tag="temperature/difference_to_ambient/scalar_summary"
+                ),
+                display_name="Difference To Ambient Temp.",
+            ),
+            api_pb2.MetricInfo(
+                name=api_pb2.MetricName(tag="delta/scalar_summary"),
+                display_name="Delta T",
+            ),
+        ],
+    )
 
 
 def run(logdir, session_id, hparams, group_name):
-  """Runs a temperature simulation.
+    """Runs a temperature simulation.
 
-  This will simulate an object at temperature `initial_temperature`
-  sitting at rest in a large room at temperature `ambient_temperature`.
-  The object has some intrinsic `heat_coefficient`, which indicates
-  how much thermal conductivity it has: for instance, metals have high
-  thermal conductivity, while the thermal conductivity of water is low.
+    This will simulate an object at temperature `initial_temperature`
+    sitting at rest in a large room at temperature `ambient_temperature`.
+    The object has some intrinsic `heat_coefficient`, which indicates
+    how much thermal conductivity it has: for instance, metals have high
+    thermal conductivity, while the thermal conductivity of water is low.
 
-  Over time, the object's temperature will adjust to match the
-  temperature of its environment. We'll track the object's temperature,
-  how far it is from the room's temperature, and how much it changes at
-  each time step.
+    Over time, the object's temperature will adjust to match the
+    temperature of its environment. We'll track the object's temperature,
+    how far it is from the room's temperature, and how much it changes at
+    each time step.
 
-  Arguments:
-    logdir: the top-level directory into which to write summary data
-    session_id: an id for the session.
-    hparams: A dictionary mapping a hyperparameter name to its value.
-    group_name: an id for the session group this session belongs to.
-  """
-  tf.reset_default_graph()
-  tf.set_random_seed(0)
+    Arguments:
+      logdir: the top-level directory into which to write summary data
+      session_id: an id for the session.
+      hparams: A dictionary mapping a hyperparameter name to its value.
+      group_name: an id for the session group this session belongs to.
+    """
+    tf.reset_default_graph()
+    tf.set_random_seed(0)
 
-  initial_temperature = hparams['initial_temperature']
-  ambient_temperature = hparams['ambient_temperature']
-  heat_coefficient = HEAT_COEFFICIENTS[hparams['material']]
-  session_dir = os.path.join(logdir, session_id)
-  writer = tf.summary.FileWriter(session_dir)
-  writer.add_summary(summary.session_start_pb(hparams=hparams,
-                                              group_name=group_name))
-  writer.flush()
-  with tf.name_scope('temperature'):
-    # Create a mutable variable to hold the object's temperature, and
-    # create a scalar summary to track its value over time. The name of
-    # the summary will appear as 'temperature/current' due to the
-    # name-scope above.
-    temperature = tf.Variable(
-        tf.constant(initial_temperature),
-        name='temperature')
-    scalar_summary.op('current', temperature,
-                      display_name='Temperature',
-                      description='The temperature of the object under '
-                                  'simulation, in Kelvins.')
+    initial_temperature = hparams["initial_temperature"]
+    ambient_temperature = hparams["ambient_temperature"]
+    heat_coefficient = HEAT_COEFFICIENTS[hparams["material"]]
+    session_dir = os.path.join(logdir, session_id)
+    writer = tf.summary.FileWriter(session_dir)
+    writer.add_summary(
+        summary.session_start_pb(hparams=hparams, group_name=group_name)
+    )
+    writer.flush()
+    with tf.name_scope("temperature"):
+        # Create a mutable variable to hold the object's temperature, and
+        # create a scalar summary to track its value over time. The name of
+        # the summary will appear as 'temperature/current' due to the
+        # name-scope above.
+        temperature = tf.Variable(
+            tf.constant(initial_temperature), name="temperature"
+        )
+        scalar_summary.op(
+            "current",
+            temperature,
+            display_name="Temperature",
+            description="The temperature of the object under "
+            "simulation, in Kelvins.",
+        )
 
-    # Compute how much the object's temperature differs from that of its
-    # environment, and track this, too: likewise, as
-    # 'temperature/difference_to_ambient'.
-    ambient_difference = temperature - ambient_temperature
-    scalar_summary.op('difference_to_ambient', ambient_difference,
-                      display_name='Difference to ambient temperature',
-                      description=('The difference between the ambient '
-                                   'temperature and the temperature of the '
-                                   'object under simulation, in Kelvins.'))
+        # Compute how much the object's temperature differs from that of its
+        # environment, and track this, too: likewise, as
+        # 'temperature/difference_to_ambient'.
+        ambient_difference = temperature - ambient_temperature
+        scalar_summary.op(
+            "difference_to_ambient",
+            ambient_difference,
+            display_name="Difference to ambient temperature",
+            description=(
+                "The difference between the ambient "
+                "temperature and the temperature of the "
+                "object under simulation, in Kelvins."
+            ),
+        )
 
-  # Newton suggested that the rate of change of the temperature of an
-  # object is directly proportional to this `ambient_difference` above,
-  # where the proportionality constant is what we called the heat
-  # coefficient. But in real life, not everything is quite so clean, so
-  # we'll add in some noise. (The value of 50 is arbitrary, chosen to
-  # make the data look somewhat interesting. :-) )
-  noise = 50 * tf.random.normal([])
-  delta = -heat_coefficient * (ambient_difference + noise)
-  scalar_summary.op('delta', delta,
-                    description='The change in temperature from the previous '
-                                'step, in Kelvins.')
+    # Newton suggested that the rate of change of the temperature of an
+    # object is directly proportional to this `ambient_difference` above,
+    # where the proportionality constant is what we called the heat
+    # coefficient. But in real life, not everything is quite so clean, so
+    # we'll add in some noise. (The value of 50 is arbitrary, chosen to
+    # make the data look somewhat interesting. :-) )
+    noise = 50 * tf.random.normal([])
+    delta = -heat_coefficient * (ambient_difference + noise)
+    scalar_summary.op(
+        "delta",
+        delta,
+        description="The change in temperature from the previous "
+        "step, in Kelvins.",
+    )
 
-  # Collect all the scalars that we want to keep track of.
-  summ = tf.summary.merge_all()
+    # Collect all the scalars that we want to keep track of.
+    summ = tf.summary.merge_all()
 
-  # Now, augment the current temperature by this delta that we computed,
-  # blocking the assignment on summary collection to avoid race conditions
-  # and ensure that the summary always reports the pre-update value.
-  with tf.control_dependencies([summ]):
-    update_step = temperature.assign_add(delta)
+    # Now, augment the current temperature by this delta that we computed,
+    # blocking the assignment on summary collection to avoid race conditions
+    # and ensure that the summary always reports the pre-update value.
+    with tf.control_dependencies([summ]):
+        update_step = temperature.assign_add(delta)
 
-  sess = tf.Session()
-  sess.run(tf.global_variables_initializer())
-  for step in xrange(FLAGS.num_steps):
-    # By asking TensorFlow to compute the update step, we force it to
-    # change the value of the temperature variable. We don't actually
-    # care about this value, so we discard it; instead, we grab the
-    # summary data computed along the way.
-    (s, _) = sess.run([summ, update_step])
-    if (step % FLAGS.summary_freq) == 0:
-      writer.add_summary(s, global_step=step)
-  writer.add_summary(summary.session_end_pb(api_pb2.STATUS_SUCCESS))
-  writer.close()
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    for step in xrange(FLAGS.num_steps):
+        # By asking TensorFlow to compute the update step, we force it to
+        # change the value of the temperature variable. We don't actually
+        # care about this value, so we discard it; instead, we grab the
+        # summary data computed along the way.
+        (s, _) = sess.run([summ, update_step])
+        if (step % FLAGS.summary_freq) == 0:
+            writer.add_summary(s, global_step=step)
+    writer.add_summary(summary.session_end_pb(api_pb2.STATUS_SUCCESS))
+    writer.close()
 
 
 def run_all(logdir, verbose=False):
-  """Run simulations on a reasonable set of parameters.
+    """Run simulations on a reasonable set of parameters.
 
-  Arguments:
-    logdir: the directory into which to store all the runs' data
-    verbose: if true, print out each run's name as it begins.
-  """
-  writer = tf.summary.FileWriter(logdir)
-  writer.add_summary(create_experiment_summary())
-  writer.close()
-  session_num = 0
-  num_sessions = (len(TEMPERATURE_LIST)*len(TEMPERATURE_LIST)*
-                  len(HEAT_COEFFICIENTS)*2)
-  for initial_temperature in TEMPERATURE_LIST:
-    for ambient_temperature in TEMPERATURE_LIST:
-      for material in HEAT_COEFFICIENTS:
-        hparams = {u'initial_temperature': initial_temperature,
-                   u'ambient_temperature': ambient_temperature,
-                   u'material': material}
-        hparam_str = str(hparams)
-        group_name = fingerprint(hparam_str)
-        for repeat_idx in xrange(2):
-          session_id = str(session_num)
-          if verbose:
-            print('--- Running training session %d/%d' % (session_num + 1,
-                                                          num_sessions))
-            print(hparam_str)
-            print('--- repeat #: %d' % (repeat_idx+1))
-          run(logdir, session_id, hparams, group_name)
-          session_num += 1
+    Arguments:
+      logdir: the directory into which to store all the runs' data
+      verbose: if true, print out each run's name as it begins.
+    """
+    writer = tf.summary.FileWriter(logdir)
+    writer.add_summary(create_experiment_summary())
+    writer.close()
+    session_num = 0
+    num_sessions = (
+        len(TEMPERATURE_LIST)
+        * len(TEMPERATURE_LIST)
+        * len(HEAT_COEFFICIENTS)
+        * 2
+    )
+    for initial_temperature in TEMPERATURE_LIST:
+        for ambient_temperature in TEMPERATURE_LIST:
+            for material in HEAT_COEFFICIENTS:
+                hparams = {
+                    u"initial_temperature": initial_temperature,
+                    u"ambient_temperature": ambient_temperature,
+                    u"material": material,
+                }
+                hparam_str = str(hparams)
+                group_name = fingerprint(hparam_str)
+                for repeat_idx in xrange(2):
+                    session_id = str(session_num)
+                    if verbose:
+                        print(
+                            "--- Running training session %d/%d"
+                            % (session_num + 1, num_sessions)
+                        )
+                        print(hparam_str)
+                        print("--- repeat #: %d" % (repeat_idx + 1))
+                    run(logdir, session_id, hparams, group_name)
+                    session_num += 1
 
 
 def main(unused_argv):
-  if tf.executing_eagerly():
-    print('Sorry, this demo currently can\'t be run in eager mode.')
-    return
+    if tf.executing_eagerly():
+        print("Sorry, this demo currently can't be run in eager mode.")
+        return
 
-  init_temperature_list()
-  shutil.rmtree(FLAGS.logdir, ignore_errors=True)
-  print('Saving output to %s.' % FLAGS.logdir)
-  run_all(FLAGS.logdir, verbose=True)
-  print('Done. Output saved to %s.' % FLAGS.logdir)
+    init_temperature_list()
+    shutil.rmtree(FLAGS.logdir, ignore_errors=True)
+    print("Saving output to %s." % FLAGS.logdir)
+    run_all(FLAGS.logdir, verbose=True)
+    print("Done. Output saved to %s." % FLAGS.logdir)
 
 
-if __name__ == '__main__':
-  app.run(main)
+if __name__ == "__main__":
+    app.run(main)
--- tensorboard/plugins/hparams/hparams_util.py
+++ tensorboard/plugins/hparams/hparams_util.py
@@ -60,135 +60,185 @@ from tensorboard.plugins.hparams import summary
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string(
-    "action", "", "The action to perform. One of {'create_experiment',"
-    " 'start_session', 'end_session'}.")
+    "action",
+    "",
+    "The action to perform. One of {'create_experiment',"
+    " 'start_session', 'end_session'}.",
+)
 flags.DEFINE_string("logdir", "", "The log directory to write the summary to.")
 
 # --action=create_experiment flags.
 flags.DEFINE_string(
-    "hparam_infos", "", "Only used when --action=create_experiment."
+    "hparam_infos",
+    "",
+    "Only used when --action=create_experiment."
     " A text-formatted HParamsInfoList protobuf describing"
-    " the hyperparameters used in the experiment.")
+    " the hyperparameters used in the experiment.",
+)
 flags.DEFINE_string(
-    "metric_infos", "", "Only used when --action=create_experiment."
+    "metric_infos",
+    "",
+    "Only used when --action=create_experiment."
     " A text-formatted MetricInfosList protobuf describing"
-    " the metrics used in the experiment.")
+    " the metrics used in the experiment.",
+)
 flags.DEFINE_string(
-    "description", "", "(Optional) only used when --action=create_experiment."
-    " The description for the experiment.")
-flags.DEFINE_string(
-    "user", getpass.getuser(),
+    "description",
+    "",
     "(Optional) only used when --action=create_experiment."
-    " The name of the user creating the experiment.")
+    " The description for the experiment.",
+)
+flags.DEFINE_string(
+    "user",
+    getpass.getuser(),
+    "(Optional) only used when --action=create_experiment."
+    " The name of the user creating the experiment.",
+)
 flags.DEFINE_float(
-    "time_created_secs", time.time(),
+    "time_created_secs",
+    time.time(),
     "(Optional) only used when --action=create_experiment."
     " The creation time of the experiment in seconds since"
-    " epoch.")
+    " epoch.",
+)
 
 # --action=start_session flags.
 flags.DEFINE_string(
-    "hparams", "", "Only used when --action=start_session."
+    "hparams",
+    "",
+    "Only used when --action=start_session."
     " A text-formatted HParams protobuf describing"
-    " the hyperparameter values used in the session.")
+    " the hyperparameter values used in the session.",
+)
 flags.DEFINE_string(
-    "model_uri", "", "(Optional) only used when --action=start_session."
+    "model_uri",
+    "",
+    "(Optional) only used when --action=start_session."
     " A uri describing the location where model checkpoints"
-    " are saved.")
+    " are saved.",
+)
 flags.DEFINE_string(
-    "monitor_url", "", "(Optional) only used when --action=start_session."
+    "monitor_url",
+    "",
+    "(Optional) only used when --action=start_session."
     " A url for a webpage showing monitoring information on"
-    " the session job.")
+    " the session job.",
+)
 flags.DEFINE_string(
-    "group_name", "", "(Optional) only used when --action=start_session."
+    "group_name",
+    "",
+    "(Optional) only used when --action=start_session."
     " The name of the group this session belongs to"
     " (empty group means the session is the only one in its"
-    " group).")
+    " group).",
+)
 flags.DEFINE_float(
-    "start_time_secs", time.time(),
+    "start_time_secs",
+    time.time(),
     "(Optional) only used when --action=start_session."
-    " The time the session started in seconds since epoch.")
+    " The time the session started in seconds since epoch.",
+)
 
 # --action=end_session flags.
 flags.DEFINE_string(
-    "status", "", "Only used when --action=end_session."
+    "status",
+    "",
+    "Only used when --action=end_session."
     " A string representation of a member of the Status enum."
-    " The status the session ended at.")
+    " The status the session ended at.",
+)
 flags.DEFINE_float(
-    "end_time_secs", time.time(),
+    "end_time_secs",
+    time.time(),
     "(Optional) only used when --action=end_session."
-    " The time the session ended in seconds since epoch.")
+    " The time the session ended in seconds since epoch.",
+)
 
 
 def main(argv):
-  del argv  # Unused.
-  if FLAGS.action == "create_experiment":
-    create_experiment()
-  elif FLAGS.action == "start_session":
-    start_session()
-  elif FLAGS.action == "end_session":
-    end_session()
-  else:
-    raise ValueError("Invalid action requested: '%s'" % FLAGS.action)
+    del argv  # Unused.
+    if FLAGS.action == "create_experiment":
+        create_experiment()
+    elif FLAGS.action == "start_session":
+        start_session()
+    elif FLAGS.action == "end_session":
+        end_session()
+    else:
+        raise ValueError("Invalid action requested: '%s'" % FLAGS.action)
 
 
 def create_experiment():
-  hparam_infos = hparams_util_pb2.HParamInfosList()
-  text_format.Merge(FLAGS.hparam_infos, hparam_infos)
-  metric_infos = hparams_util_pb2.MetricInfosList()
-  text_format.Merge(FLAGS.metric_infos, metric_infos)
-  write_summary(
-      summary.experiment_pb(hparam_infos.hparam_infos,
-                            metric_infos.metric_infos, FLAGS.user,
-                            FLAGS.description, FLAGS.time_created_secs))
+    hparam_infos = hparams_util_pb2.HParamInfosList()
+    text_format.Merge(FLAGS.hparam_infos, hparam_infos)
+    metric_infos = hparams_util_pb2.MetricInfosList()
+    text_format.Merge(FLAGS.metric_infos, metric_infos)
+    write_summary(
+        summary.experiment_pb(
+            hparam_infos.hparam_infos,
+            metric_infos.metric_infos,
+            FLAGS.user,
+            FLAGS.description,
+            FLAGS.time_created_secs,
+        )
+    )
 
 
 def start_session():
-  hparams = hparams_util_pb2.HParams()
-  text_format.Merge(FLAGS.hparams, hparams)
-  # Convert hparams.hparams values from google.protobuf.Value to Python native
-  # objects.
-  hparams = {
-      key: value_to_python(value)
-      for (key, value) in six.iteritems(hparams.hparams)
-  }
-  write_summary(
-      summary.session_start_pb(hparams, FLAGS.model_uri, FLAGS.monitor_url,
-                               FLAGS.group_name, FLAGS.start_time_secs))
+    hparams = hparams_util_pb2.HParams()
+    text_format.Merge(FLAGS.hparams, hparams)
+    # Convert hparams.hparams values from google.protobuf.Value to Python native
+    # objects.
+    hparams = {
+        key: value_to_python(value)
+        for (key, value) in six.iteritems(hparams.hparams)
+    }
+    write_summary(
+        summary.session_start_pb(
+            hparams,
+            FLAGS.model_uri,
+            FLAGS.monitor_url,
+            FLAGS.group_name,
+            FLAGS.start_time_secs,
+        )
+    )
 
 
 def value_to_python(value):
-  """Converts a google.protobuf.Value to a native python object."""
+    """Converts a google.protobuf.Value to a native python object."""
 
-  # We use the ListValue Well Known Type Value-to-native Python conversion
-  # logic to avoid depending on value's protobuf representation.
-  l = struct_pb2.ListValue(values=[value])
-  return l[0]
+    # We use the ListValue Well Known Type Value-to-native Python conversion
+    # logic to avoid depending on value's protobuf representation.
+    l = struct_pb2.ListValue(values=[value])
+    return l[0]
 
 
 def end_session():
-  write_summary(
-      summary.session_end_pb(
-          api_pb2.Status.Value(FLAGS.status), FLAGS.end_time_secs))
+    write_summary(
+        summary.session_end_pb(
+            api_pb2.Status.Value(FLAGS.status), FLAGS.end_time_secs
+        )
+    )
 
 
 def write_summary(summary_pb):
-  tf.compat.v1.enable_eager_execution()
-  writer = tf.compat.v2.summary.create_file_writer(FLAGS.logdir)
-  with writer.as_default():
-    if hasattr(tf.compat.v2.summary.experimental, "write_raw_pb"):
-      tf.compat.v2.summary.experimental.write_raw_pb(
-          summary_pb.SerializeToString(), step=0)
-    else:
-      # TODO(https://github.com/tensorflow/tensorboard/issues/2109): remove the
-      #   fallback to import_event().
-      event = tf.compat.v1.Event(summary=summary_pb)
-      tf.compat.v2.summary.import_event(
-          tf.constant(event.SerializeToString(), dtype=tf.string))
-    # The following may not be required since the context manager may
-    # already flush on __exit__, but it doesn't hurt to do it here, as well.
-    tf.compat.v2.summary.flush()
+    tf.compat.v1.enable_eager_execution()
+    writer = tf.compat.v2.summary.create_file_writer(FLAGS.logdir)
+    with writer.as_default():
+        if hasattr(tf.compat.v2.summary.experimental, "write_raw_pb"):
+            tf.compat.v2.summary.experimental.write_raw_pb(
+                summary_pb.SerializeToString(), step=0
+            )
+        else:
+            # TODO(https://github.com/tensorflow/tensorboard/issues/2109): remove the
+            #   fallback to import_event().
+            event = tf.compat.v1.Event(summary=summary_pb)
+            tf.compat.v2.summary.import_event(
+                tf.constant(event.SerializeToString(), dtype=tf.string)
+            )
+        # The following may not be required since the context manager may
+        # already flush on __exit__, but it doesn't hurt to do it here, as well.
+        tf.compat.v2.summary.flush()
 
 
 if __name__ == "__main__":
-  app.run(main)
+    app.run(main)
