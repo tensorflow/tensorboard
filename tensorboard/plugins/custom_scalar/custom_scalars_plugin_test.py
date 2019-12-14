001aab26f02b5fb2493a6bf0eb7588dddb02abcb
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-12-14 12:51:56 -0800

    Integrate 51b9e27fb096c9df055de4461cbdfb344b344700

--- tensorboard/plugins/custom_scalar/custom_scalars_plugin_test.py
+++ tensorboard/plugins/custom_scalar/custom_scalars_plugin_test.py
@@ -25,7 +25,9 @@ import numpy as np
 import tensorflow as tf
 
 from google.protobuf import json_format
-from tensorboard.backend.event_processing import plugin_event_multiplexer as event_multiplexer
+from tensorboard.backend.event_processing import (
+    plugin_event_multiplexer as event_multiplexer,
+)
 from tensorboard.plugins import base_plugin
 from tensorboard.plugins.custom_scalar import custom_scalars_plugin
 from tensorboard.plugins.custom_scalar import layout_pb2
@@ -38,209 +40,244 @@ tf.compat.v1.disable_v2_behavior()
 
 
 class CustomScalarsPluginTest(tf.test.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(CustomScalarsPluginTest, self).__init__(*args, **kwargs)
+        self.logdir = os.path.join(self.get_temp_dir(), "logdir")
+        os.makedirs(self.logdir)
 
-  def __init__(self, *args, **kwargs):
-    super(CustomScalarsPluginTest, self).__init__(*args, **kwargs)
-    self.logdir = os.path.join(self.get_temp_dir(), 'logdir')
-    os.makedirs(self.logdir)
-
-    self.logdir_layout = layout_pb2.Layout(
-        category=[
-            layout_pb2.Category(
-                title='cross entropy',
-                chart=[
-                    layout_pb2.Chart(
-                        title='cross entropy',
-                        multiline=layout_pb2.MultilineChartContent(
-                            tag=[r'cross entropy'],
-                        )),
-                ],
-                closed=True)
+        self.logdir_layout = layout_pb2.Layout(
+            category=[
+                layout_pb2.Category(
+                    title="cross entropy",
+                    chart=[
+                        layout_pb2.Chart(
+                            title="cross entropy",
+                            multiline=layout_pb2.MultilineChartContent(
+                                tag=[r"cross entropy"],
+                            ),
+                        ),
+                    ],
+                    closed=True,
+                )
             ]
-    )
-    self.foo_layout = layout_pb2.Layout(
-        category=[
-            layout_pb2.Category(
-                title='mean biases',
-                chart=[
-                    layout_pb2.Chart(
-                        title='mean layer biases',
-                        multiline=layout_pb2.MultilineChartContent(
-                            tag=[r'mean/layer0/biases', r'mean/layer1/biases'],
-                        )),
-                ]
-            ),
-            layout_pb2.Category(
-                title='std weights',
-                chart=[
-                    layout_pb2.Chart(
-                        title='stddev layer weights',
-                        multiline=layout_pb2.MultilineChartContent(
-                            tag=[r'stddev/layer\d+/weights'],
-                        )),
-                ]
-            ),
-            # A category with this name is also present in a layout for a
-            # different run (the logdir run) and also contains a duplicate chart
-            layout_pb2.Category(
-                title='cross entropy',
-                chart=[
-                    layout_pb2.Chart(
-                        title='cross entropy margin chart',
-                        margin=layout_pb2.MarginChartContent(
-                            series=[
-                                layout_pb2.MarginChartContent.Series(
-                                    value='cross entropy',
-                                    lower='cross entropy lower',
-                                    upper='cross entropy upper'),
-                            ],
-                        )),
-                    layout_pb2.Chart(
-                        title='cross entropy',
-                        multiline=layout_pb2.MultilineChartContent(
-                            tag=[r'cross entropy'],
-                        )),
-                ]
-            ),
+        )
+        self.foo_layout = layout_pb2.Layout(
+            category=[
+                layout_pb2.Category(
+                    title="mean biases",
+                    chart=[
+                        layout_pb2.Chart(
+                            title="mean layer biases",
+                            multiline=layout_pb2.MultilineChartContent(
+                                tag=[
+                                    r"mean/layer0/biases",
+                                    r"mean/layer1/biases",
+                                ],
+                            ),
+                        ),
+                    ],
+                ),
+                layout_pb2.Category(
+                    title="std weights",
+                    chart=[
+                        layout_pb2.Chart(
+                            title="stddev layer weights",
+                            multiline=layout_pb2.MultilineChartContent(
+                                tag=[r"stddev/layer\d+/weights"],
+                            ),
+                        ),
+                    ],
+                ),
+                # A category with this name is also present in a layout for a
+                # different run (the logdir run) and also contains a duplicate chart
+                layout_pb2.Category(
+                    title="cross entropy",
+                    chart=[
+                        layout_pb2.Chart(
+                            title="cross entropy margin chart",
+                            margin=layout_pb2.MarginChartContent(
+                                series=[
+                                    layout_pb2.MarginChartContent.Series(
+                                        value="cross entropy",
+                                        lower="cross entropy lower",
+                                        upper="cross entropy upper",
+                                    ),
+                                ],
+                            ),
+                        ),
+                        layout_pb2.Chart(
+                            title="cross entropy",
+                            multiline=layout_pb2.MultilineChartContent(
+                                tag=[r"cross entropy"],
+                            ),
+                        ),
+                    ],
+                ),
+            ]
+        )
+
+        # Generate test data.
+        with test_util.FileWriterCache.get(
+            os.path.join(self.logdir, "foo")
+        ) as writer:
+            writer.add_summary(summary.pb(self.foo_layout))
+            for step in range(4):
+                writer.add_summary(
+                    scalar_summary.pb("squares", step * step), step
+                )
+
+        with test_util.FileWriterCache.get(
+            os.path.join(self.logdir, "bar")
+        ) as writer:
+            for step in range(3):
+                writer.add_summary(
+                    scalar_summary.pb("increments", step + 1), step
+                )
+
+        # The '.' run lacks scalar data but has a layout.
+        with test_util.FileWriterCache.get(self.logdir) as writer:
+            writer.add_summary(summary.pb(self.logdir_layout))
+
+        self.plugin = self.createPlugin(self.logdir)
+
+    def createPlugin(self, logdir):
+        multiplexer = event_multiplexer.EventMultiplexer()
+        multiplexer.AddRunsFromDirectory(logdir)
+        multiplexer.Reload()
+        plugin_name_to_instance = {}
+        context = base_plugin.TBContext(
+            logdir=logdir,
+            multiplexer=multiplexer,
+            plugin_name_to_instance=plugin_name_to_instance,
+        )
+        scalars_plugin_instance = scalars_plugin.ScalarsPlugin(context)
+        custom_scalars_plugin_instance = custom_scalars_plugin.CustomScalarsPlugin(
+            context
+        )
+        plugin_instances = [
+            scalars_plugin_instance,
+            custom_scalars_plugin_instance,
         ]
-    )
+        for plugin_instance in plugin_instances:
+            plugin_name_to_instance[
+                plugin_instance.plugin_name
+            ] = plugin_instance
+        return custom_scalars_plugin_instance
 
-    # Generate test data.
-    with test_util.FileWriterCache.get(os.path.join(self.logdir, 'foo')) as writer:
-      writer.add_summary(summary.pb(self.foo_layout))
-      for step in range(4):
-        writer.add_summary(scalar_summary.pb('squares', step * step), step)
+    def testDownloadData(self):
+        body, mime_type = self.plugin.download_data_impl(
+            "foo", "squares/scalar_summary", "json"
+        )
+        self.assertEqual("application/json", mime_type)
+        self.assertEqual(4, len(body))
+        for step, entry in enumerate(body):
+            # The time stamp should be reasonable.
+            self.assertGreater(entry[0], 0)
+            self.assertEqual(step, entry[1])
+            np.testing.assert_allclose(step * step, entry[2])
 
-    with test_util.FileWriterCache.get(os.path.join(self.logdir, 'bar')) as writer:
-      for step in range(3):
-        writer.add_summary(scalar_summary.pb('increments', step + 1), step)
+    def testScalars(self):
+        body = self.plugin.scalars_impl("bar", "increments")
+        self.assertTrue(body["regex_valid"])
+        self.assertItemsEqual(
+            ["increments/scalar_summary"], list(body["tag_to_events"].keys())
+        )
+        data = body["tag_to_events"]["increments/scalar_summary"]
+        for step, entry in enumerate(data):
+            # The time stamp should be reasonable.
+            self.assertGreater(entry[0], 0)
+            self.assertEqual(step, entry[1])
+            np.testing.assert_allclose(step + 1, entry[2])
 
-    # The '.' run lacks scalar data but has a layout.
-    with test_util.FileWriterCache.get(self.logdir) as writer:
-      writer.add_summary(summary.pb(self.logdir_layout))
+    def testMergedLayout(self):
+        parsed_layout = layout_pb2.Layout()
+        json_format.Parse(self.plugin.layout_impl(), parsed_layout)
+        correct_layout = layout_pb2.Layout(
+            category=[
+                # A category with this name is also present in a layout for a
+                # different run (the logdir run)
+                layout_pb2.Category(
+                    title="cross entropy",
+                    chart=[
+                        layout_pb2.Chart(
+                            title="cross entropy",
+                            multiline=layout_pb2.MultilineChartContent(
+                                tag=[r"cross entropy"],
+                            ),
+                        ),
+                        layout_pb2.Chart(
+                            title="cross entropy margin chart",
+                            margin=layout_pb2.MarginChartContent(
+                                series=[
+                                    layout_pb2.MarginChartContent.Series(
+                                        value="cross entropy",
+                                        lower="cross entropy lower",
+                                        upper="cross entropy upper",
+                                    ),
+                                ],
+                            ),
+                        ),
+                    ],
+                    closed=True,
+                ),
+                layout_pb2.Category(
+                    title="mean biases",
+                    chart=[
+                        layout_pb2.Chart(
+                            title="mean layer biases",
+                            multiline=layout_pb2.MultilineChartContent(
+                                tag=[
+                                    r"mean/layer0/biases",
+                                    r"mean/layer1/biases",
+                                ],
+                            ),
+                        ),
+                    ],
+                ),
+                layout_pb2.Category(
+                    title="std weights",
+                    chart=[
+                        layout_pb2.Chart(
+                            title="stddev layer weights",
+                            multiline=layout_pb2.MultilineChartContent(
+                                tag=[r"stddev/layer\d+/weights"],
+                            ),
+                        ),
+                    ],
+                ),
+            ]
+        )
+        self.assertProtoEquals(correct_layout, parsed_layout)
 
-    self.plugin = self.createPlugin(self.logdir)
+    def testLayoutFromSingleRun(self):
+        # The foo directory contains 1 single layout.
+        local_plugin = self.createPlugin(os.path.join(self.logdir, "foo"))
+        parsed_layout = layout_pb2.Layout()
+        json_format.Parse(local_plugin.layout_impl(), parsed_layout)
+        self.assertProtoEquals(self.foo_layout, parsed_layout)
 
-  def createPlugin(self, logdir):
-    multiplexer = event_multiplexer.EventMultiplexer()
-    multiplexer.AddRunsFromDirectory(logdir)
-    multiplexer.Reload()
-    plugin_name_to_instance = {}
-    context = base_plugin.TBContext(
-        logdir=logdir,
-        multiplexer=multiplexer,
-        plugin_name_to_instance=plugin_name_to_instance)
-    scalars_plugin_instance = scalars_plugin.ScalarsPlugin(context)
-    custom_scalars_plugin_instance = custom_scalars_plugin.CustomScalarsPlugin(
-        context)
-    plugin_instances = [scalars_plugin_instance, custom_scalars_plugin_instance]
-    for plugin_instance in plugin_instances:
-      plugin_name_to_instance[plugin_instance.plugin_name] = plugin_instance
-    return custom_scalars_plugin_instance
+    def testNoLayoutFound(self):
+        # The bar directory contains no layout.
+        local_plugin = self.createPlugin(os.path.join(self.logdir, "bar"))
+        self.assertDictEqual({}, local_plugin.layout_impl())
 
-  def testDownloadData(self):
-    body, mime_type = self.plugin.download_data_impl(
-        'foo', 'squares/scalar_summary', 'json')
-    self.assertEqual('application/json', mime_type)
-    self.assertEqual(4, len(body))
-    for step, entry in enumerate(body):
-      # The time stamp should be reasonable.
-      self.assertGreater(entry[0], 0)
-      self.assertEqual(step, entry[1])
-      np.testing.assert_allclose(step * step, entry[2])
+    def testIsActive(self):
+        self.assertTrue(self.plugin.is_active())
 
-  def testScalars(self):
-    body = self.plugin.scalars_impl('bar', 'increments')
-    self.assertTrue(body['regex_valid'])
-    self.assertItemsEqual(
-        ['increments/scalar_summary'], list(body['tag_to_events'].keys()))
-    data = body['tag_to_events']['increments/scalar_summary']
-    for step, entry in enumerate(data):
-      # The time stamp should be reasonable.
-      self.assertGreater(entry[0], 0)
-      self.assertEqual(step, entry[1])
-      np.testing.assert_allclose(step + 1, entry[2])
+    def testIsNotActiveDueToNoLayout(self):
+        # The bar directory contains scalar data but no layout.
+        local_plugin = self.createPlugin(os.path.join(self.logdir, "bar"))
+        self.assertFalse(local_plugin.is_active())
 
-  def testMergedLayout(self):
-    parsed_layout = layout_pb2.Layout()
-    json_format.Parse(self.plugin.layout_impl(), parsed_layout)
-    correct_layout = layout_pb2.Layout(
-        category=[
-            # A category with this name is also present in a layout for a
-            # different run (the logdir run)
-            layout_pb2.Category(
-                title='cross entropy',
-                chart=[
-                    layout_pb2.Chart(
-                        title='cross entropy',
-                        multiline=layout_pb2.MultilineChartContent(
-                            tag=[r'cross entropy'],
-                        )),
-                    layout_pb2.Chart(
-                        title='cross entropy margin chart',
-                        margin=layout_pb2.MarginChartContent(
-                            series=[
-                                layout_pb2.MarginChartContent.Series(
-                                    value='cross entropy',
-                                    lower='cross entropy lower',
-                                    upper='cross entropy upper'),
-                            ],
-                        )),
-                ],
-                closed=True,
-            ),
-            layout_pb2.Category(
-                title='mean biases',
-                chart=[
-                    layout_pb2.Chart(
-                        title='mean layer biases',
-                        multiline=layout_pb2.MultilineChartContent(
-                            tag=[r'mean/layer0/biases', r'mean/layer1/biases'],
-                        )),
-                ]
-            ),
-            layout_pb2.Category(
-                title='std weights',
-                chart=[
-                    layout_pb2.Chart(
-                        title='stddev layer weights',
-                        multiline=layout_pb2.MultilineChartContent(
-                            tag=[r'stddev/layer\d+/weights'],
-                        )),
-                ]
-            ),
-        ]
-    )
-    self.assertProtoEquals(correct_layout, parsed_layout)
+    def testIsNotActiveDueToNoScalarsData(self):
+        # Generate a directory with a layout but no scalars data.
+        directory = os.path.join(self.logdir, "no_scalars")
+        with test_util.FileWriterCache.get(directory) as writer:
+            writer.add_summary(summary.pb(self.logdir_layout))
 
-  def testLayoutFromSingleRun(self):
-    # The foo directory contains 1 single layout.
-    local_plugin = self.createPlugin(os.path.join(self.logdir, 'foo'))
-    parsed_layout = layout_pb2.Layout()
-    json_format.Parse(local_plugin.layout_impl(), parsed_layout)
-    self.assertProtoEquals(self.foo_layout, parsed_layout)
+        local_plugin = self.createPlugin(directory)
+        self.assertFalse(local_plugin.is_active())
 
-  def testNoLayoutFound(self):
-    # The bar directory contains no layout.
-    local_plugin = self.createPlugin(os.path.join(self.logdir, 'bar'))
-    self.assertDictEqual({}, local_plugin.layout_impl())
-
-  def testIsActive(self):
-    self.assertTrue(self.plugin.is_active())
-
-  def testIsNotActiveDueToNoLayout(self):
-    # The bar directory contains scalar data but no layout.
-    local_plugin = self.createPlugin(os.path.join(self.logdir, 'bar'))
-    self.assertFalse(local_plugin.is_active())
-
-  def testIsNotActiveDueToNoScalarsData(self):
-    # Generate a directory with a layout but no scalars data.
-    directory = os.path.join(self.logdir, 'no_scalars')
-    with test_util.FileWriterCache.get(directory) as writer:
-      writer.add_summary(summary.pb(self.logdir_layout))
-
-    local_plugin = self.createPlugin(directory)
-    self.assertFalse(local_plugin.is_active())
 
 if __name__ == "__main__":
-  tf.test.main()
+    tf.test.main()
