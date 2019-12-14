5a3ab3c9282b72cd66b63dd40ebf9e5e50118b6d
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-12-14 11:45:24 -0800

    black: reformat directory tensorboard/uploader

--- tensorboard/uploader/peekable_iterator.py
+++ tensorboard/uploader/peekable_iterator.py
@@ -20,68 +20,68 @@ from __future__ import print_function
 
 
 class PeekableIterator(object):
-  """Iterator adapter that supports peeking ahead.
+    """Iterator adapter that supports peeking ahead.
 
-  As with most Python iterators, this is also iterable; its `__iter__`
-  returns itself.
+    As with most Python iterators, this is also iterable; its `__iter__`
+    returns itself.
 
-  This class is not thread-safe. Use external synchronization if
-  iterating concurrently.
-  """
-
-  def __init__(self, iterable):
-    """Initializes a peeking iterator wrapping the provided iterable.
-
-    Args:
-      iterable: An iterable to wrap.
+    This class is not thread-safe. Use external synchronization if
+    iterating concurrently.
     """
-    self._iterator = iter(iterable)
-    self._has_peeked = False
-    self._peeked_element = None
 
-  def has_next(self):
-    """Checks whether there are any more items in this iterator.
+    def __init__(self, iterable):
+        """Initializes a peeking iterator wrapping the provided iterable.
 
-    The next call to `next` or `peek` will raise `StopIteration` if and
-    only if this method returns `False`.
+        Args:
+          iterable: An iterable to wrap.
+        """
+        self._iterator = iter(iterable)
+        self._has_peeked = False
+        self._peeked_element = None
 
-    Returns:
-      `True` if there are any more items in this iterator, else `False`.
-    """
-    try:
-      self.peek()
-      return True
-    except StopIteration:
-      return False
+    def has_next(self):
+        """Checks whether there are any more items in this iterator.
 
-  def peek(self):
-    """Gets the next item in the iterator without consuming it.
+        The next call to `next` or `peek` will raise `StopIteration` if and
+        only if this method returns `False`.
 
-    Multiple consecutive calls will return the same element.
+        Returns:
+          `True` if there are any more items in this iterator, else `False`.
+        """
+        try:
+            self.peek()
+            return True
+        except StopIteration:
+            return False
 
-    Returns:
-      The value that would be returned by `next`.
+    def peek(self):
+        """Gets the next item in the iterator without consuming it.
 
-    Raises:
-      StopIteration: If there are no more items in the iterator.
-    """
-    if not self._has_peeked:
-      self._peeked_element = next(self._iterator)
-      self._has_peeked = True
-    return self._peeked_element
+        Multiple consecutive calls will return the same element.
 
-  def __iter__(self):
-    return self
+        Returns:
+          The value that would be returned by `next`.
 
-  def __next__(self):
-    if self._has_peeked:
-      self._has_peeked = False
-      result = self._peeked_element
-      self._peeked_element = None  # allow GC
-      return result
-    else:
-      return next(self._iterator)
+        Raises:
+          StopIteration: If there are no more items in the iterator.
+        """
+        if not self._has_peeked:
+            self._peeked_element = next(self._iterator)
+            self._has_peeked = True
+        return self._peeked_element
 
-  def next(self):
-    # (Like `__next__`, but Python 2.)
-    return self.__next__()
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._has_peeked:
+            self._has_peeked = False
+            result = self._peeked_element
+            self._peeked_element = None  # allow GC
+            return result
+        else:
+            return next(self._iterator)
+
+    def next(self):
+        # (Like `__next__`, but Python 2.)
+        return self.__next__()
--- tensorboard/uploader/server_info.py
+++ tensorboard/uploader/server_info.py
@@ -30,88 +30,88 @@ _REQUEST_TIMEOUT_SECONDS = 10
 
 
 def _server_info_request():
-  request = server_info_pb2.ServerInfoRequest()
-  request.version = version.VERSION
-  return request
+    request = server_info_pb2.ServerInfoRequest()
+    request.version = version.VERSION
+    return request
 
 
 def fetch_server_info(origin):
-  """Fetches server info from a remote server.
+    """Fetches server info from a remote server.
 
-  Args:
-    origin: The server with which to communicate. Should be a string
-      like "https://tensorboard.dev", including protocol, host, and (if
-      needed) port.
+    Args:
+      origin: The server with which to communicate. Should be a string
+        like "https://tensorboard.dev", including protocol, host, and (if
+        needed) port.
 
-  Returns:
-    A `server_info_pb2.ServerInfoResponse` message.
+    Returns:
+      A `server_info_pb2.ServerInfoResponse` message.
 
-  Raises:
-    CommunicationError: Upon failure to connect to or successfully
-      communicate with the remote server.
-  """
-  endpoint = "%s/api/uploader" % origin
-  post_body = _server_info_request().SerializeToString()
-  try:
-    response = requests.post(
-        endpoint,
-        data=post_body,
-        timeout=_REQUEST_TIMEOUT_SECONDS,
-        headers={"User-Agent": "tensorboard/%s" % version.VERSION},
-    )
-  except requests.RequestException as e:
-    raise CommunicationError("Failed to connect to backend: %s" % e)
-  if not response.ok:
-    raise CommunicationError(
-        "Non-OK status from backend (%d %s): %r"
-        % (response.status_code, response.reason, response.content)
-    )
-  try:
-    return server_info_pb2.ServerInfoResponse.FromString(response.content)
-  except message.DecodeError as e:
-    raise CommunicationError(
-        "Corrupt response from backend (%s): %r" % (e, response.content)
-    )
+    Raises:
+      CommunicationError: Upon failure to connect to or successfully
+        communicate with the remote server.
+    """
+    endpoint = "%s/api/uploader" % origin
+    post_body = _server_info_request().SerializeToString()
+    try:
+        response = requests.post(
+            endpoint,
+            data=post_body,
+            timeout=_REQUEST_TIMEOUT_SECONDS,
+            headers={"User-Agent": "tensorboard/%s" % version.VERSION},
+        )
+    except requests.RequestException as e:
+        raise CommunicationError("Failed to connect to backend: %s" % e)
+    if not response.ok:
+        raise CommunicationError(
+            "Non-OK status from backend (%d %s): %r"
+            % (response.status_code, response.reason, response.content)
+        )
+    try:
+        return server_info_pb2.ServerInfoResponse.FromString(response.content)
+    except message.DecodeError as e:
+        raise CommunicationError(
+            "Corrupt response from backend (%s): %r" % (e, response.content)
+        )
 
 
 def create_server_info(frontend_origin, api_endpoint):
-  """Manually creates server info given a frontend and backend.
+    """Manually creates server info given a frontend and backend.
 
-  Args:
-    frontend_origin: The origin of the TensorBoard.dev frontend, like
-      "https://tensorboard.dev" or "http://localhost:8000".
-    api_endpoint: As to `server_info_pb2.ApiServer.endpoint`.
+    Args:
+      frontend_origin: The origin of the TensorBoard.dev frontend, like
+        "https://tensorboard.dev" or "http://localhost:8000".
+      api_endpoint: As to `server_info_pb2.ApiServer.endpoint`.
 
-  Returns:
-    A `server_info_pb2.ServerInfoResponse` message.
-  """
-  result = server_info_pb2.ServerInfoResponse()
-  result.compatibility.verdict = server_info_pb2.VERDICT_OK
-  result.api_server.endpoint = api_endpoint
-  url_format = result.url_format
-  placeholder = "{{EID}}"
-  while placeholder in frontend_origin:
-    placeholder = "{%s}" % placeholder
-  url_format.template = "%s/experiment/%s/" % (frontend_origin, placeholder)
-  url_format.id_placeholder = placeholder
-  return result
+    Returns:
+      A `server_info_pb2.ServerInfoResponse` message.
+    """
+    result = server_info_pb2.ServerInfoResponse()
+    result.compatibility.verdict = server_info_pb2.VERDICT_OK
+    result.api_server.endpoint = api_endpoint
+    url_format = result.url_format
+    placeholder = "{{EID}}"
+    while placeholder in frontend_origin:
+        placeholder = "{%s}" % placeholder
+    url_format.template = "%s/experiment/%s/" % (frontend_origin, placeholder)
+    url_format.id_placeholder = placeholder
+    return result
 
 
 def experiment_url(server_info, experiment_id):
-  """Formats a URL that will resolve to the provided experiment.
+    """Formats a URL that will resolve to the provided experiment.
 
-  Args:
-    server_info: A `server_info_pb2.ServerInfoResponse` message.
-    experiment_id: A string; the ID of the experiment to link to.
+    Args:
+      server_info: A `server_info_pb2.ServerInfoResponse` message.
+      experiment_id: A string; the ID of the experiment to link to.
 
-  Returns:
-    A URL resolving to the given experiment, as a string.
-  """
-  url_format = server_info.url_format
-  return url_format.template.replace(url_format.id_placeholder, experiment_id)
+    Returns:
+      A URL resolving to the given experiment, as a string.
+    """
+    url_format = server_info.url_format
+    return url_format.template.replace(url_format.id_placeholder, experiment_id)
 
 
 class CommunicationError(RuntimeError):
-  """Raised upon failure to communicate with the server."""
+    """Raised upon failure to communicate with the server."""
 
-  pass
+    pass
