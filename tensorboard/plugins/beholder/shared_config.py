6dcd54d8d21ad519ef798ec4f36edc305ee003a2
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-12-14 11:45:21 -0800

    black: reformat directory tensorboard/plugins/beholder

--- tensorboard/plugins/beholder/shared_config.py
+++ tensorboard/plugins/beholder/shared_config.py
@@ -16,22 +16,22 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-PLUGIN_NAME = 'beholder'
-TAG_NAME = 'beholder-frame'
-SUMMARY_FILENAME = 'frame.summary'
-CONFIG_FILENAME = 'config.pkl'
-SECTION_INFO_FILENAME = 'section-info.pkl'
-SUMMARY_COLLECTION_KEY_NAME = 'summaries_beholder'
+PLUGIN_NAME = "beholder"
+TAG_NAME = "beholder-frame"
+SUMMARY_FILENAME = "frame.summary"
+CONFIG_FILENAME = "config.pkl"
+SECTION_INFO_FILENAME = "section-info.pkl"
+SUMMARY_COLLECTION_KEY_NAME = "summaries_beholder"
 
 DEFAULT_CONFIG = {
-    'values': 'trainable_variables',
-    'mode': 'variance',
-    'scaling': 'layer',
-    'window_size': 15,
-    'FPS': 10,
-    'is_recording': False,
-    'show_all': False,
-    'colormap': 'magma'
+    "values": "trainable_variables",
+    "mode": "variance",
+    "scaling": "layer",
+    "window_size": 15,
+    "FPS": 10,
+    "is_recording": False,
+    "show_all": False,
+    "colormap": "magma",
 }
 
 SECTION_HEIGHT = 128
