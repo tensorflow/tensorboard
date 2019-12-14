4d8be00b50833002bbd1452e3a00d7cdd9aff07a
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-12-14 12:53:31 -0800

    Integrate c6020666b2d93541a9324547d7a7b6677e0fc6e4

--- tensorboard/plugins/debugger_v2/debugger_v2_plugin.py
+++ tensorboard/plugins/debugger_v2/debugger_v2_plugin.py
@@ -22,36 +22,36 @@ from tensorboard.plugins import base_plugin
 
 
 class DebuggerV2Plugin(base_plugin.TBPlugin):
-  """Debugger V2 Plugin for TensorBoard."""
+    """Debugger V2 Plugin for TensorBoard."""
 
-  plugin_name = "debugger-v2"
+    plugin_name = "debugger-v2"
 
-  def __init__(self, context):
-    """Instantiates Debugger V2 Plugin via TensorBoard core.
-    Args:
-      context: A base_plugin.TBContext instance.
-    """
-    super(DebuggerV2Plugin, self).__init__(context)
+    def __init__(self, context):
+        """Instantiates Debugger V2 Plugin via TensorBoard core.
 
-  def get_plugin_apps(self):
-    # TODO(cais): Add routes as they are implemented.
-    return {}
+        Args:
+          context: A base_plugin.TBContext instance.
+        """
+        super(DebuggerV2Plugin, self).__init__(context)
 
-  def is_active(self):
-    """Check whether the Debugger V2 Plugin is always active.
+    def get_plugin_apps(self):
+        # TODO(cais): Add routes as they are implemented.
+        return {}
 
-    When no data in the tfdbg v2 format is available, a custom information
-    screen is displayed to instruct the user on how to generate such data
-    to be able to use the plugin.
+    def is_active(self):
+        """Check whether the Debugger V2 Plugin is always active.
 
-    Returns:
-      `True` if and only if data in tfdbg v2's DebugEvent format is available.
-    """
-    # TODO(cais): Implement logic.
-    return False
+        When no data in the tfdbg v2 format is available, a custom information
+        screen is displayed to instruct the user on how to generate such data
+        to be able to use the plugin.
 
-  def frontend_metadata(self):
-    return base_plugin.FrontendMetadata(
-        is_ng_component=True,
-        tab_name='Debugger V2',
-        disable_reload=True)
+        Returns:
+          `True` if and only if data in tfdbg v2's DebugEvent format is available.
+        """
+        # TODO(cais): Implement logic.
+        return False
+
+    def frontend_metadata(self):
+        return base_plugin.FrontendMetadata(
+            is_ng_component=True, tab_name="Debugger V2", disable_reload=True
+        )
