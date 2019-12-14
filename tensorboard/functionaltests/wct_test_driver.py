5d06c4034780e4d85a24b7400b9d5569c6e50137
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-12-14 12:58:14 -0800

    Integrate 7664c071a00d3b3021d56e04838ceed547fac79c

--- tensorboard/functionaltests/wct_test_driver.py
+++ tensorboard/functionaltests/wct_test_driver.py
@@ -33,114 +33,124 @@ from testing.web import webtest
 # As emitted in the "Listening on:" line of the WebfilesServer output.
 # We extract only the port because the hostname can reroute through corp
 # DNS and force auth, which fails in tests.
-_URL_RE = re.compile(br'http://[^:]*:([0-9]+)/')
+_URL_RE = re.compile(br"http://[^:]*:([0-9]+)/")
 _SUITE_PASSED_RE = re.compile(r'.*test suite passed"$')
-_SUITE_FAILED_RE = re.compile(r'.*failing test.*')
+_SUITE_FAILED_RE = re.compile(r".*failing test.*")
+
 
 def create_test_class(binary_path, web_path):
-  """Create a unittest.TestCase class to run WebComponentTester tests.
+    """Create a unittest.TestCase class to run WebComponentTester tests.
 
-  Arguments:
-    binary_path: relative path to a `tf_web_library` target;
-        e.g.: "tensorboard/components/vz_foo/test/test_web_library"
-    web_path: absolute web path to the tests page in the above web
-        library; e.g.: "/vz-foo/test/tests.html"
+    Arguments:
+      binary_path: relative path to a `tf_web_library` target;
+          e.g.: "tensorboard/components/vz_foo/test/test_web_library"
+      web_path: absolute web path to the tests page in the above web
+          library; e.g.: "/vz-foo/test/tests.html"
 
-  Result:
-    A new subclass of `unittest.TestCase`. Bind this to a variable in
-    the test file's main module.
-  """
+    Result:
+      A new subclass of `unittest.TestCase`. Bind this to a variable in
+      the test file's main module.
+    """
 
-  class BrowserLogIndicatesResult(object):
-    def __init__(self):
-      self.passed = False
-      self.log = []
+    class BrowserLogIndicatesResult(object):
+        def __init__(self):
+            self.passed = False
+            self.log = []
 
-    def __call__(self, driver):
-      # Scan through the log entries and search for a line indicating whether
-      # the test passed or failed. The method 'driver.get_log' also seems to
-      # clear the log so we aggregate it in self.log for printing later on.
-      new_log = driver.get_log("browser")
-      new_messages = [entry["message"] for entry in new_log]
-      self.log = self.log + new_log
-      if self._log_matches(new_messages, _SUITE_FAILED_RE):
-        self.passed = False
-        return True
-      if self._log_matches(new_messages, _SUITE_PASSED_RE):
-        self.passed = True
-        return True
-      # Here, we still don't know.
-      return False
+        def __call__(self, driver):
+            # Scan through the log entries and search for a line indicating whether
+            # the test passed or failed. The method 'driver.get_log' also seems to
+            # clear the log so we aggregate it in self.log for printing later on.
+            new_log = driver.get_log("browser")
+            new_messages = [entry["message"] for entry in new_log]
+            self.log = self.log + new_log
+            if self._log_matches(new_messages, _SUITE_FAILED_RE):
+                self.passed = False
+                return True
+            if self._log_matches(new_messages, _SUITE_PASSED_RE):
+                self.passed = True
+                return True
+            # Here, we still don't know.
+            return False
 
-    def _log_matches(self, messages, regexp):
-      for message in messages:
-        if regexp.match(message):
-          return True
-      return False
+        def _log_matches(self, messages, regexp):
+            for message in messages:
+                if regexp.match(message):
+                    return True
+            return False
 
-  class WebComponentTesterTest(unittest.TestCase):
-    """Tests that a family of unit tests completes successfully."""
+    class WebComponentTesterTest(unittest.TestCase):
+        """Tests that a family of unit tests completes successfully."""
 
-    def setUp(cls):
-      src_dir = os.environ["TEST_SRCDIR"]
-      binary = os.path.join(
-          src_dir,
-          "org_tensorflow_tensorboard/" + binary_path)
-      cls.process = subprocess.Popen(
-          [binary], stdin=None, stdout=None, stderr=subprocess.PIPE)
+        def setUp(cls):
+            src_dir = os.environ["TEST_SRCDIR"]
+            binary = os.path.join(
+                src_dir, "org_tensorflow_tensorboard/" + binary_path
+            )
+            cls.process = subprocess.Popen(
+                [binary], stdin=None, stdout=None, stderr=subprocess.PIPE
+            )
 
-      lines = []
-      hit_eof = False
-      while True:
-        line = cls.process.stderr.readline()
-        if line == b"":
-          # b"" means reached EOF; b"\n" means empty line.
-          hit_eof = True
-          break
-        lines.append(line)
-        if b"Listening on:" in line:
-          match = _URL_RE.search(line)
-          if match:
-            cls.port = int(match.group(1))
-            break
-          else:
-            raise ValueError("Failed to parse listening-on line: %r" % line)
-        if len(lines) >= 1024:
-          # Sanity check---something is wrong. Let us fail fast rather
-          # than spending the 15-minute test timeout consuming
-          # potentially empty logs.
-          hit_eof = True
-          break
-      if hit_eof:
-        full_output = "\n".join(repr(line) for line in lines)
-        raise ValueError(
-            "Did not find listening-on line in output:\n%s" % full_output)
+            lines = []
+            hit_eof = False
+            while True:
+                line = cls.process.stderr.readline()
+                if line == b"":
+                    # b"" means reached EOF; b"\n" means empty line.
+                    hit_eof = True
+                    break
+                lines.append(line)
+                if b"Listening on:" in line:
+                    match = _URL_RE.search(line)
+                    if match:
+                        cls.port = int(match.group(1))
+                        break
+                    else:
+                        raise ValueError(
+                            "Failed to parse listening-on line: %r" % line
+                        )
+                if len(lines) >= 1024:
+                    # Sanity check---something is wrong. Let us fail fast rather
+                    # than spending the 15-minute test timeout consuming
+                    # potentially empty logs.
+                    hit_eof = True
+                    break
+            if hit_eof:
+                full_output = "\n".join(repr(line) for line in lines)
+                raise ValueError(
+                    "Did not find listening-on line in output:\n%s"
+                    % full_output
+                )
 
-    def tearDown(cls):
-      cls.process.kill()
-      cls.process.wait()
+        def tearDown(cls):
+            cls.process.kill()
+            cls.process.wait()
 
-    def test(self):
-      driver = webtest.new_webdriver_session(
-          capabilities={"loggingPrefs": {"browser": "ALL"}}
-      )
-      url = "http://localhost:%s%s" % (self.port, web_path)
-      driver.get(url)
-      browser_log_indicates_result = BrowserLogIndicatesResult()
-      try:
-        wait.WebDriverWait(driver, 10).until(browser_log_indicates_result)
-        if not browser_log_indicates_result.passed:
-          self.fail()
-      finally:
-        # Print log as an aid for debugging.
-        log = browser_log_indicates_result.log + driver.get_log("browser")
-        self._print_log(log)
+        def test(self):
+            driver = webtest.new_webdriver_session(
+                capabilities={"loggingPrefs": {"browser": "ALL"}}
+            )
+            url = "http://localhost:%s%s" % (self.port, web_path)
+            driver.get(url)
+            browser_log_indicates_result = BrowserLogIndicatesResult()
+            try:
+                wait.WebDriverWait(driver, 10).until(
+                    browser_log_indicates_result
+                )
+                if not browser_log_indicates_result.passed:
+                    self.fail()
+            finally:
+                # Print log as an aid for debugging.
+                log = browser_log_indicates_result.log + driver.get_log(
+                    "browser"
+                )
+                self._print_log(log)
 
-    def _print_log(self, entries):
-      print("Browser log follows:")
-      print("--------------------")
-      print(" | ".join(entries[0].keys()))
-      for entry in entries:
-        print(" | ".join(str(v) for v in entry.values()))
+        def _print_log(self, entries):
+            print("Browser log follows:")
+            print("--------------------")
+            print(" | ".join(entries[0].keys()))
+            for entry in entries:
+                print(" | ".join(str(v) for v in entry.values()))
 
-  return WebComponentTesterTest
+    return WebComponentTesterTest
