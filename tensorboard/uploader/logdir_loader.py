dc41a2073f64f59584eb965184aa852a9042e02b
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-12-14 11:45:24 -0800

    black: reformat directory tensorboard/uploader

--- tensorboard/uploader/logdir_loader.py
+++ tensorboard/uploader/logdir_loader.py
@@ -30,78 +30,82 @@ logger = tb_logging.get_logger()
 
 
 class LogdirLoader(object):
-  """Loader for a root log directory, maintaining multiple DirectoryLoaders.
+    """Loader for a root log directory, maintaining multiple DirectoryLoaders.
 
-  This class takes a root log directory and a factory for DirectoryLoaders, and
-  maintains one DirectoryLoader per "logdir subdirectory" of the root logdir.
+    This class takes a root log directory and a factory for DirectoryLoaders, and
+    maintains one DirectoryLoader per "logdir subdirectory" of the root logdir.
 
-  Note that this class is not thread-safe.
-  """
-
-  def __init__(self, logdir, directory_loader_factory):
-    """Constructs a new LogdirLoader.
-
-    Args:
-      logdir: The root log directory to load from.
-      directory_loader_factory: A factory for creating DirectoryLoaders. The
-        factory should take a path and return a DirectoryLoader.
-
-    Raises:
-      ValueError: If logdir or directory_loader_factory are None.
+    Note that this class is not thread-safe.
     """
-    if logdir is None:
-      raise ValueError('A logdir is required')
-    if directory_loader_factory is None:
-      raise ValueError('A directory loader factory is required')
-    self._logdir = logdir
-    self._directory_loader_factory = directory_loader_factory
-    # Maps run names to corresponding DirectoryLoader instances.
-    self._directory_loaders = {}
 
-  def synchronize_runs(self):
-    """Finds new runs within `logdir` and makes `DirectoryLoaders` for them.
+    def __init__(self, logdir, directory_loader_factory):
+        """Constructs a new LogdirLoader.
 
-    In addition, any existing `DirectoryLoader` whose run directory no longer
-    exists will be deleted.
-    """
-    logger.info('Starting logdir traversal of %s', self._logdir)
-    runs_seen = set()
-    for subdir in io_wrapper.GetLogdirSubdirectories(self._logdir):
-      run = os.path.relpath(subdir, self._logdir)
-      runs_seen.add(run)
-      if run not in self._directory_loaders:
-        logger.info('- Adding run for relative directory %s', run)
-        self._directory_loaders[run] = self._directory_loader_factory(subdir)
-    stale_runs = set(self._directory_loaders) - runs_seen
-    if stale_runs:
-      for run in stale_runs:
-        logger.info('- Removing run for relative directory %s', run)
-        del self._directory_loaders[run]
-    logger.info('Ending logdir traversal of %s', self._logdir)
+        Args:
+          logdir: The root log directory to load from.
+          directory_loader_factory: A factory for creating DirectoryLoaders. The
+            factory should take a path and return a DirectoryLoader.
 
-  def get_run_events(self):
-    """Returns tf.Event generators for each run's `DirectoryLoader`.
+        Raises:
+          ValueError: If logdir or directory_loader_factory are None.
+        """
+        if logdir is None:
+            raise ValueError("A logdir is required")
+        if directory_loader_factory is None:
+            raise ValueError("A directory loader factory is required")
+        self._logdir = logdir
+        self._directory_loader_factory = directory_loader_factory
+        # Maps run names to corresponding DirectoryLoader instances.
+        self._directory_loaders = {}
 
-    Warning: the generators are stateful and consuming them will affect the
-    results of any other existing generators for that run; calling code should
-    ensure it takes events from only a single generator per run at a time.
+    def synchronize_runs(self):
+        """Finds new runs within `logdir` and makes `DirectoryLoaders` for
+        them.
 
-    Returns:
-      Dictionary containing an entry for each run, mapping the run name to a
-      generator yielding tf.Event protobuf objects loaded from that run.
-    """
-    runs = list(self._directory_loaders)
-    logger.info('Creating event loading generators for %d runs', len(runs))
-    run_to_loader = collections.OrderedDict()
-    for run_name in sorted(runs):
-      loader = self._directory_loaders[run_name]
-      run_to_loader[run_name] = self._wrap_loader_generator(loader.Load())
-    return run_to_loader
+        In addition, any existing `DirectoryLoader` whose run directory
+        no longer exists will be deleted.
+        """
+        logger.info("Starting logdir traversal of %s", self._logdir)
+        runs_seen = set()
+        for subdir in io_wrapper.GetLogdirSubdirectories(self._logdir):
+            run = os.path.relpath(subdir, self._logdir)
+            runs_seen.add(run)
+            if run not in self._directory_loaders:
+                logger.info("- Adding run for relative directory %s", run)
+                self._directory_loaders[run] = self._directory_loader_factory(
+                    subdir
+                )
+        stale_runs = set(self._directory_loaders) - runs_seen
+        if stale_runs:
+            for run in stale_runs:
+                logger.info("- Removing run for relative directory %s", run)
+                del self._directory_loaders[run]
+        logger.info("Ending logdir traversal of %s", self._logdir)
 
-  def _wrap_loader_generator(self, loader_generator):
-    """Wraps `DirectoryLoader` generator to swallow `DirectoryDeletedError`."""
-    try:
-      for item in loader_generator:
-        yield item
-    except directory_watcher.DirectoryDeletedError:
-      return
+    def get_run_events(self):
+        """Returns tf.Event generators for each run's `DirectoryLoader`.
+
+        Warning: the generators are stateful and consuming them will affect the
+        results of any other existing generators for that run; calling code should
+        ensure it takes events from only a single generator per run at a time.
+
+        Returns:
+          Dictionary containing an entry for each run, mapping the run name to a
+          generator yielding tf.Event protobuf objects loaded from that run.
+        """
+        runs = list(self._directory_loaders)
+        logger.info("Creating event loading generators for %d runs", len(runs))
+        run_to_loader = collections.OrderedDict()
+        for run_name in sorted(runs):
+            loader = self._directory_loaders[run_name]
+            run_to_loader[run_name] = self._wrap_loader_generator(loader.Load())
+        return run_to_loader
+
+    def _wrap_loader_generator(self, loader_generator):
+        """Wraps `DirectoryLoader` generator to swallow
+        `DirectoryDeletedError`."""
+        try:
+            for item in loader_generator:
+                yield item
+        except directory_watcher.DirectoryDeletedError:
+            return
--- tensorboard/uploader/test_util.py
+++ tensorboard/uploader/test_util.py
@@ -27,37 +27,37 @@ from tensorboard.compat.proto import summary_pb2
 
 
 class FakeTime(object):
-  """Thread-safe fake replacement for the `time` module."""
+    """Thread-safe fake replacement for the `time` module."""
 
-  def __init__(self, current=0.0):
-    self._time = float(current)
-    self._lock = threading.Lock()
+    def __init__(self, current=0.0):
+        self._time = float(current)
+        self._lock = threading.Lock()
 
-  def time(self):
-    with self._lock:
-      return self._time
+    def time(self):
+        with self._lock:
+            return self._time
 
-  def sleep(self, secs):
-    with self._lock:
-      self._time += secs
+    def sleep(self, secs):
+        with self._lock:
+            self._time += secs
 
 
 def scalar_metadata(display_name):
-  """Makes a scalar metadata proto, for constructing expected requests."""
-  metadata = summary_pb2.SummaryMetadata(display_name=display_name)
-  metadata.plugin_data.plugin_name = "scalars"
-  return metadata
+    """Makes a scalar metadata proto, for constructing expected requests."""
+    metadata = summary_pb2.SummaryMetadata(display_name=display_name)
+    metadata.plugin_data.plugin_name = "scalars"
+    return metadata
 
 
 def grpc_error(code, details):
-  # Monkey patch insertion for the methods a real grpc.RpcError would have.
-  error = grpc.RpcError("RPC error %r: %s" % (code, details))
-  error.code = lambda: code
-  error.details = lambda: details
-  return error
+    # Monkey patch insertion for the methods a real grpc.RpcError would have.
+    error = grpc.RpcError("RPC error %r: %s" % (code, details))
+    error.code = lambda: code
+    error.details = lambda: details
+    return error
 
 
 def timestamp_pb(nanos):
-  result = timestamp_pb2.Timestamp()
-  result.FromNanoseconds(nanos)
-  return result
+    result = timestamp_pb2.Timestamp()
+    result.FromNanoseconds(nanos)
+    return result
