2cc8107c21e554ce8189ca7719453f54165fffe5
Shanqing Cai <cais@google.com>
2019-12-18 17:24:25 -0500

    [DebuggerV2] Implement /runs route
    - Add unit tests with dummy tfdbg2 data

--- tensorboard/plugins/debugger_v2/debugger_v2_plugin_test.py
+++ tensorboard/plugins/debugger_v2/debugger_v2_plugin_test.py
@@ -18,26 +18,71 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import json
+import os
+import shutil
 import tempfile
 
 import tensorflow as tf
+from werkzeug import test as werkzeug_test  # pylint: disable=wrong-import-order
+from werkzeug import wrappers
 
+from tensorboard.backend import application
 from tensorboard.plugins import base_plugin
 from tensorboard.plugins.debugger_v2 import debugger_v2_plugin
 
 
+def _generate_tfdbg_v2_data(logdir):
+    writer = tf.debugging.experimental.enable_dump_debug_info(
+        logdir, circular_buffer_size=-1)
+    try:
+        @tf.function
+        def x_plus_y(x, y):
+            return x + y
+
+        x = tf.constant(40.0)
+        y = tf.constant(2.0)
+        x_plus_y(x, y)
+        writer.FlushNonExecutionFiles()
+        writer.FlushExecutionFiles()
+    finally:
+        tf.debugging.experimental.disable_dump_debug_info()
+
+
+_ROUTE_PREFIX = '/data/plugin/debugger-v2'
+
+
 class DebuggerV2PluginTest(tf.test.TestCase):
-    def testInstantiatePlugin(self):
-        dummy_logdir = tempfile.mkdtemp()
-        context = base_plugin.TBContext(logdir=dummy_logdir)
-        plugin = debugger_v2_plugin.DebuggerV2Plugin(context)
-        self.assertTrue(plugin)
+
+    def setUp(self):
+        super(DebuggerV2PluginTest, self).setUp()
+        self.logdir = tempfile.mkdtemp()
+        context = base_plugin.TBContext(logdir=self.logdir)
+        self.plugin = debugger_v2_plugin.DebuggerV2Plugin(context)
+        wsgi_app = application.TensorBoardWSGI([self.plugin])
+        self.server = werkzeug_test.Client(wsgi_app, wrappers.BaseResponse)
+
+    def tearDown(self):
+        if os.path.isdir(self.logdir):
+            shutil.rmtree(self.logdir)
+        super(DebuggerV2PluginTest, self).tearDown()
 
     def testPluginIsNotActiveByDefault(self):
-        dummy_logdir = tempfile.mkdtemp()
-        context = base_plugin.TBContext(logdir=dummy_logdir)
-        plugin = debugger_v2_plugin.DebuggerV2Plugin(context)
-        self.assertFalse(plugin.is_active())
+        self.assertFalse(self.plugin.is_active())
+
+    def testServeRunsWithoutExistingRuns(self):
+        response = self.server.get(_ROUTE_PREFIX + '/runs')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual("application/json", response.headers.get("content-type"))
+        self.assertEqual(json.loads(response.get_data()), [])
+
+    def testServeRunsWithExistingRuns(self):
+        _generate_tfdbg_v2_data(self.logdir)
+        response = self.server.get(_ROUTE_PREFIX + '/runs')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual("application/json", response.headers.get("content-type"))
+        self.assertEqual(json.loads(response.get_data()),
+                         [debugger_v2_plugin.DEFAULT_DEBUGGER_RUN_NAME])
 
 
 if __name__ == "__main__":
