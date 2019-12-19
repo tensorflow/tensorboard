03db1703ba3b739c96c2841b4be86dc29a796577
Shanqing Cai <cais@google.com>
2019-12-18 22:40:40 -0500

    [DebuggerV2] Add inactive component

--- tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/inactive_module.ts
+++ tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/inactive_module.ts
@@ -1,6 +1,4 @@
-<!--
-@license
-Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,8 +11,15 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
+==============================================================================*/
 
-<div>
-  <tf-debugger-v2-inactive></tf-debugger-v2-inactive>
-</div>
+import {NgModule} from '@angular/core';
+
+import {InactiveComponent} from './inactive_component';
+import {InactiveContainer} from './inactive_container';
+
+@NgModule({
+  declarations: [InactiveComponent, InactiveContainer],
+  exports: [InactiveContainer],
+})
+export class InactiveModule {}
