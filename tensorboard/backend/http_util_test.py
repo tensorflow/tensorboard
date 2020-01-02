549e40255de6bd66496b088a78b254859c52bba0
ydko <54426192+ydko@users.noreply.github.com>
2020-01-02 16:12:32 +0900

    csp: allow font, style and script to be served from gstatic.com and googleapis.com

--- tensorboard/backend/http_util_test.py
+++ tensorboard/backend/http_util_test.py
@@ -225,10 +225,11 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=["abcdefghi"]
         )
         expected_csp = (
-            "default-src 'self';font-src 'self';frame-ancestors *;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
-            "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "script-src 'self' 'unsafe-eval' 'sha256-abcdefghi'"
+            "default-src 'self';font-src 'self' https://fonts.gstatic.com;"
+            "frame-ancestors *;frame-src 'self';img-src 'self' data: blob:;"
+            "object-src 'none';style-src 'self' https://www.gstatic.com "
+            "https://fonts.googleapis.com data: 'unsafe-inline';"
+            "script-src 'self' 'unsafe-eval' https://www.gstatic.com 'sha256-abcdefghi'"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
@@ -239,10 +240,11 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=None
         )
         expected_csp = (
-            "default-src 'self';font-src 'self';frame-ancestors *;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
-            "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "script-src 'unsafe-eval'"
+            "default-src 'self';font-src 'self' https://fonts.gstatic.com;"
+            "frame-ancestors *;frame-src 'self';img-src 'self' data: blob:;"
+            "object-src 'none';style-src 'self' https://www.gstatic.com "
+            "https://fonts.googleapis.com data: 'unsafe-inline';"
+            "script-src 'unsafe-eval' https://www.gstatic.com"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
@@ -254,10 +256,11 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=None
         )
         expected_csp = (
-            "default-src 'self';font-src 'self';frame-ancestors *;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
-            "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "script-src 'none'"
+            "default-src 'self';font-src 'self' https://fonts.gstatic.com;"
+            "frame-ancestors *;frame-src 'self';img-src 'self' data: blob:;"
+            "object-src 'none';style-src 'self' https://www.gstatic.com "
+            "https://fonts.googleapis.com data: 'unsafe-inline';"
+            "script-src https://www.gstatic.com"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
@@ -269,10 +272,11 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=None
         )
         expected_csp = (
-            "default-src 'self';font-src 'self';frame-ancestors *;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
-            "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "script-src 'self'"
+            "default-src 'self';font-src 'self' https://fonts.gstatic.com;"
+            "frame-ancestors *;frame-src 'self';img-src 'self' data: blob:;"
+            "object-src 'none';style-src 'self' https://www.gstatic.com "
+            "https://fonts.googleapis.com data: 'unsafe-inline';"
+            "script-src 'self' https://www.gstatic.com"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
@@ -283,10 +287,11 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=["abcdefghi"]
         )
         expected_csp = (
-            "default-src 'self';font-src 'self';frame-ancestors *;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
-            "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "script-src 'self' 'sha256-abcdefghi'"
+            "default-src 'self';font-src 'self' https://fonts.gstatic.com;"
+            "frame-ancestors *;frame-src 'self';img-src 'self' data: blob:;"
+            "object-src 'none';style-src 'self' https://www.gstatic.com "
+            "https://fonts.googleapis.com data: 'unsafe-inline';"
+            "script-src 'self' https://www.gstatic.com 'sha256-abcdefghi'"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
@@ -310,12 +315,13 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=["abcd"]
         )
         expected_csp = (
-            "default-src 'self';font-src 'self';frame-ancestors *;"
-            "frame-src 'self' https://myframe.com;"
-            "img-src 'self' data: blob: https://example.com;"
-            "object-src 'none';style-src 'self' https://www.gstatic.com data: "
-            "'unsafe-inline' https://googol.com;script-src "
-            "https://tensorflow.org/tensorboard 'self' 'unsafe-eval' 'sha256-abcd'"
+            "default-src 'self';font-src 'self' https://fonts.gstatic.com;"
+            "frame-ancestors *;frame-src 'self' https://myframe.com;"
+            "img-src 'self' data: blob: https://example.com;object-src 'none';"
+            "style-src 'self' https://www.gstatic.com https://fonts.googleapis.com "
+            "data: 'unsafe-inline' https://googol.com;script-src "
+            "https://tensorflow.org/tensorboard 'self' 'unsafe-eval' "
+            "https://www.gstatic.com 'sha256-abcd'"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
