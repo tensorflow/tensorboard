673888d7efab552cf630fee1de632782c8c1ab21
Shanqing Cai <cais@google.com>
2020-01-05 15:07:30 -0500

    Flesh out doc string

--- tensorboard/backend/event_processing/io_wrapper.py
+++ tensorboard/backend/event_processing/io_wrapper.py
@@ -54,7 +54,10 @@ def IsTensorFlowEventsFile(path):
       ValueError: If the path is an empty string.
 
     Returns:
-      If path is formatted like a TensorFlowEventsFile.
+      If path is formatted like a TensorFlowEventsFile. Dummy files such as
+        those created with the '.profile-empty' suffixes and meant to hold
+        on `Summary` protos are treated as true TensorFlowEventsFiles. For
+        background, see: https://github.com/tensorflow/tensorboard/issues/2084.
     """
     if not path:
         raise ValueError("Path must be a nonempty string")
@@ -62,7 +65,18 @@ def IsTensorFlowEventsFile(path):
 
 
 def IsSummaryEventsFile(path):
-    """TODO(cais): Doc string."""
+    """Check whether the path is probably a TF Events file containing Summary.
+
+    Args:
+      path: A file path to check if it is an event file containing `Summary`
+        protos.
+
+    Returns:
+      If path is formatted like a TensorFlowEventsFile. Dummy files such as
+        those created with the '.profile-empty' suffixes and meant to hold
+        no `Summary` protos  are treated as `False`. For background, see:
+        https://github.com/tensorflow/tensorboard/issues/2084.
+    """
     return IsTensorFlowEventsFile(path) and not path.endswith(".profile-empty")
 
 
