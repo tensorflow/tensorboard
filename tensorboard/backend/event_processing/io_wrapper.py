5155ecd2829b9258d35b98423f83006954f59d2e
Shanqing Cai <cais@google.com>
2020-01-06 13:48:44 -0500

    [backend] Split IsTensorFlowEventsFile into two separate functions (#3108)
    * Motivation for features / changes
      * Fix https://github.com/tensorflow/tensorboard/issues/2084
      * The approach is suggested by @wchargin and @nfelt in https://github.com/tensorflow/tensorboard/issues/2084#issuecomment-570685175
    
    * Technical description of changes
      * Split the logic of `IsTensorFlowEventsFile()` into two separate functions
        1. An unchanged `IsTensorFlowEventsFile()` function, which only checks the existence of the 'tfevents' substring in the path string.
        2. A new `IsSummaryEventsFile()` function, which returns `True` if and only if  `IsTensorFlowEventsFile()` returns `True` for the input path name *and* the path name does not end in the special suffix `.profile-empty`.
      * This prevents the `EventAccumulator` implementation from picking up the empty events.tfevents.*.profile-empty files, which under the single-event file mode, causes TensorBoard backend to stop reading the latest summaries in the other (i.e., main, non-profiler-generated) events.* file. The *.profile-empty file was designed to make the TensorBoard backend recognize the subfolder created by the Profile plugin as a valid sub logdir, event when it contains no other events files.
    
    * Detailed steps to verify changes work correctly (as executed by you)
      * Added new unit tests
      * Manually verified that #2084 is resolved by running `bazel run -c opt tensorboard -- --logdir /path/to/logdir` using the reproduction code in #2084. 
        * Screenshot: 
    ![image](https://user-images.githubusercontent.com/16824702/71785529-9e127680-2fce-11ea-98e0-b2efecd99880.png)

--- tensorboard/backend/event_processing/io_wrapper.py
+++ tensorboard/backend/event_processing/io_wrapper.py
@@ -54,11 +54,30 @@ def IsTensorFlowEventsFile(path):
       ValueError: If the path is an empty string.
 
     Returns:
-      If path is formatted like a TensorFlowEventsFile.
+      If path is formatted like a TensorFlowEventsFile. Dummy files such as
+        those created with the '.profile-empty' suffixes and meant to hold
+        no `Summary` protos are treated as true TensorFlowEventsFiles. For
+        background, see: https://github.com/tensorflow/tensorboard/issues/2084.
     """
     if not path:
         raise ValueError("Path must be a nonempty string")
     return "tfevents" in tf.compat.as_str_any(os.path.basename(path))
+
+
+def IsSummaryEventsFile(path):
+    """Check whether the path is probably a TF Events file containing Summary.
+
+    Args:
+      path: A file path to check if it is an event file containing `Summary`
+        protos.
+
+    Returns:
+      If path is formatted like a TensorFlowEventsFile. Dummy files such as
+        those created with the '.profile-empty' suffixes and meant to hold
+        no `Summary` protos  are treated as `False`. For background, see:
+        https://github.com/tensorflow/tensorboard/issues/2084.
+    """
+    return IsTensorFlowEventsFile(path) and not path.endswith(".profile-empty")
 
 
 def ListDirectoryAbsolute(directory):
