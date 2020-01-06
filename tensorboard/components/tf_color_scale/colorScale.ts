da0c581ebfcccdf8abdceb53221085f98d25fa1e
ruro <ruro.ruro@ya.ru>
2020-01-06 17:36:13 +0300

    fix yarn lint

--- tensorboard/components/tf_color_scale/colorScale.ts
+++ tensorboard/components/tf_color_scale/colorScale.ts
@@ -60,7 +60,10 @@ namespace tf_color_scale {
       const old_identifiers = this.identifiers;
       this.identifiers = d3.map();
       old_identifiers.each((s, i) => {
-        this.identifiers.set(i, this.palette[Math.floor(Math.random() * this.palette.length)]);
+        this.identifiers.set(
+          i,
+          this.palette[Math.floor(Math.random() * this.palette.length)]
+        );
       });
       return this;
     }
@@ -73,25 +76,30 @@ namespace tf_color_scale {
   function createAutoUpdateColorScale(
     store: tf_backend.BaseStore,
     getDomain: () => string[]
-  ): [(runName: string) => string, () => ({})] {
+  ): [(runName: string) => string, () => {}] {
     const colorScale = new ColorScale();
     function updateRunsColorScale(): void {
       colorScale.setDomain(getDomain());
     }
     store.addListener(updateRunsColorScale);
     updateRunsColorScale();
-    return [(domain) => colorScale.getColor(domain), () => colorScale.reassignColors()];
+    return [
+      (domain) => colorScale.getColor(domain),
+      () => colorScale.reassignColors(),
+    ];
   }
 
-  export const [runsColorScale, runsColorReassign] = createAutoUpdateColorScale(
-    tf_backend.runsStore,
-    () => tf_backend.runsStore.getRuns()
+  export const [
+    runsColorScale,
+    runsColorReassign,
+  ] = createAutoUpdateColorScale(tf_backend.runsStore, () =>
+    tf_backend.runsStore.getRuns()
   );
 
-  export const [experimentsColorScale, experimentsColorReassign] = createAutoUpdateColorScale(
-    tf_backend.experimentsStore,
-    () => {
-      return tf_backend.experimentsStore.getExperiments().map(({name}) => name);
-    }
-  );
+  export const [
+    experimentsColorScale,
+    experimentsColorReassign,
+  ] = createAutoUpdateColorScale(tf_backend.experimentsStore, () => {
+    return tf_backend.experimentsStore.getExperiments().map(({name}) => name);
+  });
 } // tf_color_scale
