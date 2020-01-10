9f1fd29198e3462e772cecb1bb41c319c435dde4
TensorBoard Gardener <tensorboard-gardener@google.com>
2020-01-10 11:42:12 -0800

    Integrate 62fd0a0bf4facb273206f5c155943c8b69a70059

--- tensorboard/plugins/debugger_v2/debugger_v2_plugin.py
+++ tensorboard/plugins/debugger_v2/debugger_v2_plugin.py
@@ -26,6 +26,15 @@ from tensorboard.plugins.debugger_v2 import debug_data_provider
 from tensorboard.backend import http_util
 
 
+def _missing_run_error_response(request):
+    return http_util.Respond(
+        request,
+        {"error": "run parameter is not provided"},
+        "application/json",
+        code=400,
+    )
+
+
 class DebuggerV2Plugin(base_plugin.TBPlugin):
     """Debugger V2 Plugin for TensorBoard."""
 
@@ -50,6 +59,8 @@ class DebuggerV2Plugin(base_plugin.TBPlugin):
         return {
             "/runs": self.serve_runs,
             "/execution/digests": self.serve_execution_digests,
+            "/source_files/list": self.serve_source_files_list,
+            "/source_files/file": self.serve_source_file,
         }
 
     def is_active(self):
@@ -83,12 +94,7 @@ class DebuggerV2Plugin(base_plugin.TBPlugin):
         experiment = plugin_util.experiment_id(request.environ)
         run = request.args.get("run")
         if run is None:
-            return http_util.Respond(
-                request,
-                {"error": "run parameter is not provided"},
-                "application/json",
-                code=400,
-            )
+            return _missing_run_error_response(request)
         begin = int(request.args.get("begin", "0"))
         end = int(request.args.get("end", "-1"))
         run_tag_filter = debug_data_provider.execution_digest_run_tag_filter(
@@ -107,6 +113,75 @@ class DebuggerV2Plugin(base_plugin.TBPlugin):
                 "application/json",
             )
         except (IndexError, ValueError) as e:
+            return http_util.Respond(
+                request, {"error": str(e)}, "application/json", code=400,
+            )
+
+    @wrappers.Request.application
+    def serve_source_files_list(self, request):
+        """Serves a list of all source files involved in the debugged program."""
+        experiment = plugin_util.experiment_id(request.environ)
+        run = request.args.get("run")
+        if run is None:
+            return _missing_run_error_response(request)
+        run_tag_filter = debug_data_provider.source_file_list_run_tag_filter(
+            run
+        )
+        blob_sequences = self._data_provider.read_blob_sequences(
+            experiment, self.plugin_name, run_tag_filter=run_tag_filter
+        )
+        tag = next(iter(run_tag_filter.tags))
+        return http_util.Respond(
+            request,
+            self._data_provider.read_blob(blob_sequences[run][tag][0].blob_key),
+            "application/json",
+        )
+
+    @wrappers.Request.application
+    def serve_source_file(self, request):
+        """Serves the content of a given source file.
+
+        The source file is referred to by the index in the list of all source
+        files involved in the execution of the debugged program, which is
+        available via the `serve_source_files_list()`  serving route.
+
+        Args:
+          request: HTTP request.
+
+        Returns:
+          Response to the request.
+        """
+        experiment = plugin_util.experiment_id(request.environ)
+        run = request.args.get("run")
+        if run is None:
+            return _missing_run_error_response(request)
+        index = request.args.get("index")
+        # TOOD(cais): When the need arises, support serving a subset of a
+        # source file's lines.
+        if index is None:
+            return http_util.Respond(
+                request,
+                {"error": "index is not provided for source file content"},
+                "application/json",
+                code=400,
+            )
+        index = int(index)
+        run_tag_filter = debug_data_provider.source_file_run_tag_filter(
+            run, index
+        )
+        blob_sequences = self._data_provider.read_blob_sequences(
+            experiment, self.plugin_name, run_tag_filter=run_tag_filter
+        )
+        tag = next(iter(run_tag_filter.tags))
+        try:
+            return http_util.Respond(
+                request,
+                self._data_provider.read_blob(
+                    blob_sequences[run][tag][0].blob_key
+                ),
+                "application/json",
+            )
+        except IndexError as e:
             return http_util.Respond(
                 request, {"error": str(e)}, "application/json", code=400,
             )
