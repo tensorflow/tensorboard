cd5fb9b8fb843e2142c0b7ce9d650b55861a774f
William Chargin <wchargin@gmail.com>
2020-01-10 16:16:04 -0800

    data: fix `is_active`s in generic data mode (#3130)
    Summary:
    Now that the TensorBoard backend core marks plugins as enabled if the
    data provider has corresponding data (#3124), plugins can mark
    themselves inactive when they don’t have data, rather than being enabled
    unconditionally in generic data mode.
    
    Test Plan:
    Run TensorBoard against the scalars, images, and histograms demos
    (individually), and note that it behaves the same way with generic data
    mode enabled or disabled. (Note that the histogram demo has no graphs.)
    
    wchargin-branch: data-isactive-false-by-default

--- tensorboard/plugins/scalar/scalars_plugin_test.py
+++ tensorboard/plugins/scalar/scalars_plugin_test.py
@@ -281,25 +281,30 @@ class ScalarsPluginTest(tf.test.TestCase):
 
     @with_runs([_RUN_WITH_LEGACY_SCALARS])
     def test_active_with_legacy_scalars(self, plugin):
-        self.assertTrue(plugin.is_active())
+        if plugin._data_provider:
+            self.assertFalse(plugin.is_active())
+        else:
+            self.assertTrue(plugin.is_active())
 
     @with_runs([_RUN_WITH_SCALARS])
     def test_active_with_scalars(self, plugin):
-        self.assertTrue(plugin.is_active())
+        if plugin._data_provider:
+            self.assertFalse(plugin.is_active())
+        else:
+            self.assertTrue(plugin.is_active())
 
     @with_runs([_RUN_WITH_HISTOGRAM])
     def test_active_with_histogram(self, plugin):
-        if plugin._data_provider:
-            # Hack, for now.
-            self.assertTrue(plugin.is_active())
-        else:
-            self.assertFalse(plugin.is_active())
+        self.assertFalse(plugin.is_active())
 
     @with_runs(
         [_RUN_WITH_LEGACY_SCALARS, _RUN_WITH_SCALARS, _RUN_WITH_HISTOGRAM]
     )
     def test_active_with_all(self, plugin):
-        self.assertTrue(plugin.is_active())
+        if plugin._data_provider:
+            self.assertFalse(plugin.is_active())
+        else:
+            self.assertTrue(plugin.is_active())
 
     @test_util.run_v1_only("Requires contrib for db writer")
     def test_scalars_db_without_exp(self):
