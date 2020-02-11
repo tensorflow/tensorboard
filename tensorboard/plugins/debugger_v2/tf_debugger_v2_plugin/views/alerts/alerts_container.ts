d8411bf7986d25db821624698aceb90a02481813
Shanqing Cai <cais@google.com>
2020-02-11 15:39:54 -0500

    Add tests for AlertsContainer

--- tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/views/alerts/alerts_container.ts
+++ tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/views/alerts/alerts_container.ts
@@ -16,16 +16,23 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {createSelector, select, Store} from '@ngrx/store';
 
 import {getAlertsBreakdown, getNumAlerts, State} from '../../store';
-import {AlertTypeDisplay} from './alerts_component';
 
 /** @typehack */ import * as _typeHackRxjs from 'rxjs';
 
 const ALERT_TYPE_TO_DISPLAY_NAME_AND_SYMBOL: {
   [alertType: string]: {displayName: string; displaySymbol: string};
 } = {
+  FunctionRecompilesAlert: {
+    displayName: 'Function recompiles',
+    displaySymbol: 'C',
+  },
   InfNanAlert: {
     displayName: 'NaN/∞',
     displaySymbol: '∞',
+  },
+  TensorShapeAlert: {
+    displayName: 'Tensor shape',
+    displaySymbol: '■',
   },
 };
 
@@ -49,6 +56,7 @@ export class AlertsContainer {
         getAlertsBreakdown,
         (alertsBreakdown) => {
           const alertTypes = Object.keys(alertsBreakdown);
+          alertTypes.sort();
           return alertTypes.map((alertType) => {
             return {
               ...ALERT_TYPE_TO_DISPLAY_NAME_AND_SYMBOL[alertType],
