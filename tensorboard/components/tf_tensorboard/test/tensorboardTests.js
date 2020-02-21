b92233ed91a5b75cf24012a7f2045a0efb106e88
TensorBoard Gardener <tensorboard-gardener@google.com>
2020-02-21 18:22:52 +0000

    Integrate 62745f781a5f3f996ee96a70c367ae417139e38e

--- tensorboard/components/tf_tensorboard/test/tensorboardTests.js
+++ tensorboard/components/tf_tensorboard/test/tensorboardTests.js
@@ -24,77 +24,139 @@ var tf_tensorboard;
     describe('tf-tensorboard tests', () => {
         window.HTMLImports.whenReady(() => {
             let tensorboard;
-            beforeEach(function () {
-                tensorboard = fixture('tensorboardFixture');
-                tensorboard.demoDir = 'data';
-                tensorboard.autoReloadEnabled = false;
-            });
-            it('renders injected content', function () {
-                const overview = tensorboard.querySelector('#custom-overview');
-                const contentPane = tensorboard.$$('#content-pane');
-                checkSlottedUnderAncestor(overview, contentPane);
-                const headerItem1 = tensorboard.querySelector('#custom-header-item1');
-                const headerItem2 = tensorboard.querySelector('#custom-header-item2');
-                const header = tensorboard.$$('.header');
-                checkSlottedUnderAncestor(headerItem1, header);
-                checkSlottedUnderAncestor(headerItem2, header);
-            });
-            // TODO(psybuzz): Restore/remove these old tests, which fail due to broken
-            // DOM ids that changed. Previously this folder's tests did not run.
-            xit('reloads the active dashboard on request', (done) => {
-                tensorboard.$.tabs.set('selected', 'scalars');
-                setTimeout(() => {
-                    let called = false;
-                    tensorboard._selectedDashboardComponent().reload = () => {
-                        called = true;
-                    };
-                    tensorboard.reload();
-                    chai.assert.isTrue(called, 'reload was called');
-                    done();
+            describe('base tests', () => {
+                beforeEach((done) => {
+                    tensorboard = fixture('tensorboardFixture');
+                    tensorboard.demoDir = 'data';
+                    tensorboard.autoReloadEnabled = false;
+                    flush(done);
                 });
-            });
-            // TODO(psybuzz): Restore/remove these old tests, which fail due to broken
-            // DOM ids that changed. Previously this folder's tests did not run.
-            xdescribe('top right global icons', function () {
-                it('Clicking the reload button will call reload', function () {
-                    let called = false;
-                    tensorboard.reload = function () {
-                        called = true;
-                    };
-                    tensorboard.$$('#reload-button').click();
-                    chai.assert.isTrue(called);
+                it('renders injected content', function () {
+                    const overview = tensorboard.querySelector('#custom-overview');
+                    const contentPane = tensorboard.$$('#content-pane');
+                    checkSlottedUnderAncestor(overview, contentPane);
+                    const headerItem1 = tensorboard.querySelector('#custom-header-item1');
+                    const headerItem2 = tensorboard.querySelector('#custom-header-item2');
+                    const header = tensorboard.$$('.header');
+                    checkSlottedUnderAncestor(headerItem1, header);
+                    checkSlottedUnderAncestor(headerItem2, header);
                 });
-                it('settings pane is hidden', function () {
-                    chai.assert.equal(tensorboard.$.settings.style['display'], 'none');
+                it('uses "TensorBoard-X" for title text by default', () => {
+                    const title = tensorboard.shadowRoot.querySelector('.toolbar-title');
+                    chai.assert.equal(title.textContent, 'TensorBoard-X');
                 });
-                it('settings icon button opens the settings pane', function (done) {
-                    tensorboard.$$('#settings-button').click();
-                    // This test is a little hacky since we depend on polymer's
-                    // async behavior, which is difficult to predict.
-                    // keep checking until the panel is visible. error with a timeout if it
-                    // is broken.
-                    function verify() {
-                        if (tensorboard.$.settings.style['display'] !== 'none') {
-                            done();
+                it('uses div for title element by default ', () => {
+                    const title = tensorboard.shadowRoot.querySelector('.toolbar-title');
+                    chai.assert.equal(title.nodeName, 'DIV');
+                    chai.assert.isUndefined(title.href);
+                });
+                // TODO(psybuzz): Restore/remove these old tests, which fail due to broken
+                // DOM ids that changed. Previously this folder's tests did not run.
+                xit('reloads the active dashboard on request', (done) => {
+                    tensorboard.$.tabs.set('selected', 'scalars');
+                    setTimeout(() => {
+                        let called = false;
+                        tensorboard._selectedDashboardComponent().reload = () => {
+                            called = true;
+                        };
+                        tensorboard.reload();
+                        chai.assert.isTrue(called, 'reload was called');
+                        done();
+                    });
+                });
+                // TODO(psybuzz): Restore/remove these old tests, which fail due to broken
+                // DOM ids that changed. Previously this folder's tests did not run.
+                xdescribe('top right global icons', function () {
+                    it('Clicking the reload button will call reload', function () {
+                        let called = false;
+                        tensorboard.reload = function () {
+                            called = true;
+                        };
+                        tensorboard.$$('#reload-button').click();
+                        chai.assert.isTrue(called);
+                    });
+                    it('settings pane is hidden', function () {
+                        chai.assert.equal(tensorboard.$.settings.style['display'], 'none');
+                    });
+                    it('settings icon button opens the settings pane', function (done) {
+                        tensorboard.$$('#settings-button').click();
+                        // This test is a little hacky since we depend on polymer's
+                        // async behavior, which is difficult to predict.
+                        // keep checking until the panel is visible. error with a timeout if it
+                        // is broken.
+                        function verify() {
+                            if (tensorboard.$.settings.style['display'] !== 'none') {
+                                done();
+                            }
+                            else {
+                                setTimeout(verify, 3); // wait and see if it becomes true
+                            }
                         }
-                        else {
-                            setTimeout(verify, 3); // wait and see if it becomes true
-                        }
-                    }
-                    verify();
+                        verify();
+                    });
+                    it('Autoreload checkbox toggle works', function () {
+                        let checkbox = tensorboard.$$('#auto-reload-checkbox');
+                        chai.assert.equal(checkbox.checked, tensorboard.autoReloadEnabled);
+                        let oldValue = checkbox.checked;
+                        checkbox.click();
+                        chai.assert.notEqual(oldValue, checkbox.checked);
+                        chai.assert.equal(checkbox.checked, tensorboard.autoReloadEnabled);
+                    });
+                    it('Autoreload checkbox contains correct interval info', function () {
+                        let checkbox = tensorboard.$$('#auto-reload-checkbox');
+                        let timeInSeconds = tensorboard.autoReloadIntervalSecs + 's';
+                        chai.assert.include(checkbox.innerText, timeInSeconds);
+                    });
                 });
-                it('Autoreload checkbox toggle works', function () {
-                    let checkbox = tensorboard.$$('#auto-reload-checkbox');
-                    chai.assert.equal(checkbox.checked, tensorboard.autoReloadEnabled);
-                    let oldValue = checkbox.checked;
-                    checkbox.click();
-                    chai.assert.notEqual(oldValue, checkbox.checked);
-                    chai.assert.equal(checkbox.checked, tensorboard.autoReloadEnabled);
+            });
+            describe('custom path', () => {
+                let sandbox;
+                beforeEach((done) => {
+                    sandbox = sinon.sandbox.create();
+                    sandbox.stub(TEST_ONLY.lib, 'getLocation').returns({
+                        href: 'https://tensorboard.is/cool',
+                        origin: 'https://tensorboard.is',
+                    });
+                    tensorboard = fixture('tensorboardFixture');
+                    tensorboard.demoDir = 'data';
+                    tensorboard.brand = 'Custom Brand';
+                    tensorboard.autoReloadEnabled = false;
+                    tensorboard.homePath = '/awesome';
+                    // Branding and other components use `dom-if` which updates the dom in an
+                    // animation frame. Flush and remove the asynchronicity.
+                    flush(done);
                 });
-                it('Autoreload checkbox contains correct interval info', function () {
-                    let checkbox = tensorboard.$$('#auto-reload-checkbox');
-                    let timeInSeconds = tensorboard.autoReloadIntervalSecs + 's';
-                    chai.assert.include(checkbox.innerText, timeInSeconds);
+                afterEach(() => {
+                    sandbox.restore();
+                });
+                it('uses customized brand for title', () => {
+                    const title = tensorboard.shadowRoot.querySelector('.toolbar-title');
+                    chai.assert.equal(title.textContent, 'Custom Brand');
+                });
+                it('uses customized path for title element ', () => {
+                    const title = tensorboard.shadowRoot.querySelector('.toolbar-title');
+                    chai.assert.equal(title.nodeName, 'A');
+                    chai.assert.equal(title.href, 'https://tensorboard.is/awesome');
+                });
+                it('throws when homePath is one of bad wrong protocols', () => {
+                    const expectedError1 = "Expect 'homePath' to be of http: or https:. " +
+                        'javascript:alert("PWNED!")';
+                    chai.assert.throws(() => {
+                        tensorboard.homePath = 'javascript:alert("PWNED!")';
+                    }, expectedError1);
+                    const expectedError2 = "Expect 'homePath' to be of http: or https:. " +
+                        'data:text/html,<img src="HEHE" onerror="alert(\'PWNED!\')" />';
+                    chai.assert.throws(() => {
+                        tensorboard.homePath =
+                            'data:text/html,<img src="HEHE" onerror="alert(\'PWNED!\')" />';
+                    }, expectedError2);
+                });
+                it('throws when homePath is not a path', () => {
+                    const expectedError1 = "Expect 'homePath' be a path or have the same origin. " +
+                        'https://tensorboard.was/good vs. https://tensorboard.is';
+                    chai.assert.throws(() => {
+                        tensorboard.homePath = 'https://tensorboard.was/good';
+                    }, expectedError1);
                 });
             });
         });
