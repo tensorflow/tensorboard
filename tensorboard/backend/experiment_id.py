9ebf3abf7a1a69aeba4cde37b25dd5e78bc0e701
Stephan Lee <stephanwlee@gmail.com>
2020-02-24 09:44:32 -0800

    Update the doc

--- tensorboard/backend/experiment_id.py
+++ tensorboard/backend/experiment_id.py
@@ -36,7 +36,8 @@ class ExperimentIdMiddleware(object):
     its first two path components stripped, and its experiment ID stored
     onto the WSGI environment with key taken from the `WSGI_ENVIRON_KEY`
     constant. All other requests will have paths unchanged and the
-    experiment ID set to the empty string.
+    experiment ID set to the empty string. It noops if the key taken from
+    the `WSGI_ENVIRON_KEY` is already present in the environment.
 
     Instances of this class are WSGI applications (see PEP 3333).
     """
