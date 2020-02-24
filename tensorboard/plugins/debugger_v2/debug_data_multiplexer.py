2030ffb07ae5d5c63349c772e2a2efa00fc26f7b
Shanqing Cai <cais@google.com>
2020-02-24 16:41:32 -0500

    Fix test

--- tensorboard/plugins/debugger_v2/debug_data_multiplexer.py
+++ tensorboard/plugins/debugger_v2/debug_data_multiplexer.py
@@ -235,7 +235,7 @@ class DebuggerV2EventMultiplexer(object):
                 alert_type = "__MiscellaneousAlert__"
             alerts_breakdown[alert_type] = len(monitor_alerts)
             alerts_by_type[alert_type] = monitor_alerts
-
+        num_alerts = len(alerts)
         if alert_type_filter is not None:
             if alert_type_filter not in alerts_breakdown:
                 raise errors.InvalidArgumentError(
@@ -243,16 +243,15 @@ class DebuggerV2EventMultiplexer(object):
                     % alert_type_filter
                 )
             alerts = alerts_by_type[alert_type_filter]
-        end = self._checkBeginEndIndices(begin, end, alerts)
-        alerts = alerts[begin:end]
+        end = self._checkBeginEndIndices(begin, end, len(alerts))
         return {
             "begin": begin,
             "end": end,
             "alert_type": alert_type_filter,
-            "num_alerts": len(alerts),
+            "num_alerts": num_alerts,
             "alerts_breakdown": alerts_breakdown,
             "per_type_alert_limit": DEFAULT_PER_TYPE_ALERT_LIMIT,
-            "alerts": [_alert_to_json(alert) for alert in alerts],
+            "alerts": [_alert_to_json(alert) for alert in alerts[begin:end]],
         }
 
     def ExecutionDigests(self, run, begin, end):
