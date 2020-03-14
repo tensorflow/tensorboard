4635de923a63247bf4b7e682ab8224634a842a92
Shanqing Cai <cais@google.com>
2020-03-14 15:55:21 -0400

    Move toward adding a unit test

--- tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/views/source_code/load_monaco_shim.ts
+++ tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/views/source_code/load_monaco_shim.ts
@@ -1,27 +1,41 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+/** Shim for the `loadMonaco()` function in different build environments. */
+
+// TODO(cais): Explore better typing by depending on external libraries.
 declare const require: any;
+const windowWithMonaco: {[key: string]: unknown} = window;
 
 const VS_PATH_PREFIX = 'vs';
 const VS_IMPORT_PATH = '/tf-imports/vs';
 
 export function loadMonaco(): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    if ((window as any)['monaco']) {
-      console.log('20:');  // DEBUG
+  return new Promise<void>((resolve) => {
+    if (windowWithMonaco['monaco']) {
       resolve();
     }
 
     require.config({
       paths: {
-        [VS_PATH_PREFIX]: VS_IMPORT_PATH
+        [VS_PATH_PREFIX]: VS_IMPORT_PATH,
       },
       catchError: true,
     });
 
-    require([`${VS_PATH_PREFIX}/editor/editor.main`], function (monaco: any) {
+    require([`${VS_PATH_PREFIX}/editor/editor.main`], () => {
       require([`${VS_PATH_PREFIX}/python/python.contribution`], () => {
-        console.log('100: monaco=', monaco);  // DEBUG
-        console.log('100: window.monaco:', (window as any)["monaco"]);  // DEBUG
         resolve();
       });
     });
