343456b5768f290b9d8bc8483fec954c0074f4b7
Brian Dubois <bdubois@google.com>
2020-03-17 12:48:54 -0400

    Add --plugins option to uploader (#3377)
    Allow uploader users to specify the plugins for which data should be uploaded. It has two-fold purpose:
    (1) Allow users to specify "experimental" plugins that would otherwise not be uploaded.
    (2) Allow users to list a subset of launched plugins, if they do not want to upload all launched plugin data.
    
    ===
    
    Technical description of changes
    
    Add "--plugins" command line option for upload subcommand. The information is sent in the ServerInfoRequest (aka the handshake) and may impact the list of plugins returned in ServerInfoResponse.plugin_control.allowed_plugins.
    
    Sample usage:
    tensorboard dev upload --logdir --plugins scalars graphs histograms
    
    Outside of these specific changes:
    
    It's expected that supported servers will evaluate the list of plugins and decide whether it is valid. If valid, the server will respond with the entire list of plugins or perhaps a sublist. If the list is invalid then it will respond with a CompatibilityVerdict of VERDICT_ERROR and a useful detail message to print to console.
    
    If --plugins is not specified then the server is expected to respond with a default list of plugins to upload.

--- tensorboard/uploader/uploader_main.py
+++ tensorboard/uploader/uploader_main.py
@@ -159,6 +159,16 @@ def _define_flags(parser):
         help="Experiment description. Markdown format.  Max 600 characters.",
     )
 
+    upload.add_argument(
+        "--plugins",
+        type=str,
+        nargs="*",
+        default=[],
+        help="List of plugins for which data should be uploaded. If "
+        "unspecified then data will be uploaded for all plugins supported by "
+        "the server.",
+    )
+
     update_metadata = subparsers.add_parser(
         "update-metadata",
         help="change the name, description, or other user "
@@ -734,8 +744,10 @@ def _get_intent(flags):
 def _get_server_info(flags):
     origin = flags.origin or _DEFAULT_ORIGIN
     if flags.api_endpoint and not flags.origin:
-        return server_info_lib.create_server_info(origin, flags.api_endpoint)
-    server_info = server_info_lib.fetch_server_info(origin)
+        return server_info_lib.create_server_info(
+            origin, flags.api_endpoint, flags.plugins
+        )
+    server_info = server_info_lib.fetch_server_info(origin, flags.plugins)
     # Override with any API server explicitly specified on the command
     # line, but only if the server accepted our initial handshake.
     if flags.api_endpoint and server_info.api_server.endpoint:
