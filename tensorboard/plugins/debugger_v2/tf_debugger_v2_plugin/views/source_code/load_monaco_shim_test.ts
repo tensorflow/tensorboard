f89ed4765200dc27899e70e29836909b2354de5d
Shanqing Cai <cais@google.com>
2020-03-17 15:25:14 -0400

    Remove unused import

--- tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/views/source_code/load_monaco_shim_test.ts
+++ tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/views/source_code/load_monaco_shim_test.ts
@@ -13,11 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-import {
-  loadMonaco,
-  WindowWithRequireAndMonaco,
-  windowWithRequireAndMonaco,
-} from './load_monaco_shim';
+import {loadMonaco, windowWithRequireAndMonaco} from './load_monaco_shim';
 
 describe('loadMonaco shim', () => {
   function createFakeRequire(): Require {
