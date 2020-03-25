e6ad7e2905609588f72e839e0fae9619b61fefc8
Stephan Lee <stephanwlee@gmail.com>
2020-03-25 12:39:32 -0700

    comment fix

--- tensorboard/webapp/app_state.ts
+++ tensorboard/webapp/app_state.ts
@@ -16,7 +16,7 @@ limitations under the License.
 import {State as CoreState} from './core/store/core_types';
 import {State as FeatureFlagState} from './feature_flag/store/feature_flag_types';
 
-// Allows partial features in a given state.
 export type State = CoreState & FeatureFlagState;
 
+// Allows partial features in tests.
 export type StateForTesting = Partial<State>;
