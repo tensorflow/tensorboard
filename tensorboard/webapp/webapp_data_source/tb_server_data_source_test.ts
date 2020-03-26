68d2f812336ee9815d98c6bdf0e8767ba7c791fe
Stephan Lee <stephanwlee@gmail.com>
2020-03-26 10:20:43 -0700

    ng: conditionally enable debugger_v2 (#3431)
    Using the new experimentPlugin mechanism (#3344), we will control
    whether to show a plugin or not.
    
    Please use
    `http://localhost:6006/ng_index.html?experimentalPlugin=debugger-v2` to
    see the debugger-v2 in the future.

---
+++ tensorboard/webapp/webapp_data_source/tb_server_data_source_test.ts
@@ -0,0 +1,54 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+import {TestBed} from '@angular/core/testing';
+
+import {TBServerDataSource} from './tb_server_data_source';
+import {
+  TBHttpClientTestingModule,
+  HttpTestingController,
+} from './tb_http_client_testing';
+
+describe('tb_server_data_source', () => {
+  describe('TBServerDataSource', () => {
+    let dataSource: TBServerDataSource;
+    let httpMock: HttpTestingController;
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [TBHttpClientTestingModule],
+        providers: [TBServerDataSource],
+      }).compileComponents();
+
+      httpMock = TestBed.get(HttpTestingController);
+      dataSource = TestBed.get(TBServerDataSource);
+    });
+
+    describe('fetchPluginsListing', () => {
+      it('fetches from "data/plugins_listing"', () => {
+        dataSource.fetchPluginsListing([]).subscribe(jasmine.createSpy());
+        httpMock.expectOne('data/plugins_listing');
+      });
+
+      it('passes query parameter, "experimentalPlugin"', () => {
+        dataSource
+          .fetchPluginsListing(['foo', 'bar'])
+          .subscribe(jasmine.createSpy());
+        httpMock.expectOne(
+          'data/plugins_listing?experimentalPlugin=foo&experimentalPlugin=bar'
+        );
+      });
+    });
+  });
+});
