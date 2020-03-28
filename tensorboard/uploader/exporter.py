34530f4789647a81687cc2983866820096b831b1
Shanqing Cai <cais@google.com>
2020-03-27 22:04:20 -0400

    uploader: add blob exporting (#3410)
    * Motivation for features / changes
      * Support exporting of blob data from tensorboard.dev uploader
    * Technical description of changes
      * In exporter.py, branch the handling logic for `StreamExperimentDataResponse` protos
        depending on whether
        * `points` is available (for scalars), or
        * `blob_sequences` is available (for blob sequences)
      * For the blob sequences, add method to download the blob via rpc in chunks.
    * Detailed steps to verify changes work correctly (as executed by you)
      * Unit tests added, see exporter_test.py
      * Manual testing against a locally running backend (cross reference with CL/301719321)

--- tensorboard/uploader/exporter.py
+++ tensorboard/uploader/exporter.py
@@ -19,6 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 import base64
+import contextlib
 import errno
 import grpc
 import json
@@ -28,6 +29,7 @@ import time
 
 import six
 
+from tensorboard.uploader.proto import blob_pb2
 from tensorboard.uploader.proto import experiment_pb2
 from tensorboard.uploader.proto import export_service_pb2
 from tensorboard.uploader import util
@@ -50,6 +52,15 @@ _MAX_INT64 = 2 ** 63 - 1
 _FILENAME_METADATA = "metadata.json"
 # Output filename for scalar data within an experiment directory.
 _FILENAME_SCALARS = "scalars.json"
+# Output filename for blob sequences data within an experiment directory.
+# This file does not contain the actual contents of the blobs. Instead,
+# it holds `blob_file_path`s that point to the binary files that contain
+# the blobs' contents, in addition to other metadata such as run and tag names.
+_FILENAME_BLOB_SEQUENCES = "blob_sequences.json"
+
+_DIRNAME_BLOBS = "blobs"
+_FILENAME_BLOBS_PREFIX = "blob_"
+_FILENAME_BLOBS_SUFFIX = ".bin"
 
 logger = tb_logging.get_logger()
 
@@ -147,23 +158,51 @@ class TensorBoardExporter(object):
                 json.dump(experiment_metadata, outfile, sort_keys=True)
                 outfile.write("\n")
 
-            scalars_filepath = os.path.join(experiment_dir, _FILENAME_SCALARS)
             try:
-                with _open_excl(scalars_filepath) as outfile:
-                    data = self._request_scalar_data(experiment_id, read_time)
-                    for block in data:
+                data = self._request_json_data(experiment_id, read_time)
+                with contextlib.ExitStack() as stack:
+                    file_handles = {
+                        filename: stack.enter_context(
+                            _open_excl(os.path.join(experiment_dir, filename))
+                        )
+                        for filename in (
+                            _FILENAME_SCALARS,
+                            _FILENAME_BLOB_SEQUENCES,
+                        )
+                    }
+                    os.mkdir(os.path.join(experiment_dir, _DIRNAME_BLOBS))
+                    for block, filename in data:
+                        outfile = file_handles[filename]
                         json.dump(block, outfile, sort_keys=True)
                         outfile.write("\n")
                         outfile.flush()
-                yield experiment_id
+                    yield experiment_id
             except grpc.RpcError as e:
                 if e.code() == grpc.StatusCode.CANCELLED:
                     raise GrpcTimeoutException(experiment_id)
                 else:
                     raise
 
-    def _request_scalar_data(self, experiment_id, read_time):
-        """Yields JSON-serializable blocks of scalar data."""
+    def _request_json_data(self, experiment_id, read_time):
+        """Given experiment id, generates JSON data and destination file name.
+
+        The JSON data describes the run, tag, metadata, in addition to
+          - Actual data in the case of scalars
+          - Pointer to binary files in the case of blob sequences.
+
+        For the case of blob sequences, this method has the side effect of
+          downloading the contents of the blobs and writing them to files in
+          a subdirectory of the experiment directory.
+
+        Args:
+          experiment_id: The id of the experiment to request data for.
+          read_time: A fixed timestamp from which to export data, as float
+            seconds since epoch (like `time.time()`). Optional; defaults to the
+            current time.
+
+        Yields:
+          (JSON-serializable data, destination file name) tuples.
+        """
         request = export_service_pb2.StreamExperimentDataRequest()
         request.experiment_id = experiment_id
         util.set_timestamp(request.read_timestamp, read_time)
@@ -180,19 +219,126 @@ class TensorBoardExporter(object):
             metadata = base64.b64encode(
                 response.tag_metadata.SerializeToString()
             ).decode("ascii")
-            wall_times = [
-                t.ToNanoseconds() / 1e9 for t in response.points.wall_times
-            ]
-            yield {
+            json_data = {
                 u"run": response.run_name,
                 u"tag": response.tag_name,
                 u"summary_metadata": metadata,
-                u"points": {
-                    u"steps": list(response.points.steps),
-                    u"wall_times": wall_times,
-                    u"values": list(response.points.values),
-                },
             }
+            filename = None
+            if response.HasField("points"):
+                json_data[u"points"] = self._process_scalar_points(
+                    response.points
+                )
+                filename = _FILENAME_SCALARS
+            elif response.HasField("blob_sequences"):
+                json_data[u"points"] = self._process_blob_sequence_points(
+                    response.blob_sequences, experiment_id
+                )
+                filename = _FILENAME_BLOB_SEQUENCES
+            if filename:
+                yield json_data, filename
+            else:
+                logger.warning(
+                    "Skipping a response from experiment-data stream "
+                    "due to the lack of any valid data type (such as "
+                    "scalars and blob sequences): run=%s, tag=%s. "
+                    "Updating tensorboard install may fix this warning."
+                    % (response.run_name, response.tag_name)
+                )
+
+    def _process_scalar_points(self, points):
+        """Process scalar data points.
+
+        Args:
+          points: `export_service_pb2.StreamExperimentDataResponse.ScalarPoints`
+            proto.
+
+        Returns:
+          A JSON-serializable `dict` for the steps, wall_times and values of the
+            scalar data points.
+        """
+        wall_times = [t.ToNanoseconds() / 1e9 for t in points.wall_times]
+        return {
+            u"steps": list(points.steps),
+            u"wall_times": wall_times,
+            u"values": list(points.values),
+        }
+
+    def _process_blob_sequence_points(self, blob_sequences, experiment_id):
+        """Process blob sequence points.
+
+        As a side effect, also downloads the binary contents of the blobs
+        to respective files. The paths to the files relative to the
+        experiment directory is encapsulated in the returned JSON object.
+
+        Args:
+          blob_sequences:
+            `export_service_pb2.StreamDataResponse.BlobSequencePoints` proto.
+
+        Returns:
+          A JSON-serializable `dict` for the steps and wall_times, as well as
+            the blob_file_paths, which are the relative paths to the downloaded
+            blob contents.
+        """
+        wall_times = [
+            t.ToNanoseconds() / 1e9 for t in blob_sequences.wall_times
+        ]
+        json_object = {
+            u"steps": list(blob_sequences.steps),
+            u"wall_times": wall_times,
+            u"blob_file_paths": [],
+        }
+        blob_file_paths = json_object[u"blob_file_paths"]
+        for blobseq in blob_sequences.values:
+            seq_blob_file_paths = []
+            for entry in blobseq.entries:
+                if entry.blob.state == blob_pb2.BlobState.BLOB_STATE_CURRENT:
+                    blob_path = self._download_blob(
+                        entry.blob.blob_id, experiment_id
+                    )
+                    seq_blob_file_paths.append(blob_path)
+                else:
+                    seq_blob_file_paths.append(None)
+            blob_file_paths.append(seq_blob_file_paths)
+        return json_object
+
+    def _download_blob(self, blob_id, experiment_id):
+        """Download the blob via rpc.
+
+        Args:
+          blob_id: Id of the blob.
+          experiment_id: Id of the experiment that the blob belongs to.
+
+        Returns:
+          If the blob is downloaded successfully:
+            The path of the downloaded blob file relative to the experiment
+            directory.
+          Else:
+            `None`.
+        """
+        # TODO(cais): Deduplicate with internal method perhaps.
+        experiment_dir = _experiment_directory(self._outdir, experiment_id)
+        request = export_service_pb2.StreamBlobDataRequest(blob_id=blob_id)
+        blob_abspath = os.path.join(
+            experiment_dir,
+            _DIRNAME_BLOBS,
+            _FILENAME_BLOBS_PREFIX + blob_id + _FILENAME_BLOBS_SUFFIX,
+        )
+        with _open_excl(blob_abspath, "wb") as f:
+            try:
+                for response in self._api.StreamBlobData(
+                    request, metadata=grpc_util.version_metadata()
+                ):
+                    # TODO(cais, soergel): validate the various response fields
+                    f.write(response.data)
+            except grpc.RpcError as rpc_error:
+                logger.error(
+                    "Omitting blob (id: %s) due to download failure: %s",
+                    blob_id,
+                    rpc_error,
+                )
+                return None
+        return os.path.relpath(blob_abspath, experiment_dir)
 
 
 def list_experiments(api_client, fieldmask=None, read_time=None):
@@ -276,8 +422,8 @@ def _mkdir_p(path):
             raise
 
 
-def _open_excl(path):
-    """Like `open(path, "x")`, but Python 2-compatible."""
+def _open_excl(path, mode="w"):
+    """Like `open(path, mode.replace("w", "x"))`, but Python 2-compatible."""
     try:
         # `os.O_EXCL` works on Windows as well as POSIX-compliant systems.
         # See: <https://bugs.python.org/issue12760>
@@ -287,4 +433,4 @@ def _open_excl(path):
             raise OutputFileExistsError(path)
         else:
             raise
-    return os.fdopen(fd, "w")
+    return os.fdopen(fd, mode)
