5d7bcda54e0453441d2ac507fe298997c210a02f
Nick Felt <nickfelt@google.com>
2020-03-30 14:09:59 -0700

    fixup: forgot to rename one instance of run_to_tag_to_content

--- tensorboard/plugins/hparams/backend_context.py
+++ tensorboard/plugins/hparams/backend_context.py
@@ -295,11 +295,13 @@ class Context(object):
 
         return result
 
-    def _compute_metric_infos(self, experiment_id, run_to_tag_to_content):
+    def _compute_metric_infos(
+        self, experiment_id, hparams_run_to_tag_to_content
+    ):
         return (
             api_pb2.MetricInfo(name=api_pb2.MetricName(group=group, tag=tag))
             for tag, group in self._compute_metric_names(
-                experiment_id, run_to_tag_to_content
+                experiment_id, hparams_run_to_tag_to_content
             )
         )
 
