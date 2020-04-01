4d3ff3e3314aa8bcc8029bac9c08a97fec189e77
ericdnielsen <ericdnielsen@gmail.com>
2020-04-01 10:00:32 -0400

    Include the size of the blob in the WriteBlobRequest (#3457)
    * Report the final size of a blob when its uploaded to allow for
    quota allocation and enforcement.

--- tensorboard/uploader/uploader.py
+++ tensorboard/uploader/uploader.py
@@ -763,6 +763,7 @@ class _BlobRequestSender(object):
                 crc32c=None,
                 finalize_object=finalize_object,
                 final_crc32c=None,
+                blob_bytes=len(blob),
             )
             yield request
 
