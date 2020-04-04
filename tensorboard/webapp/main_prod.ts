90789895bd9019e3aecf4bfa7a7c862d6211f648
Stephan Lee <stephanwlee@gmail.com>
2020-04-03 23:38:02 -0700

    ng: remove config and use enableProdMode (#3477)
    Our config_dev.ts and config_prod.ts required the esmodules to be loaded
    in very specific order which is generally not the best assumption to
    have. In fact, JSCompiler can reorder to imports and can cause
    application to break. This change removes the config.env all together in
    favor of Angular's `enableProdMode` which can later be queried with
    `isDevMode()`.

--- tensorboard/webapp/main_prod.ts
+++ tensorboard/webapp/main_prod.ts
@@ -12,6 +12,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-// HACK: must load config before application module.
-import './config_prod';
+import {enableProdMode} from '@angular/core';
+
 import './bootstrap';
+
+enableProdMode();
