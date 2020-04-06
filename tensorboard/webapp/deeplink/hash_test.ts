f6a7ba36b57464801e1965c1a2b2f3c8a31db74f
Stephan Lee <stephanwlee@gmail.com>
2020-04-06 12:03:50 -0700

    ng: bootstrap core store from hash (#3475)
    Note that this introduces deeplink module that manages everything
    related to deeplinking.
    
    Deeplinking, whether we have a router or not, is a feature want to
    support. Since most of the deeplink states are present in the hash, the
    module is responsible for read/write with the hash directly.
    
    hash_storage module should be part of the deep link module but since it
    is already being used internally, we will have to make a careful changes
    while not breaking any apps. The change will be made in a follow up.

--- tensorboard/webapp/deeplink/hash_test.ts
+++ tensorboard/webapp/deeplink/hash_test.ts
@@ -1,4 +1,4 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,62 +13,55 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 import {TestBed} from '@angular/core/testing';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {Store} from '@ngrx/store';
-import {provideMockStore, MockStore} from '@ngrx/store/testing';
-import {CommonModule} from '@angular/common';
 
-import {getActivePlugin} from '../store';
-import {State} from '../store/core_types';
-import {pluginUrlHashChanged} from '../actions';
-
-import {HashStorageContainer} from './hash_storage_container';
-import {HashStorageComponent} from './hash_storage_component';
-import {HashDeepLinker, DeepLinkerInterface} from '../../deeplink';
+import {HashStorageComponent} from './hash';
 
 /** @typehack */ import * as _typeHackStore from '@ngrx/store';
 /** @typehack */ import * as _typeHackStoreTesting from '@ngrx/store/testing';
 
-class TestableDeeplinker implements DeepLinkerInterface {
-  getString(key: string) {
-    return key;
-  }
-  setString(key: string, value: string) {}
-  getPluginId() {
-    return 'plugin';
-  }
-  setPluginId(pluginId: string) {}
-}
-
 describe('hash storage test', () => {
   let store: MockStore<State>;
   let dispatchSpy: jasmine.Spy;
-  let setPluginIdSpy: jasmine.Spy;
-  let getPluginIdSpy: jasmine.Spy;
+  let setStringSpy: jasmine.Spy;
+  let getStringSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NoopAnimationsModule, CommonModule],
-      providers: [
-        provideMockStore(),
-        HashStorageContainer,
-        {provide: HashDeepLinker, useClass: TestableDeeplinker},
-      ],
+      providers: [provideMockStore(), HashStorageContainer],
       declarations: [HashStorageContainer, HashStorageComponent],
     }).compileComponents();
     store = TestBed.get(Store);
     dispatchSpy = spyOn(store, 'dispatch');
 
-    const deepLinker = TestBed.get(HashDeepLinker);
-    setPluginIdSpy = spyOn(deepLinker, 'setPluginId');
-    getPluginIdSpy = spyOn(deepLinker, 'getPluginId');
+    setStringSpy = jasmine.createSpy();
+    getStringSpy = jasmine.createSpy();
+
+    // Cannot safely stub out window.location.hash or rely on test framework
+    // to not make use of the hash (it does).
+
+    // Do not rely on Polymer bundle in the test.
+    const createElement = spyOn(document, 'createElement');
+    createElement.withArgs('tf-storage').and.returnValue({
+      tf_storage: {
+        setString: setStringSpy,
+        getString: getStringSpy,
+      },
+    } as any);
+    createElement.withArgs('tf-globals').and.returnValue({
+      tf_globals: {
+        setUseHash: jasmine.createSpy(),
+      },
+    } as any);
+    createElement.and.callThrough();
   });
 
   it('sets the hash to plugin id by replacing on first load', () => {
     store.overrideSelector(getActivePlugin, 'foo');
     const fixture = TestBed.createComponent(HashStorageContainer);
     fixture.detectChanges();
-    expect(setPluginIdSpy).toHaveBeenCalledWith('foo', {
+
+    expect(setStringSpy).toHaveBeenCalledWith(jasmine.any(String), 'foo', {
       useLocationReplace: true,
     });
   });
@@ -77,21 +70,21 @@ describe('hash storage test', () => {
     store.overrideSelector(getActivePlugin, 'foo');
     const fixture = TestBed.createComponent(HashStorageContainer);
     fixture.detectChanges();
-    getPluginIdSpy.and.returnValue('foo');
+    getStringSpy.and.returnValue('foo');
 
     store.overrideSelector(getActivePlugin, 'bar');
     store.refreshState();
     fixture.detectChanges();
 
-    expect(setPluginIdSpy).toHaveBeenCalledTimes(2);
-    expect(setPluginIdSpy).toHaveBeenCalledWith('bar', {});
+    expect(setStringSpy).toHaveBeenCalledTimes(2);
+    expect(setStringSpy).toHaveBeenCalledWith(jasmine.any(String), 'bar', {});
   });
 
   it('dispatches plugin changed event when hash changes', () => {
     store.overrideSelector(getActivePlugin, 'foo');
     const fixture = TestBed.createComponent(HashStorageContainer);
     fixture.detectChanges();
-    getPluginIdSpy.and.returnValue('bar');
+    getStringSpy.and.returnValue('bar');
 
     window.dispatchEvent(new Event('hashchange'));
     expect(dispatchSpy).toHaveBeenCalledWith(
