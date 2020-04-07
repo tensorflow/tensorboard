f01ac7c246d39fabc0cea9bb30d812c90704b694
Shanqing Cai <cais@google.com>
2020-04-07 14:56:15 -0400

    Revise test format

--- tensorboard/uploader/formatters_test.py
+++ tensorboard/uploader/formatters_test.py
@@ -37,33 +37,42 @@ class TensorBoardExporterTest(tb_test.TestCase):
         experiment_url = "http://tensorboard.dev/deadbeef"
         formatter = formatters.ReadableFormatter()
         output = formatter.format_experiment(experiment, experiment_url)
-        lines = output.split("\n")
-        self.assertLen(lines, 9)
-        self.assertEqual(lines[0], "http://tensorboard.dev/deadbeef")
-        self.assertEqual(lines[1], "\tName         A name for the experiment")
-        self.assertEqual(
-            lines[2], "\tDescription  A description for the experiment"
-        )
-        self.assertEqual(lines[3], "\tId           deadbeef")
-        self.assertEqual(lines[4], "\tCreated      1970-01-01 00:00:00")
-        self.assertEqual(lines[5], "\tUpdated      1970-01-01 00:00:00")
-        self.assertEqual(lines[6], "\tRuns         2")
-        self.assertEqual(lines[7], "\tTags         4")
-        self.assertEqual(lines[8], "\tScalars      60")
+        expected_lines = [
+            "http://tensorboard.dev/deadbeef",
+            "\tName         A name for the experiment",
+            "\tDescription  A description for the experiment",
+            "\tId           deadbeef",
+            "\tCreated      1970-01-01 00:00:00",
+            "\tUpdated      1970-01-01 00:00:00",
+            "\tRuns         2",
+            "\tTags         4",
+            "\tScalars      60",
+        ]
+        self.assertEqual(output.split("\n"), expected_lines)
 
     def testReadableFormatterWithEmptyNameAndDescription(self):
         experiment = experiment_pb2.Experiment(
             experiment_id="deadbeef",
             # NOTE(cais): `name` and `description` are missing here.
+            num_runs=2,
+            num_tags=4,
+            num_scalars=60,
         )
         experiment_url = "http://tensorboard.dev/deadbeef"
         formatter = formatters.ReadableFormatter()
         output = formatter.format_experiment(experiment, experiment_url)
-        lines = output.split("\n")
-        self.assertLen(lines, 9)
-        self.assertEqual(lines[0], "http://tensorboard.dev/deadbeef")
-        self.assertEqual(lines[1], "\tName         [No Name]")
-        self.assertEqual(lines[2], "\tDescription  [No Description]")
+        expected_lines = [
+            "http://tensorboard.dev/deadbeef",
+            "\tName         [No Name]",
+            "\tDescription  [No Description]",
+            "\tId           deadbeef",
+            "\tCreated      1970-01-01 00:00:00",
+            "\tUpdated      1970-01-01 00:00:00",
+            "\tRuns         2",
+            "\tTags         4",
+            "\tScalars      60",
+        ]
+        self.assertEqual(output.split("\n"), expected_lines)
 
     def testJsonFormatterWithEmptyNameAndDescription(self):
         experiment = experiment_pb2.Experiment(
@@ -76,21 +85,20 @@ class TensorBoardExporterTest(tb_test.TestCase):
         experiment_url = "http://tensorboard.dev/deadbeef"
         formatter = formatters.JsonFormatter()
         output = formatter.format_experiment(experiment, experiment_url)
-        lines = output.split("\n")
-        self.assertLen(lines, 11)
-        self.assertEqual(lines[0], "{")
-        self.assertEqual(
-            lines[1], '  "url": "http://tensorboard.dev/deadbeef",'
-        )
-        self.assertEqual(lines[2], '  "name": "",')
-        self.assertEqual(lines[3], '  "description": "",')
-        self.assertEqual(lines[4], '  "id": "deadbeef",')
-        self.assertEqual(lines[5], '  "created": "1970-01-01 00:00:00",')
-        self.assertEqual(lines[6], '  "updated": "1970-01-01 00:00:00",')
-        self.assertEqual(lines[7], '  "runs": 2,')
-        self.assertEqual(lines[8], '  "tags": 4,')
-        self.assertEqual(lines[9], '  "scalars": 60')
-        self.assertEqual(lines[10], "}")
+        expected_lines = [
+            "{",
+            '  "url": "http://tensorboard.dev/deadbeef",',
+            '  "name": "",',
+            '  "description": "",',
+            '  "id": "deadbeef",',
+            '  "created": "1970-01-01 00:00:00",',
+            '  "updated": "1970-01-01 00:00:00",',
+            '  "runs": 2,',
+            '  "tags": 4,',
+            '  "scalars": 60',
+            "}",
+        ]
+        self.assertEqual(output.split("\n"), expected_lines)
 
 
 if __name__ == "__main__":
