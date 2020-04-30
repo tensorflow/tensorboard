tensorflow/tensorboard

commit 27d0023e728aba79d0129f890e41c6c2c4c436b9

improve rerender of vz_line_chart (#3524)
We identified few several expensive operations going on underneath Plottable.
- When we modified dataset() attached to a plot, Plottable recalculated
  domain which in turn triggered redraw of the plot.
- Plot redraw is scheduled but repeated `clearTimeout` and `setTimeout`
  take considerable time which was the large part of the dataset update
- Changing a dataset caused us to updateSpecialDataset N times (N =
  number of runs)

We addressed the issue by:
- `commit` API after making all the changes
- remember which runs/dataset were changed
- update datasets only once per commit

We did not address:
- autoDomain recomputing the bounds on every data changes.
  - there is no programmatical way to disable auto domain unless we
    change the domain which causes, for instance,  zoom level to reset

Empirically, the render time:
- toggling run on the selector went down from 1740ms to 273ms
- triggering the log scale went down from 315ms to 264ms
  - from ~670ms to ~620ms when measured from mouse up

Do note that this change does not improve time for other charting
operation like zoom, smoothing changes, and etc...

tensorboard/components/tf_line_chart_data_loader/tf-line-chart-data-loader.html
@@ -219,6 +219,10 @@ limitations under the License.
           this.$.chart.setSeriesMetadata(name, metadata);
         },
 
+        commitChanges() {
+          this.$.chart.commitChanges();
+        },
+
         redraw() {
           cancelAnimationFrame(this._redrawRaf);
           this._redrawRaf = window.requestAnimationFrame(() => {

tensorboard/components/vz_line_chart2/line-chart.ts
@@ -90,7 +90,6 @@ namespace vz_line_chart2 {
     private lastPointsDataset: Plottable.Dataset;
     private fillArea?: FillArea;
     private datasets: Plottable.Dataset[];
-    private onDatasetChanged: (dataset: Plottable.Dataset) => void;
     private nanDataset: Plottable.Dataset;
     private smoothingWeight: number;
     private smoothingEnabled: boolean;
@@ -138,10 +137,6 @@ namespace vz_line_chart2 {
       // The symbol function maps series to marker. It uses a special dataset that
       // varies based on whether smoothing is enabled.
       this.symbolFunction = symbolFunction;
-
-      // need to do a single bind, so we can deregister the callback from
-      // old Plottable.Datasets. (Deregistration is done by identity checks.)
-      this.onDatasetChanged = this._onDatasetChanged.bind(this);
 
       this._defaultXRange = defaultXRange;
       this._defaultYRange = defaultYRange;
@@ -320,16 +315,6 @@ namespace vz_line_chart2 {
         groups.push(this.markersScatterPlot);
       }
       return new Plottable.Components.Group(groups);
-    }
-
-    /** Updates the chart when a dataset changes. Called every time the data of
-     * a dataset changes to update the charts.
-     */
-    private _onDatasetChanged(dataset: Plottable.Dataset) {
-      if (this.smoothingEnabled) {
-        this.resmoothDataset(dataset);
-      }
-      this.updateSpecialDatasets();
     }
 
     public ignoreYOutliers(ignoreYOutliers: boolean) {
@@ -832,25 +817,53 @@ namespace vz_line_chart2 {
     }
 
     /**
-     * Update the selected series on the chart.
+     * Stages update of visible series on the chart.
+     *
+     * Please call `commitChanges` for the changes to be reflected on the chart
+     * after making all the changes.
      */
     public setVisibleSeries(names: string[]) {
+      this.disableChanges();
       names = names.sort();
-      this.seriesNames = names;
-
       names.reverse(); // draw first series on top
-      this.datasets.forEach((d) => d.offUpdate(this.onDatasetChanged));
-      this.datasets = names.map((r) => this.getDataset(r));
-      this.datasets.forEach((d) => d.onUpdate(this.onDatasetChanged));
-      this.linePlot.datasets(this.datasets);
+      this.seriesNames = names;
+    }
 
+    private dirtyDatasets = new Set<string>();
+
+    private disableChanges() {
+      if (!this.dirtyDatasets.size) {
+        // Prevent plots from reacting to the dataset changes.
+        this.linePlot.datasets([]);
+        if (this.smoothLinePlot) {
+          this.smoothLinePlot.datasets([]);
+        }
+
+        if (this.marginAreaPlot) {
+          this.marginAreaPlot.datasets([]);
+        }
+      }
+    }
+
+    public commitChanges() {
+      this.datasets = this.seriesNames.map((r) => this.getDataset(r));
+      [...this.dirtyDatasets].forEach((d) => {
+        if (this.smoothingEnabled) {
+          this.resmoothDataset(this.getDataset(d));
+        }
+      });
+      this.updateSpecialDatasets();
+
+      this.linePlot.datasets(this.datasets);
       if (this.smoothingEnabled) {
         this.smoothLinePlot.datasets(this.datasets);
       }
       if (this.marginAreaPlot) {
         this.marginAreaPlot.datasets(this.datasets);
       }
-      this.updateSpecialDatasets();
+
+      this.measureBBoxAndMaybeInvalidateLayoutInRaf();
+      this.dirtyDatasets.clear();
     }
 
     /**
@@ -877,21 +890,30 @@ namespace vz_line_chart2 {
     }
 
     /**
-     * Sets the data of a series on the chart.
+     * Stages a data change of a series on the chart.
+     *
+     * Please call `commitChanges` for the changes to be reflected on the chart
+     * after making all the changes.
      */
     public setSeriesData(name: string, data: vz_chart_helpers.ScalarDatum[]) {
+      this.disableChanges();
       this.getDataset(name).data(data);
-      this.measureBBoxAndMaybeInvalidateLayoutInRaf();
+      this.dirtyDatasets.add(name);
     }
 
     /**
-     * Sets the metadata of a series on the chart.
+     * Sets a metadata change of a series on the chart.
+     *
+     * Please call `commitChanges` for the changes to be reflected on the chart
+     * after making all the changes.
      */
     public setSeriesMetadata(name: string, meta: any) {
-      const newMeta = Object.assign({}, this.getDataset(name).metadata(), {
+      this.disableChanges();
+      this.getDataset(name).metadata({
+        ...this.getDataset(name).metadata(),
         meta,
       });
-      this.getDataset(name).metadata(newMeta);
+      this.dirtyDatasets.add(name);
     }
 
     public smoothingUpdate(weight: number) {

tensorboard/components/vz_line_chart2/vz-line-chart2.ts
@@ -338,6 +338,11 @@ namespace vz_line_chart2 {
       }
     },
 
+    commitChanges() {
+      if (!this._chart) return;
+      this._chart.commitChanges();
+    },
+
     /**
      * Reset the chart domain. If the chart has not rendered yet, a call to this
      * method no-ops.
@@ -426,6 +431,7 @@ namespace vz_line_chart2 {
           this._chart.setSeriesMetadata(name, this._seriesMetadataCache[name]);
         });
       this._chart.setVisibleSeries(this._visibleSeriesCache);
+      this._chart.commitChanges();
     },
 
     _smoothingChanged: function() {

tensorboard/plugins/scalar/tf_scalar_dashboard/tf-scalar-card.html
@@ -226,6 +226,7 @@ limitations under the License.
               const name = this._getSeriesNameFromDatum(datum);
               scalarChart.setSeriesMetadata(name, datum);
               scalarChart.setSeriesData(name, formattedData);
+              scalarChart.commitChanges();
             };
           },
           readOnly: true,

commit f3d9f2fee3f90cd3b212088dd12b9fad892094b4

Implement commitChanges on vz-line-chart2
This is a roll forward of 46b6e615a. The commit partiall rolls back
27d0023e7 which introduces `commitChanges` which was supposed to be
invoked after setting data, visible series, or metadata.

This change addresses the regression caused by 27d0023e7 which forgot to
call the new API in all places where we use the vz-line-chart2.

tensorboard/components/tf_line_chart_data_loader/tf-line-chart-data-loader.html
@@ -219,11 +219,8 @@ limitations under the License.
           this.$.chart.setSeriesMetadata(name, metadata);
         },
 
-        /**
-         * Not yet implemented.
-         */
         commitChanges() {
-          // Temporarily rolled back due to PR curves breakage.
+          this.$.chart.commitChanges();
         },
 
         redraw() {

tensorboard/components/vz_line_chart2/line-chart.ts
@@ -90,7 +90,6 @@ namespace vz_line_chart2 {
     private lastPointsDataset: Plottable.Dataset;
     private fillArea?: FillArea;
     private datasets: Plottable.Dataset[];
-    private onDatasetChanged: (dataset: Plottable.Dataset) => void;
     private nanDataset: Plottable.Dataset;
     private smoothingWeight: number;
     private smoothingEnabled: boolean;
@@ -138,10 +137,6 @@ namespace vz_line_chart2 {
       // The symbol function maps series to marker. It uses a special dataset that
       // varies based on whether smoothing is enabled.
       this.symbolFunction = symbolFunction;
-
-      // need to do a single bind, so we can deregister the callback from
-      // old Plottable.Datasets. (Deregistration is done by identity checks.)
-      this.onDatasetChanged = this._onDatasetChanged.bind(this);
 
       this._defaultXRange = defaultXRange;
       this._defaultYRange = defaultYRange;
@@ -320,16 +315,6 @@ namespace vz_line_chart2 {
         groups.push(this.markersScatterPlot);
       }
       return new Plottable.Components.Group(groups);
-    }
-
-    /** Updates the chart when a dataset changes. Called every time the data of
-     * a dataset changes to update the charts.
-     */
-    private _onDatasetChanged(dataset: Plottable.Dataset) {
-      if (this.smoothingEnabled) {
-        this.resmoothDataset(dataset);
-      }
-      this.updateSpecialDatasets();
     }
 
     public ignoreYOutliers(ignoreYOutliers: boolean) {
@@ -833,32 +818,53 @@ namespace vz_line_chart2 {
     }
 
     /**
-     * Update the selected series on the chart.
+     * Stages update of visible series on the chart.
+     *
+     * Please call `commitChanges` for the changes to be reflected on the chart
+     * after making all the changes.
      */
     public setVisibleSeries(names: string[]) {
+      this.disableChanges();
       names = names.sort();
-      this.seriesNames = names;
-
       names.reverse(); // draw first series on top
-      this.datasets.forEach((d) => d.offUpdate(this.onDatasetChanged));
-      this.datasets = names.map((r) => this.getDataset(r));
-      this.datasets.forEach((d) => d.onUpdate(this.onDatasetChanged));
-      this.linePlot.datasets(this.datasets);
+      this.seriesNames = names;
+    }
 
+    private dirtyDatasets = new Set<string>();
+
+    private disableChanges() {
+      if (!this.dirtyDatasets.size) {
+        // Prevent plots from reacting to the dataset changes.
+        this.linePlot.datasets([]);
+        if (this.smoothLinePlot) {
+          this.smoothLinePlot.datasets([]);
+        }
+
+        if (this.marginAreaPlot) {
+          this.marginAreaPlot.datasets([]);
+        }
+      }
+    }
+
+    public commitChanges() {
+      this.datasets = this.seriesNames.map((r) => this.getDataset(r));
+      [...this.dirtyDatasets].forEach((d) => {
+        if (this.smoothingEnabled) {
+          this.resmoothDataset(this.getDataset(d));
+        }
+      });
+      this.updateSpecialDatasets();
+
+      this.linePlot.datasets(this.datasets);
       if (this.smoothingEnabled) {
         this.smoothLinePlot.datasets(this.datasets);
       }
       if (this.marginAreaPlot) {
         this.marginAreaPlot.datasets(this.datasets);
       }
-      this.updateSpecialDatasets();
-    }
 
-    /**
-     * Not yet implemented.
-     */
-    public commitChanges() {
-      // Temporarily rolled back due to PR curves breakage.
+      this.measureBBoxAndMaybeInvalidateLayoutInRaf();
+      this.dirtyDatasets.clear();
     }
 
     /**
@@ -885,21 +891,30 @@ namespace vz_line_chart2 {
     }
 
     /**
-     * Sets the data of a series on the chart.
+     * Stages a data change of a series on the chart.
+     *
+     * Please call `commitChanges` for the changes to be reflected on the chart
+     * after making all the changes.
      */
     public setSeriesData(name: string, data: vz_chart_helpers.ScalarDatum[]) {
+      this.disableChanges();
       this.getDataset(name).data(data);
-      this.measureBBoxAndMaybeInvalidateLayoutInRaf();
+      this.dirtyDatasets.add(name);
     }
 
     /**
-     * Sets the metadata of a series on the chart.
+     * Sets a metadata change of a series on the chart.
+     *
+     * Please call `commitChanges` for the changes to be reflected on the chart
+     * after making all the changes.
      */
     public setSeriesMetadata(name: string, meta: any) {
-      const newMeta = Object.assign({}, this.getDataset(name).metadata(), {
+      this.disableChanges();
+      this.getDataset(name).metadata({
+        ...this.getDataset(name).metadata(),
         meta,
       });
-      this.getDataset(name).metadata(newMeta);
+      this.dirtyDatasets.add(name);
     }
 
     public smoothingUpdate(weight: number) {

tensorboard/components/vz_line_chart2/vz-line-chart2.ts
@@ -338,11 +338,9 @@ namespace vz_line_chart2 {
       }
     },
 
-    /**
-     * Not yet implemented.
-     */
     commitChanges() {
-      // Temporarily rolled back due to PR curves breakage.
+      if (!this._chart) return;
+      this._chart.commitChanges();
     },
 
     /**
@@ -433,6 +431,7 @@ namespace vz_line_chart2 {
           this._chart.setSeriesMetadata(name, this._seriesMetadataCache[name]);
         });
       this._chart.setVisibleSeries(this._visibleSeriesCache);
+      this._chart.commitChanges();
     },
 
     _smoothingChanged: function() {

tensorboard/plugins/custom_scalar/tf_custom_scalar_dashboard/tf-custom-scalar-margin-chart-card.html
@@ -664,6 +664,7 @@ limitations under the License.
             dataSeries.getData()
           );
         });
+        this.$.loader.commitChanges();
       },
       _computeSeriesNames() {
         const runLookup = new Set(this.runs);

tensorboard/plugins/custom_scalar/tf_custom_scalar_dashboard/tf-custom-scalar-multi-line-chart-card.html
@@ -382,6 +382,7 @@ limitations under the License.
         Object.entries(_nameToDataSeries).forEach(([name, series]) => {
           this.$.loader.setSeriesData(name, series.getData());
         });
+        this.$.loader.commitChanges();
       },
       _computeSelectedRunsSet(runs) {
         const mapping = {};

tensorboard/plugins/debugger/tf_debugger_dashboard/tf-debugger-line-chart.html
@@ -110,6 +110,7 @@ limitations under the License.
           seriesData.push({step: xData[i], scalar: yData[i]});
         }
         chart.setSeriesData(this._defaultSeriesName, seriesData);
+        chart.commitChanges();
       },
     });
   </script>

tensorboard/plugins/pr_curve/tf_pr_curve_dashboard/tf-pr-curve-card.html
@@ -407,11 +407,15 @@ limitations under the License.
         for (let i = 0; i < seriesData.length; i++) {
           seriesData[i] = _.mapValues(fieldsToData, (values) => values[i]);
         }
-        this.$$('tf-line-chart-data-loader').setSeriesData(run, seriesData);
+        const loader = this.$$('tf-line-chart-data-loader');
+        loader.setSeriesData(run, seriesData);
+        loader.commitChanges();
       },
       _clearSeriesData(run) {
         // Clears data for a run in the chart.
-        this.$$('tf-line-chart-data-loader').setSeriesData(run, []);
+        const loader = this.$$('tf-line-chart-data-loader');
+        loader.setSeriesData(run, []);
+        loader.commitChanges();
       },
       _updateRunToPrCurveEntry(runToDataOverTime, runToStepCap) {
         const runToEntry = {};

tensorboard/plugins/scalar/tf_scalar_dashboard/tf-scalar-card.html
@@ -226,6 +226,7 @@ limitations under the License.
               const name = this._getSeriesNameFromDatum(datum);
               scalarChart.setSeriesMetadata(name, datum);
               scalarChart.setSeriesData(name, formattedData);
+              scalarChart.commitChanges();
             };
           },
           readOnly: true,

tensorboard/components/vz_line_chart2/microbenchmark/renders_spec.ts
@@ -61,6 +61,7 @@ benchmark({
     context.container.appendChild(context.chart);
 
     context.chart.setVisibleSeries([]);
+    context.chart.commitChanges();
 
     await polymerFlush();
   },
@@ -82,6 +83,7 @@ benchmark({
 
     context.chart.setSeriesData('sine', DATA_POINTS.sine1k);
     context.chart.setVisibleSeries(['sine']);
+    context.chart.commitChanges();
 
     await polymerFlush();
   },
@@ -103,6 +105,7 @@ benchmark({
 
     context.chart.setSeriesData('sine', DATA_POINTS.sine1k);
     context.chart.setVisibleSeries(['sine']);
+    context.chart.commitChanges();
 
     await polymerFlush();
   },
@@ -124,6 +127,7 @@ benchmark({
 
     context.chart.setSeriesData('cosine', DATA_POINTS.cosine100k);
     context.chart.setVisibleSeries(['cosine']);
+    context.chart.commitChanges();
 
     await polymerFlush();
   },
@@ -146,6 +150,7 @@ benchmark({
     context.chart.setSeriesData('sine', DATA_POINTS.sine1k);
     context.chart.setSeriesData('cosine', DATA_POINTS.cosine1k);
     context.chart.setVisibleSeries(['cosine']);
+    context.chart.commitChanges();
     context.even = true;
 
     await polymerFlush();
@@ -177,6 +182,7 @@ benchmark({
     context.chart.setVisibleSeries(
       FIVE_HUNDRED_1K_DATA_POINTS.map(({name}) => name)
     );
+    context.chart.commitChanges();
 
     await polymerFlush();
   },
@@ -201,6 +207,7 @@ benchmark({
       context.chart.setSeriesData(name, data);
     });
     context.chart.setVisibleSeries(datapoints.map(({name}) => name));
+    context.chart.commitChanges();
 
     await polymerFlush();
     context.index = 0;
@@ -234,6 +241,7 @@ benchmark({
         context.container.appendChild(chart);
         chart.setSeriesData(name, data);
         chart.setVisibleSeries([name]);
+        chart.commitChanges();
         return chart;
       }
     );
@@ -264,6 +272,7 @@ benchmark({
         chart.style.height = '50px';
         context.container.appendChild(chart);
         chart.setSeriesData(name, data);
+        chart.commitChanges();
         return chart;
       }
     );
@@ -277,12 +286,14 @@ benchmark({
     context.charts.forEach((chart: any) => {
       chart.setVisibleSeries([]);
     });
+    context.chart.commitChanges();
 
     await context.flushAsync();
 
     context.charts.forEach((chart: any, index: number) => {
       chart.setVisibleSeries([context.names[index]]);
     });
+    context.chart.commitChanges();
 
     await context.flushAsync();
   },
@@ -300,6 +311,7 @@ benchmark({
 
     context.chart.setSeriesData('cosine', DATA_POINTS.cosine1k);
     context.chart.setVisibleSeries(['cosine']);
+    context.chart.commitChanges();
     context.chart.smoothingEnabled = true;
     context.even = true;
 
@@ -328,6 +340,7 @@ benchmark({
 
     context.chart.setSeriesData('cosine', DATA_POINTS.cosine100k);
     context.chart.setVisibleSeries(['cosine']);
+    context.chart.commitChanges();
     context.chart.smoothingEnabled = true;
     context.even = true;
 
@@ -358,6 +371,7 @@ benchmark({
 
     context.chart.setSeriesData('cosine', DATA_POINTS.cosine100k);
     context.chart.setVisibleSeries(['cosine']);
+    context.chart.commitChanges();
     context.chart.smoothingEnabled = true;
     context.even = true;
 