95dc1f54d733a60bfb5220226089128eb4c7d533
Shanqing Cai <cais@google.com>
2020-05-15 18:01:14 -0400

    [DebuggerV2] Add basic reducers for graph ops (#3630)
    * Motivation for features / changes
      * Continue developing DebuggerV2, specifically its graph component
    * Technical description of changes
      * Add three actions: `graphOpFocused`, `graphOpInfoRequested` and `graphOpInfoLoaded`
      * Add reducers for them.
      * Unit tests are *not* added in the existing debugger_reducers_test.ts, because the file is getting too large in size. Instead, the new unit tests are in a new file in the same folder: debugger_graphs_reducers_test.

--- tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/store/debugger_reducers_test.ts
+++ tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/store/debugger_reducers_test.ts
@@ -16,11 +16,11 @@ import * as actions from '../actions';
 import {ExecutionDigestsResponse} from '../data_source/tfdbg2_data_source';
 import {reducers} from './debugger_reducers';
 import {
+  Alert,
+  AlertType,
   DataLoadState,
   Execution,
   StackFramesById,
-  AlertType,
-  Alert,
 } from './debugger_types';
 import {
   createAlertsState,
@@ -36,7 +36,7 @@ import {
   createTestInfNanAlert,
 } from '../testing';
 
-describe('Debugger reducers', () => {
+describe('Debugger graphs reducers', () => {
   describe('alertTypeFocusToggled', () => {
     for (const focusType of [
       AlertType.FUNCTION_RECOMPILE_ALERT,
