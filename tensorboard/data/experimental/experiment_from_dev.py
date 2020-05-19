11195311d3598f240a89a19f751f6fd074efa385
Shanqing Cai <cais@google.com>
2020-05-19 14:14:04 -0400

    DataFrame API: Let get_scalars() fail faster if pandas is not installed

--- tensorboard/data/experimental/experiment_from_dev.py
+++ tensorboard/data/experimental/experiment_from_dev.py
@@ -25,7 +25,6 @@ import time
 import grpc
 import numpy as np
 
-from tensorboard import lazy
 from tensorboard.data.experimental import base_experiment
 from tensorboard.uploader import auth
 from tensorboard.uploader import util
@@ -39,8 +38,7 @@ from tensorboard.util import grpc_util
 DEFAULT_ORIGIN = "https://tensorboard.dev"
 
 
-@lazy.lazy_load("pandas")
-def pandas():
+def import_pandas():
     """Import pandas, guarded by a user-friendly error message on failure."""
     try:
         import pandas
@@ -77,6 +75,10 @@ class ExperimentFromDev(base_experiment.BaseExperiment):
         pivot=True,
         include_wall_time=False,
     ):
+        # NOTE(#3650): Import pandas early in this method, so if the
+        # Python environment does not have pandas installed, an error can be
+        # raised early, before any rpc call is made.
+        pandas = import_pandas()
         if runs_filter is not None:
             raise NotImplementedError(
                 "runs_filter support for get_scalars() is not implemented yet."
