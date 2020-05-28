f106484f7294dce7e6a44009c3370c3bceafa2f9
Brian Dubois <bdubois@google.com>
2020-05-28 08:46:32 -0400

    Changes after further PR comments.
    More tests for different types of input.
    Rename some variables.
    Some documentation.
    Don't specially handle null values.

--- tensorboard/components/tf_wbr_string/test/tfWbrStringTests.ts
+++ tensorboard/components/tf_wbr_string/test/tfWbrStringTests.ts
@@ -19,7 +19,7 @@ namespace tf_wbr_string {
 
   window.HTMLImports.whenReady(() => {
     describe('tf-wbr-string', () => {
-      it('adds wbrs for single character', (done) => {
+      it('adds wbrs for patterns with a single character', (done) => {
         let testElement: any = fixture('tf-wbr-string');
         testElement.value = 'I/have/a/delimiter';
         testElement.delimiterPattern = '/';
@@ -27,11 +27,14 @@ namespace tf_wbr_string {
           expect(testElement.shadowRoot.innerHTML).to.have.string(
             'I/<wbr>have/<wbr>a/<wbr>delimiter<wbr>'
           );
+          expect(testElement.shadowRoot.textContent.trim()).to.equal(
+            'I/have/a/delimiter'
+          );
           done();
         });
       });
 
-      it('adds wbrs for multiple single characters', (done) => {
+      it('adds wbrs for patterns with multiple single characters', (done) => {
         let testElement: any = fixture('tf-wbr-string');
         testElement.value = 'I_have-multiple_delimiter.s';
         testElement.delimiterPattern = '[_.\\-]';
@@ -43,13 +46,86 @@ namespace tf_wbr_string {
         });
       });
 
-      it('adds wbrs for arbitrary regex patterns', (done) => {
+      it('adds wbrs for more complex patterns', (done) => {
+        let testElement: any = fixture('tf-wbr-string');
+        testElement.value = 'thea_heats_tea';
+        testElement.delimiterPattern = 'the|eat';
+        flush(() => {
+          expect(testElement.shadowRoot.innerHTML).to.have.string(
+            'the<wbr>a_heat<wbr>s_tea<wbr>'
+          );
+          done();
+        });
+      });
+
+      it('ignores overlapped matches', (done) => {
         let testElement: any = fixture('tf-wbr-string');
         testElement.value = 'the_theatre_heats_tea';
         testElement.delimiterPattern = 'the|eat';
         flush(() => {
+          // The "eat" in "theatre" is ignored.
           expect(testElement.shadowRoot.innerHTML).to.have.string(
             'the<wbr>_the<wbr>atre_heat<wbr>s_tea<wbr>'
+          );
+          done();
+        });
+      });
+
+      it('allows empty matches to consume remainder of the string', (done) => {
+        // The current handling of empty matches may not desirable but we warn
+        // against this in the documentation. We could consider improving this
+        // in the future.
+        let testElement: any = fixture('tf-wbr-string');
+        testElement.value = 'the_theatre_heats_tea';
+        testElement.delimiterPattern = '(the|eat)?';
+        flush(() => {
+          expect(testElement.shadowRoot.innerHTML).to.have.string(
+            'the<wbr>_theatre_heats_tea<wbr>'
+          );
+          done();
+        });
+      });
+
+      it('adds single wbr for empty value', (done) => {
+        let testElement: any = fixture('tf-wbr-string');
+        testElement.value = '';
+        testElement.delimiterPattern = '/';
+        flush(() => {
+          expect(testElement.shadowRoot.innerHTML).to.have.string('<wbr>');
+          expect(testElement.shadowRoot.textContent.trim()).to.equal('');
+          done();
+        });
+      });
+
+      it('adds single wbr for undefined value', (done) => {
+        let testElement: any = fixture('tf-wbr-string');
+        testElement.value = null;
+        testElement.delimiterPattern = '/';
+        flush(() => {
+          expect(testElement.shadowRoot.innerHTML).to.have.string('<wbr>');
+          expect(testElement.shadowRoot.textContent.trim()).to.equal('');
+          done();
+        });
+      });
+
+      it('adds single wbr for empty pattern', (done) => {
+        let testElement: any = fixture('tf-wbr-string');
+        testElement.value = 'Empty delimiter pattern';
+        testElement.delimiterPattern = '';
+        flush(() => {
+          expect(testElement.shadowRoot.innerHTML).to.have.string(
+            'Empty delimiter pattern<wbr>'
+          );
+          done();
+        });
+      });
+
+      it('adds single wbr for undefined pattern', (done) => {
+        let testElement: any = fixture('tf-wbr-string');
+        testElement.value = 'undefined pattern';
+        flush(() => {
+          expect(testElement.shadowRoot.innerHTML).to.have.string(
+            'undefined pattern<wbr>'
           );
           done();
         });
