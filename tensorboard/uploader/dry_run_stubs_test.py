7593c9d9b8b76cd7fae409f32d8f9d542f5687bc
Shanqing Cai <cais@google.com>
2020-06-05 12:03:24 -0400

    Remove unused dep and import

--- tensorboard/uploader/dry_run_stubs_test.py
+++ tensorboard/uploader/dry_run_stubs_test.py
@@ -18,8 +18,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from unittest import mock
-
 from tensorboard import test as tb_test
 from tensorboard.uploader import dry_run_stubs
 from tensorboard.uploader.proto import write_service_pb2
