06bf21d89d754dc53307cc20390d068da7bea87a
Alex <alex.baeuerle@uni-ulm.de>
2020-07-24 16:09:05 +0200

    Started Test Implementation
    Started implementing tests for the frontend.

---
+++ tensorboard/webapp/plugins/npmi/npmi_container_test.ts
@@ -0,0 +1,51 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+/**
+ * Unit tests for the NPMI Container.
+ */
+import {CommonModule} from '@angular/common';
+import {TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+
+import {Store} from '@ngrx/store';
+import {provideMockStore, MockStore} from '@ngrx/store/testing';
+
+import {NpmiComponent} from './npmi_component';
+import {NpmiContainer} from './npmi_container';
+
+/** @typehack */ import * as _typeHackStore from '@ngrx/store';
+
+describe('Npmi Container', () => {
+  let dispatchSpy: jasmine.Spy;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NpmiComponent, NpmiContainer],
+      providers: [
+        NpmiContainer,
+      ],
+    }).compileComponents();
+  });
+
+  it('renders nomi component', () => {
+    const fixture = TestBed.createComponent(NpmiContainer);
+    fixture.detectChanges();
+
+    const inactiveElement = fixture.debugElement.query(
+      By.css('npmi-container')
+    );
+    expect(inactiveElement).toBeTruthy();
+  });
+});
