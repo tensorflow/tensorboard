a544c2e6c77443af2bb62370c423edfcc7a9fba5
William Chargin <wchargin@gmail.com>
2020-07-24 08:59:58 -0700

    review: misc. fixups
    Mostly more removal of unused targets. Some intentionally public exports
    reinstated. One existing underapproximation revealed and corrected.
    
    Test sync: <http://cl/323013593>

--- tensorboard/data/experimental/__init__.py
+++ tensorboard/data/experimental/__init__.py
@@ -16,3 +16,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+from tensorboard.data.experimental.experiment_from_dev import (  # noqa: F401
+    ExperimentFromDev,
+)
