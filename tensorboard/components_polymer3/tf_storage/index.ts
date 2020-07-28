e06cd0a1bd25bff198b5fec4a9a48fd4c17fe8d7
Stephan Lee <stephanwlee@gmail.com>
2020-07-28 16:07:21 -0700

    migrate: tf_storage (#3897)

--- tensorboard/components_polymer3/tf_storage/index.ts
+++ tensorboard/components_polymer3/tf_storage/index.ts
@@ -1,6 +1,4 @@
-<!--
-@license
-Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,15 +11,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
-
-<link rel="import" href="../tf-imports/polymer.html" />
-<link rel="import" href="../tf-globals/tf-globals.html" />
-<link rel="import" href="../tf-imports/lodash.html" />
-
-<script src="listeners.js"></script>
-<script src="storage.js"></script>
-
-<dom-module id="tf-storage">
-  <script src="tf-storage-polymer.js"></script>
-</dom-module>
+==============================================================================*/
+export * from './listeners';
+export * from './storage';
