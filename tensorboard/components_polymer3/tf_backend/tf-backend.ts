f10a5913112994224bb662bac764ee9d813acdb2
E <psybuzz@users.noreply.github.com>
2020-07-28 10:04:54 -0700

    migrate: components_polymer3/tf-backend (#3895)

--- tensorboard/components_polymer3/tf_backend/tf-backend.ts
+++ tensorboard/components_polymer3/tf_backend/tf-backend.ts
@@ -1,4 +1,4 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,16 +12,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-import {PolymerElement} from '@polymer/polymer';
-import {customElement} from '@polymer/decorators';
-
-import * as tf_backend from './tf-backend';
-
-// HACK: this Polymer component allows stores to be accessible from
-// tf-ng-tensorboard by exposing otherwise mangled smybols.
-
-@customElement('tf-backend')
-class TfBackend extends PolymerElement {
-  _template = null;
-  tf_backend = tf_backend;
-}
+export * from './backend';
+export * from './baseStore';
+export * from './canceller';
+export * from './environmentStore';
+export * from './experimentsStore';
+export * from './requestManager';
+export * from './router';
+export * from './runsStore';
+export * from './type';
+export * from './urlPathHelpers';
