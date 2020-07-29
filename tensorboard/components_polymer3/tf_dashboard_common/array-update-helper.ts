503c4e77aa9fa58219dc2c2283600b3662841987
Stephan Lee <stephanwlee@gmail.com>
2020-07-29 11:52:56 -0700

    migrated: tensorboard/components_polymer3/tf_dashboard_common/array-update-helper.ts

--- tensorboard/components_polymer3/tf_dashboard_common/array-update-helper.ts
+++ tensorboard/components_polymer3/tf_dashboard_common/array-update-helper.ts
@@ -1,4 +1,4 @@
-/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,8 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+import {PolymerElement} from '@polymer/polymer';
 
-export const ArrayUpdateHelper = {
+export class ArrayUpdateHelper extends PolymerElement {
   updateArrayProp(
     prop: string,
     value: Array<any>,
@@ -56,5 +57,5 @@ export const ArrayUpdateHelper = {
     if (newValInd < newVal.length) {
       this.push(prop, ...newVal.slice(newValInd));
     }
-  },
-};
+  }
+}
