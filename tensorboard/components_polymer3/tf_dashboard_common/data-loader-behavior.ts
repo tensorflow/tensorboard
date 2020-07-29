87525a5023bdf1d6929b592c9203d30a0b1fc4ad
Stephan Lee <stephanwlee@gmail.com>
2020-07-29 11:53:12 -0700

    migrated: tensorboard/components_polymer3/tf_dashboard_common/data-loader-behavior.ts

--- tensorboard/components_polymer3/tf_dashboard_common/data-loader-behavior.ts
+++ tensorboard/components_polymer3/tf_dashboard_common/data-loader-behavior.ts
@@ -1,4 +1,4 @@
-/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,113 +12,109 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-
+import {PolymerElement} from '@polymer/polymer';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {dedupingMixin} from '@polymer/polymer/lib/utils/mixin';
+import {property, observe} from '@polymer/decorators';
 import * as _ from 'lodash';
 
 import {Canceller} from '../tf_backend/canceller';
+import {RequestManager} from '../tf_backend/requestManager';
 
 type CacheKey = string;
-
 // NOT_LOADED is implicit
 enum LoadState {
   LOADING,
   LOADED,
 }
 
-/**
- * @polymerBehavior
- */
-export const DataLoaderBehavior = {
-  properties: {
-    active: {
-      type: Boolean,
-      observer: '_loadDataIfActive',
-    },
-    /**
-     * A unique identifiable string. When changes, it expunges the data
-     * cache.
-     */
-    loadKey: {
-      type: String,
-      value: '',
-    },
-    // List of data to be loaded. By default, a datum is passed to
-    // `requestData` to fetch data. When the request resolves, invokes
-    // `loadDataCallback` with the datum and its response.
-    dataToLoad: {
-      type: Array,
-      value: () => [],
-    },
-    /**
-     * A function that takes a datum as an input and returns a unique
-     * identifiable string. Used for caching purposes.
-     */
-    getDataLoadName: {
-      type: Function,
-      value: () => (datum): CacheKey => String(datum),
-    },
-    /**
-     * A function that takes as inputs:
-     * 1. Implementing component of data-loader-behavior.
-     * 2. datum of the request.
-     * 3. The response received from the data URL.
-     * This function will be called when a response from a request to that
-     * data URL is successfully received.
-     */
-    loadDataCallback: Function,
-    // A function that takes a datum as argument and makes the HTTP
-    // request to fetch the data associated with the datum. It should return
-    // a promise that either fullfills with the data or rejects with an error.
-    // If the function doesn't bind 'this', then it will reference the element
-    // that includes this behavior.
-    // The default implementation calls this.requestManager.request with
-    // the value returned by this.getDataLoadUrl(datum) (see below).
-    // The only place getDataLoadUrl() is called is in the default
-    // implementation of this method. So if you override this method with
-    // an implementation that doesn't call getDataLoadUrl, it need not be
-    // provided.
-    requestData: {
-      type: Function,
-      value: function() {
-        return (datum) =>
-          this.requestManager.request(this.getDataLoadUrl(datum));
-      },
-    },
-    // A function that takes a datum and returns a string URL for fetching
-    // data.
-    getDataLoadUrl: Function,
-    dataLoading: {
-      type: Boolean,
-      readOnly: true,
-      reflectToAttribute: true,
-      value: false,
-    },
-    /*
-     * A map of a cache key to LoadState. If a cacheKey does not exist in the
-     * map, it is considered NOT_LOADED.
-     * Invoking `reload` or a change in `loadKey` clears the cache.
-     */
-    _dataLoadState: {
-      type: Object,
-      value: () => new Map<CacheKey, LoadState>(),
-    },
-    _canceller: {
-      type: Object,
-      value: () => new Canceller(),
-    },
-    _loadDataAsync: {
-      type: Number,
-      value: null,
-    },
-  },
-  observers: ['_dataToLoadChanged(isAttached, dataToLoad.*)'],
+export class DataLoader<Item, Data> extends LegacyElementMixin(PolymerElement) {
+  @property({type: Boolean, observer: '_loadDataIfActive'})
+  active!: boolean;
+
+  /**
+   * A unique identifiable string. When changes, it expunges the data
+   * cache.
+   */
+  @property({type: String})
+  loadKey = '';
+
+  // List of data to be loaded. By default, a datum is passed to
+  // `requestData` to fetch data. When the request resolves, invokes
+  // `loadDataCallback` with the datum and its response.
+  @property({type: Array})
+  dataToLoad: Item[] = [];
+
+  /**
+   * A function that takes a datum as an input and returns a unique
+   * identifiable string. Used for caching purposes.
+   */
+  @property({type: Object})
+  getDataLoadName = (datum: Item): CacheKey => String(datum);
+
+  /**
+   * A function that takes as inputs:
+   * 1. Implementing component of data-loader-behavior.
+   * 2. datum of the request.
+   * 3. The response received from the data URL.
+   * This function will be called when a response from a request to that
+   * data URL is successfully received.
+   */
+  @property({type: Object})
+  loadDataCallback!: (component: this, datum: Item, data: Data) => void;
+
+  public requestManager!: RequestManager;
+
+  // A function that takes a datum as argument and makes the HTTP
+  // request to fetch the data associated with the datum. It should return
+  // a promise that either fullfills with the data or rejects with an error.
+  // If the function doesn't bind 'this', then it will reference the element
+  // that includes this behavior.
+  // The default implementation calls this.requestManager.request with
+  // the value returned by this.getDataLoadUrl(datum) (see below).
+  // The only place getDataLoadUrl() is called is in the default
+  // implementation of this method. So if you override this method with
+  // an implementation that doesn't call getDataLoadUrl, it need not be
+  // provided.
+  @property({type: Object})
+  requestData = (datum: Item) => {
+    return this.requestManager.request(this.getDataLoadUrl(datum));
+  };
+
+  // A function that takes a datum and returns a string URL for fetching
+  // data.
+  @property({type: Object})
+  getDataLoadUrl!: (datum: Item) => string;
+
+  @property({type: Boolean, reflectToAttribute: true})
+  dataLoading = false;
+
+  /*
+   * A map of a cache key to LoadState. If a cacheKey does not exist in the
+   * map, it is considered NOT_LOADED.
+   * Invoking `reload` or a change in `loadKey` clears the cache.
+   */
+
+  private _dataLoadState = new Map<CacheKey, LoadState>();
+
+  private _canceller = new Canceller();
+
+  private _loadDataAsync: null | number = null;
+
+  _loadData = _.throttle(this._loadDataImpl, 100, {
+    leading: true,
+    trailing: true,
+  });
+
   onLoadFinish() {
     // Override to do something useful.
-  },
+  }
+
   reload() {
     this._dataLoadState.clear();
     this._loadData();
-  },
+  }
+
   reset() {
     // https://github.com/tensorflow/tensorboard/issues/1499
     // Cannot use the observer to observe `loadKey` changes directly.
@@ -129,16 +125,13 @@ export const DataLoaderBehavior = {
     if (this._canceller) this._canceller.cancelAll();
     if (this._dataLoadState) this._dataLoadState.clear();
     if (this.isAttached) this._loadData();
-  },
+  }
+
+  @observe('isAttached', 'dataToLoad.*')
   _dataToLoadChanged() {
     if (this.isAttached) this._loadData();
-  },
-  created() {
-    this._loadData = _.throttle(this._loadDataImpl, 100, {
-      leading: true,
-      trailing: true,
-    });
-  },
+  }
+
   detached() {
     // Note: Cannot call canceller.cancelAll since it will poison the cache.
     // detached gets called when a component gets unmounted from the document
@@ -151,22 +144,22 @@ export const DataLoaderBehavior = {
       this.cancelAsync(this._loadDataAsync);
       this._loadDataAsync = null;
     }
-  },
+  }
   _loadDataIfActive() {
     if (this.active) {
       this._loadData();
     }
-  },
+  }
   _loadDataImpl() {
     if (!this.active) return;
-    this.cancelAsync(this._loadDataAsync);
+    if (this._loadDataAsync !== null) this.cancelAsync(this._loadDataAsync);
     this._loadDataAsync = this.async(
       this._canceller.cancellable((result) => {
         if (result.cancelled) {
           return;
         }
         // Read-only property have a special setter.
-        this._setDataLoading(true);
+        this.dataLoading = true;
         // Promises return cacheKeys of the data that were fetched.
         const promises = this.dataToLoad
           .filter((datum) => {
@@ -181,7 +174,7 @@ export const DataLoaderBehavior = {
                 // It was resetted. Do not notify of the response.
                 if (!result.cancelled) {
                   this._dataLoadState.set(cacheKey, LoadState.LOADED);
-                  this.loadDataCallback(this, datum, result.value);
+                  this.loadDataCallback(this, datum, result.value as any);
                 }
                 return cacheKey;
               })
@@ -192,7 +185,7 @@ export const DataLoaderBehavior = {
             this._canceller.cancellable((result) => {
               // It was resetted. Do not notify of the data load.
               if (!result.cancelled) {
-                const keysFetched = result.value;
+                const keysFetched = result.value as any;
                 const fetched = new Set(keysFetched);
                 const shouldNotify = this.dataToLoad.some((datum) =>
                   fetched.has(this.getDataLoadName(datum))
@@ -206,7 +199,7 @@ export const DataLoaderBehavior = {
               ).some((loadState) => loadState === LoadState.LOADING);
               if (!isDataFetchPending) {
                 // Read-only property have a special setter.
-                this._setDataLoading(false);
+                this.dataLoading = false;
               }
             }),
             // TODO(stephanwlee): remove me when we can use  Promise.prototype.finally
@@ -223,5 +216,7 @@ export const DataLoaderBehavior = {
           );
       })
     );
-  },
-};
+  }
+}
+
+export const DataLoaderBehavior = dedupingMixin(() => DataLoader);
