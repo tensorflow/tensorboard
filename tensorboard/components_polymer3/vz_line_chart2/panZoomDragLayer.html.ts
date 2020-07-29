7f52d79acc297633e38a50112e24d7cd00f6caf3
Stephan Lee <stephanwlee@gmail.com>
2020-07-28 20:52:20 -0700

    migrate: vz_line_chart2 (#3894)

--- tensorboard/components_polymer3/vz_line_chart2/panZoomDragLayer.html.ts
+++ tensorboard/components_polymer3/vz_line_chart2/panZoomDragLayer.html.ts
@@ -1,6 +1,4 @@
-<!--
-@license
-Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,16 +11,11 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
+==============================================================================*/
 
-<link rel="import" href="../tf-imports/d3.html" />
-<link rel="import" href="../tf-imports/plottable.html" />
-<link rel="import" href="../vz-line-chart/dragZoomInteraction.html" />
-
-<script src="panZoomDragLayer.js"></script>
-
-<dom-module id="vz-pan-zoom-style">
-  <template>
+customElements.whenDefined('dom-module').then(() => {
+  const styleElement = document.createElement('dom-module');
+  styleElement.innerHTML = `
     <style>
       .help {
         align-items: center;
@@ -57,5 +50,6 @@ limitations under the License.
         }
       }
     </style>
-  </template>
-</dom-module>
+  `;
+  styleElement.register('vz-pan-zoom-style');
+});
