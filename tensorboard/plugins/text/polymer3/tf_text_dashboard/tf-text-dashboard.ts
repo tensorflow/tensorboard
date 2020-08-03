9e400342cab9ba72573a141346823eb47376c8bc
TensorBoard Gardener <tensorboard-gardener@google.com>
2020-08-03 18:12:51 +0000

    Integrate 40fe4109970b95058476822b006d1ae9a4944093

--- tensorboard/plugins/text/polymer3/tf_text_dashboard/tf-text-dashboard.ts
+++ tensorboard/plugins/text/polymer3/tf_text_dashboard/tf-text-dashboard.ts
@@ -25,11 +25,11 @@ import {RequestManager} from '../../../../components_polymer3/tf_backend/request
 import {categorizeRunTagCombinations} from '../../../../components_polymer3/tf_categorization_utils/categorizationUtils';
 import {getRouter} from '../../../../components_polymer3/tf_backend/router';
 
-import '../../../../components_polymer3/tf-categorization-utils/tf-tag-filterer';
-import '../../../../components_polymer3/tf-dashboard-common/dashboard-style';
-import '../../../../components_polymer3/tf-dashboard-common/tf-dashboard-layout';
-import '../../../../components_polymer3/tf-paginated-view/tf-category-paginated-view';
-import '../../../../components_polymer3/tf-runs-selector/tf-runs-selector';
+import '../../../../components_polymer3/tf_categorization_utils/tf-tag-filterer';
+import '../../../../components_polymer3/tf_dashboard_common/dashboard-style';
+import '../../../../components_polymer3/tf_dashboard_common/tf-dashboard-layout';
+import '../../../../components_polymer3/tf_paginated_view/tf-category-paginated-view';
+import '../../../../components_polymer3/tf_runs_selector/tf-runs-selector';
 
 import './tf-text-loader';
 
@@ -131,6 +131,7 @@ class TfTextDashboard extends LegacyElementMixin(PolymerElement) {
   _requestManager = new RequestManager();
 
   ready() {
+    super.ready();
     if (this.reloadOnReady) this.reload();
   }
 
