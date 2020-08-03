b965e759047cd011973de9191163444a3d538d51
Stephan Lee <stephanwlee@gmail.com>
2020-08-02 21:45:44 -0700

    migrate: projector (#3933)
    The plugin is fully function: `bazel run tensorboard/plugins/polymer3/vz_projector:standalone`
    works end to end.

--- tensorboard/plugins/projector/polymer3/vz_projector/analyticsLogger.ts
+++ tensorboard/plugins/projector/polymer3/vz_projector/analyticsLogger.ts
@@ -12,56 +12,52 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-namespace vz_projector {
-  export class AnalyticsLogger {
-    private eventLogging: boolean;
-    private pageViewLogging: boolean;
+import {ProjectionType} from './data';
 
-    /**
-     * Constructs an event logger using Google Analytics. It assumes there is a
-     * Google Analytics script added to the page elsewhere. If there is no such
-     * script, the logger acts as a no-op.
-     *
-     * @param pageViewLogging Whether to log page views.
-     * @param eventLogging Whether to log user interaction.
-     */
-    constructor(pageViewLogging: boolean, eventLogging: boolean) {
-      if (typeof ga === 'undefined' || ga == null) {
-        this.eventLogging = false;
-        this.pageViewLogging = false;
-        return;
-      }
-      this.eventLogging = eventLogging;
-      this.pageViewLogging = pageViewLogging;
+export class AnalyticsLogger {
+  private eventLogging: boolean;
+  private pageViewLogging: boolean;
+  /**
+   * Constructs an event logger using Google Analytics. It assumes there is a
+   * Google Analytics script added to the page elsewhere. If there is no such
+   * script, the logger acts as a no-op.
+   *
+   * @param pageViewLogging Whether to log page views.
+   * @param eventLogging Whether to log user interaction.
+   */
+  constructor(pageViewLogging: boolean, eventLogging: boolean) {
+    if (typeof ga === 'undefined' || ga == null) {
+      this.eventLogging = false;
+      this.pageViewLogging = false;
+      return;
     }
-
-    logPageView(pageTitle: string) {
-      if (this.pageViewLogging) {
-        // Always send a page view.
-        ga('send', {hitType: 'pageview', page: `/v/${pageTitle}`});
-      }
-    }
-
-    logProjectionChanged(projection: ProjectionType) {
-      if (this.eventLogging) {
-        ga('send', {
-          hitType: 'event',
-          eventCategory: 'Projection',
-          eventAction: 'click',
-          eventLabel: projection,
-        });
-      }
-    }
-
-    logWebGLDisabled() {
-      if (this.eventLogging) {
-        ga('send', {
-          hitType: 'event',
-          eventCategory: 'Error',
-          eventAction: 'PageLoad',
-          eventLabel: 'WebGL_disabled',
-        });
-      }
+    this.eventLogging = eventLogging;
+    this.pageViewLogging = pageViewLogging;
+  }
+  logPageView(pageTitle: string) {
+    if (this.pageViewLogging) {
+      // Always send a page view.
+      ga('send', {hitType: 'pageview', page: `/v/${pageTitle}`});
     }
   }
-} // namespace vz_projector
+  logProjectionChanged(projection: ProjectionType) {
+    if (this.eventLogging) {
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'Projection',
+        eventAction: 'click',
+        eventLabel: projection,
+      });
+    }
+  }
+  logWebGLDisabled() {
+    if (this.eventLogging) {
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'Error',
+        eventAction: 'PageLoad',
+        eventLabel: 'WebGL_disabled',
+      });
+    }
+  }
+}
