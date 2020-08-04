29f7ca18d18515e0d1c0396605910f33070ac3da
Alex Bäuerle <abauerle@google.com>
2020-08-04 11:03:17 +0200

    License Added in many Files

--- tensorboard/webapp/plugins/npmi/actions/result_actions.ts
+++ tensorboard/webapp/plugins/npmi/actions/result_actions.ts
@@ -1,3 +1,17 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
 import {createAction, props} from '@ngrx/store';
 import {ValueListing, MetricListing} from '../store/npmi_types';
 
@@ -5,13 +19,11 @@ import {ValueListing, MetricListing} from '../store/npmi_types';
 // https://github.com/bazelbuild/rules_nodejs/issues/1013
 /** @typehack */ import * as _typeHackModels from '@ngrx/store/src/models';
 
-export const valuesRequested = createAction(
-  '[NPMI] nPMI Values Requested'
-);
+export const valuesRequested = createAction('[NPMI] nPMI Values Requested');
 
 export const valuesLoaded = createAction(
   '[NPMI] nPMI Values Loaded',
-  props<{values: ValueListing, metrics: MetricListing}>(),
+  props<{values: ValueListing; metrics: MetricListing}>()
 );
 
 export const valuesRequestFailed = createAction(
