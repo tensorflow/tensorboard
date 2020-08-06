a44d50ea3486ecadd86892dcb09eef29490bf9b6
Mani Varadarajan <maniv@google.com>
2020-08-06 10:40:13 -0700

    checkpoint

--- tensorboard/components_polymer3/polymer3_lib.ts
+++ tensorboard/components_polymer3/polymer3_lib.ts
@@ -15,6 +15,7 @@ limitations under the License.
 
 // Add dashboards here.
 import '../plugins/profile_redirect/polymer3/tf_profile_redirect_dashboard/tf-profile-redirect-dashboard';
+import '../plugins/image/polymer3/tf_image_dashboard/tf-image-dashboard';
 import '../plugins/text/polymer3/tf_text_dashboard/tf-text-dashboard';
 
 // Exported Polymer <-> Angular interop (to be removed).
