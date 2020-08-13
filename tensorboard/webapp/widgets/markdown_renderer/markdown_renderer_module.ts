b6f78c364c1b9158d4295db6bc9d67ca4b6af4ec
Preston Fossee <pjfossee@google.com>
2020-08-13 00:49:47 +0000

    addressing changes in comments

--- tensorboard/webapp/widgets/markdown_renderer/markdown_renderer_module.ts
+++ tensorboard/webapp/widgets/markdown_renderer/markdown_renderer_module.ts
@@ -16,11 +16,11 @@ limitations under the License.
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 
-import {MarkdownRenderer} from './markdown_renderer_component';
+import {MarkdownRendererComponent} from './markdown_renderer_component';
 
 @NgModule({
-  declarations: [MarkdownRenderer],
+  declarations: [MarkdownRendererComponent],
   imports: [CommonModule],
-  exports: [MarkdownRenderer],
+  exports: [MarkdownRendererComponent],
 })
 export class MarkdownRendererModule {}
