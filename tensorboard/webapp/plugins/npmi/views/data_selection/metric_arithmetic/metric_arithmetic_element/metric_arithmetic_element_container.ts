f8099873bf7ff6307431a4d4610e8cfbfe8daed8
Alex Bäuerle <abauerle@google.com>
2020-08-25 14:40:00 +0200

    Added Tests

--- tensorboard/webapp/plugins/npmi/views/data_selection/metric_arithmetic/metric_arithmetic_element/metric_arithmetic_element_container.ts
+++ tensorboard/webapp/plugins/npmi/views/data_selection/metric_arithmetic/metric_arithmetic_element/metric_arithmetic_element_container.ts
@@ -1,3 +1,17 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
 import {Component, ChangeDetectionStrategy, Input, OnInit} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 
@@ -14,7 +28,7 @@ import * as npmiActions from '../../../../actions';
   template: `
     <metric-arithmetic-element-component
       [metric]="metric"
-      [filterExtremes]="filterExtremes$ | async"
+      [filterValues]="filterValues"
       [minFilterValid]="minFilterValid"
       [maxFilterValid]="maxFilterValid"
       (onRemove)="remove($event)"
@@ -25,7 +39,8 @@ import * as npmiActions from '../../../../actions';
 })
 export class MetricArithmeticElementContainer {
   @Input() metric!: string;
-  readonly filterExtremes$ = this.store.pipe(select(getMetricFilters)).pipe(
+  filterValues = {min: '-1.0', max: '1.0'};
+  readonly filterValueSetter$ = this.store.pipe(select(getMetricFilters)).pipe(
     map((filters) => {
       const filter = filters[this.metric];
       const min = filter.includeNaN
@@ -33,7 +48,7 @@ export class MetricArithmeticElementContainer {
         : this.rounded(filter.min).toString();
       const max =
         filter.max < filter.min ? 'NaN' : this.rounded(filter.max).toString();
-      return {min: min, max: max};
+      this.filterValues = {min: min, max: max};
     })
   );
   minFilterValid = true;
@@ -46,8 +61,12 @@ export class MetricArithmeticElementContainer {
   }
 
   filterChange(newValues: {min: string; max: string}) {
+    this.filterValues = {min: newValues.min, max: newValues.max};
     this.minFilterValid = this.entryValid(newValues.min);
-    this.maxFilterValid = this.entryValid(newValues.max);
+    this.maxFilterValid =
+      this.entryValid(newValues.max) &&
+      (parseFloat(newValues.max) >= parseFloat(newValues.min) ||
+        newValues.min == 'NaN');
     if (this.minFilterValid && this.maxFilterValid) {
       let min = newValues.min === 'NaN' ? -1 : parseFloat(newValues.min);
       let max = newValues.max === 'NaN' ? -2 : parseFloat(newValues.max);
---
+++ tensorboard/webapp/plugins/npmi/views/data_selection/metric_arithmetic/metric_arithmetic_element/metric_arithmetic_element_test.ts
@@ -0,0 +1,236 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+/**
+ * Unit tests for the metric arithmetic element.
+ */
+import {TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+
+import {Action, Store} from '@ngrx/store';
+import {provideMockStore, MockStore} from '@ngrx/store/testing';
+
+import {State} from '../../../../../../app_state';
+import {getMetricFilters} from '../../../../store/npmi_selectors';
+import * as npmiActions from '../../../../actions';
+import {appStateFromNpmiState, createNpmiState} from '../../../../testing';
+import {MetricArithmeticElementContainer} from './metric_arithmetic_element_container';
+import {MetricArithmeticElementComponent} from './metric_arithmetic_element_component';
+
+/** @typehack */ import * as _typeHackStore from '@ngrx/store';
+
+describe('Npmi Metric Arithmetic Element Container', () => {
+  let store: MockStore<State>;
+  let dispatchedActions: Action[];
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        MetricArithmeticElementContainer,
+        MetricArithmeticElementComponent,
+      ],
+      imports: [
+        CommonModule,
+        FormsModule,
+        MatInputModule,
+        MatAutocompleteModule,
+      ],
+      providers: [
+        provideMockStore({
+          initialState: appStateFromNpmiState(createNpmiState()),
+        }),
+      ],
+    }).compileComponents();
+
+    store = TestBed.inject<Store<State>>(Store) as MockStore<State>;
+
+    dispatchedActions = [];
+    spyOn(store, 'dispatch').and.callFake((action: Action) => {
+      dispatchedActions.push(action);
+    });
+    store.overrideSelector(getMetricFilters, {
+      'npmi@test': {max: 1.0, min: -1.0, includeNaN: false},
+    });
+  });
+
+  it('renders npmi metric arithmetic element component', () => {
+    const fixture = TestBed.createComponent(MetricArithmeticElementContainer);
+    fixture.componentInstance.metric = 'npmi@test';
+    fixture.detectChanges();
+
+    const filterDiv = fixture.debugElement.query(
+      By.css('.metric-arithmetic-element-text')
+    );
+    expect(filterDiv.nativeElement.textContent.trim()).toBe('npmi@test');
+  });
+
+  it('removes metric on click on remove button', () => {
+    const fixture = TestBed.createComponent(MetricArithmeticElementContainer);
+    fixture.componentInstance.metric = 'npmi@test';
+    fixture.detectChanges();
+
+    const removeButton = fixture.debugElement.query(
+      By.css('.metric-arithmetic-element-remove')
+    );
+    removeButton.nativeElement.click();
+    expect(dispatchedActions).toEqual([
+      npmiActions.npmiRemoveMetricFilter({metric: 'npmi@test'}),
+    ]);
+  });
+
+  describe('input interaction', () => {
+    describe('min input', () => {
+      it('changes the filter when interacting with the input', () => {
+        const fixture = TestBed.createComponent(
+          MetricArithmeticElementContainer
+        );
+        fixture.componentInstance.metric = 'npmi@test';
+        fixture.detectChanges();
+
+        const inputs = fixture.debugElement.queryAll(By.css('input'));
+        const input = inputs[0];
+        input.nativeElement.focus();
+        fixture.detectChanges();
+
+        input.nativeElement.value = '0.2';
+        input.nativeElement.dispatchEvent(
+          new InputEvent('input', {data: '0.2'})
+        );
+        fixture.detectChanges();
+
+        expect(dispatchedActions).toEqual([
+          npmiActions.npmiChangeMetricFilter({
+            metric: 'npmi@test',
+            max: 1.0,
+            min: 0.2,
+            includeNaN: false,
+          }),
+        ]);
+      });
+
+      it('changes the filter to NaN', () => {
+        const fixture = TestBed.createComponent(
+          MetricArithmeticElementContainer
+        );
+        fixture.componentInstance.metric = 'npmi@test';
+        fixture.detectChanges();
+
+        const inputs = fixture.debugElement.queryAll(By.css('input'));
+        const input = inputs[0];
+        input.nativeElement.focus();
+        fixture.detectChanges();
+
+        input.nativeElement.value = 'NaN';
+        input.nativeElement.dispatchEvent(
+          new InputEvent('input', {data: 'NaN'})
+        );
+        fixture.detectChanges();
+
+        expect(dispatchedActions).toEqual([
+          npmiActions.npmiChangeMetricFilter({
+            metric: 'npmi@test',
+            max: 1.0,
+            min: -1.0,
+            includeNaN: true,
+          }),
+        ]);
+      });
+
+      it('does not change the filter when an incorrect value is entered', () => {
+        const fixture = TestBed.createComponent(
+          MetricArithmeticElementContainer
+        );
+        fixture.componentInstance.metric = 'npmi@test';
+        fixture.detectChanges();
+
+        const inputs = fixture.debugElement.queryAll(By.css('input'));
+        const input = inputs[0];
+        input.nativeElement.focus();
+        fixture.detectChanges();
+
+        input.nativeElement.value = '-';
+        input.nativeElement.dispatchEvent(new InputEvent('input', {data: '-'}));
+        fixture.detectChanges();
+
+        expect(dispatchedActions).toEqual([]);
+
+        const invalidInput = fixture.debugElement.query(
+          By.css('.valueInvalid')
+        );
+        expect(invalidInput).toBeTruthy();
+      });
+    });
+
+    describe('max input', () => {
+      it('changes the filter when interacting with the input', () => {
+        const fixture = TestBed.createComponent(
+          MetricArithmeticElementContainer
+        );
+        fixture.componentInstance.metric = 'npmi@test';
+        fixture.detectChanges();
+
+        const inputs = fixture.debugElement.queryAll(By.css('input'));
+        const input = inputs[1];
+        input.nativeElement.focus();
+        fixture.detectChanges();
+
+        input.nativeElement.value = '0.2';
+        input.nativeElement.dispatchEvent(
+          new InputEvent('input', {data: '0.2'})
+        );
+        fixture.detectChanges();
+
+        expect(dispatchedActions).toEqual([
+          npmiActions.npmiChangeMetricFilter({
+            metric: 'npmi@test',
+            max: 0.2,
+            min: -1.0,
+            includeNaN: false,
+          }),
+        ]);
+      });
+
+      it('does not change the filter when a value is entered that does not go with the min value', () => {
+        const fixture = TestBed.createComponent(
+          MetricArithmeticElementContainer
+        );
+        fixture.componentInstance.metric = 'npmi@test';
+        fixture.detectChanges();
+
+        const inputs = fixture.debugElement.queryAll(By.css('input'));
+        const input = inputs[1];
+        input.nativeElement.focus();
+        fixture.detectChanges();
+
+        input.nativeElement.value = 'NaN';
+        input.nativeElement.dispatchEvent(
+          new InputEvent('input', {data: 'NaN'})
+        );
+        fixture.detectChanges();
+
+        expect(dispatchedActions).toEqual([]);
+
+        const invalidInput = fixture.debugElement.query(
+          By.css('.valueInvalid')
+        );
+        expect(invalidInput).toBeTruthy();
+      });
+    });
+  });
+});
