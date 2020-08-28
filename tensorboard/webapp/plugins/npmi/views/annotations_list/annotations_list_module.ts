14aae16cb9bdb02ca3901c4f09108aea28840b37
Alex Bäuerle <abauerle@google.com>
2020-08-28 19:22:53 +0200

    Finished PR

--- tensorboard/webapp/plugins/npmi/views/annotations_list/annotations_list_module.ts
+++ tensorboard/webapp/plugins/npmi/views/annotations_list/annotations_list_module.ts
@@ -14,13 +14,6 @@ limitations under the License.
 ==============================================================================*/
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AnnotationsListComponent} from './annotations_list_component';
 import {AnnotationsListContainer} from './annotations_list_container';
@@ -28,16 +21,7 @@ import {AnnotationsListToolbarModule} from './annotations_list_toolbar/annotatio
 
 @NgModule({
   declarations: [AnnotationsListComponent, AnnotationsListContainer],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ScrollingModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTooltipModule,
-    BrowserAnimationsModule,
-    AnnotationsListToolbarModule,
-  ],
+  imports: [CommonModule, AnnotationsListToolbarModule],
   exports: [AnnotationsListContainer],
 })
 export class AnnotationsListModule {}
