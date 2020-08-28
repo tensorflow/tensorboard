0368a144918475c9ac34d2e3f4d1efe72dedef62
William Chargin <wchargin@gmail.com>
2020-08-28 12:18:36 -0700

    test_util: remove obsolete `run_v2_only` decorator
    Summary:
    This decorator was only used in one test, and that test is not
    compatible with no-TensorFlow mode, so as of #2507 it will only ever run
    in TF 2.x. We don’t plan to support any more 1.x-only tests in the
    future, so this decorator has served its purpose and may now retire.
    
    Test Plan:
    CI suffices.
    
    wchargin-branch: remove-v2only-decorator
    wchargin-source: 3b39fd324d976ba77871cd90390ab7f7771895bf

--- tensorboard/util/test_util.py
+++ tensorboard/util/test_util.py
@@ -27,9 +27,6 @@ import unittest
 
 import tensorflow as tf
 
-# See discussion on issue #1996 for private module import justification.
-from tensorflow.python import tf2 as tensorflow_python_tf2
-
 from tensorboard.compat.proto import event_pb2
 from tensorboard.compat.proto import graph_pb2
 from tensorboard.compat.proto import meta_graph_pb2
@@ -209,10 +206,3 @@ def _run_conditionally(guard, name, default_reason=None):
         return unittest.skipUnless(guard(), reason)
 
     return _impl
-
-
-run_v2_only = _run_conditionally(
-    lambda: tensorflow_python_tf2.enabled(),
-    name="run_v2_only",
-    default_reason="Test only appropriate for TensorFlow v2",
-)
