c44bcd7df0bb6b40dae19365d13f4398cdbdf37a
Alex Bäuerle <abauerle@google.com>
2020-09-01 16:27:34 +0200

    Minor Changes

--- tensorboard/webapp/plugins/npmi/views/annotations_list/legend/legend_element/legend_element_component.ts
+++ tensorboard/webapp/plugins/npmi/views/annotations_list/legend/legend_element/legend_element_component.ts
@@ -19,6 +19,8 @@ import {
   AfterViewInit,
   OnChanges,
   SimpleChanges,
+  ViewChild,
+  ElementRef,
 } from '@angular/core';
 import * as d3 from 'd3';
 
@@ -32,33 +34,16 @@ export class LegendElementComponent implements AfterViewInit, OnChanges {
   @Input() text!: string;
   @Input() color!: string;
   @Input() shape!: string;
+  @ViewChild('glyph', {static: true, read: ElementRef})
+  private readonly glyphSVG!: ElementRef<SVGElement>;
   // Drawing containers
   private svg: any;
   private mainContainer: any;
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.redraw();
-  }
-
   ngAfterViewInit(): void {
-    this.svg = d3.select(
-      `#annotations-legend-element-glyph-${CSS.escape(this.text)}`
-    );
+    this.svg = d3.select(this.glyphSVG.nativeElement);
     this.mainContainer = this.svg.append('g');
-    this.redraw();
-  }
-
-  private redraw() {
-    if (this.initialized()) {
-      this.draw();
-    }
-  }
-
-  private initialized() {
-    if (this.svg === undefined) {
-      return false;
-    }
-    return true;
+    this.draw();
   }
 
   private draw() {
