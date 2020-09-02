b6ad7b3725ecd2f2406058f0f5e277c961f65e63
Alex Bäuerle <abauerle@google.com>
2020-09-02 17:55:53 +0200

    Removed Unused Function

--- tensorboard/webapp/plugins/npmi/views/violin_filters/violin_filter/violin_filter_component.ts
+++ tensorboard/webapp/plugins/npmi/views/violin_filters/violin_filter/violin_filter_component.ts
@@ -90,17 +90,17 @@ export class ViolinFilterComponent implements AfterViewInit, OnChanges {
   private readonly area = d3
     .area<ViolinBin>()
     .x0(
-      function(this: ViolinFilterComponent, d: ViolinBin) {
+      function (this: ViolinFilterComponent, d: ViolinBin) {
         return this.xScaleNum(-d.length);
       }.bind(this)
     )
     .x1(
-      function(this: ViolinFilterComponent, d: ViolinBin) {
+      function (this: ViolinFilterComponent, d: ViolinBin) {
         return this.xScaleNum(d.length);
       }.bind(this)
     )
     .y(
-      function(this: ViolinFilterComponent, d: ViolinBin) {
+      function (this: ViolinFilterComponent, d: ViolinBin) {
         if (d.x0! === -Infinity) {
           return this.chartHeight - this.drawMargin.top;
         }
@@ -212,8 +212,6 @@ export class ViolinFilterComponent implements AfterViewInit, OnChanges {
       .call(this.xAxis!);
   }
 
-  drawLines() {}
-
   drawPlot() {
     const plots = this.dotsGroup
       .selectAll('.violin-plot')
@@ -225,7 +223,7 @@ export class ViolinFilterComponent implements AfterViewInit, OnChanges {
       .attr('class', 'violin-plot')
       .style(
         'stroke',
-        function(
+        function (
           this: ViolinFilterComponent,
           d: [string, ViolinBin[]]
         ): string {
@@ -234,7 +232,7 @@ export class ViolinFilterComponent implements AfterViewInit, OnChanges {
       )
       .style(
         'fill',
-        function(
+        function (
           this: ViolinFilterComponent,
           d: [string, ViolinBin[]]
         ): string {
@@ -243,14 +241,14 @@ export class ViolinFilterComponent implements AfterViewInit, OnChanges {
       )
       .attr(
         'transform',
-        function(
+        function (
           this: ViolinFilterComponent,
           d: [string, ViolinBin[]]
         ): string {
           return `translate(${this.xScale(d[0])}, 0)`;
         }.bind(this)
       )
-      .datum(function(d: [string, ViolinBin[]]): ViolinBin[] {
+      .datum(function (d: [string, ViolinBin[]]): ViolinBin[] {
         return d[1];
       })
       .attr('d', this.area);
@@ -258,14 +256,14 @@ export class ViolinFilterComponent implements AfterViewInit, OnChanges {
     plots
       .attr(
         'transform',
-        function(
+        function (
           this: ViolinFilterComponent,
           d: [string, ViolinBin[]]
         ): string {
           return `translate(${this.xScale(d[0])}, 0)`;
         }.bind(this)
       )
-      .datum(function(d: [string, ViolinBin[]]): ViolinBin[] {
+      .datum(function (d: [string, ViolinBin[]]): ViolinBin[] {
         return d[1];
       })
       .attr('d', this.area);
