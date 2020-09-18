84aacde0bfe55fe8a14ce06927926f7aa2d981fc
Stanley Bileschi <bileschi@google.com>
2020-09-18 16:02:30 -0400

    fixup imports

--- tensorboard/plugins/scalar/scalars_demo_eager.py
+++ tensorboard/plugins/scalar/scalars_demo_eager.py
@@ -18,20 +18,15 @@ This short example illustrates how to use the scalar writing in eager
 mode TensorFlow 2.
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
+import random
 
 from absl import app
 from absl import flags
 from absl import logging
-
-from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
+
 from tensorboard.plugins.scalar import summary
-import random
 
 FLAGS = flags.FLAGS
 
