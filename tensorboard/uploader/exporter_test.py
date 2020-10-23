0e99dae1902e3dea57567278af8acbb8aca7f7cb
ericdnielsen <ericdnielsen@gmail.com>
2020-10-23 15:09:03 -0400

    lint

--- tensorboard/uploader/exporter_test.py
+++ tensorboard/uploader/exporter_test.py
@@ -456,9 +456,11 @@ class TensorBoardExporterTest(tb_test.TestCase):
             tensors = [tensors[key] for key in tensors.keys()]
             self.assertLen(tensors, 2)
             np.testing.assert_array_equal(
-                tensors[0], np.array(["a", "a", "a"], "|S"))
+                tensors[0], np.array(["a", "a", "a"], "|S")
+            )
             np.testing.assert_array_equal(
-                tensors[1], np.array(["aa", "aa", "aa"], "|S"))
+                tensors[1], np.array(["aa", "aa", "aa"], "|S")
+            )
 
     def test_e2e_success_case_with_blob_sequence_data(self):
         """Covers exporting of complete and incomplete blob sequences
