681f4529e7e654dddfb96ca4ed14d99b94134423
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2020-11-03 17:38:43 +0100

    Now Correctly filters out Null Values (#4263)
    The filtering of null values was not working correctly. This has now been fixed.

--- tensorboard/webapp/plugins/npmi/util/filter_annotations.ts
+++ tensorboard/webapp/plugins/npmi/util/filter_annotations.ts
@@ -91,8 +91,8 @@ function checkValuesPassMetricArithmetic(
     }
     return valueDataElements.some((valueDataElement) => {
       if (valueDataElement.metric === stripMetricString(element.metric)) {
-        if (!valueDataElement.nPMIValue && metricFilter.includeNaN) {
-          return true;
+        if (valueDataElement.nPMIValue === null) {
+          return metricFilter.includeNaN;
         } else {
           return (
             valueDataElement.nPMIValue! <= metricFilter.max &&
