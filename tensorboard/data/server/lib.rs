512d351755265678ebce4a3ff44e25bc48007c2c
William Chargin <wchargin@gmail.com>
2020-11-06 18:07:54 -0800

    [update patch]
    wchargin-branch: rust-rm-placeholders
    wchargin-source: 7e2682889aa8303416f1ebf74b841edf0d01cafd

--- tensorboard/data/server/lib.rs
+++ tensorboard/data/server/lib.rs
@@ -13,28 +13,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-<<<<<<< HEAD
 //! Core functionality for TensorBoard data loading.
-=======
-/// Adds two integers together and returns the result. Must not overflow.
-///
-/// # Examples
-///
-/// ```
-/// assert_eq!(rustboard_core::add(20, 22), 42);
-/// ```
-pub fn add(x: i32, y: i32) -> i32 {
-    x + y
-}
-
-#[cfg(test)]
-mod test {
-    use super::add;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(2, 4), 6);
-        assert_eq!(add(999, 999), 1998);
-    }
-}
->>>>>>> 78df9b17477e9d114ae31b0c03212e6fec6b89a6
