d743728d8707cce167b836302044287a6e6a7f8f
William Chargin <wchargin@gmail.com>
2020-11-11 18:43:05 -0800

    [update patch]
    wchargin-branch: rust-demo-tonic-server
    wchargin-source: 2181a31c718433d5113cbb1fe2e51c7c98193cc1

--- tensorboard/data/server/main.rs
+++ tensorboard/data/server/main.rs
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#![allow(clippy::needless_update)] // https://github.com/rust-lang/rust-clippy/issues/6323
+
 use tonic::{transport::Server, Request, Response};
 
 use rustboard_core::proto::demo as pb;
