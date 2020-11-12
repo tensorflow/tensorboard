3ef14763dfdf0f9485e677f102e93eb54b0840a9
William Chargin <wchargin@gmail.com>
2020-11-11 21:02:59 -0800

    [update patch]
    wchargin-branch: rust-demo-tonic-server
    wchargin-source: 89356565a9557266c069dcfdf0422f2e2225e3ab

--- tensorboard/data/server/gen_protos_tool.rs
+++ tensorboard/data/server/gen_protos_tool.rs
@@ -28,7 +28,7 @@ fn main() -> std::io::Result<()> {
     };
     tonic_build::configure()
         .out_dir(&out_dir)
-<<<<<<< HEAD
+        .format(false) // don't run `rustfmt`; shouldn't be needed to build
         .compile(
             &[
                 "tensorboard/compat/proto/event.proto",
@@ -36,10 +36,6 @@ fn main() -> std::io::Result<()> {
             ],
             &["."],
         )
-=======
-        .format(false) // don't run `rustfmt`; shouldn't be needed to build
-        .compile(&["tensorboard/compat/proto/event.proto"], &["."])
->>>>>>> 6a5ef83ffd54f1184fb441e360aed7b4c36b4d32
         .expect("compile_protos");
     Ok(())
 }
