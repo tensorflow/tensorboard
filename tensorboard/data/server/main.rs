5f3b099444fdb69fb3988e312cfbc8408812e303
William Chargin <wchargin@gmail.com>
2020-11-25 09:22:51 -0800

    rust: implement `ListRuns` RPC (#4385)
    Summary:
    This patch connects the `//tensorboard/data/server` main module to the
    `Commit` and `LogdirLoader` infrastructure, and implements `ListRuns` as
    a proof of concept.
    
    For now, we just take a logdir path as a positional command line
    argument. A future patch will bring nicer command-line argument parsing.
    
    Test Plan:
    Unit tests included. Note that testing these data provider methods is
    nice and easy: you just call them. No mocking or networking required.
    
    For a manual `grpc_cli` test, start just the data server, and run:
    
    ```
    $ grpc_cli --channel_creds_type=insecure \
    >   --protofiles tensorboard/data/proto/data_provider.proto \
    >   call localhost:6806 TensorBoardDataProvider.ListRuns '' 2>/dev/null
    runs {
      name: "lr_1E-03,conv=1,fc=2"
      start_time: 1563406327
    }
    // etc.
    ```
    
    For an end-to-end test, cherry-pick #4356, then run both the data server
    and a copy of TensorBoard concurrently:
    
    ```
    bazel run //tensorboard/data/server -- ~/tensorboard_data/mnist &
    bazel run //tensorboard -- \
        --bind_all \
        --generic_data true \
        --grpc_data_provider localhost:6806 \
        ;
    ```
    
    Then navigate to `localhost:6006/data/runs` and note that the response
    includes a correct list of runs.
    
    wchargin-branch: rust-listruns

--- tensorboard/data/server/main.rs
+++ tensorboard/data/server/main.rs
@@ -13,15 +13,44 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+use std::path::PathBuf;
+use std::time::{Duration, Instant};
 use tonic::transport::Server;
 
+use rustboard_core::commit::Commit;
+use rustboard_core::logdir::LogdirLoader;
 use rustboard_core::proto::tensorboard::data::tensor_board_data_provider_server::TensorBoardDataProviderServer;
 use rustboard_core::server::DataProviderHandler;
+
+const RELOAD_INTERVAL: Duration = Duration::from_secs(5);
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::0]:6806".parse::<std::net::SocketAddr>()?;
-    let handler = DataProviderHandler;
+    let logdir = match std::env::args_os().nth(1) {
+        Some(d) => PathBuf::from(d),
+        None => {
+            eprintln!("fatal: specify logdir as first command-line argument");
+            std::process::exit(1);
+        }
+    };
+
+    // Leak the commit object, since the Tonic server must have only 'static references. This only
+    // leaks the outer commit structure (of constant size), not the pointers to the actual data.
+    let commit: &'static Commit = Box::leak(Box::new(Commit::new()));
+    std::thread::spawn(move || {
+        let mut loader = LogdirLoader::new(commit, logdir);
+        loop {
+            eprintln!("beginning load cycle");
+            let start = Instant::now();
+            loader.reload();
+            let end = Instant::now();
+            eprintln!("finished load cycle ({:?})", end - start);
+            std::thread::sleep(RELOAD_INTERVAL);
+        }
+    });
+
+    let handler = DataProviderHandler { commit };
     Server::builder()
         .add_service(TensorBoardDataProviderServer::new(handler))
         .serve(addr)
