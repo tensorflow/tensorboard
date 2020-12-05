3e4d7b8e6f0691d2d1a1770db17a009e371a372c
Stephan Lee <stephanwlee@gmail.com>
2020-12-04 18:00:09 -0800

    ng: dispatch coreLoaded after router is initialized
    Only after the router is initialized, we can make sure we are at the
    correct routes only at which point we should start to initialize and
    make requests. Otherwise, we can make requests at the wrong paths which
    may/may-not be correct.
    
    With this change, we fire the coreLoaded after router is initialized
    which is confirmed by checking for non-nullness of the activeRoute.

--- tensorboard/webapp/app_container.ts
+++ tensorboard/webapp/app_container.ts
@@ -12,26 +12,38 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-import {Component, OnInit, ViewContainerRef} from '@angular/core';
+import {Component, ViewContainerRef} from '@angular/core';
 import {Store} from '@ngrx/store';
+import {filter, take} from 'rxjs/operators';
+
+import {getActiveRoute} from './selectors';
 import {coreLoaded} from './core/actions';
-import {State} from './core/store';
+import {State} from './app_state';
 
 @Component({
   selector: 'tb-webapp',
   templateUrl: './app_container.ng.html',
   styleUrls: ['./app_container.css'],
 })
-export class AppContainer implements OnInit {
+export class AppContainer {
   // vcRef is required by ngx-color-picker in order for it to place the popup
   // in the root node in a modal mode.
   // https://github.com/zefoy/ngx-color-picker/blob/94a7c862bb61d7207f21281526fcd94453219b54/projects/lib/src/lib/color-picker.directive.ts#L168-L175
   constructor(
     private readonly store: Store<State>,
     readonly vcRef: ViewContainerRef
-  ) {}
-
-  ngOnInit() {
-    this.store.dispatch(coreLoaded());
+  ) {
+    // Wait for route to be initialized before dispatching a coreLoaded.
+    this.store
+      .select(getActiveRoute)
+      .pipe(
+        filter((route) => Boolean(route)),
+        take(1)
+      )
+      .subscribe(() => {
+        // TODO(stephanwlee): deprecated coreLoaded and use the router actions when all
+        // apps are using the router.s
+        this.store.dispatch(coreLoaded());
+      });
   }
 }
