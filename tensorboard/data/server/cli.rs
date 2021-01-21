6e022952ab442231974a3754c34985f94fe3efbe
William Chargin <wchargin@gmail.com>
2021-01-20 17:57:05 -0800

    tensorboard-data-server 0.2.0

--- tensorboard/data/server/cli.rs
+++ tensorboard/data/server/cli.rs
@@ -36,7 +36,7 @@ use crate::server::DataProviderHandler;
 use data::tensor_board_data_provider_server::TensorBoardDataProviderServer;
 
 #[derive(Clap, Debug)]
-#[clap(name = "rustboard", version = "0.2.0-alpha.0")]
+#[clap(name = "rustboard", version = "0.2.0")]
 struct Opts {
     /// Log directory to load
     ///
