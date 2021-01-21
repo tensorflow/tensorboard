b9e344e80d9be5f607201c5ac4905dfc81b5a2d4
Stephan Lee <stephanwlee@gmail.com>
2021-01-20 21:14:16 -0800

    infra: create dev target
    New `tensorboard:dev` target provides a development instance of the
    application which optimizes for development experience. While the
    changes are omitted in this commit for brevity, we are looking to stream
    line the FE building to optimize for its incremental build time.

---
+++ tensorboard/main_dev.py
@@ -0,0 +1,54 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""TensorBoard main module.
+
+This module ties together `tensorboard.program` and
+`tensorboard.default_plugins` to provide standard TensorBoard. It's
+meant to be tiny and act as little other than a config file. Those
+wishing to customize the set of plugins or static assets that
+TensorBoard uses can swap out this file with their own.
+"""
+
+import inspect
+import os
+import sys
+
+from absl import app
+from tensorboard import default
+from tensorboard import main_lib
+from tensorboard import program
+from tensorboard.plugins import base_plugin
+from tensorboard.uploader import uploader_subcommand
+
+
+def run_main():
+    """Initializes flags and calls main()."""
+    main_lib.global_init()
+
+    path = os.path.join(
+        os.path.dirname(inspect.getfile(sys._getframe(1))), "dev_webfiles.zip"
+    )
+
+    tensorboard = program.TensorBoard(
+        default.get_plugins(),
+        lambda: open(path, "rb"),
+        subcommands=[uploader_subcommand.UploaderSubcommand()],
+    )
+
+    try:
+        app.run(tensorboard.main, flags_parser=tensorboard.configure)
+    except base_plugin.FlagsError as e:
+        print("Error: %s" % e, file=sys.stderr)
+        sys.exit(1)
