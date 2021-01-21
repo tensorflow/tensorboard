048129e7c5727fe7b51da59865fdd32cc6d92894
William Chargin <wchargin@gmail.com>
2021-01-20 17:01:27 -0800

    rust: bump data server from 0.1.1 to 0.2.0a0

--- tensorboard/data/server/cli.rs
+++ tensorboard/data/server/cli.rs
@@ -36,7 +36,7 @@ use crate::server::DataProviderHandler;
 use data::tensor_board_data_provider_server::TensorBoardDataProviderServer;
 
 #[derive(Clap, Debug)]
-#[clap(name = "rustboard", version = "0.1.1")]
+#[clap(name = "rustboard", version = "0.2.0-alpha.0")]
 struct Opts {
     /// Log directory to load
     ///
