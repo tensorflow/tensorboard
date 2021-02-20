97fcead8e6422397d45563de6e8e56fe23202749
Jie-Wei Wu <jieweiwu@google.com>
2021-02-19 16:07:16 -0800

    add license

---
+++ tensorboard/webapp/notification_center/index.ts
@@ -0,0 +1,4 @@
+import * as notification_center_container from './_views/notification_center_container';
+
+export const NotificationCenterContainer =
+  notification_center_container.NotificationCenterContainer;
