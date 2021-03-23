548352c7a8911aa2b1ed5f8b6d58182149ac6f93
Jie-Wei Wu <jieweiwu@google.com>
2021-03-22 23:53:42 -0700

    fix test default

--- tensorboard/webapp/notification_center/_redux/testing.ts
+++ tensorboard/webapp/notification_center/_redux/testing.ts
@@ -23,7 +23,7 @@ export function buildNotificationState(
 ): NotificationState {
   return {
     notifications: [],
-    lastReadTimestampInMs: 0,
+    lastReadTimestampInMs: null,
     ...override,
   };
 }
