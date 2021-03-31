ca97f2e6b7b702289440b43eafb09099c9cdf923
Jie-Wei Wu <jieweiwu@google.com>
2021-03-31 16:44:43 -0700

    add routeContexed state

--- tensorboard/webapp/notification_center/_redux/notification_center_reducers.ts
+++ tensorboard/webapp/notification_center/_redux/notification_center_reducers.ts
@@ -13,21 +13,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 import {Action, createReducer, on} from '@ngrx/store';
+import {createRouteContextedState} from '../../app_routing/route_contexted_reducer_helper';
 import * as actions from './notification_center_actions';
 import {
   NotificationState,
   NOTIFICATION_LAST_READ_TIME_KEY,
 } from './notification_center_types';
-import {notificationNotes} from './notification_notes';
 
-const initialState: NotificationState = {
-  notifications: notificationNotes,
-  lastReadTimestampInMs: window.localStorage.getItem(
-    NOTIFICATION_LAST_READ_TIME_KEY
-  )
-    ? parseInt(window.localStorage.getItem(NOTIFICATION_LAST_READ_TIME_KEY)!)
-    : null,
-};
+const {initialState, reducers: routeContextReducer} = createRouteContextedState(
+  {
+    // Backend data.
+    notifications: [],
+    lastReadTimestampInMs: window.localStorage.getItem(
+      NOTIFICATION_LAST_READ_TIME_KEY
+    )
+      ? parseInt(window.localStorage.getItem(NOTIFICATION_LAST_READ_TIME_KEY)!)
+      : null,
+  } as NotificationState,
+  {},
+  (state) => {
+    return {...state};
+  }
+);
 
 const reducer = createReducer(
   initialState,
