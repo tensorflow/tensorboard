586b04804e0ebab4d6bc56fef81506c3e93c1948
Karthik Ramasamy <karthikv2k@users.noreply.github.com>
2021-04-16 16:29:00 -0700

    Adding remote_ip and x_forwarded_for to request context. (#4863)

--- tensorboard/context_test.py
+++ tensorboard/context_test.py
@@ -14,32 +14,53 @@
 # ==============================================================================
 """Tests for tensorboard.context."""
 
+import ipaddress
+
 from tensorboard import auth as auth_lib
 from tensorboard import context
 from tensorboard import test as tb_test
+
+REMOTE_IP = ipaddress.ip_address("192.168.0.1")
+X_FORWARDED_FOR_IPS = (ipaddress.ip_address("2001:db8::"), REMOTE_IP)
 
 
 class RequestContextTest(tb_test.TestCase):
     def test_defaults(self):
         ctx = context.RequestContext()
         self.assertIsInstance(ctx.auth, auth_lib.AuthContext)
+        self.assertEqual(ctx.x_forwarded_for, ())
 
     def test_args(self):
         auth = auth_lib.AuthContext({}, {"REQUEST_METHOD": "GET"})
-        ctx = context.RequestContext(auth=auth)
+        ctx = context.RequestContext(
+            auth=auth, remote_ip=REMOTE_IP, x_forwarded_for=X_FORWARDED_FOR_IPS
+        )
         self.assertEqual(ctx.auth, auth)
+        self.assertEqual(ctx.remote_ip, REMOTE_IP)
+        self.assertEqual(ctx.x_forwarded_for, X_FORWARDED_FOR_IPS)
 
     def test_environ(self):
         environ = {"one": "two", "three": "four"}
         auth = auth_lib.AuthContext({}, environ)
-        self.assertNotEqual(context.from_environ(environ).auth, auth)
+        req_context = context.from_environ(environ)
+        self.assertNotEqual(req_context.auth, auth)
+        self.assertNotEqual(req_context.remote_ip, REMOTE_IP)
+        self.assertNotEqual(req_context.x_forwarded_for, X_FORWARDED_FOR_IPS)
 
         context.set_in_environ(
-            environ, context.from_environ(environ).replace(auth=auth)
+            environ,
+            context.from_environ(environ).replace(
+                auth=auth,
+                remote_ip=REMOTE_IP,
+                x_forwarded_for=X_FORWARDED_FOR_IPS,
+            ),
         )
         self.assertEqual(environ["one"], "two")
         self.assertEqual(environ["three"], "four")
-        self.assertEqual(context.from_environ(environ).auth, auth)
+        req_context = context.from_environ(environ)
+        self.assertEqual(req_context.auth, auth)
+        self.assertEqual(req_context.remote_ip, REMOTE_IP)
+        self.assertEqual(req_context.x_forwarded_for, X_FORWARDED_FOR_IPS)
 
 
 if __name__ == "__main__":
