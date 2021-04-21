9519153ed2501bd3d6ca1ee5bb9c95e1d6975520
Stephan Lee <stephanwlee@gmail.com>
2021-04-21 14:45:40 -0700

    timeseries: remove Polymer based line chart (#4893)
    This change removes vz-line-chart based TimeSeries scalar chart. With
    2.5.0, we released the Gpu line chart with an option to downgrade
    temporarily to use the older behavior.
    
    After the release, we now want to remove our tech debts by removing the
    old code paths and update the tests accordingly.
    
    After this change, we still have to:
    1. remove tb-line-chart.
    2. remove the feature flag.

--- tensorboard/webapp/metrics/views/card_renderer/scalar_card_component.ts
+++ tensorboard/webapp/metrics/views/card_renderer/scalar_card_component.ts
@@ -25,50 +25,19 @@ import {
 import {MatDialog} from '@angular/material/dialog';
 
 import {DataLoadState} from '../../../types/data';
-import {RunColorScale} from '../../../types/ui';
-import {
-  formatNumber,
-  formatRelativeTimeInMs,
-} from '../../../util/value_formatter';
-import {LineChartComponent} from '../../../widgets/line_chart/line_chart_component';
-import {
-  EvaluationPoint,
-  Point,
-  SeriesData,
-  TooltipColumnSpec,
-  XAxisType as ChartXAxisType,
-  YAxisType,
-} from '../../../widgets/line_chart/line_chart_types';
 import {relativeTimeFormatter} from '../../../widgets/line_chart_v2/lib/formatter';
-import {LineChartComponent as NewLineChartComponent} from '../../../widgets/line_chart_v2/line_chart_component';
+import {LineChartComponent} from '../../../widgets/line_chart_v2/line_chart_component';
 import {
   RendererType,
   ScaleType,
   TooltipDatum,
 } from '../../../widgets/line_chart_v2/types';
-import {ScalarStepDatum} from '../../data_source';
 import {TooltipSort, XAxisType} from '../../types';
 import {
   ScalarCardDataSeries,
   ScalarCardSeriesMetadata,
   ScalarCardSeriesMetadataMap,
 } from './scalar_card_types';
-
-const RESIZE_REDRAW_DEBOUNCE_TIME_IN_MS = 50;
-
-interface Metadata {
-  displayName: string;
-}
-
-type StepDatum = ScalarStepDatum;
-
-export type SeriesPoint = Point<StepDatum>;
-
-export type LegacySeriesDataList = Array<SeriesData<Metadata, StepDatum>>;
-
-export type ScalarChartEvalPoint = EvaluationPoint<Metadata, StepDatum>;
-
-export type TooltipColumns = Array<TooltipColumnSpec<Metadata, StepDatum>>;
 
 type ScalarTooltipDatum = TooltipDatum<
   ScalarCardSeriesMetadata & {
@@ -77,44 +46,6 @@ type ScalarTooltipDatum = TooltipDatum<
   }
 >;
 
-const DEFAULT_TOOLTIP_COLUMNS: TooltipColumns = [
-  {
-    title: 'Name',
-    evaluate: (d: ScalarChartEvalPoint) => {
-      return d.dataset.metadata().meta.displayName;
-    },
-  },
-  {
-    title: 'Value',
-    evaluate: (d: ScalarChartEvalPoint) => {
-      return formatNumber(d.datum.y);
-    },
-  },
-  {
-    title: 'Step',
-    evaluate: (d: ScalarChartEvalPoint) => {
-      return d.datum.step.toString();
-    },
-  },
-  {
-    title: 'Time',
-    evaluate: (d: ScalarChartEvalPoint) => {
-      // sec to ms.
-      const date = new Date(d.datum.wallTime * 1000);
-      return date.toLocaleString();
-    },
-  },
-  {
-    title: 'Relative',
-    evaluate: (d: ScalarChartEvalPoint) => {
-      const data = d.dataset.data();
-      const firstTime = data.length > 0 ? data[0].wallTime : 0;
-      const relativeTimeInMs = (d.datum.wallTime - firstTime) * 1000;
-      return formatRelativeTimeInMs(relativeTimeInMs);
-    },
-  },
-];
-
 @Component({
   selector: 'scalar-card-component',
   templateUrl: 'scalar_card_component.ng.html',
@@ -122,96 +53,50 @@ const DEFAULT_TOOLTIP_COLUMNS: TooltipColumns = [
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScalarCardComponent<Downloader> {
-  readonly RESIZE_REDRAW_DEBOUNCE_TIME_IN_MS = RESIZE_REDRAW_DEBOUNCE_TIME_IN_MS;
   readonly DataLoadState = DataLoadState;
   readonly RendererType = RendererType;
+  readonly ScaleType = ScaleType;
+  readonly XAxisType = XAxisType;
 
   @Input() cardId!: string;
+  @Input() chartMetadataMap!: ScalarCardSeriesMetadataMap;
+  @Input() DataDownloadComponent!: ComponentType<Downloader>;
+  @Input() dataSeries!: ScalarCardDataSeries[];
+  @Input() ignoreOutliers!: boolean;
+  @Input() isCardVisible!: boolean;
+  @Input() isEverVisible!: boolean;
+  @Input() isPinned!: boolean;
   @Input() loadState!: DataLoadState;
-  @Input() title!: string;
+  @Input() showFullSize!: boolean;
+  @Input() smoothingEnabled!: boolean;
   @Input() tag!: string;
+  @Input() title!: string;
   @Input() tooltipSort!: TooltipSort;
   @Input() xAxisType!: XAxisType;
-  @Input() showFullSize!: boolean;
-  @Input() isPinned!: boolean;
-
-  @Input() DataDownloadComponent!: ComponentType<Downloader>;
-  @Input() newXScaleType!: ScaleType;
-
-  // Legacy chart related; to be removed.
-  @Input() runColorScale!: RunColorScale;
-  @Input() ignoreOutliers!: boolean;
-  @Input() scalarSmoothing!: number;
-  @Input() seriesDataList!: LegacySeriesDataList;
-
-  // gpu chart related props.
-  @Input() isCardVisible!: boolean;
-  @Input() smoothingEnabled!: boolean;
-  @Input() gpuLineChartEnabled!: boolean;
-  @Input() dataSeries!: ScalarCardDataSeries[];
-  @Input() chartMetadataMap!: ScalarCardSeriesMetadataMap;
-  @Input() isEverVisible!: boolean;
+  @Input() xScaleType!: ScaleType;
 
   @Output() onFullSizeToggle = new EventEmitter<void>();
   @Output() onPinClicked = new EventEmitter<boolean>();
 
-  // Line chart may not exist when no data is present (*ngIf).
+  // Line chart may not exist when was never visible (*ngIf).
   @ViewChild(LineChartComponent)
-  lineChart?: LineChartComponent<Metadata, StepDatum>;
-
-  // Line chart may not exist when no data is present (*ngIf).
-  @ViewChild(NewLineChartComponent)
-  newLineChart?: NewLineChartComponent;
+  lineChart?: LineChartComponent;
 
   // Controls whether to enable/disable the fit-to-domain button.
-  isViewBoxOverridden: boolean = !this.gpuLineChartEnabled;
+  isViewBoxOverridden: boolean = false;
 
   constructor(private readonly ref: ElementRef, private dialog: MatDialog) {}
 
-  yAxisType = YAxisType.LINEAR;
-  newYScaleType = ScaleType.LINEAR;
+  yScaleType = ScaleType.LINEAR;
 
-  readonly XAxisType = XAxisType;
-
-  chartXAxisType() {
-    switch (this.xAxisType) {
-      case XAxisType.STEP:
-        return ChartXAxisType.STEP;
-      case XAxisType.WALL_TIME:
-        return ChartXAxisType.WALL_TIME;
-      case XAxisType.RELATIVE:
-        return ChartXAxisType.RELATIVE;
-      default:
-        throw new Error('Invalid xAxisType for line chart.');
-    }
-  }
-
-  tooltipColumns: Array<
-    TooltipColumnSpec<Metadata, StepDatum>
-  > = DEFAULT_TOOLTIP_COLUMNS;
-
-  toggleYAxisType() {
-    this.yAxisType =
-      this.yAxisType === YAxisType.LINEAR ? YAxisType.LOG : YAxisType.LINEAR;
-    this.newYScaleType =
-      this.yAxisType === YAxisType.LINEAR ? ScaleType.LINEAR : ScaleType.LOG10;
+  toggleYScaleType() {
+    this.yScaleType =
+      this.yScaleType === ScaleType.LINEAR ? ScaleType.LOG10 : ScaleType.LINEAR;
   }
 
   resetDomain() {
     if (this.lineChart) {
-      this.lineChart.resetDomain();
-    }
-    if (this.newLineChart) {
-      this.newLineChart.viewBoxReset();
-    }
-  }
-
-  redraw() {
-    if (this.lineChart) {
-      // Only redraw when it is visible (and thus have width and height).
-      if (this.ref.nativeElement.clientWidth) {
-        this.lineChart.redraw();
-      }
+      this.lineChart.viewBoxReset();
     }
   }
 
