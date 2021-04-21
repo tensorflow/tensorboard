a2d0e0d14e198c68e6bb9b8f7a9ef4a3a8e4d1b5
Stephan Lee <stephanwlee@gmail.com>
2021-04-20 18:20:30 -0700

    timeseries: remove Polymer based line chart
    This change removes vz-line-chart based TimeSeries scalar chart. With
    2.5.0, we released the Gpu line chart with an option to downgrade
    temporarily to use the older behavior.
    
    After the release, we now want to remove our tech debts by removing the
    old code paths and update the tests accordingly.
    
    After this change, we still have to:
    1. remove tb-line-chart.
    2. remove the feature flag.

--- tensorboard/webapp/metrics/views/card_renderer/scalar_card_module.ts
+++ tensorboard/webapp/metrics/views/card_renderer/scalar_card_module.ts
@@ -19,7 +19,6 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
-import {LineChartModule} from '../../../widgets/line_chart/line_chart_module';
 import {LineChartModule as LineChartV2Module} from '../../../widgets/line_chart_v2/line_chart_module';
 import {ResizeDetectorModule} from '../../../widgets/resize_detector_module';
 import {TruncatedPathModule} from '../../../widgets/text/truncated_path_module';
@@ -33,7 +32,6 @@ import {ScalarCardContainer} from './scalar_card_container';
   imports: [
     CommonModule,
     DataDownloadModule,
-    LineChartModule,
     LineChartV2Module,
     MatButtonModule,
     MatIconModule,
