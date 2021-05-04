8b55b661c170ba5eabf6a0be5fce2281b1b88813
Stephan Lee <stephanwlee@gmail.com>
2021-05-03 21:12:47 -0700

    types: declare types to be public
    - experiments/types which is used to type the backend response should
      not be mangled.
    - route parameter, since defined as strings in the route configurations,
      cannot be mangled.

--- tensorboard/webapp/experiments/types.ts
+++ tensorboard/webapp/experiments/types.ts
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-export interface Experiment {
+export declare interface Experiment {
   id: string;
   name: string;
   start_time: number;
