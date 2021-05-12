43f34c6fb382f1da4774d27c27c6b549d476b7cb
Jie-Wei Wu <jieweiwu@google.com>
2021-05-11 17:54:22 -0700

    export notification actions

--- tensorboard/webapp/notification_center/index.ts
+++ tensorboard/webapp/notification_center/index.ts
@@ -12,5 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+import * as _notificationActions from './_redux/notification_center_actions';
 
 export {NotificationCenterModule} from './_notification_center_module';
+export const notificationActions = _notificationActions;
