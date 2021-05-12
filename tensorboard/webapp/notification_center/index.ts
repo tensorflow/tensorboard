8ce56cab455880b7912a9b49592ee2b1a153230d
Jie-Wei Wu <jieweiwu@google.com>
2021-05-11 18:42:12 -0700

    change export

--- tensorboard/webapp/notification_center/index.ts
+++ tensorboard/webapp/notification_center/index.ts
@@ -12,7 +12,5 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-import * as _notificationActions from './_redux/notification_center_actions';
-
 export {NotificationCenterModule} from './_notification_center_module';
-export const notificationActions = _notificationActions;
+export {* as notificationActions} from './_redux/notification_center_actions';
