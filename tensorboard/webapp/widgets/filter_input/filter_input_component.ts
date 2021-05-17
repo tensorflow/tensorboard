tensorflow/tensorboard

commit df038ab4e0b73cfda133c1fcdd9cd1bad1cc1ae0

timeseries: replace matInput with reusable FilterInputComponent (#4938)
The Time Series tag filter input and run selector were using Angular's
`matInput` whose styles are only included when another `mat-form-field`
has been rendered on the page before [1].

To make the filter input styles deterministic, drop the `matInput` directive
and replace it with a custom, styled widget for filter inputs.

[1] https://github.com/angular/components/issues/21871

tensorboard/webapp/widgets/filter_input/filter_input_component.ts
@@ -1,0 +1,53 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+import {Component, Input, ViewChild} from '@angular/core';
+import {MatAutocompleteTrigger} from '@angular/material/autocomplete';
+
+/**
+ * A text input field intended for filtering items.
+ */
+@Component({
+  selector: 'tb-filter-input',
+  template: `
+    <mat-icon svgIcon="search_24px"></mat-icon>
+
+    <!-- Note: to allow falsy 'matAutocomplete' values, we need 'matAutocompleteDisabled'
+    to prevent runtime errors. -->
+    <input
+      type="text"
+      autocomplete="off"
+      [placeholder]="placeholder"
+      [matAutocomplete]="matAutocomplete"
+      [matAutocompleteDisabled]="!matAutocomplete"
+      [value]="value"
+      (keyup)="onInputKeyUp($event)"
+    />
+  `,
+  styleUrls: [`filter_input_component.css`],
+})
+export class FilterInputComponent {
+  @Input() value: string = '';
+  @Input() matAutocomplete?: string;
+  @Input() placeholder: string = '';
+
+  @ViewChild(MatAutocompleteTrigger)
+  private autocompleteTrigger!: MatAutocompleteTrigger;
+
+  onInputKeyUp(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.autocompleteTrigger.closePanel();
+    }
+  }
+}

tensorboard/webapp/widgets/filter_input/filter_input_test.ts
@@ -1,0 +1,125 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+import {OverlayContainer} from '@angular/cdk/overlay';
+import {Component, Input} from '@angular/core';
+import {TestBed} from '@angular/core/testing';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {By} from '@angular/platform-browser';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+import {KeyType, sendKey} from '../../testing/dom';
+import {MatIconTestingModule} from '../../testing/mat_icon_module';
+import {getAutocompleteOptions} from '../../testing/material';
+
+import {FilterInputModule} from './filter_input_module';
+
+@Component({
+  selector: 'test',
+  template: `
+    <tb-filter-input [value]="value" [matAutocomplete]="filterMatches">
+    </tb-filter-input>
+
+    <mat-autocomplete
+      #filterMatches="matAutocomplete"
+      (optionSelected)="onCompletionAccepted($event.option.value)"
+    >
+      <mat-option *ngFor="let completion of completions" [value]="completion">{{
+        completion
+      }}</mat-option>
+    </mat-autocomplete>
+  `,
+})
+class TestableInputWithCompletions {
+  @Input() value?: string;
+  @Input() completions: string[] = [];
+}
+
+@Component({
+  selector: 'test',
+  template: ` <tb-filter-input></tb-filter-input> `,
+})
+class TestableInputNoProps {
+  @Input() value?: string;
+}
+
+describe('filter input widget', () => {
+  let overlayContainer: OverlayContainer;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        MatAutocompleteModule,
+        MatIconTestingModule,
+        FilterInputModule,
+      ],
+      declarations: [TestableInputNoProps, TestableInputWithCompletions],
+    }).compileComponents();
+
+    overlayContainer = TestBed.inject(OverlayContainer);
+  });
+
+  it('renders the value', () => {
+    const fixture = TestBed.createComponent(TestableInputWithCompletions);
+    fixture.componentInstance.value = 'foo';
+    fixture.detectChanges();
+
+    const input = fixture.debugElement.query(By.css('input'));
+    expect(input.nativeElement.value).toBe('foo');
+  });
+
+  it('renders with no value', () => {
+    const fixture = TestBed.createComponent(TestableInputNoProps);
+    fixture.detectChanges();
+
+    const input = fixture.debugElement.query(By.css('input'));
+    expect(input.nativeElement.value).toBe('');
+    expect(input.nativeElement.placeholder).toBe('');
+  });
+
+  it('does not set matAutocomplete with an empty value', () => {
+    const fixture = TestBed.createComponent(TestableInputNoProps);
+    fixture.detectChanges();
+
+    const input = fixture.debugElement.query(By.css('input'));
+    const isAutocompleteDisabled =
+      input.attributes['ng-reflect-autocomplete-disabled'] === 'true';
+    const hasAutocomplete = !!input.attributes['ng-reflect-autocomplete'];
+    expect(isAutocompleteDisabled || !hasAutocomplete).toBe(true);
+  });
+
+  it('shows autocomplete and closes on Enter', () => {
+    const fixture = TestBed.createComponent(TestableInputWithCompletions);
+    fixture.componentInstance.completions = ['a', 'b', 'c'];
+    fixture.detectChanges();
+
+    const input = fixture.debugElement.query(By.css('input'));
+    input.nativeElement.focus();
+    fixture.detectChanges();
+
+    const options = getAutocompleteOptions(overlayContainer);
+    expect(options.length).toBe(3);
+
+    sendKey(fixture, input, {
+      type: KeyType.SPECIAL,
+      prevString: input.properties.value,
+      key: 'Enter',
+      startingCursorIndex: input.properties.selectionStart,
+    });
+
+    const options2 = getAutocompleteOptions(overlayContainer);
+    expect(options2.length).toBe(0);
+  });
+});

tensorboard/webapp/metrics/views/main_view/filter_input_component.ts
@@ -19,9 +19,7 @@ import {
   HostBinding,
   Input,
   Output,
-  ViewChild,
 } from '@angular/core';
-import {MatAutocompleteTrigger} from '@angular/material/autocomplete';
 
 import {escapeForRegex} from '../../../util/string';
 
@@ -36,15 +34,6 @@ export class MetricsFilterInputComponent {
   @HostBinding('class.valid') @Input() isRegexFilterValid!: boolean;
   @Input() completions!: string[];
   @Output() onRegexFilterValueChange = new EventEmitter<string>();
-
-  @ViewChild(MatAutocompleteTrigger, {static: true})
-  autocompleteTrigger!: MatAutocompleteTrigger;
-
-  onFilterKeyUp(event: KeyboardEvent) {
-    if (event.key === 'Enter') {
-      this.autocompleteTrigger.closePanel();
-    }
-  }
 
   onCompletionAccepted(completion: string) {
     this.onRegexFilterValueChange.emit(escapeForRegex(completion));

tensorboard/webapp/metrics/views/main_view/filter_input_test.ts
@@ -17,7 +17,6 @@ import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
 import {By} from '@angular/platform-browser';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {Action, Store} from '@ngrx/store';
@@ -27,6 +26,7 @@ import {State} from '../../../app_state';
 import {sendKeys} from '../../../testing/dom';
 import {MatIconTestingModule} from '../../../testing/mat_icon_module';
 import {getAutocompleteOptions} from '../../../testing/material';
+import {FilterInputModule} from '../../../widgets/filter_input/filter_input_module';
 import {metricsTagFilterChanged} from '../../actions';
 import {PluginType} from '../../data_source';
 import * as selectors from '../../store/metrics_selectors';
@@ -47,7 +47,7 @@ describe('metrics filter input', () => {
         MatAutocompleteModule,
         MatButtonModule,
         MatIconTestingModule,
-        MatInputModule,
+        FilterInputModule,
       ],
       declarations: [MetricsFilterInputComponent, MetricsFilterInputContainer],
       providers: [
@@ -225,26 +225,6 @@ describe('metrics filter input', () => {
       fixture.detectChanges();
 
       expect(fixture.debugElement.query(By.css('.error-icon'))).not.toBeNull();
-    });
-
-    it('closes autocomplete on Enter', () => {
-      store.overrideSelector(selectors.getMetricsTagFilter, '');
-      const fixture = TestBed.createComponent(MetricsFilterInputContainer);
-      fixture.detectChanges();
-
-      const input = fixture.debugElement.query(By.css('input'));
-      input.nativeElement.focus();
-      fixture.detectChanges();
-
-      const options = getAutocompleteOptions(overlayContainer);
-      expect(options.length).toBeGreaterThan(1);
-
-      input.nativeElement.dispatchEvent(
-        new KeyboardEvent('keyup', {key: 'Enter'})
-      );
-
-      const options2 = getAutocompleteOptions(overlayContainer);
-      expect(options2.length).toBe(0);
     });
   });
 });

tensorboard/webapp/BUILD
@@ -349,6 +349,7 @@ tf_ng_web_test_suite(
         "//tensorboard/webapp/webapp_data_source:webapp_data_source_test_lib",
         "//tensorboard/webapp/widgets:resize_detector_test",
         "//tensorboard/webapp/widgets:resize_detector_testing",
+        "//tensorboard/webapp/widgets/filter_input:filter_input_test",
         "//tensorboard/webapp/widgets/histogram:histogram_test",
         "//tensorboard/webapp/widgets/line_chart_v2:line_chart_v2_tests",
         "//tensorboard/webapp/widgets/line_chart_v2/lib:lib_tests",

tensorboard/webapp/metrics/views/main_view/BUILD
@@ -95,6 +95,7 @@ tf_ng_module(
         "//tensorboard/webapp/types:ui",
         "//tensorboard/webapp/util:string",
         "//tensorboard/webapp/util:types",
+        "//tensorboard/webapp/widgets/filter_input",
         "@npm//@angular/common",
         "@npm//@angular/core",
         "@npm//@ngrx/store",
@@ -117,7 +118,6 @@ tf_ts_library(
         "//tensorboard/webapp/angular:expect_angular_core_testing",
         "//tensorboard/webapp/angular:expect_angular_material_autocomplete",
         "//tensorboard/webapp/angular:expect_angular_material_button",
-        "//tensorboard/webapp/angular:expect_angular_material_input",
         "//tensorboard/webapp/angular:expect_angular_platform_browser_animations",
         "//tensorboard/webapp/angular:expect_ngrx_store_testing",
         "//tensorboard/webapp/core/store",
@@ -131,6 +131,7 @@ tf_ts_library(
         "//tensorboard/webapp/testing:mat_icon",
         "//tensorboard/webapp/testing:material",
         "//tensorboard/webapp/types:ui",
+        "//tensorboard/webapp/widgets/filter_input",
         "@npm//@angular/core",
         "@npm//@angular/platform-browser",
         "@npm//@ngrx/store",

tensorboard/webapp/metrics/views/main_view/filter_input_component.ng.html
@@ -15,16 +15,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 <div class="tag-filter">
-  <mat-icon svgIcon="search_24px"></mat-icon>
-  <input
-    matInput
-    autocomplete="off"
+  <tb-filter-input
     placeholder="Filter tags (regex)"
     [value]="regexFilterValue"
     (input)="onRegexFilterValueChange.emit($event.target.value)"
-    (keyup)="onFilterKeyUp($event)"
     [matAutocomplete]="filterMatches"
-  />
+  ></tb-filter-input>
   <mat-icon
     svgIcon="error_24px"
     *ngIf="!isRegexFilterValid"

tensorboard/webapp/metrics/views/main_view/main_view_module.ts
@@ -20,6 +20,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 
+import {FilterInputModule} from '../../../widgets/filter_input/filter_input_module';
 import {CardRendererModule} from '../card_renderer/card_renderer_module';
 import {RightPaneModule} from '../right_pane/right_pane_module';
 
@@ -55,6 +56,7 @@ import {PinnedViewContainer} from './pinned_view_container';
   imports: [
     CardRendererModule,
     CommonModule,
+    FilterInputModule,
     MatAutocompleteModule,
     MatButtonModule,
     MatIconModule,

commit 234c9450a867ce00c95f2af07f15daabc978e8bd

Fix sync errors about property renaming and linting (#4981)
The sync broke in https://github.com/tensorflow/tensorboard/pull/4938
because of property renaming-related errors. This change also fixes a
minor lint warning.

Test sync cl/374274713

tensorboard/webapp/widgets/filter_input/filter_input_component.ts
@@ -43,7 +43,7 @@ export class FilterInputComponent {
   @Input() placeholder: string = '';
 
   @ViewChild(MatAutocompleteTrigger)
-  private autocompleteTrigger!: MatAutocompleteTrigger;
+  private readonly autocompleteTrigger!: MatAutocompleteTrigger;
 
   onInputKeyUp(event: KeyboardEvent) {
     if (event.key === 'Enter') {

tensorboard/webapp/widgets/filter_input/filter_input_test.ts
@@ -114,9 +114,9 @@ describe('filter input widget', () => {
 
     sendKey(fixture, input, {
       type: KeyType.SPECIAL,
-      prevString: input.properties.value,
+      prevString: input.properties['value'],
       key: 'Enter',
-      startingCursorIndex: input.properties.selectionStart,
+      startingCursorIndex: input.properties['selectionStart'],
     });
 
     const options2 = getAutocompleteOptions(overlayContainer);