69c07f7974e887940d123cdb39d7b5e49d9cc9ba
Stanley Bileschi <bileschi@google.com>
2021-05-17 17:49:22 -0400

    Add JSDoc to DeepLinkProvider interface definition

--- tensorboard/webapp/app_routing/deep_link_provider.ts
+++ tensorboard/webapp/app_routing/deep_link_provider.ts
@@ -20,11 +20,30 @@ import {State} from '../app_state';
 
 import {SerializableQueryParams} from './types';
 
+/**
+ * The `DeepLinkProvider` interface standardizes the relationship between the
+ * app state and the URL parameters.  It is expected that when either an update
+ * to the URL parameters should change app state, or, in the other direction,
+ * when an update to the state should cause a change in the URL parameters,
+ * that logic should be invoked through a `DeepLinkProvider`.
+ */
 @Injectable()
 export abstract class DeepLinkProvider {
+
+  /**
+   * Modifies the URL to match the state of the app.
+   * @param store The ngrx store containing the state of the app.
+   * @return An observable which will emit the query parameters matching the state.
+   */
   abstract serializeStateToQueryParams(
     store: Store<State>
   ): Observable<SerializableQueryParams>;
 
+  /**
+   * Given the query params, updates the ngrx state to match.
+   * @param queryParams TensorBoard URL SeralizableQueryParams that should be used
+   *     to update the app state.
+   * @return A JS object describing a patch that should be made to the ngrx state.
+   */
   abstract deserializeQueryParams(queryParams: SerializableQueryParams): object;
 }
