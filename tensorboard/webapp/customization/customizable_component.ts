a19df6d0444dfa03020e269664466f913618f2f3
Yating <ytjing@google.com>
2021-06-01 17:49:25 +0000

    infra: add customization component (#5026)
    * experiment lists: add customization component
    
    * lint customization module
    
    * run buildifier
    
    * update cookbook example
    
    * deduplicate code in test
    
    * fix type
    
      change from `null` to `undefined`

---
+++ tensorboard/webapp/customization/customizable_component.ts
@@ -0,0 +1,101 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+import {
+  Component,
+  ComponentFactoryResolver,
+  Input,
+  OnInit,
+  Type,
+  ViewContainerRef,
+} from '@angular/core';
+
+/**
+ * A Component that defines a customization point. Ideal for use for small
+ * surgical customizations deep within the component hierarchy. The
+ * customizations can be provided dynamically via Angular dependency injection
+ * and providers.
+ *
+ * Cookbook:
+ *
+ * 1. Define a customizable Component, for example a button:
+ *
+ *    const CustomizableButton = new InjectionToken<Type<unknown>>('Customizable Button');
+ *
+ * 2. Where the customization point is desired, use this Component to wrap some
+ *    default behavior. Bind to some possibly-empty variable with the
+ *    [customizableComponent] attribute:
+ *
+ *    <tb-customization [customizableComponent]="customButtonIfProvided">
+ *      <button mat-button>I am the default button.</button>
+ *    </tb-customization>
+ *
+ * 3. In the constructor of the component containing the customization point,
+ *    optionally inject a custom button:
+ *
+ *    constructor(
+ *      @Optional() @Inject(CustomizableButton)
+ *      readonly customButtonIfProvided: Type<unknown>)
+ *
+ * If you do not wish to customize the behavior for a certain TensorBoard
+ * service (in this case, a button), you're done. The TensorBoard service
+ * will receive the default behavior.
+ *
+ * However, if you need to define a customization for the button:
+ *
+ * 4. Define a Component with your customization:
+ *
+ *    @Injectable()
+ *    @Component({
+ *      selector: 'my-custom-button-component',
+ *      template: '<button mat-button>I am a special button!</button>'
+ *    })
+ *    export class MyCustomButtonComponent {}
+ *
+ * 5. Provide the customized Component in an NgModule:
+ *
+ *    @NgModule({
+ *      declarations: [MyCustomButtonComponent],
+ *      entryComponents: [MyCustomButtonComponent],
+ *      providers: [{
+ *        provide: CustomizableButton,
+ *        useClass: MyCustomButtonComponent,
+ *      }]
+ *    })
+ */
+@Component({
+  selector: 'tb-customization',
+  template: `
+    <ng-container *ngIf="!customizableComponent">
+      <ng-content></ng-content>
+    </ng-container>
+  `,
+})
+export class CustomizableComponent implements OnInit {
+  @Input() customizableComponent!: {constructor: Type<unknown>} | undefined;
+
+  constructor(
+    private readonly viewContainerRef: ViewContainerRef,
+    private readonly componentFactoryResolver: ComponentFactoryResolver
+  ) {}
+
+  ngOnInit() {
+    if (this.customizableComponent) {
+      const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+        this.customizableComponent.constructor
+      );
+      this.viewContainerRef.createComponent(componentFactory);
+    }
+  }
+}
---
+++ tensorboard/webapp/customization/customization_test.ts
@@ -0,0 +1,108 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+import {Component, NgModule, Optional} from '@angular/core';
+import {TestBed} from '@angular/core/testing';
+
+import {CustomizationModule} from './customization_module';
+
+/**
+ * Marker class for the component that can be customized in these tests.
+ * Customizations should implement this type and be provided.
+ */
+export class CustomizableComponentType {}
+
+/**
+ * Parent class that uses the <tb-customization> component.
+ */
+@Component({
+  selector: 'parent-component',
+  template: `
+    <tb-customization [customizableComponent]="customizableComponent">
+      <span>Showing </span>
+      <span>Default Text!</span>
+    </tb-customization>
+  `,
+})
+export class ParentComponent {
+  constructor(
+    @Optional() readonly customizableComponent: CustomizableComponentType
+  ) {}
+}
+
+/**
+ * Imports CustomizationModule for ParentComponent.
+ */
+@NgModule({
+  imports: [CustomizationModule],
+  declarations: [ParentComponent],
+  entryComponents: [ParentComponent],
+})
+export class ParentComponentModule {}
+
+/**
+ * An implementation of CustomizableComponentType to be provided and injected
+ * into the ParentComponent for some tests.
+ */
+@Component({
+  selector: 'customizable-component',
+  template: ` <div>Showing Customized Text!</div> `,
+})
+export class CustomizableComponent {}
+
+/**
+ * Declares and provides the implementation of CustomizableComponentType.
+ */
+@NgModule({
+  declarations: [CustomizableComponent],
+  entryComponents: [CustomizableComponent],
+  providers: [
+    {
+      provide: CustomizableComponentType,
+      useClass: CustomizableComponent,
+    },
+  ],
+})
+export class CustomizableComponentModule {}
+
+async function setUp(extraImports: any[] = []) {
+  await TestBed.configureTestingModule({
+    imports: [CustomizationModule, ...extraImports],
+    declarations: [ParentComponent],
+  }).compileComponents();
+}
+
+describe('tb-customization', () => {
+  it('renders default if no customization provided', async () => {
+    await setUp();
+
+    const fixture = TestBed.createComponent(ParentComponent);
+    fixture.detectChanges();
+    expect(fixture.debugElement.nativeElement.textContent).toBe(
+      'Showing Default Text!'
+    );
+  });
+
+  it('renders customization if provided', async () => {
+    // In this test we import the Module that provides an implementation
+    // of CustomizableComponentType.
+    await setUp([CustomizableComponentModule]);
+
+    const fixture = TestBed.createComponent(ParentComponent);
+    fixture.detectChanges();
+    expect(fixture.debugElement.nativeElement.textContent).toBe(
+      'Showing Customized Text!'
+    );
+  });
+});
