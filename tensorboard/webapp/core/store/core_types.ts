fc35c0a367c1fa6b6b68d649f1a27342eab4b7cd
Brian Dubois <bdubois@google.com>
2021-06-28 13:47:53 -0400

    Remove remnants of settings properties from core. (#5095)
    In #5090 I moved some properties from core to settings, along with the corresponding redux. There were some compatibility shims left behind in core for google-internal code. Now that all google-internal code has been migrated, we remove the shims.
    
    Tested:
    
    Patched this change into a local repository and verified that other versions of tensorboard still built and behaved propertly.

--- tensorboard/webapp/core/store/core_types.ts
+++ tensorboard/webapp/core/store/core_types.ts
@@ -20,7 +20,6 @@ limitations under the License.
 // remove this import, and write the divergent state types explicitly here.
 import {Environment, PluginId, PluginsListing} from '../../types/api';
 import {DataLoadState, LoadState} from '../../types/data';
-import {State as SettingsState} from '../../settings';
 
 import {PluginsListFailureCode, Run, RunId} from '../types';
 
@@ -72,9 +71,7 @@ interface FailedPluginsListLoadState extends LoadState {
   failureCode: PluginsListFailureCode;
 }
 
-// TODO(bdubois): Remove composition with SettingsState when appropriate callers
-// are migrated to use SettingsState directly.
-export interface State extends SettingsState {
+export interface State {
   [CORE_FEATURE_KEY]?: CoreState;
 }
 
