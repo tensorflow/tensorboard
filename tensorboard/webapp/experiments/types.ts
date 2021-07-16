fdc569e0b9fad8ee8275ce24819108730b247714
ericdnielsen <ericdnielsen@gmail.com>
2021-07-16 14:31:15 -0400

    Add optional repository_id to Experiment (#5132)
    * Add optional repository_id to Experiment
    
    * reorder fields

--- tensorboard/webapp/experiments/types.ts
+++ tensorboard/webapp/experiments/types.ts
@@ -14,6 +14,7 @@ limitations under the License.
 ==============================================================================*/
 export declare interface Experiment {
   id: string;
+  repository_id?: string;
   name: string;
   start_time: number;
   owner?: string;
