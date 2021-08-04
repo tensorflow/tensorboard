6c7fe16b176c2d48ac5a1f426e0cdea4ff0ea8cd
Stephan Lee <stephanwlee@gmail.com>
2021-08-03 23:11:43 -0700

    whoops?

--- tensorboard/webapp/runs/views/runs_table/regex_edit_dialog_container.ts
+++ tensorboard/webapp/runs/views/runs_table/regex_edit_dialog_container.ts
@@ -51,8 +51,8 @@ const INPUT_CHANGE_DEBOUNCE_INTERVAL_MS = 500;
       :host,
       regex-edit-dialog-component {
         display: block;
-        width: 100;
         height: 100%;
+        width: 100%;
       }
     `,
   ],
