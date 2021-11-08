39a3137d13e8f4a03acad1209da58a881b438f81
Stephan Lee <stephanwlee@gmail.com>
2021-11-08 11:24:25 -0800

    widget: content wrapping input
    This change open sources content_wrapping_input, a component that
    behaves a lot like content-editable element that tightly wraps content.
    
    A conventional `<input>` element has a fixed width invariable to size of
    the content. If content is too long, there exists mechanisms to scroll
    within the input. This behavior is not something that we do not want in
    some UI elements and this widget helps with that.

---
+++ tensorboard/webapp/widgets/content_wrapping_input/content_wrapping_input_module.ts
@@ -0,0 +1,31 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+import {NgModule} from '@angular/core';
+
+import {ContentWrappingInputComponent} from './content_wrapping_input_component';
+
+/**
+ * Provides <content-wrapping-input> that behaves like an <input> but with
+ * different UX. Its width changes based on value typed, or placeholder when
+ * value is empty. When not focused, it looks like a simple span with a text.
+ *
+ * UX was largely inpsired by the title editor in Google Docs.
+ */
+@NgModule({
+  exports: [ContentWrappingInputComponent],
+  declarations: [ContentWrappingInputComponent],
+})
+export class ContentWrappingInputModule {}
