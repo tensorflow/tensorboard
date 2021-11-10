ff907e91c1b7925cf1333ed773d0973cf136750a
Yating Jing <ytjing@google.com>
2021-11-10 01:31:56 +0000

    small change

--- tensorboard/data_compat.py
+++ tensorboard/data_compat.py
@@ -89,11 +89,6 @@ def _migrate_histogram_value(value):
     order to avoid introducing "backwards" buckets (where left edge > right
     edge), we first must drop all empty buckets on the left and right ends.
     """
-    summary_metadata = histogram_metadata.create_summary_metadata(
-        display_name=value.metadata.display_name or value.tag,
-        description=value.metadata.summary_description,
-    )
-
     histogram_value = value.histo
     bucket_counts = histogram_value.bucket
     # Find the indices of the leftmost and rightmost non-empty buckets.
@@ -103,22 +98,27 @@ def _migrate_histogram_value(value):
     if start > end:
         # If all input buckets were empty, treat it as a zero-bucket
         # new-style histogram.
-        return make_summary(
-            value.tag, summary_metadata, np.zeros([0, 3], dtype=np.float32)
-        )
-    # Discard empty buckets on both ends, and keep only the "inner" edges
-    # from the remaining buckets. Note that bucket indices range from `start`
-    # to `end` inclusive, but bucket_limit indices are exclusive of `end` -
-    # this is because bucket_limit[i] is the right-hand edge for bucket[i].
-    bucket_counts = bucket_counts[start : end + 1]
-    inner_edges = histogram_value.bucket_limit[start:end]
-    # Use min as the left-hand limit for the first non-empty bucket.
-    bucket_lefts = [histogram_value.min] + inner_edges
-    # Use max as the right-hand limit for the last non-empty bucket.
-    bucket_rights = inner_edges + [histogram_value.max]
-    buckets = np.array(
-        [bucket_lefts, bucket_rights, bucket_counts], dtype=np.float32
-    ).transpose()
+        buckets = np.zeros([0, 3], dtype=np.float32)
+    else:
+        # Discard empty buckets on both ends, and keep only the "inner"
+        # edges from the remaining buckets. Note that bucket indices range
+        # from `start` to `end` inclusive, but bucket_limit indices are
+        # exclusive of `end` - this is because bucket_limit[i] is the
+        # right-hand edge for bucket[i].
+        bucket_counts = bucket_counts[start : end + 1]
+        inner_edges = histogram_value.bucket_limit[start:end]
+        # Use min as the left-hand limit for the first non-empty bucket.
+        bucket_lefts = [histogram_value.min] + inner_edges
+        # Use max as the right-hand limit for the last non-empty bucket.
+        bucket_rights = inner_edges + [histogram_value.max]
+        buckets = np.array(
+            [bucket_lefts, bucket_rights, bucket_counts], dtype=np.float32
+        ).transpose()
+
+    summary_metadata = histogram_metadata.create_summary_metadata(
+        display_name=value.metadata.display_name or value.tag,
+        description=value.metadata.summary_description,
+    )
 
     return make_summary(value.tag, summary_metadata, buckets)
 
