97b6f6f0a6a1985ba079244cf3d1505f65752c4e
Stephan Lee <stephanwlee@gmail.com>
2021-11-09 16:10:03 -0800

    alias: implement edit mode (#5410)
    Depending on whether the alias component is in an edit mode or not, it
    renders a content-wrapping-input.

--- tensorboard/webapp/widgets/experiment_alias/experiment_alias_test.ts
+++ tensorboard/webapp/widgets/experiment_alias/experiment_alias_test.ts
@@ -12,28 +12,115 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+import {Component, Input} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
 
+import {ExperimentAlias} from '../../experiments/types';
+import {sendKeys} from '../../testing/dom';
+import {ContentWrappingInputModule} from '../content_wrapping_input/content_wrapping_input_module';
 import {ExperimentAliasComponent} from './experiment_alias_component';
+
+@Component({
+  selector: 'testable',
+  template: `<tb-experiment-alias
+    [alias]="alias"
+    [aliasEditable]="aliasEditable"
+    [isAliasNameLegal]="isAliasNameLegal"
+    (aliasChanged)="aliasChanged($event)"
+  ></tb-experiment-alias>`,
+})
+class TestableComponent {
+  @Input()
+  alias!: ExperimentAlias;
+
+  @Input()
+  aliasEditable!: boolean;
+
+  @Input()
+  isAliasNameLegal?: boolean;
+
+  @Input()
+  aliasChanged: () => void = () => {};
+}
 
 describe('experiment alias widget', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [],
-      declarations: [ExperimentAliasComponent],
+      imports: [ContentWrappingInputModule],
+      declarations: [ExperimentAliasComponent, TestableComponent],
     }).compileComponents();
   });
 
-  it('renders the alias', () => {
-    const fixture = TestBed.createComponent(ExperimentAliasComponent);
-    fixture.componentInstance.alias = {aliasText: 'my_alias', aliasNumber: 1};
-    fixture.detectChanges();
+  describe('non-edit mode', () => {
+    it('renders the alias', () => {
+      const fixture = TestBed.createComponent(TestableComponent);
+      fixture.componentInstance.alias = {aliasText: 'my_alias', aliasNumber: 1};
+      fixture.detectChanges();
 
-    const spans = fixture.debugElement.queryAll(By.css('span'));
-    expect(spans[0].nativeElement.textContent).toBe('1');
-    expect(spans[1].nativeElement.textContent).toBe('my_alias');
-    const numberSpan = fixture.debugElement.queryAll(By.css('.alias-number'));
-    expect(numberSpan[0].nativeElement.textContent).toBe('1');
+      const spans = fixture.debugElement.queryAll(By.css('span'));
+      expect(spans[0].nativeElement.textContent).toBe('1');
+      expect(spans[1].nativeElement.textContent).toBe('my_alias');
+      const numberSpan = fixture.debugElement.queryAll(By.css('.alias-number'));
+      expect(numberSpan[0].nativeElement.textContent).toBe('1');
+    });
+
+    it('puts class "illegal" if alias name is not legal', () => {
+      const fixture = TestBed.createComponent(TestableComponent);
+      fixture.componentInstance.alias = {aliasText: '$', aliasNumber: 1};
+      fixture.componentInstance.isAliasNameLegal = false;
+      fixture.detectChanges();
+
+      expect(fixture.debugElement.query(By.css('.illegal'))).not.toBeNull();
+    });
+  });
+
+  describe('edit mode', () => {
+    it('renders content-wrapping-input', () => {
+      const fixture = TestBed.createComponent(TestableComponent);
+      fixture.componentInstance.alias = {aliasText: 'tb rocks', aliasNumber: 1};
+      fixture.componentInstance.aliasEditable = true;
+      fixture.componentInstance.isAliasNameLegal = true;
+      fixture.detectChanges();
+
+      expect(
+        fixture.debugElement.query(By.css('content-wrapping-input'))
+      ).not.toBeNull();
+      expect(
+        fixture.debugElement.query(By.css('input')).nativeElement.value
+      ).toBe('tb rocks');
+    });
+
+    it('renders input with error ui when alias name is not legal', () => {
+      const fixture = TestBed.createComponent(TestableComponent);
+      fixture.componentInstance.alias = {aliasText: '$', aliasNumber: 1};
+      fixture.componentInstance.aliasEditable = true;
+      fixture.componentInstance.isAliasNameLegal = true;
+      fixture.detectChanges();
+
+      const input = fixture.debugElement.query(
+        By.css('content-wrapping-input')
+      );
+      expect(input.componentInstance.style).not.toBe('error');
+
+      fixture.componentInstance.isAliasNameLegal = false;
+      fixture.detectChanges();
+      expect(input.componentInstance.style).toBe('error');
+    });
+
+    it('bubbles up alias changes to via aliasChanged emitter', () => {
+      const aliasChangedSpy = jasmine.createSpy();
+      const fixture = TestBed.createComponent(TestableComponent);
+      fixture.componentInstance.alias = {aliasText: 'tb rocks', aliasNumber: 1};
+      fixture.componentInstance.aliasEditable = true;
+      fixture.componentInstance.aliasChanged = aliasChangedSpy;
+      fixture.detectChanges();
+
+      const debugEl = fixture.debugElement.query(By.css('input'));
+      sendKeys(fixture, debugEl, 'ye');
+
+      expect(aliasChangedSpy).toHaveBeenCalledWith({value: 'y'});
+      expect(aliasChangedSpy).toHaveBeenCalledWith({value: 'ye'});
+    });
   });
 });
