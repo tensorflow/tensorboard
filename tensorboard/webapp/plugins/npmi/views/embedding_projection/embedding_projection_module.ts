9052c2926aa636270279a5e12c4ba04b98ab3318
Stephan Lee <stephanwlee@gmail.com>
2021-12-15 14:49:30 -0800

    fix prettier lint errors

--- tensorboard/webapp/plugins/npmi/views/embedding_projection/embedding_projection_module.ts
+++ tensorboard/webapp/plugins/npmi/views/embedding_projection/embedding_projection_module.ts
@@ -15,10 +15,8 @@ limitations under the License.
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-
 import {EmbeddingProjectionComponent} from './embedding_projection_component';
 import {EmbeddingProjectionContainer} from './embedding_projection_container';
 
