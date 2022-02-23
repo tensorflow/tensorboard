8945089de0e39cfaa0b8b926f7bdff15d139f77a
Jie-Wei Wu <jieweiwu@google.com>
2022-02-23 14:30:47 -0800

    test non-moving slider

--- tensorboard/webapp/metrics/views/card_renderer/image_card_test.ts
+++ tensorboard/webapp/metrics/views/card_renderer/image_card_test.ts
@@ -697,6 +697,31 @@ describe('image card', () => {
       expect(slider.nativeElement.getAttribute('aria-valuenow')).toBe('2');
     });
 
+    it('does not move slider thumb on selected step with no image', () => {
+      store.overrideSelector(selectors.getMetricsSelectedTime, {
+        start: {step: 15},
+        end: null,
+      });
+      const timeSeries = [
+        {wallTime: 100, imageId: 'ImageId1', step: 10},
+        {wallTime: 101, imageId: 'ImageId2', step: 20},
+        {wallTime: 102, imageId: 'ImageId3', step: 30},
+        {wallTime: 103, imageId: 'ImageId4', step: 40},
+      ];
+      provideMockCardSeriesData(
+        selectSpy,
+        PluginType.IMAGES,
+        'card1',
+        null /* metadataOverride */,
+        timeSeries,
+        2 /* stepIndex */
+      );
+      const fixture = createImageCardContainer('card1');
+      fixture.detectChanges();
+      let slider = fixture.debugElement.query(By.css('mat-slider'));
+      expect(slider.nativeElement.getAttribute('aria-valuenow')).toBe('2');
+    });
+
     it('dose not move slider thumb to selected time on range selection', () => {
       store.overrideSelector(selectors.getMetricsSelectedTime, {
         start: {step: 15},
