41b980cd3494627bc52bb5613b4f5904e74cf2bd
Yating Jing <ytjing@google.com>
2022-03-10 17:56:25 +0000

    tslint: output should be read-only

--- tensorboard/webapp/widgets/dropdown/dropdown_component.ts
+++ tensorboard/webapp/widgets/dropdown/dropdown_component.ts
@@ -52,5 +52,5 @@ export interface DropdownOption {
 export class DropdownComponent {
   @Input() value = '';
   @Input() options: DropdownOption[] = [];
-  @Output() selectionChange = new EventEmitter<any>();
+  @Output() readonly selectionChange = new EventEmitter<any>();
 }
