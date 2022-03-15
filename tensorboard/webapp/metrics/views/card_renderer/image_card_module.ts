3ea6981c697ac137e29db7bd706ef49ac72e766f
Jie-Wei Wu <jieweiwu@google.com>
2022-03-14 20:17:10 -0700

    add clipped text hint on image card

--- tensorboard/webapp/metrics/views/card_renderer/image_card_module.ts
+++ tensorboard/webapp/metrics/views/card_renderer/image_card_module.ts
@@ -22,6 +22,7 @@ import {TruncatedPathModule} from '../../../widgets/text/truncated_path_module';
 import {ImageCardComponent} from './image_card_component';
 import {ImageCardContainer} from './image_card_container';
 import {RunNameModule} from './run_name_module';
+import {VisSelectedTimeClippedModule} from './vis_selected_time_clipped_module';
 
 @NgModule({
   declarations: [ImageCardContainer, ImageCardComponent],
@@ -34,6 +35,7 @@ import {RunNameModule} from './run_name_module';
     MatSliderModule,
     RunNameModule,
     TruncatedPathModule,
+    VisSelectedTimeClippedModule,
   ],
 })
 export class ImageCardModule {}
