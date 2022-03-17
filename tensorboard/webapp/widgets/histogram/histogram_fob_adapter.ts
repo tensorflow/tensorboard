8e9b062c9f522f9f23a519e1c38e4aed1c2d4eda
jameshollyer <j.c.hollyer@gmail.com>
2022-03-17 20:34:50 +0000

    add histogram_fob_adapter files

---
+++ tensorboard/webapp/widgets/histogram/histogram_fob_adapter.ts
@@ -0,0 +1,59 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+import {FobCardAdapter} from '../linked_time_fob/linked_time_types';
+import {TemporalScale} from './histogram_component';
+
+export class HistogramFobAdapter implements FobCardAdapter {
+  scale: TemporalScale;
+  steps: number[];
+  containerRect: DOMRect;
+  constructor(scale: TemporalScale, steps: number[], containerRect: DOMRect) {
+    this.scale = scale;
+    this.steps = steps;
+    this.containerRect = containerRect;
+  }
+  getHighestStep(): number {
+    return this.steps[this.steps.length - 1];
+  }
+  getLowestStep(): number {
+    return this.steps[0];
+  }
+  stepToPixel(step: number): number {
+    return this.scale(step);
+  }
+
+  getStepHigherThanMousePosition(position: number): number {
+    let stepIndex = 0;
+    while (
+      position - this.containerRect.top > this.scale(this.steps[stepIndex]) &&
+      stepIndex < this.steps.length
+    ) {
+      stepIndex++;
+    }
+    return this.steps[stepIndex];
+  }
+
+  getStepLowerThanMousePosition(position: number): number {
+    let stepIndex = this.steps.length - 1;
+    while (
+      position - this.containerRect.top < this.scale(this.steps[stepIndex]) &&
+      stepIndex > 0
+    ) {
+      stepIndex--;
+    }
+    return this.steps[stepIndex];
+  }
+}
