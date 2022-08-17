fd10fa1ecdc98e14569d9c4ba19f637daa85c8e4
Nick Groszewski <ngroszewski@google.com>
2022-08-17 00:35:07 +0000

    Run flake8 for all targeted python versions

--- tensorboard/data/server/pip_package/setup.py
+++ tensorboard/data/server/pip_package/setup.py
@@ -45,6 +45,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Rust",
         "Topic :: Scientific/Engineering :: Mathematics",
         "Topic :: Software Development :: Libraries :: Python Modules",
