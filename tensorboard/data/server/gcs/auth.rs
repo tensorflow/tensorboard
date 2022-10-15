0cd3e3021af3f1a7c61e2d6697203d2ebf6dc817
corwinpro <corwinat@gmail.com>
2022-10-15 14:39:25 +0100

    raise not option

--- tensorboard/data/server/gcs/auth.rs
+++ tensorboard/data/server/gcs/auth.rs
@@ -26,6 +26,7 @@ limitations under the License.
 //! [RFC 6749]: https://tools.ietf.org/html/rfc6749
 //! ["Refreshing Access Tokens"]: https://www.oauth.com/oauth2-servers/access-tokens/refreshing-access-tokens/
 
+use log::warn;
 use std::fmt::{self, Debug};
 use std::sync::RwLock;
 
@@ -37,7 +38,7 @@ const SCOPES: &[&str] = &["https://www.googleapis.com/auth/cloud-platform"];
 
 static AUTH_MANAGER: OnceCell<AuthenticationManager> = OnceCell::const_new();
 
-fn get_token() -> AccessToken {
+fn get_token() -> Result<AccessToken, gcp_auth::Error> {
     async fn authentication_manager() -> &'static AuthenticationManager {
         AUTH_MANAGER
             .get_or_init(|| async {
@@ -57,9 +58,9 @@ fn get_token() -> AccessToken {
         .enable_all()
         .build()
         .unwrap()
-        .block_on(service_account_token());
+        .block_on(service_account_token())?;
 
-    AccessToken::from_gcs_token(token.ok())
+    Ok(AccessToken::from_gcs_token(Some(token)))
 }
 
 /// A potentially active token. Use [`authenticate`][Self::authenticate] to add an `Authorization`
@@ -151,7 +152,13 @@ impl TokenStore {
             }
         };
         // If we get here, we need a fresh token.
-        *token = Some(get_token());
+        match get_token() {
+            Ok(t) => *token = Some(t),
+            Err(e) => {
+                warn!("GCS authentication failed: {}", e);
+                return rb;
+            }
+        }
         if let Some(ref t) = *token {
             // debug!(
             //     "Obtained new access token, live for the next {:?}",
@@ -160,7 +167,7 @@ impl TokenStore {
             if t.is_valid() {
                 return t.authenticate(rb);
             }
-        };
+        }
         rb
     }
 }
