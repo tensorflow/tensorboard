f01a188fc35f2cb0a6c9bed84a6f0f95525338d3
Riley Jones <rileyjones@google.com>
2022-12-05 17:12:15 +0000

    address comments

--- tensorboard/webapp/widgets/line_chart_v2/sub_view/line_chart_axis_view.ts
+++ tensorboard/webapp/widgets/line_chart_v2/sub_view/line_chart_axis_view.ts
@@ -35,6 +35,8 @@ const AXIS_FONT = '11px Roboto, sans-serif';
   templateUrl: 'line_chart_axis_view.ng.html',
   styleUrls: ['line_chart_axis_view.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  // This prevents the CSS generated from being namespaced thus allowing CSS
+  // to reach into the line_chart_component and card_fob_controller_component
   encapsulation: ViewEncapsulation.None,
 })
 export class LineChartAxisComponent {
