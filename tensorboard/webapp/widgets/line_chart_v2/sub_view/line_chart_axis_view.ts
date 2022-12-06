5f89593728e7ca8a603fa443c3440820251ea996
Riley Jones <78179109+rileyajones@users.noreply.github.com>
2022-12-06 15:09:39 -0800

    Bug Fix: Update line chart encapsulation (#6092)
    ## Motivation for features / changes
    #6083 changed the way scss is namespaced. This fixed one issue but
    created others
    
    For Googlers:
    Before cl/493268254
    After cl/493373147
    
    ## Technical description of changes
    I created a do nothing component that simply disables scss encapsulation
    to enable intercomponent styling.
    
    ## Screenshots of UI changes
    Before:
    
    ![image](https://user-images.githubusercontent.com/78179109/206013391-afbda6cb-b05d-467f-b60f-7165c50af74a.png)
    
    After:
    
    ![image](https://user-images.githubusercontent.com/78179109/206012972-d04d4b41-1d1b-49b6-95a4-4d8eeeab6018.png)
    
    ## Detailed steps to verify changes work correctly (as executed by you)
    1) Start tensorboard
    2) Navigate to
    http://localhost:6006/?enableLinkedTime=true&enableDataTable=true&allowRangeSelection=true&enableProspectiveFob=true#timeseries
    3) Hover the X Axis of a scalar card
    4) Hover the Y Axis and assert the edit button appears in the top left
    corner (not the top right)
    5) Assert the axis edit button appears
    6) View an image card and assert the axis is NOT gray
    
    ## Alternate designs / implementations considered
    We could have manually encapsulated all the css classes

--- tensorboard/webapp/widgets/line_chart_v2/sub_view/line_chart_axis_view.ts
+++ tensorboard/webapp/widgets/line_chart_v2/sub_view/line_chart_axis_view.ts
@@ -18,7 +18,6 @@ import {
   EventEmitter,
   Input,
   Output,
-  ViewEncapsulation,
 } from '@angular/core';
 import {Dimension, Formatter, Scale} from '../lib/public_types';
 import {LinearScale, TemporalScale} from '../lib/scale';
@@ -35,9 +34,6 @@ const AXIS_FONT = '11px Roboto, sans-serif';
   templateUrl: 'line_chart_axis_view.ng.html',
   styleUrls: ['line_chart_axis_view.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  // This prevents the CSS generated from being namespaced thus allowing CSS
-  // to reach into the line_chart_component and card_fob_controller_component
-  encapsulation: ViewEncapsulation.None,
 })
 export class LineChartAxisComponent {
   @Input()
