00e07338bd99abe17083dfd76368f102f4fb211b
Adrian RC <adrianrc@google.com>
2022-12-15 01:55:06 +0000

    Implements "limited-input device" auth flow, to replace the deprecated OOB flow.

--- tensorboard/uploader/auth.py
+++ tensorboard/uploader/auth.py
@@ -15,14 +15,18 @@
 # Lint as: python3
 """Provides authentication support for TensorBoardUploader."""
 
+from typing import Iterable
 
+import datetime
 import errno
 import json
 import os
+import requests
 import sys
+import time
 import webbrowser
 
-import google_auth_oauthlib.flow
+import google_auth_oauthlib.flow as auth_flows
 import grpc
 import google.auth
 import google.auth.transport.requests
@@ -34,7 +38,6 @@ from tensorboard.util import tb_logging
 
 logger = tb_logging.get_logger()
 
-
 # OAuth2 scopes used for OpenID Connect:
 # https://developers.google.com/identity/protocols/OpenIDConnect#scope-param
 OPENID_CONNECT_SCOPES = (
@@ -42,10 +45,18 @@ OPENID_CONNECT_SCOPES = (
     "https://www.googleapis.com/auth/userinfo.email",
 )
 
-
+# A config used to authenticate using a browser in the same machine or
+# environment where the uploader is running.
+#
 # The client "secret" is public by design for installed apps. See
 # https://developers.google.com/identity/protocols/OAuth2?csw=1#installed
-OAUTH_CLIENT_CONFIG = """
+#
+# The implementation of the auth flow that uses this config currently accepts
+# only configs for "installed" or "web" apps, so there's a separate config below
+# for the "limited-input device" flow, which is used to authenticate from a
+# separate device when the uploader is running in an environment without a
+# browser.
+_BROWSER_OAUTH_CLIENT_CONFIG = """
 {
   "installed": {
     "client_id": "373649185512-8v619h5kft38l4456nm2dj4ubeqsrvh6.apps.googleusercontent.com",
@@ -54,11 +65,27 @@ OAUTH_CLIENT_CONFIG = """
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_secret": "pOyAuU2yq2arsM98Bw5hwYtr",
-    "redirect_uris": [
-      "urn:ietf:wg:oauth:2.0:oob",
-      "http://localhost"
-    ]
+    "redirect_uris": ["http://localhost"]
   }
+}
+"""
+
+# The config for authenticating using a separate device with a browser, to be
+# used when the environment where this uploader is running does not have a
+# browser available.
+#
+# The client "secret" is considered public, as it's distributed to the client
+# devices where this runs. See:
+# https://developers.google.com/identity/protocols/oauth2/limited-input-device
+#
+# Also see comment in previous constant.
+_CONSOLE_OAUTH_CLIENT_CONFIG = """
+{
+    "client_id": "373649185512-26ojik4u7dt0rdtfdmfnhpajqqh579qd.apps.googleusercontent.com",
+    "device_uri": "https://oauth2.googleapis.com/device/code",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "client_secret": "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",
+    "grant_type": "urn:ietf:params:oauth:grant-type:device_code"
 }
 """
 
@@ -149,36 +176,173 @@ class CredentialsStore:
                 raise
 
 
-def build_installed_app_flow(client_config):
-    """Returns a `CustomInstalledAppFlow` for the given config.
+def authenticate_user(
+    force_console=False) -> google.oauth2.credentials.Credentials:
+    """Makes the user authenticate to retrieve auth credentials."""
+    flow = _TbUploaderAuthFlow(
+        _BROWSER_OAUTH_CLIENT_CONFIG,
+        _CONSOLE_OAUTH_CLIENT_CONFIG,
+        OPENID_CONNECT_SCOPES)
+    return flow.run(force_console)
 
-    Args:
-      client_config (Mapping[str, Any]): The client configuration in the Google
-          client secrets format.
 
-    Returns:
-      CustomInstalledAppFlow: the constructed flow.
+class _TbUploaderAuthFlow(auth_flows.Flow):
+    """Customized version of an OAuth2 flow for the TB.dev uploader.
+
+    When a browser is not available or the `--auth_force_console` flag is
+    specified, it will use the [limited-input device flow]
+    (http://developers.google.com/identity/protocols/oauth2/limited-input-device#allowedscopes)
+    to authenticate the user.
+
+    Otherwise, it will use [a flow similar to the one used for installed apps]
+    (http://developers.google.com/identity/protocols/oauth2/native-app), in
+    which the response will include a redirect to localhost and a local web
+    server will handle the request after the redirect, with the appropriate
+    information.
     """
-    return CustomInstalledAppFlow.from_client_config(
-        client_config, scopes=OPENID_CONNECT_SCOPES
-    )
 
+    def __init__(
+        self,
+        installed_app_client_config,
+        limited_input_device_client_config,
+        scopes):
+        self._installed_app_client_config = (
+            json.loads(installed_app_client_config))
+        self._limited_input_device_client_config = (
+            json.loads(limited_input_device_client_config))
+        self.scopes = scopes
 
-class CustomInstalledAppFlow(google_auth_oauthlib.flow.InstalledAppFlow):
-    """Customized version of the Installed App OAuth2 flow."""
+    def run(self, force_console=False) -> google.oauth2.credentials.Credentials:
+        """Runs an auth flow to authenticate the user.
 
-    def run(self, force_console=False):
-        """Run the flow using a local server if possible, otherwise the
-        console."""
+        The default behavior is to have the user authenticate on the browser in
+        the same machine where this is running, which would produce a redirect
+        response with an authorization code as a parameter in the http request,
+        which would be received by a webserver started here locally.
+
+        If any of the following is true, a different auth flow will be used:
+        - the flag `--auth_force_console` is set to true, or
+        - a browser is not available, or
+        - a local web server cannot be started
+
+        In this case, a "limited-input device" flow will be used, in which the
+        user is presented with a URL and a short code that they'd need to use to
+        authenticate and authorize access in a separate device, after which the
+        uploader will poll for access until the access is granted or rejected,
+        or the initiated auth request expires.
+        """
         # TODO(b/141721828): make auto-detection smarter, especially for macOS.
         if not force_console and os.getenv("DISPLAY"):
             try:
-                return self.run_local_server(port=0)
+                return self.run_installed_app_flow()
             except webbrowser.Error:
                 sys.stderr.write(
-                    "Falling back to console authentication flow...\n"
-                )
-        return self.run_console()
+                    "Falling back to remote authentication flow...\n")
+        return self.run_limited_input_device_flow()
+
+    def run_installed_app_flow(self) -> google.oauth2.credentials.Credentials:
+        flow = auth_flows.InstalledAppFlow.from_client_config(
+            self._installed_app_client_config, scopes=self.scopes)
+        return flow.run_local_server(port=0)
+
+    def run_limited_input_device_flow(
+        self, **kwargs) -> google.oauth2.credentials.Credentials:
+        print("Using authentication without a browser.\n")
+        device_response = self._send_device_auth_request()
+        prompt_message = (
+            "Please visit this URL in another device, and enter the provided "
+                "code to authenticate with the TensorBoard Uploader:\n"
+                "\n"
+                "url: {url}\n"
+                "code: {code}\n"
+            .format(
+                url=device_response["verification_url"],
+                code=device_response["user_code"]))
+        print(prompt_message)
+
+        auth_response = self._poll_for_auth_token(
+            device_code=device_response["device_code"],
+            grant_type=self._limited_input_device_client_config["grant_type"],
+            polling_interval=device_response["interval"],
+            expiration_seconds=device_response["expires_in"])
+
+        if not auth_response:
+            # Should not happen, as any failed authentication should have raised
+            # a different error when polling for access.
+            raise RuntimeError(
+                "An unexpected error occurred while authenticating, "
+                "please try again.")
+
+        return self._build_credentials(auth_response)
+
+    def _send_device_auth_request(self):
+        device_uri = self._limited_input_device_client_config["device_uri"]
+        client_id = self._limited_input_device_client_config["client_id"]
+        params = {
+            "client_id": client_id,
+            "scope": " ".join(self.scopes)
+        }
+        r = requests.post(device_uri, data=params).json()
+        if "device_code" not in r:
+            raise RuntimeError(
+                "Auth service temporarily unavailable, try again later.")
+        return r
+
+    def _poll_for_auth_token(
+        self,
+        device_code: str,
+        grant_type: str,
+        polling_interval: int,
+        expiration_seconds: int):
+
+        token_uri = self._limited_input_device_client_config["token_uri"]
+        client_id = self._limited_input_device_client_config["client_id"]
+        client_secret = (
+            self._limited_input_device_client_config["client_secret"])
+        params = {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "device_code": device_code,
+            "grant_type": grant_type
+            }
+        expiration_time = time.time() + expiration_seconds
+        # Error cases documented in
+        # https://developers.google.com/identity/protocols/oauth2/limited-input-device#step-2:-handle-the-authorization-server-response
+        while time.time() < expiration_time:
+            resp = requests.post(token_uri, data=params)
+            r = resp.json()
+            if "access_token" in r:
+                return r
+            if "error" in r and r["error"] == "access_denied":
+                raise PermissionError("Auth was denied.")
+            if resp.status_code in {400, 401}:
+                raise ValueError("There must be an error in the request.")
+
+            if "error" in r and r["error"] == "authorization_pending":
+                time.sleep(polling_interval)
+            elif "error" in r and r["error"] == "slow_down":
+                time.sleep(int(polling_interval * 1.5))
+            else:
+                raise RuntimeError(
+                    "An unexpected error occurred while authenticating.")
+        raise TimeoutError("Timed out waiting for authorization.")
+
+    def _build_credentials(
+        self, auth_response) -> google.oauth2.credentials.Credentials:
+        expiration_timestamp = datetime.datetime.utcfromtimestamp(
+            int(time.time()) + auth_response["expires_in"])
+
+        return google.oauth2.credentials.Credentials(
+            auth_response["access_token"],
+            refresh_token=auth_response["refresh_token"],
+            id_token=auth_response["id_token"],
+            token_uri=self._limited_input_device_client_config["token_uri"],
+            client_id=self._limited_input_device_client_config["client_id"],
+            client_secret= (
+                self._limited_input_device_client_config["client_secret"]),
+            scopes=self.scopes,
+            expiry=expiration_timestamp
+        )
 
 
 class IdTokenAuthMetadataPlugin(grpc.AuthMetadataPlugin):
--- tensorboard/uploader/auth_test.py
+++ tensorboard/uploader/auth_test.py
@@ -15,12 +15,17 @@
 # Lint as: python3
 """Tests for tensorboard.uploader.auth."""
 
-
+from datetime import datetime
 import json
 import os
+import requests
+import time
+from typing import Dict
+from unittest import mock
 
+import google_auth_oauthlib.flow as auth_flows
 import google.auth.credentials
-import google.oauth2.credentials
+from google.oauth2.credentials import Credentials
 
 from tensorboard.uploader import auth
 from tensorboard import test as tb_test
@@ -147,6 +152,260 @@ class CredentialsStoreTest(tb_test.TestCase):
             f.write("{}")
         with self.assertRaises(ValueError):
             store.read_credentials()
+
+
+class FakeHttpResponse():
+    """A fake implementation of the response from the requests library."""
+
+    def __init__(self, data: Dict, status:int = 200):
+        self.status_code = status
+        self._data = data
+
+    def json(self):
+        return self._data
+
+
+class AuthFlowTest(tb_test.TestCase):
+
+    # Original config passed to the auth flow for the "browser" auth flow (aka
+    # installed app flow).
+    _INSTALLED_APP_AUTH_CONFIG_JSON = """
+        {
+            "installed": {
+                "client_id": "installed_client_id",
+                "auth_uri": "https://google.com/auth",
+                "token_uri": "https://google.com/token",
+                "client_secret": "installed_client_secret",
+                "redirect_uris": ["http://localhost"]
+            }
+        }
+        """
+
+    # Original config passed to the auth flow for the "no browser" auth flow
+    # (aka "console" or "limited-input device" flow).
+    _CONSOLE_AUTH_CONFIG_JSON = """
+        {
+            "client_id": "console_client_id",
+            "device_uri": "https://google.com/device",
+            "token_uri": "https://google.com/token",
+            "client_secret": "console_client_secret",
+            "grant_type": "urn:ietf:params:oauth:grant-type:device_code"
+        }
+        """
+
+    _SCOPES = ["email", "openid"]
+
+    _DEVICE_RESPONSE = FakeHttpResponse({
+            "device_code": "resp_device_code",
+            "verification_url": "auth.google.com/device",
+            "user_code": "resp_user_code",
+            "interval": 5,
+            "expires_in": 300
+        })
+
+    _AUTH_PENDING_RESPONSE = FakeHttpResponse({
+            "error": "authorization_pending"
+        }, status=428)
+
+    _AUTH_GRANTED_RESPONSE = FakeHttpResponse({
+        "access_token": "some_access_token",
+        "refresh_token": "some_refresh_token",
+        "id_token": "some_id_token",
+        "expires_in": 3600,
+    })
+
+    def setUp(self):
+        super().setUp()
+
+        # The faked installed app flow that we use from the common library.
+        # That class should already be tested, so we mostly want to test
+        # high-level interactions with it.
+        self.fake_auth_flow = FakeInstalledAppFlow(Credentials("access_token"))
+
+        self.mocked_time = self.enter_context(
+            mock.patch.object(time, 'time', autospec=True))
+        # Timestamps from a fake clock. The values don't matter in most tests.
+        self.mocked_time.side_effect = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        self.mocked_sleep = self.enter_context(
+            mock.patch.object(time, 'sleep', autospec=True))
+
+        self.mocked_post = self.enter_context(
+            mock.patch.object(requests, 'post', autospec=True))
+
+        self.mocked_auth_flow_creator_fn = self.enter_context(
+            mock.patch.object(
+                auth_flows.InstalledAppFlow,
+                'from_client_config',
+                return_value=self.fake_auth_flow))
+
+        # Used to estimate if a browser is available in this env.
+        self.mocked_getenv = self.enter_context(
+            mock.patch.object(os, 'getenv', return_value="some_display"))
+
+        self.flow = auth._TbUploaderAuthFlow(
+            self._INSTALLED_APP_AUTH_CONFIG_JSON,
+            self._CONSOLE_AUTH_CONFIG_JSON,
+            self._SCOPES)
+
+    def test_auth_flow__browser__uses_intalled_app_flow(self):
+        self.flow.run()
+        self.mocked_auth_flow_creator_fn.assert_called_once()
+        self.assertTrue(self.fake_auth_flow.run_local_server_was_called)
+
+    def test_auth_flow__console__device_request_fails__raises(self):
+        mocked_post = self.enter_context(
+            mock.patch.object(requests, 'post', autospec=True))
+        mocked_post.return_value = (
+            FakeHttpResponse({"error": "quota exceeded"}, status=403))
+
+        with self.assertRaisesRegex(
+            RuntimeError, "Auth service temporarily unavailable"):
+            self.flow.run(force_console=True)
+
+    def test_auth_flow__console__polling__auth_pending_response__keeps_polling(
+        self):
+        self.mocked_post.side_effect = [
+            self._DEVICE_RESPONSE,
+            self._AUTH_PENDING_RESPONSE,
+            self._AUTH_GRANTED_RESPONSE,
+        ]
+
+        self.flow.run(force_console=True)
+
+        device_params = {
+            "client_id": "console_client_id",
+            "scope": "email openid"
+        }
+        polling_params = {
+            "client_id": "console_client_id",
+            "client_secret": "console_client_secret",
+            "device_code": "resp_device_code",
+            "grant_type": "urn:ietf:params:oauth:grant-type:device_code"
+        }
+        device_uri = "https://google.com/device"
+        token_uri = "https://google.com/token"
+        # One device code request, then two polling calls:
+        # the first poll returned auth_pending, the second one returned success.
+        expected_post_requests = [
+            mock.call(device_uri, data=device_params),
+            mock.call(token_uri, data=polling_params),
+            mock.call(token_uri, data=polling_params),
+        ]
+        self.assertSequenceEqual(
+            expected_post_requests,
+            self.mocked_post.call_args_list)
+        sleep_time = self._DEVICE_RESPONSE.json()["interval"]
+        self.mocked_sleep.assert_called_once_with(sleep_time)
+
+    def test_auth_flow__console__polling__access_granted__returns_credentials(
+        self):
+        self.mocked_post.side_effect = [
+            self._DEVICE_RESPONSE,
+            self._AUTH_GRANTED_RESPONSE,
+        ]
+        # Based on these mocked responses, time.time() is called 3 times:
+        # 1. To generate an expiration time for polling
+        # 2. While polling to check if we reached the expiration time
+        # 3. To calculate an expiration time for the credentials (useful below)
+        #
+        # The third timestamp corresponds to: 2022/12/13 16:00:00Z
+        #
+        # Note that I could have simply used 3, it doesn't really matter if this
+        # is a realistic timestamp, but for the sake of clarity and having a
+        # realistic setup, I decided to use this.
+        now_timestamp = 1670947200
+        self.mocked_time.side_effect = [1, 2, now_timestamp]
+
+        creds = self.flow.run(force_console=True)
+
+        auth_response = self._AUTH_GRANTED_RESPONSE.json()
+        console_config = json.loads(self._CONSOLE_AUTH_CONFIG_JSON)
+
+        expected_expiration_timestamp = datetime.utcfromtimestamp(
+            now_timestamp + auth_response["expires_in"])
+
+        expected_credentials = Credentials(
+            auth_response["access_token"],
+            refresh_token=auth_response["refresh_token"],
+            id_token=auth_response["id_token"],
+            token_uri=console_config["token_uri"],
+            client_id=console_config["client_id"],
+            client_secret= console_config["client_secret"],
+            scopes=self._SCOPES,
+            expiry=expected_expiration_timestamp
+        )
+        self.assertEqual(creds.to_json(), expected_credentials.to_json())
+
+    def test_auth_flow__console__polling__access_denied__raises(self):
+        access_denied_response = FakeHttpResponse(
+            {"error": "access_denied"}, 403)
+        self.mocked_post.side_effect = [
+            self._DEVICE_RESPONSE,
+            access_denied_response,
+        ]
+
+        with self.assertRaisesRegex(PermissionError, "Auth was denied."):
+            self.flow.run(force_console=True)
+
+    def test_auth_flow__console__polling__slow_down_response__waits_longer(
+        self):
+        slow_down_response = FakeHttpResponse({"error": "slow_down"})
+        self.mocked_post.side_effect = [
+            self._DEVICE_RESPONSE,
+            slow_down_response,
+            self._AUTH_GRANTED_RESPONSE
+        ]
+
+        self.flow.run(force_console=True)
+
+        interval = self._DEVICE_RESPONSE.json()["interval"]
+        sleep_time = int(interval * 1.5)
+        self.mocked_sleep.assert_called_once_with(sleep_time)
+
+    def test_auth_flow__console__polling__bad_request_response__raises(self):
+        bad_request_response = FakeHttpResponse(
+            {"error": "bad_request"}, 401)
+        self.mocked_post.side_effect = [
+            self._DEVICE_RESPONSE,
+            bad_request_response,
+        ]
+
+        with self.assertRaisesRegex(
+            ValueError, "There must be an error in the request."):
+            self.flow.run(force_console=True)
+
+    def test_auth_flow__console__polling__timed_out__raises(self):
+        self.mocked_post.return_value = self._DEVICE_RESPONSE
+        # Before polling, the time increased more than the expiration time from
+        # the device response.
+        self.mocked_time.side_effect = [1, 5000]
+
+        with self.assertRaisesRegex(
+            TimeoutError, "Timed out waiting for authorization."):
+            self.flow.run(force_console=True)
+
+    def test_auth_flow__console__polling__unexpected_error__raises(self):
+        unexpected_response = FakeHttpResponse({"error": "unexpected 500"}, 500)
+        self.mocked_post.side_effect = [
+            self._DEVICE_RESPONSE,
+            unexpected_response,
+        ]
+
+        with self.assertRaisesRegex(
+            RuntimeError, "An unexpected error occurred while authenticating."):
+            self.flow.run(force_console=True)
+
+
+class FakeInstalledAppFlow():
+    """A minimal fake for the InstalledApp flow with the function we call."""
+    def __init__(self, credentials):
+        self.run_local_server_was_called = False
+        self._creds = credentials
+
+    def run_local_server(self, port=8080):
+        self.run_local_server_was_called = True
+        return self._creds
 
 
 if __name__ == "__main__":
