c5158e97c7767c96fb16340c820a31d62420cb8a
Nick Felt <nickfelt@google.com>
2023-01-13 13:42:41 -0800

    infra: mark Python 3.11 as supported version

--- tensorboard/pip_package/setup.py
+++ tensorboard/pip_package/setup.py
@@ -76,6 +76,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Scientific/Engineering :: Mathematics",
         "Topic :: Software Development :: Libraries :: Python Modules",
