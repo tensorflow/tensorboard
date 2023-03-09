f75ba455cc22bf4fe3359feeea825c4bc1c1b5f8
Nick Groszewski <groszewn@gmail.com>
2023-03-09 13:36:04 -0500

    Revert "Temporarily disable fsspec glob test for sync (#6231)"
    This reverts commit 4cd43485ce05852c6e62e5287929a6a4674b7225.

--- tensorboard/compat/tensorflow_stub/io/gfile_fsspec_test.py
+++ tensorboard/compat/tensorflow_stub/io/gfile_fsspec_test.py
@@ -15,7 +15,6 @@
 
 
 import posixpath
-import unittest
 
 from tensorboard import test as tb_test
 from tensorboard.compat.tensorflow_stub import errors
@@ -532,9 +531,6 @@ class GFileFSSpecTest(tb_test.TestCase):
             ],
         )
 
-    @unittest.skip(
-        "Temporarily disabling to avoid internal sync breakage across fsspec versions"
-    )
     def testGlobAbsolute(self):
         """
         This tests glob with in memory file system which does return
