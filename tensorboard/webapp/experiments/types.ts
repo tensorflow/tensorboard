a5da6201f51849cf625d9b53b8a44c15103f0f06
jameshollyer <j.c.hollyer@gmail.com>
2023-03-09 17:59:53 +0000

    correct comment

--- tensorboard/webapp/experiments/types.ts
+++ tensorboard/webapp/experiments/types.ts
@@ -24,6 +24,9 @@ export declare interface Experiment {
   related_links?: Array<{name: string; url: string}>;
   // These state values were chosen to follow these AIPs
   // https://google.aip.dev/164 and https://google.aip.dev/216.
+  // However, we replaced 'delete' with 'hidden' as this feature is different
+  // from the soft delete described in the AIP. In this case 'hidden' simply
+  // means that the experiment is not shown in the experiment list.
   state?: 'active' | 'hidden' | 'unspecified';
 }
 
