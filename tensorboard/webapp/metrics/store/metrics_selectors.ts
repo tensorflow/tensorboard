0146d9cbeb51f81907063b5fb5f0b186f625e73c
japie <jieweiwu@google.com>
2023-03-13 10:20:42 -0700

    use bracket to index cardStateMap vlaue.
    Co-authored-by: Riley Jones <78179109+rileyajones@users.noreply.github.com>

--- tensorboard/webapp/metrics/store/metrics_selectors.ts
+++ tensorboard/webapp/metrics/store/metrics_selectors.ts
@@ -486,7 +486,7 @@ export const isMetricsSlideoutMenuOpen = createSelector(
 export const getMetricsCardMinMax = createSelector(
   getCardStateMap,
   (cardStateMap: CardStateMap, cardId: CardId): MinMaxStep | undefined => {
-    if (!Object.keys(cardStateMap).includes(cardId)) return undefined;
+    if (!cardStateMap[cardId]) return;
 
     return getMinMaxStepFromCardState(cardStateMap[cardId]);
   }
