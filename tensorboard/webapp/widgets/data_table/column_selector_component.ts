405d32779f8cda489f742ea0dfaac1de99697c40
Riley Jones <rileyjones@google.com>
2023-06-02 22:28:15 +0000

    create column selector component

---
+++ tensorboard/webapp/widgets/data_table/column_selector_component.ts
@@ -0,0 +1,125 @@
+/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+import {
+  Component,
+  EventEmitter,
+  Output,
+  Input,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  HostListener,
+  OnInit,
+  AfterContentInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import {ColumnHeader} from './types';
+import {BehaviorSubject} from 'rxjs';
+
+@Component({
+  selector: 'tb-data-table-column-selector-component',
+  templateUrl: 'column_selector_component.ng.html',
+  styleUrls: ['column_selector_component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ColumnSelectorComponent implements OnInit, AfterViewInit {
+  @Input() selectableColumns: ColumnHeader[] = [];
+  @Output() columnSelected = new EventEmitter<ColumnHeader>();
+
+  @ViewChild('search')
+  private readonly searchField!: ElementRef;
+
+  @ViewChild('columnList')
+  private readonly columnList!: ElementRef;
+
+  searchInput = '';
+  selectedIndex$ = new BehaviorSubject(0);
+
+  ngOnInit() {
+    this.selectedIndex$.subscribe(() => {
+      if (!this.columnList) {
+        return;
+      }
+      const selectedButton: HTMLButtonElement =
+        this.columnList.nativeElement.querySelector('button.selected');
+      if (!selectedButton) return;
+
+      const scrollAreaHeight: number =
+        this.columnList.nativeElement.getBoundingClientRect().height;
+      const buttonHeight = selectedButton.getBoundingClientRect().height;
+      const scrollTop = this.columnList.nativeElement.scrollTop;
+      if (this.selectedIndex$.getValue() * buttonHeight < scrollTop) {
+        this.columnList.nativeElement.scrollTop =
+          this.selectedIndex$.getValue() * buttonHeight;
+      }
+
+      if (
+        (this.selectedIndex$.getValue() + 1) * buttonHeight >
+        scrollTop + scrollAreaHeight
+      ) {
+        this.columnList.nativeElement.scrollTop =
+          (this.selectedIndex$.getValue() + 1) * buttonHeight -
+          scrollAreaHeight;
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.searchInput = '';
+    this.searchField.nativeElement.focus();
+    this.selectedIndex$.next(0);
+  }
+
+  focus() {
+    this.searchField?.nativeElement.focus();
+  }
+
+  getFilteredColumns() {
+    return this.selectableColumns.filter((columnHeader) =>
+      columnHeader.name.toLowerCase().match(this.searchInput.toLowerCase())
+    );
+  }
+
+  searchInputChanged() {
+    this.selectedIndex$.next(0);
+  }
+
+  selectColumn(header: ColumnHeader) {
+    this.selectedIndex$.next(0);
+    this.columnSelected.emit(header);
+  }
+
+  @HostListener('document:keydown.arrowup', ['$event'])
+  onUpArrow() {
+    this.selectedIndex$.next(Math.max(this.selectedIndex$.getValue() - 1, 0));
+  }
+
+  @HostListener('document:keydown.arrowdown', ['$event'])
+  onDownArrow() {
+    this.selectedIndex$.next(
+      Math.min(
+        this.selectedIndex$.getValue() + 1,
+        this.getFilteredColumns().length - 1
+      )
+    );
+  }
+
+  @HostListener('document:keydown.enter', ['$event'])
+  onEnterPressed() {
+    this.selectColumn(
+      this.getFilteredColumns()[this.selectedIndex$.getValue()]
+    );
+  }
+}
---
+++ tensorboard/webapp/widgets/data_table/column_selector_test.ts
@@ -0,0 +1,148 @@
+/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  flush,
+} from '@angular/core/testing';
+import {ColumnSelectorComponent} from './column_selector_component';
+import {ColumnHeader, ColumnHeaderType} from './types';
+import {FormsModule} from '@angular/forms';
+import {By} from '@angular/platform-browser';
+
+describe('column selector', () => {
+  let fixture: ComponentFixture<ColumnSelectorComponent>;
+  function renderComponent(props: {selectableColumns: ColumnHeader[]}) {
+    const fixture = TestBed.createComponent(ColumnSelectorComponent);
+    if (props.selectableColumns) {
+      fixture.componentInstance.selectableColumns = props.selectableColumns;
+    }
+    fixture.detectChanges();
+
+    return fixture;
+  }
+
+  function getSelectedButton() {
+    return fixture.debugElement.query(By.css('button.selected'));
+  }
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ColumnSelectorComponent],
+      imports: [FormsModule],
+    }).compileComponents();
+
+    fixture = renderComponent({
+      selectableColumns: [
+        {
+          type: ColumnHeaderType.RUN,
+          name: 'runs',
+          displayName: 'Runs',
+          enabled: true,
+        },
+        {
+          type: ColumnHeaderType.HPARAM,
+          name: 'lr',
+          displayName: 'Learning Rate',
+          enabled: true,
+        },
+      ],
+    });
+  });
+
+  describe('keyboard navigation', () => {
+    it('deccreases selected index when the up arrow is pressed', () => {
+      fixture.componentInstance.selectedIndex$.next(1);
+      fixture.detectChanges();
+      expect(getSelectedButton().nativeElement.innerText).toEqual(
+        'Learning Rate'
+      );
+
+      const event = new KeyboardEvent('keydown', {key: 'arrowup'});
+      document.dispatchEvent(event);
+      fixture.detectChanges();
+      expect(getSelectedButton().nativeElement.innerText).toEqual('Runs');
+    });
+
+    it('increases selected index when the down arrow is pressed', () => {
+      expect(getSelectedButton().nativeElement.innerText).toEqual('Runs');
+      const event = new KeyboardEvent('keydown', {key: 'arrowdown'});
+      document.dispatchEvent(event);
+      fixture.detectChanges();
+      expect(getSelectedButton().nativeElement.innerText).toEqual(
+        'Learning Rate'
+      );
+    });
+
+    it('resets selected index when search input changes', () => {
+      fixture.componentInstance.selectedIndex$.next(1);
+      fixture.detectChanges();
+      expect(getSelectedButton().nativeElement.innerText).toEqual(
+        'Learning Rate'
+      );
+
+      fixture.componentInstance.searchInputChanged();
+      fixture.detectChanges();
+
+      expect(getSelectedButton().nativeElement.innerText).toEqual('Runs');
+    });
+
+    it('selects a column when the enter key is pressed', fakeAsync(() => {
+      let selectedColumn: ColumnHeader;
+      fixture.componentInstance.columnSelected.subscribe((column) => {
+        selectedColumn = column;
+      });
+
+      const event = new KeyboardEvent('keydown', {key: 'enter'});
+      document.dispatchEvent(event);
+      flush();
+      expect(selectedColumn!.name).toEqual('runs');
+    }));
+
+    it('cannot select indexes below 0', () => {
+      expect(getSelectedButton().nativeElement.innerText).toEqual('Runs');
+      const event = new KeyboardEvent('keydown', {key: 'arrowup'});
+      document.dispatchEvent(event);
+      fixture.detectChanges();
+      expect(getSelectedButton().nativeElement.innerText).toEqual('Runs');
+    });
+
+    it('cannot select indexes greater than the number of selectable columns', () => {
+      fixture.componentInstance.selectedIndex$.next(1);
+      fixture.detectChanges();
+      expect(getSelectedButton().nativeElement.innerText).toEqual(
+        'Learning Rate'
+      );
+      const event = new KeyboardEvent('keydown', {key: 'arrowdown'});
+      document.dispatchEvent(event);
+      fixture.detectChanges();
+      expect(getSelectedButton().nativeElement.innerText).toEqual(
+        'Learning Rate'
+      );
+    });
+  });
+
+  it('selects a column when the it is clicked', fakeAsync(() => {
+    let selectedColumn: ColumnHeader;
+    fixture.componentInstance.columnSelected.subscribe((column) => {
+      selectedColumn = column;
+    });
+    getSelectedButton().nativeElement.click();
+    flush();
+
+    expect(selectedColumn!.name).toEqual('runs');
+  }));
+});
