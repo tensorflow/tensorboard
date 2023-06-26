24038fd7118faff4299c207743272aaa12efe8d0
Riley Jones <rileyjones@google.com>
2023-06-26 17:22:55 +0000

    add tests

--- tensorboard/webapp/metrics/views/metrics_container_test.ts
+++ tensorboard/webapp/metrics/views/metrics_container_test.ts
@@ -22,6 +22,8 @@ import {getEnableHparamsInTimeSeries} from '../../feature_flag/store/feature_fla
 import {RunsSelectorContainer} from '../../runs/views/runs_selector/runs_selector_container';
 import {provideMockTbStore} from '../../testing/utils';
 import {MetricsDashboardContainer} from './metrics_container';
+import {getRunsTableFullScreen} from '../../core/store/core_selectors';
+import {MainViewContainer} from './main_view/main_view_container';
 
 describe('metrics view', () => {
   let store: MockStore<State>;
@@ -63,5 +65,15 @@ describe('metrics view', () => {
       fixture.debugElement.query(By.css('runs-selector')).componentInstance
         .showHparamsAndMetrics
     ).toBeFalse();
+  });
+
+  it('hides main view when the runs table is full screen is true', () => {
+    store.overrideSelector(getRunsTableFullScreen, true);
+    const fixture = TestBed.createComponent(MetricsDashboardContainer);
+    fixture.detectChanges();
+
+    expect(
+      fixture.debugElement.query(By.directive(MainViewContainer))
+    ).toBeNull();
   });
 });
