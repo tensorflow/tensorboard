5534f1e9a91310e15fc151b47f4a2ee0918b956c
Riley Jones <78179109+rileyajones@users.noreply.github.com>
2023-06-26 19:45:20 -0700

    Chore: Fix broken tests (#6458)
    ## Motivation for features / changes
    Somehow some tests got broken on master and caused nightly to fail
    
    https://github.com/tensorflow/tensorboard/actions/runs/5377348327/jobs/9755711856#step:11:2529

--- tensorboard/webapp/core/views/layout_test.ts
+++ tensorboard/webapp/core/views/layout_test.ts
@@ -86,6 +86,8 @@ describe('layout test', () => {
       dispatchedActions.push(action);
     });
     store.overrideSelector(getSideBarWidthInPercent, 10);
+    // When the runs table is full screen the width is overridden to 100%.
+    store.overrideSelector(getRunsTableFullScreen, false);
   });
 
   afterEach(() => {
