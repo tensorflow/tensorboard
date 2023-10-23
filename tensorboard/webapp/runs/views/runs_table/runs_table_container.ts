33b2bae8166f912551499978204981d7ce2be43f
Riley Jones <rileyjones@google.com>
2023-10-23 22:34:00 +0000

    sort runs with numeric prefixes differently

--- tensorboard/webapp/runs/views/runs_table/runs_table_container.ts
+++ tensorboard/webapp/runs/views/runs_table/runs_table_container.ts
@@ -182,6 +182,24 @@ function sortRunTableItems(
   return sortedItems;
 }
 
+function parseNumericPrefix(value: string | number) {
+  if (typeof value === 'number') {
+    return value;
+  }
+  const numbers = new Set('0123456789'.split(''));
+
+  for (let i = 0; i < value.length; i++) {
+    if (!numbers.has(value[i])) {
+      if (i === 0) return;
+      return parseInt(value.slice(0, i));
+    }
+  }
+
+  return;
+}
+
+const POTENTIALLY_NUMERIC = new Set(['string', 'number']);
+
 function sortTableDataItems(
   items: TableData[],
   sort: SortingInfo
@@ -205,9 +223,47 @@ function sortTableDataItems(
       return bValue === undefined ? -1 : 1;
     }
 
-    return aValue < bValue === (sort.order === SortingOrder.ASCENDING) ? -1 : 1;
+    if (
+      POTENTIALLY_NUMERIC.has(typeof aValue) &&
+      POTENTIALLY_NUMERIC.has(typeof bValue)
+    ) {
+      const aPrefix = parseNumericPrefix(aValue as string | number);
+      const bPrefix = parseNumericPrefix(bValue as string | number);
+      if (aPrefix === undefined || bPrefix === undefined) {
+        return bPrefix === undefined ? -1 : 1;
+      }
+      if (aPrefix !== undefined && bPrefix !== undefined) {
+        if (aPrefix === bPrefix) {
+          const aPostfix =
+            aValue.toString().slice(aPrefix.toString().length) || undefined;
+          const bPostfix =
+            bValue.toString().slice(bPrefix.toString().length) || undefined;
+          if (!aPrefix || !bPostfix) {
+            return !bPostfix ? -1 : 1;
+          }
+          return orderFromLocalComparison(aPostfix, bPostfix);
+        }
+
+        return orderFromLocalComparison(aPrefix, bPrefix);
+      }
+    }
+
+    return orderFromLocalComparison(aValue, bValue);
   });
   return sortedItems;
+
+  function orderFromLocalComparison(
+    a: TableData[string] | undefined,
+    b: TableData[string] | undefined
+  ) {
+    if (a === undefined) {
+      return sort.order === SortingOrder.ASCENDING ? -1 : 1;
+    }
+    if (b === undefined) {
+      return sort.order === SortingOrder.ASCENDING ? 1 : -1;
+    }
+    return a < b === (sort.order === SortingOrder.ASCENDING) ? -1 : 1;
+  }
 }
 
 function matchFilter(
