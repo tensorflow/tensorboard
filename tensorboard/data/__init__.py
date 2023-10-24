ac7574d01330e91f56886cecb9bdfbd2c30f7782
Brian Dubois <bdubois@google.com>
2023-10-24 07:20:08 -0400

    tbdev turndown: Remove dataframe api. (#6644)
    Remove the experimental API that generates pandas data frames from
    TensorBoard.dev data. We are turning down TensorBoard.dev.

--- tensorboard/data/__init__.py
+++ tensorboard/data/__init__.py
@@ -12,6 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
-
-from tensorboard.data import experimental  # noqa: F401
