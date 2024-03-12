0313b4b168276b461c52858b56dd39caa454d6a2
Riley Jones <rileyjones@google.com>
2024-03-12 22:45:05 +0000

    stop using type imports 😢

--- tensorboard/webapp/routes/dashboard_deeplink_provider_types.ts
+++ tensorboard/webapp/routes/dashboard_deeplink_provider_types.ts
@@ -12,9 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-import {type URLDeserializedState as MetricsURLDeserializedState} from '../metrics/types';
-import {type URLDeserializedState as RunsURLDeserializedState} from '../runs/types';
-import {type URLDeserializedState as CoreURLDeserializedState} from '../core/types';
+import {URLDeserializedState as MetricsURLDeserializedState} from '../metrics/types';
+import {URLDeserializedState as RunsURLDeserializedState} from '../runs/types';
+import {URLDeserializedState as CoreURLDeserializedState} from '../core/types';
 
 // No need to deserialize the Experimental Plugins as it is immutable and is only read at
 // the start of the application.
