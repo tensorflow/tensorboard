24ac27090a25dc7d95dc99934142ef43e083187b
Hoonji <736199+hoonji@users.noreply.github.com>
2024-03-22 17:15:21 +0900

    Removes fdescribe test

--- tensorboard/webapp/util/dom_test.ts
+++ tensorboard/webapp/util/dom_test.ts
@@ -15,7 +15,7 @@ limitations under the License.
 
 import {isMouseEventInElement} from './dom';
 
-fdescribe('dom utils', () => {
+describe('dom utils', () => {
   describe('isMouseEventInElement', () => {
     [
       {
