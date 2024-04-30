af346056e1c158b6ed6a38b136af648b2e12a986
Seayeon Lee <seayeon@google.com>
2024-04-30 15:26:00 +0900

    [Global Pins] Shows a confirmation dialog when disabling the global pin feature (#6843)
    ## Motivation for features / changes
    Regarding #6831 , this PR adds confirmation dialog" that appears when
    the user clears the checkbox to make the user aware that local storage
    is disappearing.
    
    ## Technical description of changes
    0e63b07 created a `saving pins dialog` component
    
    05e06f5 Created a `saving pins checkbox` component
    * The Save Pin checkbox prevents the default click and must be done
    after confirmation in the modal. So I added
    `MAT_CHECKBOX_DEFAULT_OPTIONS`
    (https://v15.material.angular.io/comComponents/checkbox/overview#click-action-config)
    to prevent the default click event. However, if I add this directly to
    the `settings view component`, all checkboxes will be disabled,
    including other unwanted ones. So, I created a separate checkbox for the
    Save Pin checkbox. Please let me know is there any other good way to do
    this.
    
    6ba778e Make the dialog shows up when user clears the checkbox
    * Passes to `onEnableSavingPinsToggled` whether the checkbox is
    selected.
    * If the checkbox is selected and user clicks it, it means that the user
    disables the global pin, so a dialog box is displayed.
    * If the checkbox is not checked but is clicked, it means the user is
    trying to enable a global pin and no dialog box will be displayed.
    * `onEnableSavingPinsToggled` and `isSavingPinsEnabled` is passed to
    `saving-pins-checkbox` component.
    
    ## Screenshots of UI changes (or N/A)
    
    ![image](https://github.com/tensorflow/tensorboard/assets/24772412/eb442565-4f39-45e7-b893-4866ee3fc1e4)
    
    ## Detailed steps to verify changes work correctly (as executed by you)
    unit test passes + TAP presubmit passes 
    ## Alternate designs / implementations considered (or N/A)

---
+++ tensorboard/webapp/metrics/views/right_pane/saving_pins_dialog/saving_pins_dialog_module.ts
@@ -0,0 +1,30 @@
+/* Copyright 2024 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+
+import {SavingPinsDialogComponent} from './saving_pins_dialog_component';
+
+/**
+ * Provides <saving-pins-dialog>.
+ */
+@NgModule({
+  declarations: [SavingPinsDialogComponent],
+  exports: [SavingPinsDialogComponent],
+  imports: [CommonModule, MatButtonModule, MatDialogModule],
+})
+export class SavingPinsDialogModule {}
