c7a7f973eeec3f63d57e3ec7f053f80b7af46dfd
Hamza Jazmati <hamzaj@meta.com>
2024-08-29 13:29:27 -0400

    Allow Custom File Systems To Override The Walk Method

--- tensorboard/compat/tensorflow_stub/io/gfile.py
+++ tensorboard/compat/tensorflow_stub/io/gfile.py
@@ -929,7 +929,7 @@ def walk(top, topdown=True, onerror=None):
     top = compat.as_str_any(top)
     fs = get_filesystem(top)
 
-    if hasattr(fs, 'walk'):
+    if hasattr(fs, "walk"):
         for item in fs.walk(top, topdown=topdown, onerror=onerror):
             yield item
         return
