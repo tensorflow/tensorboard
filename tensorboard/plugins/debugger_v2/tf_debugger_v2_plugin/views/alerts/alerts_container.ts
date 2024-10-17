309d335a211601877ebc8561598f0401e5fabbd1
Adrian RC <adrianrc@google.com>
2024-10-17 00:33:36 +0000

    Fixing linter issues.

--- tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/views/alerts/alerts_container.ts
+++ tensorboard/plugins/debugger_v2/tf_debugger_v2_plugin/views/alerts/alerts_container.ts
@@ -73,8 +73,8 @@ export class AlertsContainer {
             count: alertsBreakdown[alertType],
           };
         });
-      }),
-    ),
+      })
+    )
   );
 
   readonly focusType$ = this.store.pipe(select(getAlertsFocusType));
