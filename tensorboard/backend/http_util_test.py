tensorflow/tensorboard

commit cc9dc8929c2d7fcd2dc747400d6cd87b9fe8c19b

Allow embedding Perfetto UI in Tensorboard pages (#7048)
## Motivation for features / changes
This is a required step for open sourcing the new Megascale Perfetto
tool in xprof.

Googlers see b/476134328

## Technical description of changes
Adds https://ui.perfetto.dev to the whitelist of domains that are
allowed to be embedded in a frame on the page. A follow up change will
embed this UI in the xprof (Profiler) plugin.

## Screenshots of UI changes (or N/A)
N/A

## Detailed steps to verify changes work correctly (as executed by you)
Tested on Tensorboard (in combination with other necessary changes) and
verified that it works as expected.

## Alternate designs / implementations considered (or N/A)
N/A

tensorboard/backend/http_util.py
@@ -36,7 +36,7 @@ _DISALLOWED_CHAR_IN_DOMAIN = re.compile(r"\s")
 # @vaadin/vaadin-lumo-styles/font-icons(via vaadin-grid) uses data URI for
 # loading font icons.
 _CSP_FONT_DOMAINS_WHITELIST = ["data:"]
-_CSP_FRAME_DOMAINS_WHITELIST = []
+_CSP_FRAME_DOMAINS_WHITELIST = ["https://ui.perfetto.dev"]
 _CSP_IMG_DOMAINS_WHITELIST = []
 _CSP_SCRIPT_DOMAINS_WHITELIST = []
 _CSP_CONNECT_DOMAINS_WHITELIST = []

commit 491288d7148b55185083ee50f8d40cc839fb783b

Fix http_util_test by including perfetto url in expected CSP string (#7049)
This is a fix for a unit test breakage introduced in
https://github.com/tensorflow/tensorboard/pull/7048.

tensorboard/backend/http_util_test.py
@@ -229,10 +229,14 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=["abcdefghi"]
         )
         expected_csp = (
-            "default-src 'self';font-src 'self' data:;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
+            "default-src 'self';"
+            "font-src 'self' data:;"
+            "frame-src 'self' https://ui.perfetto.dev;"
+            "img-src 'self' data: blob:;"
+            "object-src 'none';"
             "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "connect-src 'self';script-src 'self' 'unsafe-eval' 'sha256-abcdefghi'"
+            "connect-src 'self';"
+            "script-src 'self' 'unsafe-eval' 'sha256-abcdefghi'"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
@@ -243,10 +247,14 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=None
         )
         expected_csp = (
-            "default-src 'self';font-src 'self' data:;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
+            "default-src 'self';"
+            "font-src 'self' data:;"
+            "frame-src 'self' https://ui.perfetto.dev;"
+            "img-src 'self' data: blob:;"
+            "object-src 'none';"
             "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "connect-src 'self';script-src 'unsafe-eval'"
+            "connect-src 'self';"
+            "script-src 'unsafe-eval'"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
@@ -258,10 +266,14 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=None
         )
         expected_csp = (
-            "default-src 'self';font-src 'self' data:;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
+            "default-src 'self';"
+            "font-src 'self' data:;"
+            "frame-src 'self' https://ui.perfetto.dev;"
+            "img-src 'self' data: blob:;"
+            "object-src 'none';"
             "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "connect-src 'self';script-src 'none'"
+            "connect-src 'self';"
+            "script-src 'none'"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
@@ -273,10 +285,14 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=None
         )
         expected_csp = (
-            "default-src 'self';font-src 'self' data:;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
+            "default-src 'self';"
+            "font-src 'self' data:;"
+            "frame-src 'self' https://ui.perfetto.dev;"
+            "img-src 'self' data: blob:;"
+            "object-src 'none';"
             "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "connect-src 'self';script-src 'self'"
+            "connect-src 'self';"
+            "script-src 'self'"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 
@@ -287,10 +303,14 @@ class RespondTest(tb_test.TestCase):
             q, "<b>hello</b>", "text/html", csp_scripts_sha256s=["abcdefghi"]
         )
         expected_csp = (
-            "default-src 'self';font-src 'self' data:;"
-            "frame-src 'self';img-src 'self' data: blob:;object-src 'none';"
+            "default-src 'self';"
+            "font-src 'self' data:;"
+            "frame-src 'self' https://ui.perfetto.dev;"
+            "img-src 'self' data: blob:;"
+            "object-src 'none';"
             "style-src 'self' https://www.gstatic.com data: 'unsafe-inline';"
-            "connect-src 'self';script-src 'self' 'sha256-abcdefghi'"
+            "connect-src 'self';"
+            "script-src 'self' 'sha256-abcdefghi'"
         )
         self.assertEqual(r.headers.get("Content-Security-Policy"), expected_csp)
 