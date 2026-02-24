1d9e029f736e0164f8fea51891bfe52247d0f736
Cinthia Davalos <cinthiad@google.com>
2026-02-23 18:02:31 -0600

    Fixed internal build errors

--- tensorboard/plugins/mesh/tf_mesh_dashboard/types.d.ts
+++ tensorboard/plugins/mesh/tf_mesh_dashboard/types.d.ts
@@ -1,4 +1,4 @@
-/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2024 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,16 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-interface AnalyticsEventType {
-  hitType: string;
-  page?: string;
-  eventCategory?: string;
-  eventAction?: string;
-  eventLabel?: string;
-  eventValue?: number;
-}
-
-declare let ga: (command: string, eventObj: AnalyticsEventType) => void;
 
 // This declaration fixes the Google internal build. Bazel/concatjs needs the
 // .js suffix to find the file (newer concatjs versions don't fix this), but
